fc609a8681462054dab83de87f58ca10f1618ee4
a.schouten <a.schouten>
1999-12-09 16:34:12 +0000

    I_CmsConstants moved to core.

--- src/com/opencms/core/I_CmsConstants.java
+++ src/com/opencms/core/I_CmsConstants.java
@@ -1,13 +1,13 @@
-package com.opencms.file;
+package com.opencms.core;
 
 /**
  * This interface is a pool for cms-constants. All classes may implement this
  * class to get access to this contsnats.
  * 
  * @author Andreas Schouten
- * @version $Revision: 1.1 $ $Date: 1999/12/09 16:28:43 $
+ * @version $Revision: 1.1 $ $Date: 1999/12/09 16:34:12 $
  */
 public interface I_CmsConstants
 {
-	public static String C_VERSION = "Release 3.2 \"Caladan Leto\"  $name$";
+	public static String C_VERSION = "Release 3.2 \"Caladan Leto\"  $Name:  $";
 }
