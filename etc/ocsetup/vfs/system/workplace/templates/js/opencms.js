025b90ba04f48a51477bee6ef64d3535b017766b
a.schouten <a.schouten>
2000-01-25 14:20:53 +0000

    no message

---
+++ etc/ocsetup/vfs/system/workplace/templates/js/opencms.js
@@ -0,0 +1,229 @@
+//------------------------------------------
+// global variables und functions
+// m.schleich 03.01.2000
+//------------------------------------------
+// Formularfelder (Textfelder) auf Inhalt überprüfen
+function check_textfeld(formular, feld)
+{
+if(document[formular][feld].value=="")
+	{
+		alert('Bitte geben Sie einen Namen an!');
+		document[formular][feld].focus();
+		return false;
+	}
+return true;
+}
+
+//------------------------------------------------------------------------------------
+// special functions
+// m.schleich 05.01.1999
+//------------------------------------------------------------------------------------
+function default_dateiangaben() //to set defaults in user sets for filelist
+{
+	document.EINST_EXPL.DANGABEN[0].checked=true;
+	document.EINST_EXPL.DANGABEN[1].checked=false;
+	document.EINST_EXPL.DANGABEN[2].checked=true;
+	document.EINST_EXPL.DANGABEN[3].checked=false;
+	document.EINST_EXPL.DANGABEN[4].checked=true;
+	document.EINST_EXPL.DANGABEN[5].checked=false;
+	document.EINST_EXPL.DANGABEN[6].checked=true;
+	document.EINST_EXPL.DANGABEN[7].checked=false;
+	document.EINST_EXPL.DANGABEN[8].checked=false;
+}
+
+//------------------------------------------------------------------------------------
+// mouseover- and clicked-effects
+// m.schleich 20.12.1999
+//------------------------------------------------------------------------------------
+
+var imgEndOn = "_on.gif";
+var imgEndOff = "_off.gif";
+var imgEndPush = "_push.gif";
+var imgEndInakitv = "_in.gif";
+var pfad="../pics/";
+
+
+function ChOn(imgID) //changes a imageobject on mouseover
+{
+document.images[imgID].src = pfad + imgID + imgEndOn;
+}
+
+function ChOff(imgID) //changes a imageobject on mouseout
+{
+document.images[imgID].src = pfad + imgID + imgEndOff;
+}
+
+function ChPush(imgID) //changes a imageobject on clicked or mousedown
+{
+document.images[imgID].src = pfad + imgID + imgEndPush;
+}
+
+
+function ChImg(imgID,src)
+{
+document.images[imgID].src = src;
+}
+
+
+//------------------------------------------------------------------------------------
+// open new window
+// m.schleich 20.12.1999
+//------------------------------------------------------------------------------------
+
+var workplace;
+
+function openwin(url, name, w, h) //opens a new window with parameter URL, Windowname (free choosable), width and height
+{
+	if(w==0) w=screen.availWidth-20;
+	if(h==0) h=screen.availHeight-100;
+
+	workplace = window.open(url,name, 'toolbar=no,location=no,directories=no,status=yes,menubar=0,scrollbars=yes,resizable=yes,width='+w+',height='+h);
+	workplace.moveTo(0,0);
+	if(workplace != null)
+	   {
+	      if (workplace.opener == null)
+	      {
+	         workplace.opener = self;
+	      }
+   		}
+	workplace.focus();
+
+}
+
+//------------------------------------------------------------------------------------
+// which radiobutton is checked
+// m.schleich 11.01.1999
+// 
+// parameters:
+// formname: name of the form
+// radionae: name of the radiobottuns group
+// i: how many radiobuttons are there
+// return: the value of the checked radiobutton
+//------------------------------------------------------------------------------------
+
+function check_radio(formname, radioname, i)
+{
+	for(j=0;j<i;j++)
+	{
+		if(document[formname][radioname][j].checked)
+		{
+			return document[formname][radioname][j].value;
+		}
+
+	}
+
+}
+
+//------------------------------------------------------------------------------------
+// ceck date
+// 
+// author:	Michaela Schleich
+// company:	mindfact interaktive medien ag
+// date:	01.11.1999
+// update:	14.01.2000
+//
+// input date (tt.mm.jjjj)
+//
+// returns FALSE if date is not a valid date
+// returns TRUE if date valid
+//------------------------------------------------------------------------------------
+function checkDate(eDat)
+{
+// get date
+	schaltjahr=false;
+	aktDat = new Date;
+	aktTag= aktDat.getDate();
+	aktMonat= aktDat.getMonth()+1;
+	aktJahr= aktDat.getFullYear();
+	
+
+// split the userdate
+	eTag = eDat.substring(0,2);
+	eMonat = eDat.substring(3,5);
+	eJahr = eDat.substring(6,10);
+	
+
+	if(eJahr<aktJahr)
+	{
+		alert("Die Fälligkeit liegt in der Vergangenheit");
+		return false;
+	}
+	else
+	{
+		if(eMonat<aktMonat)
+		{
+			alert("Die Fälligkeit liegt in der Vergangenheit");
+			return false;
+		}
+		else
+		{
+			if(eTag<aktTag)
+			{
+				alert("Die Fälligkeit liegt in der Vergangenheit");
+				return false;
+			}
+		}
+	}
+	
+	
+	if(eDat.length != 10 || isNaN(eTag) || isNaN(eMonat) || isNaN(eJahr))
+		{
+		alert("Bitte das Datum in Form von tt.mm.jjjj eingeben.");
+		return false;
+		}
+	else
+		{
+		if(eMonat < 01 || eMonat > 12)
+		{
+		alert("Bitte überprüfen Sie den eingegeben Monat.");
+		return false;
+		}
+		else
+			{
+			if(eTag > 31 || eTag<=00)
+			{
+			alert("Bitte ueberprüfen Sie den Tag des Datums.");
+			return false;
+			}
+			else
+			{
+
+// if must be split, to long for browser interpreter
+				if(((eTag == 31) && (eMonat == 02)) || ((eTag == 31) && (eMonat == 04)) || ((eTag == 31) && (eMonat == 06)))
+				{
+				alert('Bitte ueberpruefen Sie den Tag des Datums.');
+				return false;
+				}
+				if((eTag == 30 && eMonat == 02) || (eTag == 31 && eMonat==9) || (eTag==31 && eMonat==11))
+				{
+				alert('Bitte ueberpruefen Sie den Tag des Datums.');
+				return false;
+				}
+				else
+				{
+				if(eTag==29 && eMonat == 02)
+					{
+					sj = eJahr%4;
+					if(sj==0)
+					schaltjahr=true;
+
+					sj = eJahr%100;
+					if(sj==0)
+					schaltjahr=false;
+		
+					sj = eJahr%400;
+					if(sj==0)
+					schaltjahr=true;
+
+					if(!schaltjahr)
+					{
+					alert(eJahr + " ist kein Schaltjahr.");
+					return false;
+					}
+					}
+				}
+			}
+		}		
+	}
+	return true;
+}
---
+++ etc/ocsetup/vfs/system/workplace/templates/js/opencms_kontextmenu.js
@@ -0,0 +1,329 @@
+// listen the event mous click
+document.onclick = mouseGeklickt;
+
+//---------------------------------
+// help to choose the right browser
+// m.schleich 21.12.1999
+// changed am 03.01.2000
+//---------------------------------
+ns = (document.layers)? true:false;
+ie = (document.all)? true:false;
+if(ie)
+{
+	if (navigator.userAgent.indexOf('MSIE 5')>0)
+	{
+		ie5 = true;
+	}
+	else
+	{
+		ie5 = false;
+	}
+}
+
+if(ns)
+{
+	document.captureEvents(Event.CLICK);
+	var layerzeigen_01= 'document.layers.';
+	var layerzeigen_02= '.visibility="show"';
+	var layerverstecken_01= 'document.layers.';
+	var layerverstecken_02= '.visibility="hide"';
+	var xpos_01= 'document.';
+	var xpos_02= '.left=';
+	var ypos_01= 'document.';
+	var ypos_02= '.top=';
+	var xoffset= '+3';
+	var yoffset= '+3';
+}
+else
+{
+	var layerzeigen_01= 'document.all.';
+	var layerzeigen_02= '.style.visibility="visible"';
+	var layerverstecken_01= 'document.all.';
+	var layerverstecken_02= '.style.visibility="hidden"';
+	var xpos_01= 'document.all.';
+	var xpos_02= '.style.left=';
+	var ypos_01= 'document.all.';
+	var ypos_02= '.style.top=';
+	var xoffset= '+3';
+	var yoffset= '+3';
+	if(ie5)
+	{
+		var xpos_01= 'document.all.';
+		var xpos_02= '.style.left=';
+		var ypos_01= 'document.all.';
+		var ypos_02= '.style.top=';
+		var xoffset= '+3+document.body.scrollLeft';
+		var yoffset= '+3+document.body.scrollTop';
+	}
+}
+
+//------------------------------------------
+// global variables and global functions
+// m.schleich 03.01.2000
+//------------------------------------------
+var shown = false;
+var letztelyr='null';
+var x=0;
+var y=0;
+
+// which operation is clicked
+var DO_LOCK=1;
+var DO_LOCKUSER=2;
+var DO_UNLOCK=3;
+
+var DO_COPY=4;
+var DO_MOVE=5;
+var DO_RENAME=6;
+var DO_DELETE=7;
+
+var DO_CHOWN=8;
+var DO_CHGRP=9;
+var DO_CHMOD=10;
+var DO_CHTYPE=11;
+var DO_META=12;
+var DO_METAVIEW=13;
+
+var DO_EDIT=14;
+var DO_HTMLEDIT=15;
+var DO_VIEW=16;
+var DO_EXPORT=17;
+
+var DO_TEMPLATEEDIT=12;
+var DO_DATAEDIT=13;
+
+var DO_EDITDOC=20;
+var DO_ATE=21;
+var DO_MAILEDIT=22;
+
+var DO_COPYF=30;
+var DO_MOVEF=31;
+var DO_RENAMEF=32;
+var DO_DELETEF=33;
+
+var DO_HIST=40;
+var DO_HISTVIEW=41;
+
+// mousekoordinates on click
+function mouseGeklickt(e)
+{
+		if (ie) {x=event.x; y=event.y;}
+		if (ns) {x=e.x; y=e.y;}
+		hidemenu(letztelyr);
+}
+
+//----------------------------------------
+// function to display the filecontextmenu
+// parameters:
+// welche: layer name
+// id: each link for layer must have a unic id number
+// m.schleich 03.12.1999
+//----------------------------------------
+function showkontext(welche, id)
+{
+	if (!shown || id!=altid)
+	{
+		if(y >= (screen.availHeight/2))
+		{
+			if(ie)lyrheight=checklyrheight(welche);
+			if(ns)lyrheight=document.layers[welche].clip.height;
+			
+			lyrheight='-'+lyrheight;
+			eval(ypos_01+welche+ypos_02+'y'+yoffset+lyrheight);
+		}
+		else
+		{
+			lyrheight='-0';
+		}
+		eval(xpos_01+welche+xpos_02+'x'+xoffset);
+		eval(ypos_01+welche+ypos_02+'y'+yoffset+lyrheight);
+		
+		hidemenu(letztelyr);
+		eval(layerzeigen_01+welche+layerzeigen_02);
+		
+		shown = true;
+	}
+	else
+	{
+		hidemenu(letztelyr);
+		shown = false;
+	}
+	letztelyr=welche;
+	altid=id;
+}
+
+
+// hides the file context (layer)
+function hidemenu(welche)
+{
+	if(welche!='null')
+	{
+		eval(layerverstecken_01+welche+layerverstecken_02);
+		//shown=false;
+	}
+	else
+	{
+		return;
+	}
+}
+
+function checklyrheight(welche)
+{
+	switch(welche)
+	     {
+	       case 'plain':
+	       	{
+	         return 220;
+	         break;
+	       	}
+			case 'plainlock':
+	     	{
+	         return 220;
+	         break;
+	       	}
+			case 'plainlockuser':
+			{
+			 return 220;
+			 break;
+	       	}
+			case 'pic':
+	     	{
+	         return 220;
+	         break;
+	       	}
+	       case 'online_p':
+	       	{
+	         return 81;
+	         break;
+	        }
+			default:
+			{
+			 return 81;
+			 break;
+	        }
+        }
+
+}
+
+function doAction(action) // which action on layer is clicked
+{
+	hidemenu(letztelyr);
+	
+	switch(action)
+	{
+		case 1:
+		{
+			location.href='explorer_files_lock.html';
+			break;
+		}
+		case 2:
+		{
+			location.href='explorer_files_lockchange.html';
+			break;
+		}
+		case 3:
+		{
+			location.href='explorer_files_unlock.html';
+			break;
+		}
+		
+		case 4:
+		{
+			location.href='explorer_files_copy.html';
+			break;
+		}
+		case 5:
+		{
+			location.href='explorer_files_move.html';
+			break;
+		}
+		case 6:
+		{
+			location.href='explorer_files_rename.html';
+			break;
+		}
+		case 7:
+		{
+			location.href='explorer_files_delete.html';
+			break;
+		}
+		
+		case 8:
+		{
+			location.href='explorer_files_owner.html';
+			break;
+		}
+		case 9:
+		{
+			location.href='explorer_files_group.html';
+			break;
+		}
+		case 10:
+		{
+			location.href='explorer_files_right.html';
+			break;
+		}
+		case 11:
+		{
+			location.href='explorer_files_type.html';
+			break;
+		}
+		case 12:
+		{
+			location.href='explorer_files_meta.html';
+			break;
+		}
+		case 13:
+		{
+			location.href='explorer_files_metashow.html';
+			break;
+		}
+		
+		case 14:
+		{
+			top.location.href='edit_text.html';
+			break;
+		}
+		case 15:
+		{
+			top.location.href='edit_html.html';
+			break;
+		}
+		case 16:
+		{
+			top.location.href='edit_view.html';
+			break;
+		}
+		
+		case 30:
+		{
+			location.href='explorer_files_copyf.html';
+			break;
+		}
+		case 31:
+		{
+			location.href='explorer_files_movef.html';
+			break;
+		}
+		case 32:
+		{
+			location.href='explorer_files_renamef.html';
+			break;
+		}
+		case 33:
+		{
+			location.href='explorer_files_deletef.html';
+			break;
+		}
+		
+		case 40:
+		{
+			location.href='explorer_files_history.html';
+			break;
+		}
+		case 41:
+		{
+			location.href='explorer_files_historyshow.html';
+			break;
+		}
+	}
+}
