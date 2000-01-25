19f2ef0147a97f52365dcf0772c03b5744628c47
a.lucas <a.lucas>
2000-01-25 17:20:15 +0000

    workplace constants added

---
+++ src/com/opencms/workplace/I_CmsWpConstants.java
@@ -0,0 +1,29 @@
+package com.opencms.workplace;
+
+public interface I_CmsWpConstants {
+    /** Name of the template containing button definitions */
+    public static final String C_BUTTONTEMPLATE = "ButtonTemplate";
+
+    
+    
+    
+    // Parameters for buttons
+    
+    /** Name of the button */
+    public static final String C_BUTTON_NAME = "name";
+    
+    /** Action for the button */
+    public static final String C_BUTTON_ACTION = "action";
+    
+    /** Alt text of the button */
+    public static final String C_BUTTON_ALT = "alt";
+
+    
+    
+    
+    // Constants for language file control
+    
+    /** Prefix for button texts in the language file */
+    public static final String C_LANG_BUTTON = "button";
+    
+}
