2b140dc78e9931f10fb661519c24161af39ad857
a.lucas <a.lucas>
2000-02-11 18:51:29 +0000

    methods added for accessing via template editor (rudimentary)

--- src/com/opencms/template/CmsXmlTemplateFile.java
+++ src/com/opencms/template/CmsXmlTemplateFile.java
@@ -6,12 +6,13 @@ import org.w3c.dom.*;
 import org.xml.sax.*;
 
 import java.util.*;
+import java.io.*;
 
 /**
  * Content definition for XML template files.
  * 
  * @author Alexander Lucas
- * @version $Revision: 1.4 $ $Date: 2000/01/26 17:06:50 $
+ * @version $Revision: 1.5 $ $Date: 2000/02/11 18:51:29 $
  */
 public class CmsXmlTemplateFile extends A_CmsXmlContent {
 
@@ -128,8 +129,13 @@ public class CmsXmlTemplateFile extends A_CmsXmlContent {
     public Enumeration getAllSubElements(String selector) throws CmsException {
         String templateDatablockName = getTemplateDatablockName(selector);
         Element templateElement = getData(templateDatablockName);
-        NodeList nl = templateElement.getElementsByTagName("ELEMENT");
-        return getNamesFromNodeList(nl);
+        NodeList nl = templateElement.getChildNodes();
+        return getNamesFromNodeList(nl, "ELEMENT", false);
+    }
+    
+    public Enumeration getAllSections() throws CmsException {
+        NodeList nl = ((Element)getXmlDocument().getDocumentElement()).getChildNodes();             
+        return getNamesFromNodeList(nl, "TEMPLATE", true);
     }
     
     /**
@@ -140,8 +146,8 @@ public class CmsXmlTemplateFile extends A_CmsXmlContent {
      */
     public Enumeration getParameterNames(String elementName) throws CmsException {
         Element elementDefinition = getData("elementdef." + elementName);
-        NodeList parameterTags = elementDefinition.getElementsByTagName("PARAMETER");
-        return getNamesFromNodeList(parameterTags);            
+        NodeList parameterTags = elementDefinition.getChildNodes();
+        return getNamesFromNodeList(parameterTags, "PARAMETER", false);            
     }
     
     /**
@@ -152,7 +158,22 @@ public class CmsXmlTemplateFile extends A_CmsXmlContent {
     public String getParameter(String elementName, String parameterName) throws CmsException {
         return getDataValue("ELEMENTDEF." + elementName + ".PARAMETER." + parameterName);        
     }
+        
+    public Element getBodyTag() throws CmsException {
+        Element result = null;
+        if(hasData("bodyTag")) {
+            result = getData("bodytag");
+        } else {
+            if(A_OpenCms.isLogging()) {
+                A_OpenCms.log(C_OPENCMS_INFO, getClassName() + "Cannot find \"bodytag\" tag in XML template file " + getFilename() + ".");
+            }
+        }
+        return result;
+    }
     
+    public void setBodyTag(Element data) throws CmsException {
+        setData("bodytag", data);
+    }
     
     /**
      * Gets the processed data of the appropriate <code>&lt;TEMPLATE&gt;</code> section of
@@ -173,6 +194,168 @@ public class CmsXmlTemplateFile extends A_CmsXmlContent {
         return getProcessedDataValue(datablockName, callingObject, parameters);
     }        
 
+    /**
+     * Gets the data of the appropriate <code>&lt;TEMPLATE&gt;</code> section of
+     * this workplace template file.
+     * <P>
+     * The correct datablock name for the template datablock will be taken
+     * from <code>getTemplateDatablockName</code>.
+     * 
+     * @param callingObject reference to the calling object. Used to look up user methods while processing.
+     * @param parameters hashtable containing all user parameters.
+     * @param templateSelector Name of the template section or null if the default section is requested.
+     * @return Processed template data.
+     * @exception CmsException
+     * @see #getTemplateDatablockName
+     */
+    public String getTemplateContent(Object callingObject, Hashtable parameters, String templateSelector) throws CmsException {
+        String datablockName = this.getTemplateDatablockName(templateSelector);
+        return getDataValue(datablockName);
+    }        
+    
+ 
+    public void setEditedTemplateContent(String content, String templateSelector, boolean html) throws CmsException {
+        System.err.println("*** NOW WRITING BACK CONTENT. HTML = " + html);
+        System.err.println(content);
+        System.err.println("-----------------");
+        String datablockName = this.getTemplateDatablockName(templateSelector);
+        Element data = getData(datablockName);
+
+        if(html) {
+            int startIndex = content.indexOf("<BODY");
+            startIndex = content.indexOf(">", startIndex + 1) + 1;
+            int endIndex = content.lastIndexOf("</BODY>");          
+            if(startIndex > 0) {
+                content = content.substring(startIndex, endIndex);
+            }
+        }
+                
+        StringBuffer tempXmlString = new StringBuffer();
+        tempXmlString.append("<?xml version=\"1.0\"?>\n");
+        tempXmlString.append("<" + getXmlDocumentTagName() + ">");
+        tempXmlString.append("<template>\n");
+        
+        if(html) {
+            tempXmlString.append("<![CDATA[");
+            content = replace(content, "[", "]]><");
+            //content = replace(content, "]", "><![CDATA[");             
+            tempXmlString.append(content);
+            tempXmlString.append("]]>");
+        } else {
+            tempXmlString.append(content);
+        }            
+        tempXmlString.append("</template>\n");
+        tempXmlString.append("</" + getXmlDocumentTagName() + ">\n");
+        
+        System.err.println(new String(tempXmlString));
+        
+        I_CmsXmlParser parser = getXmlParser();
+        StringReader parserReader = new StringReader(tempXmlString.toString());
+        Document tempDoc = null;
+        try {
+            tempDoc = parser.parse(parserReader);            
+        } catch(Exception e) {
+            throwException("PARSING ERROR!", CmsException.C_XML_PARSING_ERROR);
+        }
+        
+        Element templateNode = (Element)tempDoc.getDocumentElement().getFirstChild();
+        System.err.println("### NOW SETTING DATABLOCK WITH NEW CONTENT: " + datablockName);
+        setData(datablockName, templateNode);
+    }
+                                        
+    public String getTextEditableTemplateContent(Object callingObject, Hashtable parameters, String templateSelector) throws CmsException {
+        String datablockName = this.getTemplateDatablockName(templateSelector);
+        Element data = getData(datablockName);
+        StringBuffer result = new StringBuffer();
+        
+        Document tempDoc = (Document)getXmlDocument().cloneNode(true);
+        Element rootElem = tempDoc.getDocumentElement();
+        
+        while(rootElem.hasChildNodes()) {
+            rootElem.removeChild(rootElem.getFirstChild());
+        }
+        data = (Element)getXmlParser().importNode(tempDoc, data);
+        rootElem.appendChild(data);       
+        StringWriter out = new StringWriter();        
+        getXmlParser().getXmlText(tempDoc, out);
+        String xmlString = out.toString();
+        
+        int endOpeningXmlTag = xmlString.indexOf(">");
+        int endOpeningDocTag = xmlString.indexOf(">", endOpeningXmlTag + 1);
+        int endOpeningBodyTag = xmlString.indexOf(">", endOpeningDocTag + 1) + 1;
+        
+        int startClosingDocTag = xmlString.lastIndexOf("<");
+        int startClosingBodyTag = xmlString.lastIndexOf("<", startClosingDocTag - 1);
+        
+        xmlString = xmlString.substring(endOpeningBodyTag, startClosingBodyTag);
+        xmlString = xmlString.trim();
+                
+        return xmlString;
+    }
+        
+    public String getEditableTemplateContent(Object callingObject, Hashtable parameters, String templateSelector) throws CmsException {
+        String datablockName = this.getTemplateDatablockName(templateSelector);
+        Element data = getData(datablockName);
+        StringBuffer result = new StringBuffer();
+        
+        Document tempDoc = (Document)getXmlDocument().cloneNode(true);
+        Element rootElem = tempDoc.getDocumentElement();
+        
+        while(rootElem.hasChildNodes()) {
+            rootElem.removeChild(rootElem.getFirstChild());
+        }
+        data = (Element)getXmlParser().importNode(tempDoc, data);
+        rootElem.appendChild(data);       
+                
+        // Scan for cdatas
+        Node n = data;
+        Vector cdatas = new Vector();
+        while(n != null) {
+            if(n.getNodeType() == n.CDATA_SECTION_NODE) {
+                cdatas.addElement(n.getNodeValue());                
+                n.setNodeValue("");
+            }
+            n = treeWalker(n);
+        }
+                
+        StringWriter out = new StringWriter();        
+        getXmlParser().getXmlText(tempDoc, out);
+        String xmlString = out.toString();
+        
+        int endOpeningXmlTag = xmlString.indexOf(">");
+        int endOpeningDocTag = xmlString.indexOf(">", endOpeningXmlTag + 1);
+        int endOpeningBodyTag = xmlString.indexOf(">", endOpeningDocTag + 1) + 1;
+        
+        int startClosingDocTag = xmlString.lastIndexOf("<");
+        int startClosingBodyTag = xmlString.lastIndexOf("<", startClosingDocTag - 1);
+        
+        xmlString = xmlString.substring(endOpeningBodyTag, startClosingBodyTag);
+        xmlString = xmlString.trim();
+
+        
+        int cdataStart = xmlString.indexOf("<![CDATA[");
+        int currentPos = 0;
+        int loop = 0;
+        
+        result.append("<HTML>\n<HEAD></HEAD>\n");
+        result.append("<BODY " + getProcessedDataValue("bodytag", callingObject, parameters) + ">\n");
+                
+        while(cdataStart != -1) {
+            result.append(xmlString.substring(currentPos, cdataStart).replace('<', '[').replace('>', ']'));
+            result.append((String)cdatas.elementAt(loop++));
+            cdataStart = xmlString.indexOf("<![CDATA[", cdataStart + 1);
+            currentPos = xmlString.indexOf("]]>", currentPos + 1) + 3;
+        }
+        result.append(xmlString.substring(currentPos).replace('<', '[').replace('>', ']'));
+        
+        result.append("\n</BODY>\n</HTML>");        
+        return result.toString();
+    }
+        
+        
+   
+                            
+            
     /**
      * Gets the processed data of the default <code>&lt;TEMPLATE&gt;</code> section of
      * this workplace template file.
@@ -225,28 +408,72 @@ public class CmsXmlTemplateFile extends A_CmsXmlContent {
     
     /**
      * Internal utility method to extract the values of the "name" attribute
-     * from all nodes of a given nodelist.
+     * from defined nodes of a given nodelist.
      * @param nl NodeList to extract.
+     * @param tag Name of the tag whose "name" attribute should be extracted
+     * @param unnamedAllowed Indicates if unnamed tags are allowed or an exception should
+     * be thrown.
      * @return Enumeration of all "name" attributes.
      * @exception CmsException
      */
-    private Enumeration getNamesFromNodeList(NodeList nl) throws CmsException {
+    private Enumeration getNamesFromNodeList(NodeList nl, String tag, boolean unnamedAllowed) throws CmsException {
         int numElements = nl.getLength();
         Vector collectNames = new Vector();
 
         for(int i=0; i<numElements; i++) {
-            Element n = (Element)nl.item(i);
-            String name = n.getAttribute("name");
-            if(name == null || "".equals(name)) {
-                // unnamed element found.
-                // this is bad. throw an exception.
-                if(A_OpenCms.isLogging()) {
-                    A_OpenCms.log(C_OPENCMS_CRITICAL, "[CmsXmlControlFile] unnamed <" + n.getNodeName() + "> found in OpenCms control file " + getAbsoluteFilename() + ".");
-                }
-                throw new CmsException("Unnamed \"" + n.getNodeName() + "\" found in OpenCms control file " + getAbsoluteFilename() + ".", CmsException.C_XML_TAG_MISSING);
+            Node n = (Node)nl.item(i);                        
+            if(n.getNodeType() == n.ELEMENT_NODE 
+                    && n.getNodeName().toLowerCase().equals(tag.toLowerCase())) {
+                String name = ((Element)n).getAttribute("name");
+                if(name == null || "".equals(name)) {
+                    // unnamed element found.
+                    if(unnamedAllowed) {                        
+                        name = "-- unnamed --";
+                    } else {
+                        if(A_OpenCms.isLogging()) {
+                            A_OpenCms.log(C_OPENCMS_CRITICAL, "[CmsXmlControlFile] unnamed <" + n.getNodeName() + "> found in OpenCms control file " + getAbsoluteFilename() + ".");
+                        }
+                        throw new CmsException("Unnamed \"" + n.getNodeName() + "\" found in OpenCms control file " + getAbsoluteFilename() + ".", CmsException.C_XML_TAG_MISSING);
+                    }
+                } 
+                collectNames.addElement(name);
             }
-            collectNames.addElement(name);
         }
         return collectNames.elements();
     }
+    
+    
+    private int min(int a, int b) {
+        if(a==-1) {
+            return b;
+        }
+        if(b==-1) {
+            return a;
+        }
+        return a<b?a:b;
+    }
+    
+    private String replace(String s, String search, String replace) {
+        StringBuffer tempContent = new StringBuffer();                
+        //int index = s.indexOf(search);
+        int index = min(s.indexOf("["), s.indexOf("]"));
+        int lastindex = 0;
+        while(index != -1) {
+            System.err.println("### substring: lastindex " + lastindex + "    index " + index);
+            String sub = s.substring(lastindex, index);
+            tempContent.append(sub);
+            if(s.charAt(index) == '[') {                
+                tempContent.append("]]><");
+            } else {
+                tempContent.append("><![CDATA[");
+            }
+            lastindex = index + 1;
+            //index = s.indexOf(search, index+1);
+            index = min(s.indexOf("[", index+1), s.indexOf("]", index+1));
+        }
+        System.err.println("### substring: lastindex " + lastindex);
+        tempContent.append(s.substring(lastindex));
+        return new String(tempContent);
+    }
+        
 }
