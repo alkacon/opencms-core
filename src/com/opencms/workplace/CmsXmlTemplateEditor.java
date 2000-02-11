3e1b98a03620c530737146b1fedcdda0b8c90107
a.lucas <a.lucas>
2000-02-11 18:48:15 +0000

    initial checkin

---
+++ src/com/opencms/workplace/CmsXmlTemplateEditor.java
@@ -0,0 +1,394 @@
+package com.opencms.workplace;
+
+import com.opencms.file.*;
+import com.opencms.core.*;
+import com.opencms.util.*;
+import com.opencms.template.*;
+
+import org.xml.sax.*;
+import org.w3c.dom.*;
+
+import java.util.*;
+import java.io.*;
+
+import javax.servlet.http.*;
+
+/**
+ * Template class for displaying the XML template editor of the OpenCms workplace.<P>
+ * Reads template files of the content type <code>CmsXmlWpTemplateFile</code>.
+ * 
+ * @author Alexander Lucas
+ * @version $Revision: 1.1 $ $Date: 2000/02/11 18:48:15 $
+ * @see com.opencms.workplace.CmsXmlWpTemplateFile
+ */
+public class CmsXmlTemplateEditor extends CmsWorkplaceDefault implements I_CmsConstants {
+                
+    /**
+     * Indicates if the results of this class are cacheable.
+     * 
+     * @param cms A_CmsObject Object for accessing system resources
+     * @param templateFile Filename of the template file 
+     * @param elementName Element name of this template in our parent template.
+     * @param parameters Hashtable with all template class parameters.
+     * @param templateSelector template section that should be processed.
+     * @return <EM>true</EM> if cacheable, <EM>false</EM> otherwise.
+     */
+    public boolean isCacheable(A_CmsObject cms, String templateFile, String elementName, Hashtable parameters, String templateSelector) {
+        return false;
+    }    
+    
+    /**
+     * Gets the content of a defined section in a given template file and its subtemplates
+     * with the given parameters. 
+     * 
+     * @see getContent(A_CmsObject cms, String templateFile, String elementName, Hashtable parameters)
+     * @param cms A_CmsObject Object for accessing system resources.
+     * @param templateFile Filename of the template file.
+     * @param elementName Element name of this template in our parent template.
+     * @param parameters Hashtable with all template class parameters.
+     * @param templateSelector template section that should be processed.
+     */
+    public byte[] getContent(A_CmsObject cms, String templateFile, String elementName, Hashtable parameters, String templateSelector) throws CmsException {
+        if(C_DEBUG && A_OpenCms.isLogging()) {
+            A_OpenCms.log(C_OPENCMS_DEBUG, getClassName() + "getting content of element " + ((elementName==null)?"<root>":elementName));
+            A_OpenCms.log(C_OPENCMS_DEBUG, getClassName() + "template file is: " + templateFile);
+            A_OpenCms.log(C_OPENCMS_DEBUG, getClassName() + "selected template section is: " + ((templateSelector==null)?"<default>":templateSelector));
+        }
+
+        A_CmsRequestContext reqCont = cms.getRequestContext();
+        HttpSession session = ((HttpServletRequest)reqCont.getRequest().getOriginalRequest()).getSession(true);
+        CmsFile editFile = null;
+        CmsXmlControlFile controlFile = null;
+        
+        String content = (String)parameters.get("CONTENT");
+        String file = (String)parameters.get("file");
+        String exit = (String)parameters.get("EXIT");
+        String save = (String)parameters.get("save");
+        String body = (String)parameters.get("body");
+        String bodychange = (String)parameters.get("bodychange");
+        String editor = (String)parameters.get("editor");
+        String title = (String)parameters.get("title");
+        String layoutTemplateFilename = (String)parameters.get("template");
+        String bodyElementClassName = (String)parameters.get("bodyclass");
+        String bodyElementFilename = (String)parameters.get("bodyfile");
+        String oldEdit = (String)session.getValue("te_oldedit");
+        String bodytag = (String)session.getValue("bodytag");
+        String oldLayoutFilename = (String)session.getValue("te_oldlayout");
+        String oldTitle = (String)session.getValue("te_title");
+        String layoutTemplateClassName = (String)session.getValue("te_templateclass");
+
+        boolean existsContentParam = (content!=null && (!"".equals(content)));
+        boolean existsFileParam = (file!=null && (!"".equals(file)));
+        boolean existsBodyParam = (body!=null && (!"".equals(body)));
+        boolean existsTemplateParam = (layoutTemplateFilename!=null && (!"".equals(layoutTemplateFilename)));
+        boolean saveRequested = ((save != null) && "1".equals(save));
+        boolean exitRequested = ((exit != null) && "1".equals(exit));
+        boolean bodychangeRequested = ((exit != null) && "1".equals(bodychange));
+        boolean templatechangeRequested = (oldLayoutFilename != null && layoutTemplateFilename != null
+                                           && (!(oldLayoutFilename.equals(layoutTemplateFilename))));
+        boolean titlechangeRequested = (oldTitle != null && title != null && (!(oldTitle.equals(title))));
+        
+        // Check if there is a file parameter in the request
+        if(! existsFileParam) {
+            throwException("No \"file\" parameter given. Don't know which file should be edited.");
+        }
+        
+        // OK, the file parameter is checked successfully.
+        if(!existsContentParam || templatechangeRequested) {
+            // In case of a new editor call or a template change we need the control file.
+            controlFile = new CmsXmlControlFile(cms, file);
+        }    
+        
+        if(!existsContentParam) {
+            if(controlFile.isElementClassDefined("body")) {
+                bodyElementClassName = controlFile.getElementClass("body");
+            } 
+                
+            if(controlFile.isElementTemplateDefined("body")) {
+                bodyElementFilename = controlFile.getElementTemplate("body");
+            }
+            
+            if((bodyElementClassName == null) || (bodyElementFilename == null)) {
+                // Either the template class or the template file 
+                // for the "body" element could not be determined.
+                // BUG: Send error here
+            }
+            
+            // The content file must be locked before editing            
+            A_CmsResource tempFile = cms.readFileHeader(bodyElementFilename);
+            if(!tempFile.isLocked()) {
+                cms.lockResource(bodyElementFilename);
+            }
+            
+            // Now get the currently selected master template file
+            layoutTemplateFilename = controlFile.getMasterTemplate();
+            layoutTemplateClassName = controlFile.getTemplateClass();
+            
+            oldLayoutFilename = layoutTemplateFilename;          
+            
+           if(editor == null || "".equals(editor)) {
+                editor = this.C_SELECTBOX_EDITORVIEWS[C_SELECTBOX_EDITORVIEWS_DEFAULT];    
+                parameters.put("editor", editor);
+            }
+            oldEdit = editor; 
+            
+            // And finally the document title
+            title = cms.readMetainformation(file, C_METAINFO_TITLE);
+            if(title == null) {
+                title = "";
+            }
+            oldTitle = title;            
+        } 
+        
+        if(templatechangeRequested) {
+            // The user requested a change of the layout template
+            controlFile.setMasterTemplate(layoutTemplateFilename);
+            controlFile.write();
+        }
+        
+        if(titlechangeRequested) {
+            // The user entered a new document title
+            try {
+                cms.writeMetainformation(file, C_METAINFO_TITLE, title);
+            } catch(CmsException e) {
+                if(A_OpenCms.isLogging()) {
+                    A_OpenCms.log(C_OPENCMS_INFO, getClassName() + "Could not write metainformation " + C_METAINFO_TITLE + " for file " + file + ".");                    
+                    A_OpenCms.log(C_OPENCMS_INFO, getClassName() + e);
+                }
+            }
+        }
+                
+        // Get the XML parsed content of the layout file.
+        // This can be done by calling the getOwnTemplateFile() method of the
+        // layout's template class.
+        // The content is needed to determine the HTML style of the body element.
+        Object tempObj = CmsTemplateClassManager.getClassInstance(cms, layoutTemplateClassName);
+        CmsXmlTemplate layoutTemplateClassObject = (CmsXmlTemplate)tempObj;
+        CmsXmlTemplateFile layoutTemplateFile = layoutTemplateClassObject.getOwnTemplateFile(cms, layoutTemplateFilename, null, parameters, null);                   
+        
+        // Get the XML parsed content of the body file.        
+        // This can be done by calling the getOwnTemplateFile() method of the
+        // body's template class.
+        tempObj = CmsTemplateClassManager.getClassInstance(cms, bodyElementClassName);
+        CmsXmlTemplate bodyElementClassObject = (CmsXmlTemplate)tempObj;
+        CmsXmlTemplateFile bodyTemplateFile = bodyElementClassObject.getOwnTemplateFile(cms, bodyElementFilename, "body", parameters, null);
+
+        // Include the datablocks of the layout file into the body file.
+        // So the "bodytag" and "style" data can be accessed by the body file.
+        Element bodyTag = layoutTemplateFile.getBodyTag();
+        bodyTemplateFile.setBodyTag(bodyTag);
+        
+               
+        if(existsContentParam) {
+            // save file contents to our temporary file.
+            Encoder encoder = new Encoder();
+            content = encoder.unescape(content);
+            
+            try {
+                bodyTemplateFile.setEditedTemplateContent(content, templateSelector, oldEdit.equals(C_SELECTBOX_EDITORVIEWS[0]));
+            } catch(CmsException e) {
+                if(e.getType() == e.C_XML_PARSING_ERROR) {
+                    CmsXmlWpTemplateFile errorTemplate = (CmsXmlWpTemplateFile)getOwnTemplateFile(cms, templateFile, elementName, parameters, "parseerror");
+                    return startProcessing(cms, errorTemplate, elementName, parameters, "parseerror");
+                }
+                else throw e;
+            }
+            System.err.println("*** Writing body template file");
+            bodyTemplateFile.write();
+            System.err.println("*** Ready");
+        }
+            
+        // Check if we should leave th editor instead of start processing
+        if(exitRequested) {
+            try {
+                cms.getRequestContext().getResponse().sendCmsRedirect(getConfigFile(cms).getWorkplaceMainPath());
+            } catch(IOException e) {
+                throwException("Could not send redirect to workplace main screen.", e);
+            }
+            return "".getBytes();
+        }
+
+        // Load the body!                                        
+        if(editor.equals(C_SELECTBOX_EDITORVIEWS[0])) {
+            // HTML editor
+            content = bodyTemplateFile.getEditableTemplateContent(this, parameters, body);
+        } else {                                            
+            content = bodyTemplateFile.getTextEditableTemplateContent(this, parameters, body);
+        }
+
+        System.err.println("##########################");
+        System.err.println("### SETTING CONTENT ");
+        System.err.println(content);
+        Encoder encoder = new Encoder();
+        content = encoder.escape(content);
+        parameters.put("CONTENT", content);
+        parameters.put("bodyfile", bodyElementFilename);
+        parameters.put("bodyclass", bodyElementClassName);
+        parameters.put("template", layoutTemplateFilename);
+                
+        // remove all parameters that could be relevant for the
+        // included editor.
+        parameters.remove("file");
+        parameters.remove("EXIT");
+        parameters.remove("save");
+                        
+        int numEditors = C_SELECTBOX_EDITORVIEWS.length;
+        for(int i=0; i<numEditors; i++) {
+            if(editor.equals(C_SELECTBOX_EDITORVIEWS[i])) {
+                parameters.put("editor._CLASS_", C_SELECTBOX_EDITORVIEWS_CLASSES[i]);
+                parameters.put("editor._TEMPLATE_", getConfigFile(cms).getWorkplaceTemplatePath() + C_SELECTBOX_EDITORVIEWS_TEMPLATES[i]);
+            }
+        }
+        
+        session.putValue("te_oldedit", editor);
+        session.putValue("te_oldlayout", oldLayoutFilename);       
+        session.putValue("te_title", oldTitle);       
+        session.putValue("te_templateclass", layoutTemplateClassName);       
+        
+        CmsXmlWpTemplateFile xmlTemplateDocument = (CmsXmlWpTemplateFile)getOwnTemplateFile(cms, templateFile, elementName, parameters, templateSelector);
+        xmlTemplateDocument.setXmlData("editor", editor);
+        xmlTemplateDocument.setXmlData("bodyfile", bodyElementFilename);
+        xmlTemplateDocument.setXmlData("bodyclass", bodyElementClassName);
+        
+        return startProcessing(cms, xmlTemplateDocument, elementName, parameters, templateSelector);
+    }            
+
+    /**
+     * Gets all views available in the workplace screen.
+     * <P>
+     * The given vectors <code>names</code> and <code>values</code> will 
+     * be filled with the appropriate information to be used for building
+     * a select box.
+     * <P>
+     * <code>names</code> will contain language specific view descriptions
+     * and <code>values</code> will contain the correspondig URL for each
+     * of these views after returning from this method.
+     * <P>
+     * 
+     * @param cms A_CmsObject Object for accessing system resources.
+     * @param lang reference to the currently valid language file
+     * @param names Vector to be filled with the appropriate values in this method.
+     * @param values Vector to be filled with the appropriate values in this method.
+     * @param parameters Hashtable containing all user parameters <em>(not used here)</em>.
+     * @return Index representing the user's current workplace view in the vectors.
+     * @exception CmsException
+     */
+    public Integer getBodys(A_CmsObject cms, CmsXmlLanguageFile lang, Vector names, Vector values, Hashtable parameters) 
+            throws CmsException {
+        
+        String currentBodySection = (String)parameters.get("body");
+        String bodyFilename = (String)parameters.get("bodyfile");
+        String bodyClassName = (String)parameters.get("bodyclass");
+
+        Object tempObj = CmsTemplateClassManager.getClassInstance(cms, bodyClassName);
+        CmsXmlTemplate bodyElementClassObject = (CmsXmlTemplate)tempObj;
+        CmsXmlTemplateFile bodyTemplateFile = bodyElementClassObject.getOwnTemplateFile(cms, bodyFilename, "body", parameters, null);
+            
+        Enumeration allBodys = bodyTemplateFile.getAllSections();
+        int loop=0;
+        int currentBodySectionIndex = 0;
+
+        while(allBodys.hasMoreElements()) {
+            String bodyname = (String)allBodys.nextElement();
+            if(bodyname.equals(currentBodySection)) {
+                currentBodySectionIndex = loop;
+            }
+            values.addElement(bodyname);
+            names.addElement(bodyname);
+            loop++;
+        }
+                
+        return new Integer(currentBodySectionIndex);
+    }    
+    
+    public Integer getAvailableTemplates(A_CmsObject cms, CmsXmlLanguageFile lang, Vector names, Vector values, Hashtable parameters) 
+            throws CmsException {
+        CmsXmlWpConfigFile configFile = getConfigFile(cms);
+        String templatePath = configFile.getCommonTemplatePath();
+        Vector allTemplateFiles = cms.getFilesInFolder(templatePath);
+        
+        String currentTemplate = (String)parameters.get("template");
+        int currentTemplateIndex = 0;
+        
+        int numTemplates = allTemplateFiles.size();
+        for(int i=0; i<numTemplates; i++) {
+            A_CmsResource file = (A_CmsResource)allTemplateFiles.elementAt(i);
+            String filename = file.getName();
+            String title = cms.readMetainformation(file.getAbsolutePath(), "Title");
+            if(title == null || "".equals(title)) {
+                title = file.getName();
+            }
+            values.addElement(file.getAbsolutePath());
+            names.addElement(title);
+            if(currentTemplate.equals(file.getAbsolutePath()) 
+                    || currentTemplate.equals(file.getName())) {
+                currentTemplateIndex = i;
+            }
+        }
+        return new Integer(currentTemplateIndex);
+    }
+        
+        
+        
+
+    /** Gets all editor views available in the template editor screens.
+     * <P>
+     * The given vectors <code>names</code> and <code>values</code> will 
+     * be filled with the appropriate information to be used for building
+     * a select box.
+     * <P>
+     * Used to build font select boxes in editors.
+     * 
+     * @param cms A_CmsObject Object for accessing system resources.
+     * @param lang reference to the currently valid language file
+     * @param names Vector to be filled with the appropriate values in this method.
+     * @param values Vector to be filled with the appropriate values in this method.
+     * @param parameters Hashtable containing all user parameters <em>(not used here)</em>.
+     * @return Index representing the user's current workplace view in the vectors.
+     * @exception CmsException
+     */
+    public Integer getEditorViews(A_CmsObject cms, CmsXmlLanguageFile lang, Vector names, Vector values, Hashtable parameters) 
+            throws CmsException {
+        getConstantSelectEntries(names, values, C_SELECTBOX_EDITORVIEWS, lang);        
+        int currentIndex = values.indexOf((String)parameters.get("editor"));        
+        return new Integer(currentIndex);
+    }    
+
+    public Object setText(A_CmsObject cms, String tagcontent, A_CmsXmlContent doc, Object userObj) 
+            throws CmsException {
+        
+        Hashtable parameters = (Hashtable)userObj;
+        String filename = (String)parameters.get("file");
+
+        String content = (String)parameters.get("CONTENT");        
+        boolean existsContentParam = (content!=null && (!"".equals(content)));
+                
+        // Check the existance of the "file" parameter
+        if(! existsContentParam) {
+            String errorMessage = getClassName() + "No content found.";
+            if(A_OpenCms.isLogging()) {
+                A_OpenCms.log(C_OPENCMS_CRITICAL, errorMessage);
+            }
+            return("no content");
+            // throw new CmsException(errorMessage, CmsException.C_BAD_NAME);
+        }
+                    
+        // Escape the text for including it in HTML text
+        return content;
+    }
+
+     /**
+     * Pre-Sets the value of the title input field.
+     * This method is directly called by the content definiton.
+     * @param Cms The CmsObject.
+     * @param lang The language file.
+     * @return Value that is pre-set into the title field.
+     * @exception CmsExeption if something goes wrong.
+     */
+    public String setTitle(A_CmsObject cms, CmsXmlLanguageFile lang)
+        throws CmsException {
+        HttpSession session= ((HttpServletRequest)cms.getRequestContext().getRequest().getOriginalRequest()).getSession(true);        
+        String name=(String)session.getValue("te_title");      
+        return name;
+    }    
+}
