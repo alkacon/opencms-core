ac86aa04ab2c623c5038108679382309b48f99cf
a.lucas <a.lucas>
2000-02-20 16:10:05 +0000

    URL for pictures corrected

--- src/com/opencms/workplace/CmsPictureBrowser.java
+++ src/com/opencms/workplace/CmsPictureBrowser.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/workplace/Attic/CmsPictureBrowser.java,v $
- * Date   : $Date: 2000/02/19 10:17:12 $
- * Version: $Revision: 1.6 $
+ * Date   : $Date: 2000/02/20 16:10:05 $
+ * Version: $Revision: 1.7 $
  *
  * Copyright (C) 2000  The OpenCms Group 
  * 
@@ -43,13 +43,10 @@ import javax.servlet.http.*;
  * Reads template files of the content type <code>CmsXmlWpTemplateFile</code>.
  * 
  * @author Alexander Lucas
- * @version $Revision: 1.6 $ $Date: 2000/02/19 10:17:12 $
+ * @version $Revision: 1.7 $ $Date: 2000/02/20 16:10:05 $
  * @see com.opencms.workplace.CmsXmlWpTemplateFile
  */
 public class CmsPictureBrowser extends CmsWorkplaceDefault {
-
-    /** Protocol used for the URL in the pics browser */
-    public final static String C_PROTOCOL_NAME = "http://";
 
     /**
      * Indicates if the results of this class are cacheable.
@@ -162,16 +159,18 @@ public class CmsPictureBrowser extends CmsWorkplaceDefault {
         
         String picsUrl = getConfigFile(cms).getCommonPictureUrl();
         HttpServletRequest req = (HttpServletRequest)(cms.getRequestContext().getRequest().getOriginalRequest());
+        HttpServletResponse resp = (HttpServletResponse)(cms.getRequestContext().getResponse().getOriginalResponse());
         String servletPath = req.getServletPath();
-        String hostName = req.getServerName();
-                                     
+        String hostName = req.getScheme() + "://" + req.getHeader("HOST");
+                           
+             
         // Generate the picture list
         for(int i=from; i<to; i++) {
             CmsFile file = (CmsFile)filteredPics.elementAt(i);
             String filename = file.getName();
             if(inFilter(filename, filter) && isImage(filename)) {
                 result.append(xmlTemplateDocument.getProcessedXmlDataValue("picstartseq", this, userObj));
-                result.append(C_PROTOCOL_NAME + hostName + servletPath + picsUrl + file.getName());
+                result.append(hostName + servletPath + picsUrl + file.getName());
                 result.append(xmlTemplateDocument.getProcessedXmlDataValue("picendseq", this, userObj));
                 result.append(xmlTemplateDocument.getProcessedXmlDataValue("textstartseq", this, userObj));
                 result.append(file.getName() + " (" + file.getLength() + " Bytes)\n");
