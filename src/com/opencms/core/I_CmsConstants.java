55b7065fa78adf4f8379fe14a663614453415b14
a.schouten <a.schouten>
2000-02-22 13:35:47 +0000

    Build_19

--- src/com/opencms/core/I_CmsConstants.java
+++ src/com/opencms/core/I_CmsConstants.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/core/Attic/I_CmsConstants.java,v $
- * Date   : $Date: 2000/02/21 22:28:01 $
- * Version: $Revision: 1.49 $
+ * Date   : $Date: 2000/02/22 13:35:47 $
+ * Version: $Revision: 1.50 $
  *
  * Copyright (C) 2000  The OpenCms Group 
  * 
@@ -36,14 +36,14 @@ package com.opencms.core;
  * @author Michael Emmerich
  * @author Michaela Schleich
  * 
- * @version $Revision: 1.49 $ $Date: 2000/02/21 22:28:01 $
+ * @version $Revision: 1.50 $ $Date: 2000/02/22 13:35:47 $
  */
 public interface I_CmsConstants
 {
 	/**
 	 * The version-string for the cvs.
 	 */
-	static String C_VERSION = "Version 4.0a18 Giedi Prime: Rabban";
+	static String C_VERSION = "Version 4.0a19 Giedi Prime: Rabban";
 	
 	/**
 	 * The copyright message for the cvs.
