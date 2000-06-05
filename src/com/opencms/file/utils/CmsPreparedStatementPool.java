4e8a5f23345235b60af8bb417369d3d0d1c0a706
mEmmerich <m.emmerich@alkacon.com>
2000-06-05 15:43:33 +0000

    Added ConnectionPooling

--- src/com/opencms/file/utils/CmsPreparedStatementPool.java
+++ src/com/opencms/file/utils/CmsPreparedStatementPool.java
@@ -1,5 +1,33 @@
-// TODO: Quelltext kommentieren :o((
-package test;
+/*
+ *
+ * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/file/utils/Attic/CmsPreparedStatementPool.java,v $
+ * Date   : $Date: 2000/06/05 15:43:33 $
+ * Version: $Revision: 1.1 $
+ *
+ * Copyright (C) 2000  The OpenCms Group 
+ * 
+ * This File is part of OpenCms -
+ * the Open Source Content Mananagement System
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * For further information about OpenCms, please see the
+ * OpenCms Website: http://www.opencms.com
+ * 
+ * You should have received a  of the GNU General Public License
+ * long with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.opencms.file.utils;
 
 import java.sql.*;
 import java.util.*;
