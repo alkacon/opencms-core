933a2eff48e019d2768adca1152abed7f2346534
h.riege <h.riege>
2000-06-20 15:13:49 +0000

    add class exportPoint and AccessFilesystem

---
+++ src/com/opencms/file/CmsExportPoint.java
@@ -0,0 +1,101 @@
+/*
+ * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/file/Attic/CmsExportPoint.java,v $
+ * Date   : $Date: 2000/06/20 15:13:48 $
+ * Version: $Revision: 1.1 $
+ *
+ * Copyright (C) 2000  The OpenCms Group 
+ * 
+ * This File is part of OpenCms -
+ * the Open Source Content Mananagement System
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * For further information about OpenCms, please see the
+ * OpenCms Website: http://www.opencms.com
+ * 
+ * You should have received a copy of the GNU General Public License
+ * long with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.opencms.file;
+
+import com.opencms.core.*;
+
+/**
+ * This class describes a exportpoint. A exportpoint defines the type of a filesystem
+ * containing data used my the Cms and where it is mounted in to the logical
+ * filesystem of  the Cms.
+ * 
+ * @author Michael Emmerich
+ * @version $Revision: 1.1 $ $Date: 2000/06/20 15:13:48 $
+ */
+
+public class CmsExportPoint implements I_CmsConstants {
+    /**
+     * Definition of the location of this exportpoint in the logical Cms fielsystem.
+     */
+	private String m_exportpoint = null;
+    
+    /**
+     * The the exportpath of a exportpoint in a real filesystem.
+     *  
+     */
+	private String m_exportpath = null;
+    
+
+	 /**
+	 * Constructs a new CmsExportPoint. 
+	 * This constructor creates a new exportpoint for the disc filesystem
+	 * 
+	 * @param exportpoint The export point in the Cms filesystem.
+	 * @param expottpath The physical location this export point directs to. 
+	 */
+	public CmsExportPoint(String exportpoint, String exportpath) {
+		m_exportpoint = exportpoint;
+		m_exportpath = exportpath;
+	}
+    
+	
+	/**
+	 * Returns the exportpoint of a CmsExportPoint Object.
+	 * @return The exportpoint in the OpenCms filesystem.
+	 */
+	public String getExportpoint() {
+	  return m_exportpoint;
+	}
+	
+	/**
+	 * Returns the exportpath of a CmsExportPoint Object.
+	 * 
+	 * @return The physical location this exportpoint directs to or null.
+	 */
+	public String getExportPath() {
+		return m_exportpath;
+	}
+    
+
+    /**
+	 * Returns a string-representation for this object.
+	 * This can be used for debugging.
+	 * 
+	 * @return string-representation for this object.
+	 */
+	public String toString() {
+        StringBuffer output=new StringBuffer();
+        output.append("[ExportPoint]:");
+        output.append(m_exportpoint);
+        output.append(" , Exportpath=");
+        output.append(m_exportpath);
+        return output.toString();
+	}
+	
+}
---
+++ src/com/opencms/file/utils/CmsAccessFilesystem.java
@@ -0,0 +1,140 @@
+/*
+ * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/file/utils/Attic/CmsAccessFilesystem.java,v $
+ * Date   : $Date: 2000/06/20 15:13:49 $
+ * Version: $Revision: 1.1 $
+ *
+ * Copyright (C) 2000  The OpenCms Group 
+ * 
+ * This File is part of OpenCms -
+ * the Open Source Content Mananagement System
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * For further information about OpenCms, please see the
+ * OpenCms Website: http://www.opencms.com
+ * 
+ * You should have received a copy of the GNU General Public License
+ * long with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.opencms.file.utils;
+
+import java.util.*;
+import java.io.*;
+import java.sql.*;
+import com.opencms.file.*;
+
+import com.opencms.core.*;
+
+
+public class CmsAccessFilesystem implements I_CmsConstants {
+
+    /**
+    * This is the Hashtable of exportpoints.
+    */
+    private Hashtable m_exportpointStorage  = null;
+    
+    /**
+     * Constructor, creartes a new CmsAccessFilefilesystem object.
+     *
+     * @param mountpoint The mountpoint of this filesystem access module.
+     * 
+     * @exception CmsException Throws CmsException if connection fails.
+     * 
+     */
+    public CmsAccessFilesystem(Hashtable exportpoints)	
+        throws CmsException {
+        m_exportpointStorage = exportpoints;
+    }
+
+	/**
+	 * Creates or writes a file with the given content.
+     *
+	 * @param filename The complete name of the new file (including pathinformation).
+	 * @param contents The contents of the new file.
+	 * 
+	 * 
+     * @exception CmsException Throws CmsException if operation was not succesful
+     */    
+	 public void writeFile(String filename, String key, byte[] contents)
+							
+         throws CmsException {
+         
+             File discFile= new File (absoluteName(filename,key));
+             try {
+                 // write the new file to disk
+                 OutputStream s = new FileOutputStream(discFile);
+                 s.write(contents);
+                 s.close();
+             } catch (Exception e) {
+               throw new CmsException("[" + this.getClass().getName() + "] "+e.getMessage());
+             }	
+     }
+	
+	/**
+	 * Creates a new folder 
+	 * 
+	 * @param foldername The complete path to the folder. 
+	 *  
+	 * 
+	 * @exception CmsException Throws CmsException if operation was not succesful.
+	 */
+	 public void createFolder(String foldername, String key)
+         throws CmsException {
+
+          // create folder
+		  File discFolder=new File(absoluteName(foldername,key));
+          // check if this folder already exits
+		  if (!discFolder.exists())	{
+			boolean success=discFolder.mkdir();
+			if (!success) {
+				throw new CmsException("[" + this.getClass().getName() + "] "+foldername,CmsException.C_FILESYSTEM_ERROR);
+			}
+          }
+     }
+
+     /**
+      * Deletes a file or folder in the filesytem. 
+      * 
+	  * @param filename The complete path of the file or folder.
+      * @exception CmsException Throws CmsException if operation was not succesful
+      */
+     public void removeResource(String filename, String key) 
+        throws CmsException{
+         
+         File discFile=new File(absoluteName(filename,key));
+		 // check if file exists
+         if (discFile.exists()){
+			boolean success=discFile.delete();
+			if (!success) {
+					 throw new CmsException("[" + this.getClass().getName() + "] "+filename,CmsException.C_FILESYSTEM_ERROR);
+			}
+         }
+     }
+
+	/**
+	 * Calculates the absolute path in the filesystem.
+	 * 
+	 * @param filename Name of a file in the OpenCms system.
+	 * @param exportpoint the key in the Hashtable m_exportpointStorage.
+	 * @return Absolute path of a the file in the disk filesystem.
+	 */
+	private String absoluteName(String filename, String exportpoint){
+		
+		String path = null;
+		String exportpath = (String) m_exportpointStorage.get(exportpoint);
+		path = exportpath + filename.substring(exportpoint.length());
+		return path;
+	}
+
+
+}
