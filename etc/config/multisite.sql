bbdeeadd6c2b2b62b5e30597a6baa47d33adca3f
unknown <github@alkacon.com>
2000-09-15 11:18:27 +0000

    The file is added for easy addition of data to the database when using multisite. This script will possibly change soon - while the multisite team is developing the functionality.

---
+++ etc/config/multisite.sql
@@ -0,0 +1,131 @@
+####################################################################################
+# SQL script for adding multisite features to OpenCMS.
+#
+# The script should be run on top of a "clean" import of standard data. The script 
+# contains test data for to additional sites (next to the standard online site.
+#
+# Author: Henrik Kjær Hansen
+# Date  : 2000-09-15 
+####################################################################################
+
+
+
+####################################################################################
+# Dumping data for table 'CMS_PROJECTS'
+#
+DELETE FROM CMS_PROJECTS where PROJECT_NAME = 'COM corporate';
+DELETE FROM CMS_PROJECTS where PROJECT_NAME = 'DK corporate';
+INSERT INTO CMS_PROJECTS (PROJECT_ID, USER_ID, GROUP_ID, MANAGERGROUP_ID, TASK_ID, PROJECT_NAME, PROJECT_DESCRIPTION, PROJECT_FLAGS, PROJECT_CREATEDATE, PROJECT_PUBLISHDATE, PROJECT_PUBLISHED_BY, PROJECT_TYPE) VALUES ( '4', '2', '1', '3', '1', 'COM corporate', 'International corporate site - online', '0', '2000-09-07 11:06:41', '2000-09-07 11:06:41', '0', '0');
+INSERT INTO CMS_PROJECTS (PROJECT_ID, USER_ID, GROUP_ID, MANAGERGROUP_ID, TASK_ID, PROJECT_NAME, PROJECT_DESCRIPTION, PROJECT_FLAGS, PROJECT_CREATEDATE, PROJECT_PUBLISHDATE, PROJECT_PUBLISHED_BY, PROJECT_TYPE) VALUES ( '5', '2', '1', '3', '1', 'DK corporate', 'Danish corporate site - online', '0', '2000-09-15 10:44:56', '2000-09-15 10:44:56', '0', '0');
+
+
+
+####################################################################################
+# Table structure for table 'CMS_SITES'
+#
+
+DROP TABLE IF EXISTS CMS_SITES;
+CREATE TABLE CMS_SITES (
+   SITE_ID int(11) DEFAULT '0' NOT NULL,
+   NAME text,
+   DESCRIPTION text,
+   CATEGORY_ID int(11),
+   LANGUAGE_ID int(11),
+   COUNTRY_ID int(11),
+   ONLINEPROJECT_ID int(11),
+   PRIMARY KEY (SITE_ID)
+);
+
+#
+# Dumping data for table 'CMS_SITES'
+#
+
+INSERT INTO CMS_SITES (SITE_ID, NAME, DESCRIPTION, CATEGORY_ID, LANGUAGE_ID, COUNTRY_ID, ONLINEPROJECT_ID) VALUES ( '1', 'company.com', 'Corporate International', '1', '1', '0', '4');
+INSERT INTO CMS_SITES (SITE_ID, NAME, DESCRIPTION, CATEGORY_ID, LANGUAGE_ID, COUNTRY_ID, ONLINEPROJECT_ID) VALUES ( '2', 'company.dk', 'Corporate Danish', '1', '2', '2', '5');
+INSERT INTO CMS_SITES (SITE_ID, NAME, DESCRIPTION, CATEGORY_ID, LANGUAGE_ID, COUNTRY_ID, ONLINEPROJECT_ID) VALUES ( '0', 'Default', 'Default online project', '0', '1', '0', '1');
+
+
+
+####################################################################################
+# Table structure for table 'CMS_SITE_URLS'
+#
+
+DROP TABLE IF EXISTS CMS_SITE_URLS;
+CREATE TABLE CMS_SITE_URLS (
+   URL_ID int(11) DEFAULT '0' NOT NULL,
+   URL varchar(255) NOT NULL,
+   SITE_ID int(11),
+   PRIMARYURL int(11),
+   PRIMARY KEY (URL_ID),
+   UNIQUE URL (URL)
+);
+
+#
+# Dumping data for table 'CMS_SITE_URLS'
+#
+
+INSERT INTO CMS_SITE_URLS (URL_ID, URL, SITE_ID, PRIMARYURL) VALUES ( '0', 'www.default.com', '0', '0');
+INSERT INTO CMS_SITE_URLS (URL_ID, URL, SITE_ID, PRIMARYURL) VALUES ( '1', 'www.company.com', '1', '1');
+INSERT INTO CMS_SITE_URLS (URL_ID, URL, SITE_ID, PRIMARYURL) VALUES ( '2', 'www.company.dk', '2', '2');
+
+
+
+####################################################################################
+# Table structure for table 'CMS_CATEGORY'
+#
+
+DROP TABLE IF EXISTS CMS_CATEGORY;
+CREATE TABLE CMS_CATEGORY (
+   CATEGORY_ID int(11) DEFAULT '0' NOT NULL,
+   NAME text,
+   DESCRIPTION text NOT NULL,
+   PRIMARY KEY (CATEGORY_ID)
+);
+
+#
+# Dumping data for table 'CMS_CATEGORY'
+#
+
+INSERT INTO CMS_CATEGORY (CATEGORY_ID, NAME, DESCRIPTION) VALUES ( '1', 'Corporate', 'Corporate site');
+INSERT INTO CMS_CATEGORY (CATEGORY_ID, NAME, DESCRIPTION) VALUES ( '0', 'Default', 'Default Category');
+
+
+
+####################################################################################
+# Table structure for table 'CMS_LANGUAGE'
+#
+
+DROP TABLE IF EXISTS CMS_LANGUAGE;
+CREATE TABLE CMS_LANGUAGE (
+   LANGUAGE_ID int(11) DEFAULT '0' NOT NULL,
+   NAME text,
+   PRIMARY KEY (LANGUAGE_ID)
+);
+
+#
+# Dumping data for table 'CMS_LANGUAGE'
+#
+
+INSERT INTO CMS_LANGUAGE (LANGUAGE_ID, NAME) VALUES ( '1', 'English (uk)');
+INSERT INTO CMS_LANGUAGE (LANGUAGE_ID, NAME) VALUES ( '2', 'Danish');
+
+
+
+####################################################################################
+# Table structure for table 'CMS_COUNTRY'
+#
+
+DROP TABLE IF EXISTS CMS_COUNTRY;
+CREATE TABLE CMS_COUNTRY (
+   COUNTRY_ID int(11) DEFAULT '0' NOT NULL,
+   NAME text,
+   PRIMARY KEY (COUNTRY_ID)
+);
+
+#
+# Dumping data for table 'CMS_COUNTRY'
+#
+
+INSERT INTO CMS_COUNTRY (COUNTRY_ID, NAME) VALUES ( '0', 'International');
+INSERT INTO CMS_COUNTRY (COUNTRY_ID, NAME) VALUES ( '2', 'Denmark');
+
