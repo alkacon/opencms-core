df8ee4b9f842503edda807c49d2b0f118c31287b
h.riege <h.riege>
2001-01-26 08:46:51 +0000

    updated version-number

--- src/com/opencms/core/I_CmsConstants.java
+++ src/com/opencms/core/I_CmsConstants.java
@@ -1,8 +1,8 @@
 
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/core/Attic/I_CmsConstants.java,v $
-* Date   : $Date: 2001/01/24 10:15:26 $
-* Version: $Revision: 1.126 $
+* Date   : $Date: 2001/01/26 08:45:47 $
+* Version: $Revision: 1.127 $
 *
 * Copyright (C) 2000  The OpenCms Group 
 * 
@@ -37,14 +37,14 @@ package com.opencms.core;
  * @author Michael Emmerich
  * @author Michaela Schleich
  * 
- * @version $Revision: 1.126 $ $Date: 2001/01/24 10:15:26 $
+ * @version $Revision: 1.127 $ $Date: 2001/01/26 08:45:47 $
  */
 public interface I_CmsConstants {
     
     /**
      * The version-string for the cvs.
      */
-    static String C_VERSION = "Version 4.1.52 Arakis ";
+    static String C_VERSION = "Version 4.1.53 Arakis ";
     
     /**
      * The copyright message for the cvs.
