83ff5a4d4a28ef7ed281b999f24260e6424827c5
m.fabritius <m.fabritius>
2001-02-21 15:44:16 +0000

    This is the form that is required for the searchengine.

---
+++ modules/searchform/src/com/opencms/modules/search/form/I_CmsSearchEngine.java
@@ -0,0 +1,64 @@
+package com.opencms.modules.search.form;
+
+/**
+ * Insert the type's description here.
+ * Creation date: (17.11.00 10:22:45)
+ * @author: Markus Fabritius
+ */
+public interface I_CmsSearchEngine {
+/**
+ * Insert the method's description here.
+ * Creation date: (17.11.00 10:30:43)
+ */
+public String getExcerpt();
+/**
+ * Insert the method's description here.
+ * Creation date: (17.11.00 10:30:43)
+ */
+public int getFirstDisplay();
+/**
+ * Insert the method's description here.
+ * Creation date: (17.11.00 10:30:43)
+ */
+public int getLastDisplay();
+/**
+ * Insert the method's description here.
+ * Creation date: (17.11.00 10:30:43)
+ */
+public int getMatch();
+/**
+ * Insert the method's description here.
+ * Creation date: (17.11.00 10:30:43)
+ */
+public String getModified();
+/**
+ * Insert the method's description here.
+ * Creation date: (17.11.00 10:30:43)
+ */
+public int getPages();
+/**
+ * Insert the method's description here.
+ * Creation date: (17.11.00 10:30:43)
+ */
+public int getPercentMatch();
+/**
+ * Insert the method's description here.
+ * Creation date: (17.11.00 10:30:43)
+ */
+public String getSearchWord();
+/**
+ * Insert the method's description here.
+ * Creation date: (17.11.00 10:30:43)
+ */
+public int getSize();
+/**
+ * Insert the method's description here.
+ * Creation date: (17.11.00 10:30:43)
+ */
+public String getTitle();
+/**
+ * Insert the method's description here.
+ * Creation date: (17.11.00 10:30:43)
+ */
+public String getUrl();
+}
