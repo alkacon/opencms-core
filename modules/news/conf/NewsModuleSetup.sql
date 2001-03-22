90b67ae4e6430b061f42c0ce7c036d449fee9dfd
t.kellermeier <t.kellermeier>
2001-03-22 15:30:00 +0000

    Version 3

--- modules/news/conf/NewsModuleSetup.sql
+++ modules/news/conf/NewsModuleSetup.sql
@@ -11,8 +11,8 @@ create table news_channel				(id int not null,
 										 unique(name));
 
 # Testdaten										 
-INSERT INTO news_channel VALUES (1,'ARD','eins',-1);
-INSERT INTO news_channel VALUES (2,'ZDF','zwei',-1);
+INSERT INTO news_channel VALUES (1,'Channel1','eins',-1);
+INSERT INTO news_channel VALUES (2,'Channel2','zwei',-1);
 
 # Table news										 
 create table news_entry					(id int not null,
@@ -22,7 +22,7 @@ create table news_entry					(id int not null,
 										 author varchar(40),
 										 link varchar(100),
 										 linkText varchar(40),
-										 date date,
+										 date timestamp(14),
 										 lockstate int not null,
 										 channel int not null,
 										 a_info1 varchar (100),
@@ -30,9 +30,9 @@ create table news_entry					(id int not null,
 										 a_info3 varchar (100),
 										 primary key(id));
 										 
-INSERT INTO news_entry VALUES (1,'opencms noch besser!','opencms ist jetzt noch besser!','opencms ist jetzt noch besser!\r\nViele neue Features wurden eingebaut!','Theo Tester','http://www.opencms.com','opencms','1999-12-11',-1,1,'','','');
-INSERT INTO news_entry VALUES (2,'Neue OpenCms-Version','Version 5.33 von OpenCms ist jetzt online!','Version 5.33 von OpenCms ist jetzt online!\r\nDie Version weist viele Verbesserungen auf!','Max Mustermann','http://opencms.com','oc','1999-12-11',-1,1,'Dateigrösse: 5.3 Mb','','');
-INSERT INTO news_entry VALUES (3,'OpenCms Homepage im neuen Design!','Die OpenCms Homepage wurde runderneuert!','Die OpenCms Homepage wurde runderneuert!','Theo Tester','http://www.opencms.com','oc','1999-12-11',-1,2,'','','');
+INSERT INTO news_entry VALUES (1,'opencms noch besser!','opencms ist jetzt noch besser!','opencms ist jetzt noch besser!\r\nViele neue Features wurden eingebaut!','Theo Tester','http://www.opencms.com','opencms','20000321000001',-1,1,'','','');
+INSERT INTO news_entry VALUES (2,'Neue OpenCms-Version','Version 5.33 von OpenCms ist jetzt online!','Version 5.33 von OpenCms ist jetzt online!\r\nDie Version weist viele Verbesserungen auf!','Max Mustermann','http://opencms.com','oc','20000321000002',-1,1,'Dateigrösse: 5.3 Mb','','');
+INSERT INTO news_entry VALUES (3,'OpenCms Homepage im neuen Design!','Die OpenCms Homepage wurde runderneuert!','Die OpenCms Homepage wurde runderneuert!','Theo Tester','http://www.opencms.com','oc','20000321000003',-1,2,'','','');
 
 
 # Table cms_systemid
