7dc39b5ae4aa530f0dd25262269fa25c58cf3d1e
a.schouten <a.schouten>
2001-04-09 13:56:38 +0000

    Update History / version-number

--- src/com/opencms/core/I_CmsConstants.java
+++ src/com/opencms/core/I_CmsConstants.java
@@ -1,8 +1,8 @@
 
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/core/Attic/I_CmsConstants.java,v $
-* Date   : $Date: 2001/04/06 11:53:52 $
-* Version: $Revision: 1.149 $
+* Date   : $Date: 2001/04/09 13:55:58 $
+* Version: $Revision: 1.150 $
 *
 * Copyright (C) 2000  The OpenCms Group
 *
@@ -37,14 +37,14 @@ package com.opencms.core;
  * @author Michael Emmerich
  * @author Michaela Schleich
  *
- * @version $Revision: 1.149 $ $Date: 2001/04/06 11:53:52 $
+ * @version $Revision: 1.150 $ $Date: 2001/04/09 13:55:58 $
  */
 public interface I_CmsConstants {
 
     /**
      * The version-string for the cvs.
      */
-    static String C_VERSION = "Version 4.3.4 Bela Tegeuse ";
+    static String C_VERSION = "Version 4.3.5 Bela Tegeuse ";
 
     /**
      * The copyright message for OpenCms.
