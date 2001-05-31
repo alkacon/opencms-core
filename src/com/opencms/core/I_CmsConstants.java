5acbe7828111728d3372e9b01b08c614128ae0b3
h.riege <h.riege>
2001-05-31 12:10:30 +0000

    update of history and version nummber

--- src/com/opencms/core/I_CmsConstants.java
+++ src/com/opencms/core/I_CmsConstants.java
@@ -1,8 +1,8 @@
 
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/core/Attic/I_CmsConstants.java,v $
-* Date   : $Date: 2001/05/29 13:52:59 $
-* Version: $Revision: 1.160 $
+* Date   : $Date: 2001/05/31 12:10:30 $
+* Version: $Revision: 1.161 $
 *
 * Copyright (C) 2000  The OpenCms Group
 *
@@ -37,14 +37,14 @@ package com.opencms.core;
  * @author Michael Emmerich
  * @author Michaela Schleich
  *
- * @version $Revision: 1.160 $ $Date: 2001/05/29 13:52:59 $
+ * @version $Revision: 1.161 $ $Date: 2001/05/31 12:10:30 $
  */
 public interface I_CmsConstants {
 
     /**
      * The version-string for the cvs.
      */
-    static String C_VERSION = "Version 4.3.15 Bela Tegeuse ";
+    static String C_VERSION = "Version 4.3.16 Bela Tegeuse ";
 
     /**
      * The copyright message for OpenCms.
