dba2d51ccd3c09438dd91d8c85e01b7fc0f04cdf
a.schouten <a.schouten>
2001-07-23 08:33:56 +0000

    Update of shellscripts to start the cmsshell.

--- etc/cmsshell.bat
+++ etc/cmsshell.bat
@@ -1,3 +1,3 @@
 rem Start script for the OpenCms Shell
 
-java -jar lib/opencmsboot.jar
+java -jar lib/opencmsboot.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
--- etc/cmsshell.sh
+++ etc/cmsshell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Start script for the OpenCms Shell
 
-java -jar lib/opencmsboot.jar
+java -jar lib/opencmsboot.jar $1 $2 $3 $4 $5 $6 $7 $8 $9
