01909196ecefac0d36f7ea0076c583bcf0769698
a.schouten <a.schouten>
2001-07-26 14:24:16 +0000

    Fixed bug 1076: No java-script errors when using the download-gallery contextmenus.

--- etc/ocsetup/vfs/system/workplace/templates/js/opencms_kontextmenu.js
+++ etc/ocsetup/vfs/system/workplace/templates/js/opencms_kontextmenu.js
@@ -1,8 +1,8 @@
 // mouseevent
 
 function start() {
-	document.onclick = mouseClicked;
-	if(ns){document.captureEvents(Event.CLICK);	}
+    document.onclick = mouseClicked;
+    if(ns){document.captureEvents(Event.CLICK); }
 }
 
 //------------------------------------------
@@ -49,7 +49,7 @@ var DO_EDITDOC=20;
 var DO_ATE=21;
 var DO_MAILEDIT=22;
 var DO_NEWSEDIT=23;
-var	DO_LINKEDIT=24;
+var DO_LINKEDIT=24;
 
 var DO_COPYF=30;
 var DO_MOVEF=31;
@@ -84,9 +84,9 @@ var DO_TTAKE=10;
 // mouse coordinates on click
 function mouseClicked(e)
 {
-		if (ie) {x=event.x; y=event.y;}
-		if (ns) {x=e.x; y=e.y;}
-		hidemenu(letztelyr);
+        if (ie) {x=event.x; y=event.y;}
+        if (ns) {x=e.x; y=e.y;}
+        hidemenu(letztelyr);
 }
 
 //----------------------------------------
@@ -99,320 +99,322 @@ function mouseClicked(e)
 //----------------------------------------
 function showkontext(welche, parameter, id)
 {
-	// set the kontextparameter
-	kontextparam = parameter.replace(/\//g, "%2F");
-	kontextid = id;	
-	if(welche!='')
-	{
-	
-	if (!shown || id!=altid)
-	{
-	    	
-		if(y >= (screen.availHeight/2))
-		{
-			
-			if(ie)lyrheight=checklyrheight(welche);
-			
-			if(ns)lyrheight=document.layers[welche].clip.height;
-			
-			lyrheight='-'+lyrheight;
-			eval(ypos_01+welche+ypos_02+'y'+yoffset+lyrheight);
-		}
-		else
-		{
-			lyrheight='-0';
-		}
-		eval(xpos_01+welche+xpos_02+'x'+xoffset);
-		eval(ypos_01+welche+ypos_02+'y'+yoffset+lyrheight);
-		
-		hidemenu(letztelyr);
-		eval(layerzeigen_01+welche+layerzeigen_02);
-		
-		shown = true;
-	}
-	else
-	{
-		hidemenu(letztelyr);
-		shown = false;
-	}
-	letztelyr=welche;
-	altid=id;
-	}
+    if(document.layers != null) {
+        // set the kontextparameter
+        kontextparam = parameter.replace(/\//g, "%2F");
+        kontextid = id; 
+        if(welche!='')
+        {
+
+        if (!shown || id!=altid)
+        {
+
+            if(y >= (screen.availHeight/2))
+            {
+
+                if(ie)lyrheight=checklyrheight(welche);
+
+                if(ns)lyrheight=document.layers[welche].clip.height;
+
+                lyrheight='-'+lyrheight;
+                eval(ypos_01+welche+ypos_02+'y'+yoffset+lyrheight);
+            }
+            else
+            {
+                lyrheight='-0';
+            }
+            eval(xpos_01+welche+xpos_02+'x'+xoffset);
+            eval(ypos_01+welche+ypos_02+'y'+yoffset+lyrheight);
+
+            hidemenu(letztelyr);
+            eval(layerzeigen_01+welche+layerzeigen_02);
+
+            shown = true;
+        }
+        else
+        {
+            hidemenu(letztelyr);
+            shown = false;
+        }
+        letztelyr=welche;
+        altid=id;
+        }
+    }
 }
 
 
 // hides the file context (layer)
 function hidemenu(welche)
 {
-	if(welche!='null')
-	{
-		eval(layerverstecken_01+welche+layerverstecken_02);
-		//shown=false;
-	}
-	else
-	{
-		return;
-	}
+    if(welche!='null')
+    {
+        eval(layerverstecken_01+welche+layerverstecken_02);
+        //shown=false;
+    }
+    else
+    {
+        return;
+    }
 }
 
 function checklyrheight(welche)
 {
-	switch(welche)
-	     {
-	       case 'plain':
-	       	{
-	         return 220;
-	         break;
-	       	}
-			case 'plainlock':
-	     	{
-	         return 220;
-	         break;
-	       	}
-			case 'plainlockuser':
-			{
-			 return 220;
-			 break;
-	       	}
-			case 'pic':
-	     	{
-	         return 220;
-	         break;
-	       	}
-	       case 'online_p':
-	       	{
-	         return 81;
-	         break;
-	        }
-			default:
-			{
-			 return 220;
-			 break;
-	        }
+    switch(welche)
+         {
+           case 'plain':
+            {
+             return 220;
+             break;
+            }
+            case 'plainlock':
+            {
+             return 220;
+             break;
+            }
+            case 'plainlockuser':
+            {
+             return 220;
+             break;
+            }
+            case 'pic':
+            {
+             return 220;
+             break;
+            }
+           case 'online_p':
+            {
+             return 81;
+             break;
+            }
+            default:
+            {
+             return 220;
+             break;
+            }
         }
 
 }
 
 // which action on layer is clicked
 function doAction(action) {
-	hidemenu(letztelyr);
-	var encLasturl = simpleEscape(location.href);
-	
-	switch(action)	{
-		case 1: {
-			location.href='lock.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		} case 2:	{
-			location.href='lockchange.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		} case 3: {
-			location.href='unlock.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		} case 4: {
-			location.href='copy.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		} case 5: {
-			location.href='move.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		} case 6: {
-			location.href='rename.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		} case 7: {
-			location.href='delete.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		} case 8: {
-			location.href='chown.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		} case 9: {
-			location.href='chgrp.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		} case 10: {
-			location.href='chmod.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		} case 11: {
-			location.href='chtype.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		} case 12: {
-		    location.href='property.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		}
-		case 13:
-		{
-			location.href='explorer_files_metashow.html';
-			break;
-		} case 14: {
-			top.location.href='edit_text.html?file='+ kontextparam;
-			break;
-		} case 15: {
-			top.location.href='edit_html.html?file='+ kontextparam;
-			break;
-		}
-		case 16:
-		{
-			top.location.href='edit_view.html';
-			break;
-		}
-	    case 18: 
-		{
-		    // Template Editor
-		    top.location.href='edit_page.html?file='+ kontextparam;
-			break;
-		}
+    hidemenu(letztelyr);
+    var encLasturl = simpleEscape(location.href);
+    
+    switch(action)  {
+        case 1: {
+            location.href='lock.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        } case 2:   {
+            location.href='lockchange.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        } case 3: {
+            location.href='unlock.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        } case 4: {
+            location.href='copy.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        } case 5: {
+            location.href='move.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        } case 6: {
+            location.href='rename.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        } case 7: {
+            location.href='delete.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        } case 8: {
+            location.href='chown.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        } case 9: {
+            location.href='chgrp.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        } case 10: {
+            location.href='chmod.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        } case 11: {
+            location.href='chtype.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        } case 12: {
+            location.href='property.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        }
+        case 13:
+        {
+            location.href='explorer_files_metashow.html';
+            break;
+        } case 14: {
+            top.location.href='edit_text.html?file='+ kontextparam;
+            break;
+        } case 15: {
+            top.location.href='edit_html.html?file='+ kontextparam;
+            break;
+        }
+        case 16:
+        {
+            top.location.href='edit_view.html';
+            break;
+        }
+        case 18: 
+        {
+            // Template Editor
+            top.location.href='edit_page.html?file='+ kontextparam;
+            break;
+        }
 
-	    case 23: 
-		{
-		    // News Editor
-		    top.body.location.href='news_edit.html?action=edit&lasturl=' + simpleEscape(top.body.location.href) + '&file='+ kontextparam + '&initial=true';
-			break;
-		}
-		
-		case 24:
-		{ 
-			location.href='explorer_files_new_link.html?file='+ kontextparam + '&newlink=false&lasturl=' + encLasturl;
-			break;
-		}
-		
-		case 30:
-		{
-			location.href='explorer_files_copyf.html';
-			break;
-		}
-		case 31:
-		{
-			location.href='explorer_files_movef.html';
-			break;
-		}
-		case 32:
-		{
-			location.href='explorer_files_renamef.html';
-			break;
-		}
-		case 33:
-		{
-			location.href='explorer_files_deletef.html';
-			break;
-		} case 40:{
-		    location.href='history.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
-			break;
-		}
-		case 41:
-		{
-			location.href='explorer_files_historyshow.html';
-			break;
-		} case 42: {
-			location.href='copy.html?file='+ kontextparam;
-			break;
-		} case 98:{
-		    location.href='NewsEditBodyBackoffice.html?id=' + kontextid;
-			break;
-		} case 97:{
-		    window.top.location.href='NewsDeleteBackoffice.html?id=' + kontextid;
-			break;
-		}		
-	}
+        case 23: 
+        {
+            // News Editor
+            top.body.location.href='news_edit.html?action=edit&lasturl=' + simpleEscape(top.body.location.href) + '&file='+ kontextparam + '&initial=true';
+            break;
+        }
+        
+        case 24:
+        { 
+            location.href='explorer_files_new_link.html?file='+ kontextparam + '&newlink=false&lasturl=' + encLasturl;
+            break;
+        }
+        
+        case 30:
+        {
+            location.href='explorer_files_copyf.html';
+            break;
+        }
+        case 31:
+        {
+            location.href='explorer_files_movef.html';
+            break;
+        }
+        case 32:
+        {
+            location.href='explorer_files_renamef.html';
+            break;
+        }
+        case 33:
+        {
+            location.href='explorer_files_deletef.html';
+            break;
+        } case 40:{
+            location.href='history.html?lasturl=' + encLasturl + '&file='+ kontextparam + '&initial=true';
+            break;
+        }
+        case 41:
+        {
+            location.href='explorer_files_historyshow.html';
+            break;
+        } case 42: {
+            location.href='copy.html?file='+ kontextparam;
+            break;
+        } case 98:{
+            location.href='NewsEditBodyBackoffice.html?id=' + kontextid;
+            break;
+        } case 97:{
+            window.top.location.href='NewsDeleteBackoffice.html?id=' + kontextid;
+            break;
+        }       
+    }
 }
 
 function doPAction(action) // which action on layer Project is clicked
 {
-	hidemenu(letztelyr); 
-	var encLasturl = simpleEscape(location.href);
-	
-	switch(action)
-	{
-		case 1:
-		{
-			location.href='administration_content_lockchange.html?lasturl=' + encLasturl + '&projectid=' + kontextparam+ '&initial=true';
-			break;
-		}
-		case 2:
-		{
-			confirm('Sind Sie sicher, dass das Projekt veröffentlicht werden soll? \nEs sind noch Dateien in Bearbeitung.');
-			break;
-		}
-		case 3:
-		{
-			location.href='administration_content_publish_project.html?lasturl=' + encLasturl + '&projectid=' + kontextparam+ '&initial=true';
-			break;
-		}
-		case 4:
-		{
-			location.href='administration_content_delete_project.html?lasturl=' + encLasturl + '&projectid=' + kontextparam+ '&initial=true';
-			break;
-		}
-		case 10:
-		{
-			location.href='tasks_content_new_forproject.html?lasturl=' + encLasturl;
-			break;
-		}
-		case 20:
-		{
-			location.href='administration_content_projecthistory_react.html?lasturl=' + encLasturl;
-			break;
-		}
-		case 99:
-		{
-			location.href='tasks_content_new_forfile.html';
-			break;
-		}
-	}
+    hidemenu(letztelyr); 
+    var encLasturl = simpleEscape(location.href);
+    
+    switch(action)
+    {
+        case 1:
+        {
+            location.href='administration_content_lockchange.html?lasturl=' + encLasturl + '&projectid=' + kontextparam+ '&initial=true';
+            break;
+        }
+        case 2:
+        {
+            confirm('Sind Sie sicher, dass das Projekt veröffentlicht werden soll? \nEs sind noch Dateien in Bearbeitung.');
+            break;
+        }
+        case 3:
+        {
+            location.href='administration_content_publish_project.html?lasturl=' + encLasturl + '&projectid=' + kontextparam+ '&initial=true';
+            break;
+        }
+        case 4:
+        {
+            location.href='administration_content_delete_project.html?lasturl=' + encLasturl + '&projectid=' + kontextparam+ '&initial=true';
+            break;
+        }
+        case 10:
+        {
+            location.href='tasks_content_new_forproject.html?lasturl=' + encLasturl;
+            break;
+        }
+        case 20:
+        {
+            location.href='administration_content_projecthistory_react.html?lasturl=' + encLasturl;
+            break;
+        }
+        case 99:
+        {
+            location.href='tasks_content_new_forfile.html';
+            break;
+        }
+    }
 }
 
 function doTAction(action) // which action on layer Task is clicked
 {
-	hidemenu(letztelyr);
-	var encLasturl = simpleEscape(location.href);
-	
-	switch(action)
-	{
-		case 1:
-		{
-			location.href='tasks_content_dialogquery.html?lasturl=' + encLasturl + '&action=query&taskid='+ kontextparam;
-			break;
-		}
-		case 2:
-		{ 
-			location.href='tasks_content_detail.html?lasturl=' + encLasturl + '&action=accept&taskid='+ kontextparam; 
-			break;
-		}
-		case 3:
-		{
-			location.href='tasks_content_detail.html?lasturl=' + encLasturl + '&action=due&taskid='+ kontextparam;
-			break;
-		}
-		case 4:
-		{
-			location.href='tasks_content_dialogpriority.html?lasturl=' + encLasturl + '&taskid='+ kontextparam;
-			break;
-		}
-		case 5:
-		{
-			location.href='tasks_content_dialogcomment.html?lasturl=' + encLasturl + '&taskid='+ kontextparam;
-			break;
-		}
-		case 6:
-		{
-			location.href='tasks_content_dialogforward.html?lasturl=' + encLasturl + '&taskid='+ kontextparam;
-			break;
-		} 
-		case 7:
-		{
-			location.href='tasks_content_detail.html?lasturl=' + encLasturl + '&action=ok&taskid='+ kontextparam; 
-			break;
-		} 
-		case 8:
-		{
-			location.href='tasks_content_dialogmsg.html?lasturl=' + encLasturl + '&action=message&taskid='+ kontextparam; 
-			break;
-		}
-		case 9:
-		{
-			location.href='tasks_content_dialogreakt.html?lasturl=' + encLasturl + '&taskid='+ kontextparam; 
-			break;
-		}
-		case 10:
-		{
-			location.href='tasks_content_detail.html?lasturl=' + encLasturl + '&action=take&taskid='+ kontextparam;
-			break;
-		} 
-	}
+    hidemenu(letztelyr);
+    var encLasturl = simpleEscape(location.href);
+    
+    switch(action)
+    {
+        case 1:
+        {
+            location.href='tasks_content_dialogquery.html?lasturl=' + encLasturl + '&action=query&taskid='+ kontextparam;
+            break;
+        }
+        case 2:
+        { 
+            location.href='tasks_content_detail.html?lasturl=' + encLasturl + '&action=accept&taskid='+ kontextparam; 
+            break;
+        }
+        case 3:
+        {
+            location.href='tasks_content_detail.html?lasturl=' + encLasturl + '&action=due&taskid='+ kontextparam;
+            break;
+        }
+        case 4:
+        {
+            location.href='tasks_content_dialogpriority.html?lasturl=' + encLasturl + '&taskid='+ kontextparam;
+            break;
+        }
+        case 5:
+        {
+            location.href='tasks_content_dialogcomment.html?lasturl=' + encLasturl + '&taskid='+ kontextparam;
+            break;
+        }
+        case 6:
+        {
+            location.href='tasks_content_dialogforward.html?lasturl=' + encLasturl + '&taskid='+ kontextparam;
+            break;
+        } 
+        case 7:
+        {
+            location.href='tasks_content_detail.html?lasturl=' + encLasturl + '&action=ok&taskid='+ kontextparam; 
+            break;
+        } 
+        case 8:
+        {
+            location.href='tasks_content_dialogmsg.html?lasturl=' + encLasturl + '&action=message&taskid='+ kontextparam; 
+            break;
+        }
+        case 9:
+        {
+            location.href='tasks_content_dialogreakt.html?lasturl=' + encLasturl + '&taskid='+ kontextparam; 
+            break;
+        }
+        case 10:
+        {
+            location.href='tasks_content_detail.html?lasturl=' + encLasturl + '&action=take&taskid='+ kontextparam;
+            break;
+        } 
+    }
 }
