9c8f4bf212f915efebde1ddfd415cefd3820771b
e.falkenhan <e.falkenhan>
2001-07-27 13:45:23 +0000

    Bugfix 1034: store filter and projectid into session only

--- src/com/opencms/workplace/CmsAdminProjectResentFiles.java
+++ src/com/opencms/workplace/CmsAdminProjectResentFiles.java
@@ -1,8 +1,8 @@
 
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/workplace/Attic/CmsAdminProjectResentFiles.java,v $
-* Date   : $Date: 2001/07/16 18:24:16 $
-* Version: $Revision: 1.15 $
+* Date   : $Date: 2001/07/27 13:45:23 $
+* Version: $Revision: 1.16 $
 *
 * Copyright (C) 2000  The OpenCms Group
 *
@@ -44,83 +44,11 @@ import javax.servlet.http.*;
  * editing news.
  *
  * @author Alexander Lucas
- * @version $Revision: 1.15 $ $Date: 2001/07/16 18:24:16 $
+ * @version $Revision: 1.16 $ $Date: 2001/07/27 13:45:23 $
  * @see com.opencms.workplace.CmsXmlWpTemplateFile
  */
 
-public class CmsAdminProjectResentFiles extends CmsWorkplaceDefault implements I_CmsConstants,I_CmsFileListUsers {
-
-    /**
-     * Checks if a resource should be added to the vector. If the check was true
-     * it adds the resource.
-     *
-     * @param resources The Vector with the resources to return.
-     * @param filter The filter to check the state.
-     * @param resource The resource to check and add.
-     */
-
-    private void addResource(Vector resources, String filter, CmsResource resource) {
-        if("all".equals(filter)) {
-
-            // add in all cases
-            resources.addElement(resource);
-        }
-        else {
-            if(("new".equals(filter)) && (resource.getState() == C_STATE_NEW)) {
-
-                // add if resource is new
-                resources.addElement(resource);
-            }
-            else {
-                if(("changed".equals(filter)) && (resource.getState() == C_STATE_CHANGED)) {
-
-                    // add if resource is changed
-                    resources.addElement(resource);
-                }
-                else {
-                    if(("deleted".equals(filter)) && (resource.getState() == C_STATE_DELETED)) {
-
-                        // add if resource is deleted
-                        resources.addElement(resource);
-                    }
-                    else {
-                        if(("locked".equals(filter)) && (resource.isLocked())) {
-
-                            // add if resource is locked
-                            resources.addElement(resource);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * Check if this resource should be displayed in the filelist.
-     * @param cms The CmsObject
-     * @param res The resource to be checked.
-     * @return True or false.
-     * @exception CmsException if something goes wrong.
-     */
-
-    private boolean checkAccess(CmsObject cms, CmsResource res) throws CmsException {
-        boolean access = false;
-        int accessflags = res.getAccessFlags();
-
-        // First check if the user may have access by one of his groups.
-        boolean groupAccess = false;
-        Enumeration allGroups =
-                cms.getGroupsOfUser(cms.getRequestContext().currentUser().getName()).elements();
-        while((!groupAccess) && allGroups.hasMoreElements()) {
-            groupAccess = cms.readGroup(res).equals((CmsGroup)allGroups.nextElement());
-        }
-        if(((accessflags & C_ACCESS_PUBLIC_VISIBLE) > 0) || (cms.readOwner(res).equals(cms.getRequestContext().currentUser())
-                && (accessflags & C_ACCESS_OWNER_VISIBLE) > 0) || (groupAccess && (accessflags & C_ACCESS_GROUP_VISIBLE) > 0)
-                || (cms.getRequestContext().currentUser().getName().equals(C_USER_ADMIN))) {
-            access = true;
-        }
-        return access;
-    }
+public class CmsAdminProjectResentFiles extends CmsWorkplaceDefault implements I_CmsConstants{
 
     /**
      * Gets the content of a defined section in a given template file and its subtemplates
@@ -136,149 +64,40 @@ public class CmsAdminProjectResentFiles extends CmsWorkplaceDefault implements I
 
     public byte[] getContent(CmsObject cms, String templateFile, String elementName,
             Hashtable parameters, String templateSelector) throws CmsException {
-
         // get the session
         I_CmsSession session = cms.getRequestContext().getSession(true);
-        int currentProjectId = cms.getRequestContext().currentProject().getId();
-
         // load the template file
         CmsXmlWpTemplateFile xmlTemplateDocument = (CmsXmlWpTemplateFile)getOwnTemplateFile(cms,
                 templateFile, elementName, parameters, templateSelector);
         String filter = (String)parameters.get("filter");
+        String projectId = (String)parameters.get("projectid");
         String action = (String)parameters.get("action");
-        xmlTemplateDocument.setData("onload", "");
-        if("restoreproject".equals(action)) {
-
-            // restore the old project..
-            Integer oldId = (Integer)session.getValue("oldProjectId");
-            if(oldId != null) {
-                cms.getRequestContext().setCurrentProject(oldId.intValue());
+        if(filter == null || "".equalsIgnoreCase(filter)){
+            filter=(String)session.getValue("filter");
+            if(filter == null || "".equalsIgnoreCase(filter)){
+                filter = "all";
             }
-            session.removeValue("oldProjectId");
-
-            // redirect to the needed headfile.
-            try {
-                cms.getRequestContext().getResponse().sendCmsRedirect(getConfigFile(cms).getWorkplaceActionPath()
-                        + "administration_head_5.html");
-            }
-            catch(IOException exc) {
-                throw new CmsException("Could not redirect to administration_head_5.html", exc);
-            }
-
-            //return "".getBytes();
-            return null;
+            xmlTemplateDocument.setData("loadheader",xmlTemplateDocument.getProcessedDataValue("LOADHEADER", this));
+        } else {
+            xmlTemplateDocument.setData("loadheader",xmlTemplateDocument.getProcessedDataValue("NOTLOADHEADER", this));
         }
-        if(filter == null) {
-
-            // this is the first time, this page is called
-            filter = "all";
-            if(session.getValue("oldProjectId") == null) {
-                int projectId = Integer.parseInt((String)parameters.get("projectid"));
-                session.putValue("oldProjectId", new Integer(cms.getRequestContext().currentProject().getId()));
-
-                // set this project temporarly
-                cms.getRequestContext().setCurrentProject(projectId);
-
-                // update the head-frame
-                xmlTemplateDocument.setData("onload",
-                        "window.top.body.admin_head.location.href='administration_head_4.html';");
+        if(projectId == null || "".equalsIgnoreCase(projectId)){
+            projectId = (String)session.getValue("projectid");
+            if(projectId == null || "".equalsIgnoreCase(projectId)){
+                projectId = ""+cms.getRequestContext().currentProject().getId();
             }
         }
 
-        // store the chosen filter into the session
+        // store the chosen filter and projectid into the session
         session.putValue("filter", filter);
+        session.putValue("projectid", projectId);
 
+        if(action != null && "restore".equalsIgnoreCase(action)){
+            session.removeValue("projectid");
+            session.removeValue("filter");
+        }
         // Finally start the processing
-
-        //return startProcessing(cms, xmlTemplateDocument, elementName, parameters, templateSelector);
-
-        // Finally start the processing
-        byte[] content = startProcessing(cms, xmlTemplateDocument, elementName,
-                parameters, templateSelector);
-
-        //set the current project back
-        //cms.getRequestContext().setCurrentProject(currentProjectId);
-        return content;
-    }
-
-    /**
-     * From interface <code>I_CmsFileListUsers</code>.
-     * <P>
-     * Fills all customized columns with the appropriate settings for the given file
-     * list entry. Any column filled by this method may be used in the customized template
-     * for the file list.
-     * @param cms Cms object for accessing system resources.
-     * @param filelist Template file containing the definitions for the file list together with
-     * the included customized defintions.
-     * @param res CmsResource Object of the current file list entry.
-     * @param lang Current language file.
-     * @exception CmsException if access to system resources failed.
-     * @see I_CmsFileListUsers
-     */
-
-    public void getCustomizedColumnValues(CmsObject cms, CmsXmlWpTemplateFile filelistTemplate,
-            CmsResource res, CmsXmlLanguageFile lang) throws CmsException {
-        String name = res.getName();
-        String path = res.getPath();
-        String link = "#";
-        String servlets = cms.getRequestContext().getRequest().getServletUrl();
-        if(res.isFile() && (res.getState() != C_STATE_DELETED)) {
-            link = "javascript:openwinfull('" + servlets + res.getAbsolutePath()
-                    + "', 'preview', 0, 0);";
-        }
-        filelistTemplate.setData("LINK_VALUE", link);
-        filelistTemplate.setData("SHORTNAME_VALUE", name);
-        filelistTemplate.setData("PATH_VALUE", path);
-    }
-
-    /**
-     * From interface <code>I_CmsFileListUsers</code>.
-     * <P>
-     * Collects all folders and files that are displayed in the file list.
-     * @param cms The CmsObject.
-     * @return A vector of folder and file objects.
-     * @exception Throws CmsException if something goes wrong.
-     */
-
-    public Vector getFiles(CmsObject cms) throws CmsException {
-        Vector resources = new Vector();
-
-        // get the session
-        I_CmsSession session = cms.getRequestContext().getSession(true);
-        String filter = (String)session.getValue("filter");
-        if(filter == null) {
-            filter = "all";
-        }
-
-        // get all ressources, that are changed in this project
-        getFiles(cms, C_ROOT, resources, filter);
-        return resources;
-    }
-
-    /**
-     * Get all changed files in a project.
-     *
-     * @param cms The cms object.
-     * @param folderName The name of the folder to start.
-     * @param resources A Vector to store the result into.
-     * @param filter The filter-value, to select the files.
-     */
-
-    private void getFiles(CmsObject cms, String folderName, Vector resources, String filter)
-            throws CmsException {
-        CmsResource resource;
-        Vector resourcesToTest;
-
-        // get resources
-        resourcesToTest = cms.readFileHeaders(cms.getRequestContext().currentProject().getId());
-
-        //copy the values into the resources, if they where changed
-        for(int i = 0;i < resourcesToTest.size();i++) {
-            resource = (CmsResource)resourcesToTest.elementAt(i);
-            if(resource.getState() != C_STATE_UNCHANGED) {
-                addResource(resources, filter, resource);
-            }
-        }
+        return startProcessing(cms, xmlTemplateDocument, elementName, parameters, templateSelector);
     }
 
     /**
@@ -295,22 +114,5 @@ public class CmsAdminProjectResentFiles extends CmsWorkplaceDefault implements I
     public boolean isCacheable(CmsObject cms, String templateFile, String elementName,
             Hashtable parameters, String templateSelector) {
         return false;
-    }
-
-    /**
-     * From interface <code>I_CmsFileListUsers</code>.
-     * <P>
-     * Used to modify the bit pattern for hiding and showing columns in
-     * the file list.
-     * @param cms Cms object for accessing system resources.
-     * @param prefs Old bit pattern.
-     * @return New modified bit pattern.
-     * @see I_CmsFileListUsers
-     */
-
-    public int modifyDisplayedColumns(CmsObject cms, int prefs) {
-        prefs = ((prefs & C_FILELIST_NAME) == 0) ? prefs : (prefs - C_FILELIST_NAME);
-        prefs = ((prefs & C_FILELIST_TITLE) == 0) ? prefs : (prefs - C_FILELIST_TITLE);
-        return prefs;
     }
 }
