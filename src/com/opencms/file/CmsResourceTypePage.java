7743381a8b6ed4960ab056aaf5f4a632b69be3cd
e.falkenhan <e.falkenhan>
2001-08-03 13:17:53 +0000

    checkFolders: check if corresponding folder is locked before unlocking

--- src/com/opencms/file/CmsResourceTypePage.java
+++ src/com/opencms/file/CmsResourceTypePage.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/file/Attic/CmsResourceTypePage.java,v $
-* Date   : $Date: 2001/08/03 12:26:57 $
-* Version: $Revision: 1.13 $
+* Date   : $Date: 2001/08/03 13:17:53 $
+* Version: $Revision: 1.14 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -45,7 +45,7 @@ import com.opencms.file.genericSql.*;
  * Access class for resources of the type "Page".
  *
  * @author Alexander Lucas
- * @version $Revision: 1.13 $ $Date: 2001/08/03 12:26:57 $
+ * @version $Revision: 1.14 $ $Date: 2001/08/03 13:17:53 $
  */
 public class CmsResourceTypePage implements I_CmsResourceType, Serializable, I_CmsConstants, com.opencms.workplace.I_CmsWpConstants {
 
@@ -843,7 +843,10 @@ public class CmsResourceTypePage implements I_CmsResourceType, Serializable, I_C
                   cms.doChmod(newfolder.getAbsolutePath(),folder.getAccessFlags());
                   cms.doChown(newfolder.getAbsolutePath(),cms.readOwner(folder).getName());
                   try{
-                    cms.doUnlockResource(newfolder.getAbsolutePath());
+                    CmsFolder correspondingFolder = cms.readFolder(foldername);
+                    if(!correspondingFolder.isLocked()){
+                        cms.doUnlockResource(newfolder.getAbsolutePath());
+                    }
                   } catch (CmsException ex){
                     // unable to unlock folder if parent folder is locked
                   }
