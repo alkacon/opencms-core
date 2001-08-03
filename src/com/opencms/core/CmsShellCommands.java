29a86798b4c214c82c52ea60adf47ab502613622
e.falkenhan <e.falkenhan>
2001-08-03 06:46:11 +0000

    Bugfix 1187: keeping the access flags when copying a folder

--- src/com/opencms/core/CmsShellCommands.java
+++ src/com/opencms/core/CmsShellCommands.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/core/Attic/CmsShellCommands.java,v $
-* Date   : $Date: 2001/07/31 15:50:12 $
-* Version: $Revision: 1.36 $
+* Date   : $Date: 2001/08/03 06:46:11 $
+* Version: $Revision: 1.37 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -19,7 +19,7 @@
 * Lesser General Public License for more details.
 *
 * For further information about OpenCms, please see the
-* OpenCms Website: http://www.opencms.org 
+* OpenCms Website: http://www.opencms.org
 *
 * You should have received a copy of the GNU Lesser General Public
 * License along with this library; if not, write to the Free Software
@@ -40,7 +40,7 @@ import source.org.apache.java.util.*;
  *
  * @author Andreas Schouten
  * @author Anders Fugmann
- * @version $Revision: 1.36 $ $Date: 2001/07/31 15:50:12 $
+ * @version $Revision: 1.37 $ $Date: 2001/08/03 06:46:11 $
  */
 public class CmsShellCommands implements I_CmsConstants {
 
@@ -431,7 +431,8 @@ public class CmsShellCommands implements I_CmsConstants {
      */
     public void copyFolder(String source, String destination) {
         try {
-            m_cms.copyFolder(source, destination);
+            // copy the folder with keeping the flags
+            m_cms.copyResource(source, destination, true);
         }
         catch(Exception exc) {
             CmsShell.printException(exc);
