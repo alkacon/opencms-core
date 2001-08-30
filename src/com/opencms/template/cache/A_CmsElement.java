c77d2a3f67ebc5109211086e9f26ab2b243db0ed
h.riege <h.riege>
2001-08-30 14:01:31 +0000

    improved RPC 2218, toString instead of getMessage

--- src/com/opencms/template/cache/A_CmsElement.java
+++ src/com/opencms/template/cache/A_CmsElement.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/template/cache/Attic/A_CmsElement.java,v $
-* Date   : $Date: 2001/08/30 13:50:01 $
-* Version: $Revision: 1.23 $
+* Date   : $Date: 2001/08/30 14:01:31 $
+* Version: $Revision: 1.24 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -401,7 +401,7 @@ public abstract class A_CmsElement implements com.opencms.boot.I_CmsLogChannels 
                                 if(cms.C_USER_TYPE_SYSTEMUSER == cms.getRequestContext().currentUser().getType()
                                     && !cms.C_GROUP_GUEST.equals(cms.getRequestContext().currentGroup().getName())){
                                     // a systemuser gets the real message.(except guests)
-                                    errorMessage = e.getMessage();
+                                    errorMessage = e.toString();
                                 }
                                 subEl = null;
                                 buffer = null;
