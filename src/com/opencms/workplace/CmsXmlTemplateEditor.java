71d92f0ebcb78877b92aae4e4184a1a9ef2d8f9e
e.falkenhan <e.falkenhan>
2001-09-13 10:23:12 +0000

    remove last revision

--- src/com/opencms/workplace/CmsXmlTemplateEditor.java
+++ src/com/opencms/workplace/CmsXmlTemplateEditor.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/workplace/Attic/CmsXmlTemplateEditor.java,v $
-* Date   : $Date: 2001/09/13 09:14:23 $
-* Version: $Revision: 1.51 $
+* Date   : $Date: 2001/09/13 10:23:12 $
+* Version: $Revision: 1.52 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -44,7 +44,7 @@ import javax.servlet.http.*;
  * Reads template files of the content type <code>CmsXmlWpTemplateFile</code>.
  *
  * @author Alexander Lucas
- * @version $Revision: 1.51 $ $Date: 2001/09/13 09:14:23 $
+ * @version $Revision: 1.52 $ $Date: 2001/09/13 10:23:12 $
  * @see com.opencms.workplace.CmsXmlWpTemplateFile
  */
 
@@ -100,20 +100,11 @@ public class CmsXmlTemplateEditor extends CmsWorkplaceDefault implements I_CmsCo
                 // Vary bad. We should not go on here since we may run
                 // in an endless loop.
                 throw e;
-            } else {
-                try {
-                    cms.deleteResource(temporaryFilename);
-                    cms.copyFile(file.getAbsolutePath(), temporaryFilename);
-                    cms.chmod(temporaryFilename, 91);
-                } catch (CmsException ex){
-                    cms.getRequestContext().setCurrentProject(curProject);
-                    throw ex;
-                }
             }
             ok = false;
         }
         extendedTempFile = temporaryFilename;
-/*
+
         int loop = 0;
         while(!ok) {
             ok = true;
@@ -136,7 +127,7 @@ public class CmsXmlTemplateEditor extends CmsWorkplaceDefault implements I_CmsCo
                 ok = false;
             }
         }
-*/
+
         cms.getRequestContext().setCurrentProject(curProject);
         // Oh. we have found a temporary file.
         temporaryFilename = extendedTempFile;
