ad492bd16f7900006698e414831f20aca2832c26
e.falkenhan <e.falkenhan>
2001-11-14 10:12:35 +0000

    When undeleting the file use method for reading files include deleted

--- src/com/opencms/file/CmsResourceTypePage.java
+++ src/com/opencms/file/CmsResourceTypePage.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/file/Attic/CmsResourceTypePage.java,v $
-* Date   : $Date: 2001/10/18 07:02:48 $
-* Version: $Revision: 1.22 $
+* Date   : $Date: 2001/11/14 10:12:35 $
+* Version: $Revision: 1.23 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -45,7 +45,7 @@ import com.opencms.file.genericSql.*;
  * Access class for resources of the type "Page".
  *
  * @author Alexander Lucas
- * @version $Revision: 1.22 $ $Date: 2001/10/18 07:02:48 $
+ * @version $Revision: 1.23 $ $Date: 2001/11/14 10:12:35 $
  */
 public class CmsResourceTypePage implements I_CmsResourceType, Serializable, I_CmsConstants, com.opencms.workplace.I_CmsWpConstants {
 
@@ -477,7 +477,7 @@ public class CmsResourceTypePage implements I_CmsResourceType, Serializable, I_C
     * has not the appropriate rights to undelete the file.
     */
     public void undeleteResource(CmsObject cms, String filename) throws CmsException{
-        CmsFile file = cms.readFile(filename);
+        CmsFile file = cms.readFile(filename, true);
         cms.doUndeleteFile(filename);
         String bodyPath = checkBodyPath(cms, (CmsFile)file);
         if (bodyPath != null){
