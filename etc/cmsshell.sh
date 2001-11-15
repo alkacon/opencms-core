b230f126fc84222b2787bdc300825334b640d6ba
a.schouten <a.schouten>
2001-11-15 16:51:32 +0000

    Initial checkin of CmsCronScheduler.

--- etc/cmsshell.sh
+++ etc/cmsshell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Start script for the OpenCms Shell
 
-java -Dfile.encoding=ISO8859_1 -jar lib/opencmsboot.jar "$@"
+java -Dfile.encoding=ISO8859_1 -Dopencms.disableScheduler=true -jar lib/opencmsboot.jar "$@"
