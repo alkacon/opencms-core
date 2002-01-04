8c3d7ea3668b97701c48922eceeef28ee247749e
h.riege <h.riege>
2002-01-04 10:51:25 +0000

    no message

--- src/com/opencms/launcher/CmsLauncherManager.java
+++ src/com/opencms/launcher/CmsLauncherManager.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/launcher/Attic/CmsLauncherManager.java,v $
-* Date   : $Date: 2001/07/31 15:50:16 $
-* Version: $Revision: 1.18 $
+* Date   : $Date: 2002/01/04 10:51:25 $
+* Version: $Revision: 1.19 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -19,7 +19,7 @@
 * Lesser General Public License for more details.
 *
 * For further information about OpenCms, please see the
-* OpenCms Website: http://www.opencms.org 
+* OpenCms Website: http://www.opencms.org
 *
 * You should have received a copy of the GNU Lesser General Public
 * License along with this library; if not, write to the Free Software
@@ -38,7 +38,7 @@ import java.util.*;
  * given launcher id.
  *
  * @author Alexander Lucas
- * @version $Revision: 1.18 $ $Date: 2001/07/31 15:50:16 $
+ * @version $Revision: 1.19 $ $Date: 2002/01/04 10:51:25 $
  */
 public class CmsLauncherManager implements I_CmsLogChannels {
 
