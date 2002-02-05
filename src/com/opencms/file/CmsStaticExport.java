ec36028269aa2cba212b6df32ec635e0eb91ccc4
h.riege <h.riege>
2002-02-05 14:55:57 +0000

    bugfix static export for search

--- src/com/opencms/file/CmsStaticExport.java
+++ src/com/opencms/file/CmsStaticExport.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/file/Attic/CmsStaticExport.java,v $
-* Date   : $Date: 2002/02/04 16:42:08 $
-* Version: $Revision: 1.16 $
+* Date   : $Date: 2002/02/05 14:55:57 $
+* Version: $Revision: 1.17 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -40,7 +40,7 @@ import org.apache.oro.text.perl.*;
  * to the filesystem.
  *
  * @author Hanjo Riege
- * @version $Revision: 1.16 $ $Date: 2002/02/04 16:42:08 $
+ * @version $Revision: 1.17 $ $Date: 2002/02/05 14:55:57 $
  */
 public class CmsStaticExport implements I_CmsConstants{
 
@@ -123,6 +123,7 @@ public class CmsStaticExport implements I_CmsConstants{
                      throws CmsException{
         m_cms = cms;
         m_startpoints = startpoints;
+        m_changedLinks = changedLinks;
         m_exportPath = cms.getStaticExportPath();
         c_perlUtil = new Perl5Util();
         if(changedResources != null){
