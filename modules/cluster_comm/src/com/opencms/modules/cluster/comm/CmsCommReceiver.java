1ee5c4e0b84430c25c3319adea9eb2f266ded5c5
h.riege <h.riege>
2002-07-25 15:34:53 +0000

    added debug logs

--- modules/cluster_comm/src/com/opencms/modules/cluster/comm/CmsCommReceiver.java
+++ modules/cluster_comm/src/com/opencms/modules/cluster/comm/CmsCommReceiver.java
@@ -11,17 +11,49 @@ import java.lang.reflect.*;
 
 public class CmsCommReceiver extends CmsXmlTemplate {
 
+    private static final boolean C_DEBUG = false;
+
     public byte[] getContent(CmsObject cms, String templateFile, String elementName,
             Hashtable parameters, String templateSelector) throws CmsException {
 
             try {
+                if(C_DEBUG){
+                    System.err.println("");
+                    System.err.println("-------------------------------------------");
+                    System.err.println("---   Communication module is started   ---");
+                    System.err.println("    read container...");
+                }
                 // read the container from stream
                 CmsCommandContainer container = readContainerFromRequest(cms);
                 // login the user
+                if(C_DEBUG){
+                    System.err.println("    login user...");
+                }
                 cms.loginUser(container.getUsername(), container.getPassword());
                 // invoke the methods specified
+                if(C_DEBUG){
+                    System.err.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXX system info before invoke methods XXXXXXXXXXXXXXXXXXXXXXX");
+                    printSystemInfo(cms);
+                    System.err.println("    invoke methods...");
+                }
                 invokeMethods(cms, container);
+                if(C_DEBUG){
+                    System.err.println("    ...done.");
+                    System.err.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXX system info after invoke methods XXXXXXXXXXXXXXXXXXXXXXX");
+                    printSystemInfo(cms);
+                    System.err.println("--------------------------------------------");
+                    System.err.println("");
+                }
             } catch(Exception exc) {
+                if(C_DEBUG){
+                    System.err.println("    ... EXCEPTION:");
+                    exc.printStackTrace();
+                    System.err.println("");
+                    System.err.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXX system info after invoke methods XXXXXXXXXXXXXXXXXXXXXXX");
+                    printSystemInfo(cms);
+                    System.err.println("");
+                    System.err.println("");
+                }
                 if(I_CmsLogChannels.C_PREPROCESSOR_IS_LOGGING && A_OpenCms.isLogging() ) {
                     A_OpenCms.log(A_OpenCms.C_MODULE_DEBUG,
                         " module com.opencms.modules.cluster.comm cant invoce methods."
@@ -49,8 +81,19 @@ public class CmsCommReceiver extends CmsXmlTemplate {
         ArrayList commands = container.getCommands();
         for(int i = 0; i < commands.size(); i++) {
             try{
+                if(C_DEBUG){
+                    System.err.println("        invoke methode "+ ((CmsCommand) commands.get(i)).toString());
+                }
                 invokeMethod(cms, (CmsCommand) commands.get(i));
+                if(C_DEBUG){
+                    System.err.println("        ok.");
+                }
             }catch(Exception e){
+                if(C_DEBUG){
+                    System.err.println("        ... EXCEPTION:");
+                    e.printStackTrace();
+                    System.err.println("");
+                }
                 if(I_CmsLogChannels.C_PREPROCESSOR_IS_LOGGING && A_OpenCms.isLogging() ) {
                     A_OpenCms.log(A_OpenCms.C_MODULE_DEBUG,
                         " module com.opencms.modules.cluster.comm cant invoke methode "
@@ -87,5 +130,55 @@ public class CmsCommReceiver extends CmsXmlTemplate {
         return new CmsCacheDirectives(false);
     }
 
-
+    /**
+     * private methode to print some system inforamtion. Just used for debug reasons.
+     *
+     * @param cms The cms object
+     */
+    private void printSystemInfo(CmsObject cms){
+        try{
+            System.err.println("");
+            System.err.println("***********************************************************************");
+            System.err.println("**                          SYSTEM INFO                              **");
+            System.err.println("***********************************************************************");
+            System.err.println("    online ElementCache:");
+            Vector cacheInfo = cms.getOnlineElementCache().getCacheInfo();
+            System.err.println("        uri: "+((Integer)cacheInfo.elementAt(1)).toString() + " | " +
+                                                    ((Integer)cacheInfo.elementAt(0)).toString());
+            System.err.println("        element: "+((Integer)cacheInfo.elementAt(1)).toString() + " | " +
+                                                    ((Integer)cacheInfo.elementAt(0)).toString());
+            System.err.println("");
+            System.err.println("    cache:");
+            System.err.println("        "+cms.getCacheInfo().toString());
+                System.err.println("");
+                System.err.println("The dynamic rulesets created by OpenCms:");
+                System.err.println("----------------------------------------");
+                System.err.println("");
+                System.err.println("---- Online ----");
+                if(CmsStaticExport.m_dynamicExportRulesOnline != null){
+                    for(int i=0; i<CmsStaticExport.m_dynamicExportRulesOnline.size();System.err.println(CmsStaticExport.m_dynamicExportRulesOnline.elementAt(i++)));
+                }
+                System.err.println("");
+                System.err.println("---- Extern ----");
+                if(CmsStaticExport.m_dynamicExportRulesExtern != null){
+                    for(int i=0; i<CmsStaticExport.m_dynamicExportRulesExtern.size();System.err.println(CmsStaticExport.m_dynamicExportRulesExtern.elementAt(i++)));
+                }
+                System.err.println("");
+                System.err.println("---- nicename Online ----");
+                if(CmsStaticExport.m_dynamicExportNameRules != null){
+                    for(int i=0; i<CmsStaticExport.m_dynamicExportNameRules.size();System.err.println(CmsStaticExport.m_dynamicExportNameRules.elementAt(i++)));
+                }
+                System.err.println("");
+                System.err.println("---- nicename  Extern----");
+                if(CmsStaticExport.m_dynamicExportNameRulesExtern != null){
+                    for(int i=0; i<CmsStaticExport.m_dynamicExportNameRulesExtern.size();System.err.println(CmsStaticExport.m_dynamicExportNameRulesExtern.elementAt(i++)));
+                }
+                System.err.println("");
+            System.err.println("***********************************************************************");
+            System.err.println("");
+        }catch(Exception e){
+            System.err.println(" Exception while printing Systeminfo.");
+            e.printStackTrace();
+        }
+    }
 }
