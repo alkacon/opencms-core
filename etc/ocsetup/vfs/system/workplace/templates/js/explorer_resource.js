106c270d35b2c428a1a0af1ba42b9fbf7b4ee0d7
mEmmerich <m.emmerich@alkacon.com>
2002-08-02 12:33:39 +0000

    Added changes made by Gridnine: Added encoding to all xml templates

--- etc/ocsetup/vfs/system/workplace/templates/js/explorer_resource.js
+++ etc/ocsetup/vfs/system/workplace/templates/js/explorer_resource.js
@@ -1,4 +1,4 @@
-<?xml version="1.0"?>
+<?xml version="1.0" encoding="ISO-8859-1"?>
 <PAGE>
 	<CLASS>com.opencms.workplace.CmsExplorerResources</CLASS>
 	<MASTERTEMPLATE>/system/workplace/templates/js/explorer_resource</MASTERTEMPLATE>
