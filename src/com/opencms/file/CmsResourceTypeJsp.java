0b0fd87c4cc3aa176faf0d4da90f4096996e7635
aKandzior <a.kandzior@alkacon.com>
2002-08-22 08:21:41 +0000

    Merged FLEX branch into main branch

---
+++ src/com/opencms/file/CmsResourceTypeJsp.java
@@ -0,0 +1,52 @@
+/*
+* File   : $Source: /alkacon/cvs/opencms/src/com/opencms/file/Attic/CmsResourceTypeJsp.java,v $
+* Date   : $Date: 2002/08/22 08:21:41 $
+* Version: $Revision: 1.2 $
+*
+* This library is part of OpenCms -
+* the Open Source Content Mananagement System
+*
+* Copyright (C) 2001  The OpenCms Group
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* For further information about OpenCms, please see the
+* OpenCms Website: http://www.opencms.org 
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package com.opencms.file;
+
+/**
+ * This class describes the resource-type JSP.
+ * It was needed because JSP should have property "export" set to "false" by default.
+ *
+ * @author Alexander Kandzior (a.kandzior@alkacon.com)
+ * @version 1.0
+ */
+
+public class CmsResourceTypeJsp extends com.opencms.file.CmsResourceTypePlain {
+
+    public static final String C_TYPE_RESOURCE_NAME = "jsp";
+
+    public com.opencms.file.CmsResource createResource(com.opencms.file.CmsObject cms, String folder, String name, java.util.Hashtable properties, byte[] contents) throws com.opencms.core.CmsException{
+        com.opencms.file.CmsResource res = cms.doCreateFile(folder, name, contents, C_TYPE_RESOURCE_NAME, properties);
+        String filename = folder + name;
+        // Lock the new file
+        cms.lockResource(filename);
+        // Attach default JSP properties
+        cms.writeProperty(filename, C_PROPERTY_EXPORT, "false");
+        return res;
+    }
+}
