78f91ace4de32b67fb4453d9b1d1fc4d2425af9c
a.schouten <a.schouten>
2002-08-22 10:04:35 +0000

    Update of shellscripts for new classloader

--- etc/cmsshell.sh
+++ etc/cmsshell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Start script for the OpenCms Shell
 
-java -Dfile.encoding=ISO8859_1 -Dopencms.disableScheduler=true -jar lib/opencmsboot.jar "$@"
+java -Dfile.encoding=ISO8859_1 -Dopencms.disableScheduler=true  -classpath lib/opencms.jar:lib/opencmsboot.jar:lib/activation.jar:lib/jakarta-oro-2.0.6.jar:lib/mm.mysql-2.0.4-bin.jar:lib/Tidy.jar:lib/mail.jar:lib/fesi.jar:lib/xerces-1_4_4.jar com.opencms.boot.CmsMain "$@"
