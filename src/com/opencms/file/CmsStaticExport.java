651c5855b9876bee16778c642d5cba17a7068e31
aKandzior <a.kandzior@alkacon.com>
2002-09-02 07:52:23 +0000

    Added internal implementation of parseQuerySting() method to prevent usage of deprecated class HttpUtils

--- src/com/opencms/file/CmsStaticExport.java
+++ src/com/opencms/file/CmsStaticExport.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/file/Attic/CmsStaticExport.java,v $
-* Date   : $Date: 2002/08/02 12:12:57 $
-* Version: $Revision: 1.29 $
+* Date   : $Date: 2002/09/02 07:52:23 $
+* Version: $Revision: 1.30 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -41,7 +41,7 @@ import org.apache.oro.text.perl.*;
  * to the filesystem.
  *
  * @author Hanjo Riege
- * @version $Revision: 1.29 $ $Date: 2002/08/02 12:12:57 $
+ * @version $Revision: 1.30 $ $Date: 2002/09/02 07:52:23 $
  */
 public class CmsStaticExport implements I_CmsConstants{
 
@@ -499,6 +499,106 @@ public class CmsStaticExport implements I_CmsConstants{
     }
 
     /**
+     * Parse a name in the query string.
+     */
+    static private String parseName(String s, StringBuffer sb) {
+    sb.setLength(0);
+    for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i); 
+        switch (c) {
+        case '+':
+        sb.append(' ');
+        break;
+        case '%':
+        try {
+            sb.append((char) Integer.parseInt(s.substring(i+1, i+3), 
+                              16));
+            i += 2;
+        } catch (NumberFormatException e) {
+            // XXX
+            // need to be more specific about illegal arg
+            throw new IllegalArgumentException();
+        } catch (StringIndexOutOfBoundsException e) {
+            String rest  = s.substring(i);
+            sb.append(rest);
+            if (rest.length()==2)
+            i++;
+        }
+        
+        break;
+        default:
+        sb.append(c);
+        break;
+        }
+    }
+    return sb.toString();
+    }
+
+    /**
+     *
+     * Parses a query string passed from the client to the
+     * server and builds a <code>HashTable</code> object
+     * with key-value pairs. 
+     * The query string should be in the form of a string
+     * packaged by the GET or POST method, that is, it
+     * should have key-value pairs in the form <i>key=value</i>,
+     * with each pair separated from the next by a & character.
+     *
+     * <p>A key can appear more than once in the query string
+     * with different values. However, the key appears only once in 
+     * the hashtable, with its value being
+     * an array of strings containing the multiple values sent
+     * by the query string.
+     * 
+     * <p>The keys and values in the hashtable are stored in their
+     * decoded form, so
+     * any + characters are converted to spaces, and characters
+     * sent in hexadecimal notation (like <i>%xx</i>) are
+     * converted to ASCII characters.
+     *
+     * @param s     a string containing the query to be parsed
+     *
+     * @return      a <code>HashTable</code> object built
+     *          from the parsed key-value pairs
+     *
+     * @exception IllegalArgumentException  if the query string 
+     *                      is invalid
+     *
+     */
+    static public Hashtable parseQueryString(String s) {
+    String valArray[] = null;
+    if (s == null) {
+        throw new IllegalArgumentException();
+    }
+    Hashtable ht = new Hashtable();
+    StringBuffer sb = new StringBuffer();
+    StringTokenizer st = new StringTokenizer(s, "&");
+    while (st.hasMoreTokens()) {
+        String pair = (String)st.nextToken();
+        int pos = pair.indexOf('=');
+        if (pos == -1) {
+        // XXX
+        // should give more detail about the illegal argument
+        throw new IllegalArgumentException();
+        }
+        String key = parseName(pair.substring(0, pos), sb);
+        String val = parseName(pair.substring(pos+1, pair.length()), sb);
+        if (ht.containsKey(key)) {
+        String oldVals[] = (String []) ht.get(key);
+        valArray = new String[oldVals.length + 1];
+        for (int i = 0; i < oldVals.length; i++) 
+            valArray[i] = oldVals[i];
+        valArray[oldVals.length] = val;
+        } else {
+        valArray = new String[1];
+        valArray[0] = val;
+        }
+        ht.put(key, valArray);
+    }
+    return ht;
+    }
+
+    /**
      * exports one single link and adds sublinks to the allLinks vector.
      *
      * @param link The link to export may have html parameters
@@ -533,8 +633,7 @@ public class CmsStaticExport implements I_CmsConstants{
                 return;
             }
             if(paraStart >= 0){
-                Hashtable parameter = javax.servlet.http.HttpUtils.parseQueryString(
-                                        link.substring(paraStart +1));
+                Hashtable parameter = parseQueryString(link.substring(paraStart +1));
                 link = link.substring(0, paraStart);
                 dReq.setParameters(parameter);
             }
