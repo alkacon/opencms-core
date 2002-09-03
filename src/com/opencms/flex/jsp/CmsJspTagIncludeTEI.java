72d86c9ef75cbeb20a0143a52f8cc8cf9567bd96
aKandzior <a.kandzior@alkacon.com>
2002-09-03 19:44:30 +0000

    TEI for the include tag

---
+++ src/com/opencms/flex/jsp/CmsJspTagIncludeTEI.java
@@ -0,0 +1,86 @@
+/*
+* File   : $Source $
+* Date   : $Date $
+* Version: $Revision $
+*
+* This library is part of OpenCms -
+* the Open Source Content Mananagement System
+*
+* Copyright (C) 2002  The OpenCms Group
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* For further information about OpenCms, please see the
+* OpenCms Website: http://www.opencms.org
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package com.opencms.flex.jsp;
+
+import javax.servlet.jsp.tagext.TagData;
+import javax.servlet.jsp.tagext.TagExtraInfo;
+
+/**
+ * This is a TagExtraInfo evaluation class that checks the attibutes of 
+ * the <code>cms:include</code> tag.
+ *
+ * @author  Alexander Kandzior (a.kandzior@alkacon.com)
+ * @version $Revision: 1.1 $
+ */
+public class CmsJspTagIncludeTEI extends TagExtraInfo {
+
+    final private static String C_ATTR_PROPERTY = "property";
+    final private static String C_ATTR_FILE = "file";
+    final private static String C_ATTR_PAGE = "page";
+    final private static String C_ATTR_SUFFIX = "suffix";
+    final private static String C_ATTR_BODY = "body";
+    
+    /**
+     * Returns true if the given attribute name is specified, false otherwise.
+     */
+    public static boolean isSpecified(TagData data, String attributeName) {
+        return (data.getAttribute(attributeName) != null);
+    }
+    
+    /**
+     * Checks the validity of the <code>cms:include</code> attributs.
+     * The rules are:<ol>
+     * 
+     * <li>If any value appears in the tag body, only the suffix attribute is allowed.
+     * 
+     * <li>If the <code>file</code> or <code>page</code> attributes are used, 
+     * no other attributes may appear and the body must be empty.
+     * 
+     * <li>If the <code>property</code> attribute is used, only the <code>suffix</code>
+     * may optionally be used, too. The body must be empty.</ol>
+     */
+    public boolean isValid(TagData data) {
+        
+        boolean hasFile = isSpecified(data, C_ATTR_FILE) || isSpecified(data, C_ATTR_PAGE);
+        boolean hasSuffix = isSpecified(data, C_ATTR_SUFFIX);
+        boolean hasProperty = isSpecified(data, C_ATTR_PROPERTY);
+        boolean hasBody = isSpecified(data, C_ATTR_BODY);
+
+        if (hasBody && (hasFile || hasSuffix || hasProperty)) return false;
+        if (hasBody) {
+            String isTrue = (String)data.getAttribute(C_ATTR_BODY);
+            if (! "eval".equals(isTrue)) return false;
+        }
+        if (hasFile && (hasSuffix || hasProperty)) return false;
+        if (hasSuffix && !hasProperty) return false;
+        if (! (hasProperty || hasFile || hasBody)) return false;
+
+        return true;
+    }        
+}
