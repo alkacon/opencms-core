cea971be601f06373414c9d23a27e04c674f7cf1
aKandzior <a.kandzior@alkacon.com>
2002-09-05 12:43:29 +0000

    Modified shell scipts for new jar names

--- etc/cmsshell.bat
+++ etc/cmsshell.bat
@@ -1,3 +1,3 @@
 rem Start script for the OpenCms Shell
 
-java -Dfile.encoding=ISO8859_1 -Dopencms.disableScheduler=true  -classpath lib/opencms.jar;lib/opencmsboot.jar;lib/activation.jar;lib/jakarta-oro-2.0.6.jar;lib/mm.mysql-2.0.4-bin.jar;lib/Tidy.jar;lib/mail.jar;lib/fesi.jar;lib/xerces-1_4_4.jar com.opencms.boot.CmsMain %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Dfile.encoding=ISO8859_1 -Dopencms.disableScheduler=true  -classpath lib/opencms.jar;lib/activation.jar;lib/jakarta-oro-2_0_6.jar;lib/mysql-connector-java-2_0_14-bin.jar;lib/Tidy.jar;lib/mail.jar;lib/fesi.jar;lib/xerces-1_4_4.jar com.opencms.boot.CmsMain %1 %2 %3 %4 %5 %6 %7 %8 %9
--- etc/cmsshell.sh
+++ etc/cmsshell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Start script for the OpenCms Shell
 
-java -Dfile.encoding=ISO8859_1 -Dopencms.disableScheduler=true  -classpath lib/opencms.jar:lib/opencmsboot.jar:lib/activation.jar:lib/jakarta-oro-2.0.6.jar:lib/mm.mysql-2.0.4-bin.jar:lib/Tidy.jar:lib/mail.jar:lib/fesi.jar:lib/xerces-1_4_4.jar com.opencms.boot.CmsMain "$@"
+java -Dfile.encoding=ISO8859_1 -Dopencms.disableScheduler=true  -classpath lib/opencms.jar:lib/activation.jar:lib/jakarta-oro-2_0_6.jar:lib/mysql-connector-java-2_0_14-bin.jar:lib/Tidy.jar:lib/mail.jar:lib/fesi.jar:lib/xerces-1_4_4.jar com.opencms.boot.CmsMain "$@"
