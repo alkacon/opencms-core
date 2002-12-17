1655535a764021c765972b46ac4f6d4d9ac7ad0b
aKandzior <a.kandzior@alkacon.com>
2002-12-17 12:11:05 +0000

    Added path to "servlet.jar" to shell scripts

--- etc/cmsshell.bat
+++ etc/cmsshell.bat
@@ -1,3 +1,4 @@
-rem Start script for the OpenCms Shell
+@rem Start script for the OpenCms Shell
+@rem Please make sure that "servlet.jar" is found, the path used below was tested with Tomcat 4.1
 
-java -Dfile.encoding=ISO8859_1 -Dopencms.disableScheduler=true  -classpath lib/opencms.jar;lib/activation.jar;lib/jakarta-oro-2_0_6.jar;lib/mysql-connector-java-2_0_14-bin.jar;lib/Tidy.jar;lib/mail.jar;lib/fesi.jar;lib/xerces-1_4_4.jar com.opencms.boot.CmsMain %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Dfile.encoding=ISO8859_1 -Dopencms.disableScheduler=true  -classpath ../../../common/lib/servlet.jar;lib/opencms.jar;lib/activation.jar;lib/jakarta-oro-2_0_6.jar;lib/mysql-connector-java-2_0_14-bin.jar;lib/Tidy.jar;lib/mail.jar;lib/fesi.jar;lib/xerces-1_4_4.jar com.opencms.boot.CmsMain %1 %2 %3 %4 %5 %6 %7 %8 %9
--- etc/cmsshell.sh
+++ etc/cmsshell.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Start script for the OpenCms Shell
+# Please make sure that "servlet.jar" is found, the path used below was tested with Tomcat 4.1
 
-java -Dfile.encoding=ISO8859_1 -Dopencms.disableScheduler=true  -classpath lib/opencms.jar:lib/activation.jar:lib/jakarta-oro-2_0_6.jar:lib/mysql-connector-java-2_0_14-bin.jar:lib/Tidy.jar:lib/mail.jar:lib/fesi.jar:lib/xerces-1_4_4.jar com.opencms.boot.CmsMain "$@"
+java -Dfile.encoding=ISO8859_1 -Dopencms.disableScheduler=true  -classpath ../../../common/lib/servlet.jar:lib/opencms.jar:lib/activation.jar:lib/jakarta-oro-2_0_6.jar:lib/mysql-connector-java-2_0_14-bin.jar:lib/Tidy.jar:lib/mail.jar:lib/fesi.jar:lib/xerces-1_4_4.jar com.opencms.boot.CmsMain "$@"
