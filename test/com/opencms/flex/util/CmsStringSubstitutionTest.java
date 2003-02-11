26094c68c2ef1241269ba27c8a9a2f02d0d09ad8
aZahner <a.zahner@alkacon.com>
2003-02-11 17:40:52 +0000

    updated comments

--- test/com/opencms/flex/util/CmsStringSubstitutionTest.java
+++ test/com/opencms/flex/util/CmsStringSubstitutionTest.java
@@ -1,3 +1,33 @@
+/*
+ * File   : $Source: /alkacon/cvs/opencms/test/com/opencms/flex/util/Attic/CmsStringSubstitutionTest.java,v $
+ * Date   : $Date: 2003/02/11 17:40:52 $
+ * Version: $Revision: 1.3 $
+ *
+ * This library is part of OpenCms -
+ * the Open Source Content Mananagement System
+ *
+ * Copyright (C) 2003  The OpenCms Group
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about OpenCms, please see the
+ * OpenCms Website: http://www.opencms.org
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * First created on 11.02.2003
+ */
+
 package com.opencms.flex.util;
 
 import com.opencms.flex.util.CmsStringSubstitution;
@@ -8,7 +38,7 @@ import junit.framework.TestCase;
  * Test cases for the class "CmsStringSubstitution"
  * 
  * @author Andreas Zahner (a.zahner@alkacon.com)
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  * 
  * @since 5.0
  */
