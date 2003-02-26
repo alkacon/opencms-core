d7a0bb97a78dc9676e8b098a0c4197b6773e1d0a
aKandzior <a.kandzior@alkacon.com>
2003-02-26 15:49:13 +0000

    Removed unused imports

--- src/com/opencms/workplace/CmsXmlTemplateEditor.java
+++ src/com/opencms/workplace/CmsXmlTemplateEditor.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/workplace/Attic/CmsXmlTemplateEditor.java,v $
-* Date   : $Date: 2003/02/26 15:29:42 $
-* Version: $Revision: 1.86 $
+* Date   : $Date: 2003/02/26 15:49:13 $
+* Version: $Revision: 1.87 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -50,10 +50,8 @@ import com.opencms.util.Encoder;
 import com.opencms.util.Utils;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Hashtable;
-import java.util.List;
 import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
@@ -65,7 +63,7 @@ import org.w3c.dom.Element;
  * Reads template files of the content type <code>CmsXmlWpTemplateFile</code>.
  *
  * @author Alexander Lucas
- * @version $Revision: 1.86 $ $Date: 2003/02/26 15:29:42 $
+ * @version $Revision: 1.87 $ $Date: 2003/02/26 15:49:13 $
  * @see com.opencms.workplace.CmsXmlWpTemplateFile
  */
 
