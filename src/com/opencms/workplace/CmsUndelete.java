463760c17245c1a1d8a5e90ce211012cf4a1ca6a
tWeckert <t.weckert@alkacon.com>
2003-03-05 12:16:42 +0000

    Fixed the call to readFileHeader to allow deleted files.

--- src/com/opencms/workplace/CmsUndelete.java
+++ src/com/opencms/workplace/CmsUndelete.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/workplace/Attic/CmsUndelete.java,v $
-* Date   : $Date: 2003/01/20 23:59:19 $
-* Version: $Revision: 1.6 $
+* Date   : $Date: 2003/03/05 12:16:42 $
+* Version: $Revision: 1.7 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -43,7 +43,7 @@ import java.util.Hashtable;
  * Reads template files of the content type <code>CmsXmlWpTemplateFile</code>.
  *
  * @author Edna Falkenhan
- * @version $Revision: 1.6 $ $Date: 2003/01/20 23:59:19 $
+ * @version $Revision: 1.7 $ $Date: 2003/03/05 12:16:42 $
  */
 
 public class CmsUndelete extends CmsWorkplaceDefault implements I_CmsWpConstants,I_CmsConstants {
@@ -83,7 +83,7 @@ public class CmsUndelete extends CmsWorkplaceDefault implements I_CmsWpConstants
         if (filename.endsWith("/")){
             file = (CmsResource)cms.readFolder(filename, true);
         } else {
-            file = (CmsResource)cms.readFileHeader(filename);
+            file = (CmsResource)cms.readFileHeader(filename, true);
         }
         try{
             cms.undeleteResource(file.getAbsolutePath());
