0958a626e2fbffaf04f3e1de83a17e172c69ac08
cWeinholz <c.weinholz@alkacon.com>
2003-03-25 09:15:31 +0000

    *** empty log message ***

--- src/com/opencms/boot/CmsSetup.java
+++ src/com/opencms/boot/CmsSetup.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/boot/Attic/CmsSetup.java,v $
-* Date   : $Date: 2003/03/24 18:34:49 $
-* Version: $Revision: 1.19 $
+* Date   : $Date: 2003/03/25 09:15:02 $
+* Version: $Revision: 1.20 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -28,10 +28,17 @@
 
 package com.opencms.boot;
 
-import source.org.apache.java.util.*;
-import java.io.*;
-import java.util.*;
 import com.opencms.flex.util.CmsUUID;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import source.org.apache.java.util.ExtendedProperties;
 
 /**
  * Bean with get / set methods for all properties stored in the
@@ -133,7 +140,7 @@ public class CmsSetup {
 	 */
 	public String getExtProperty(String key) {
 		Object value = null;
-System.err.println(key);            
+            
 		return ((value = m_ExtProperties.get(key)) != null) ? value.toString() : "";
 	}
 
@@ -794,25 +801,25 @@ System.err.println("MAC: " + ethernetAddress);
 	}
 
     /** Set the default tablespace when creating a new oracle user */
-     public void setDbDefaultTablespace( String dbDefaultTablespace) { 
-         this.setDbProperty(this.getResourceBroker() + ".defaultTablespace", dbDefaultTablespace);         
-     }
-    
-     /** Get the default tablespace when creating a new oracle user */
-     public String getDbDefaultTablespace() {
-         return this.getDbProperty(this.getResourceBroker() + ".defaultTablespace");
-     }
-    
-     /** Set the temporary tablespace when creating a new oracle user */
-     public void setDbTemporaryTablespace( String dbTemporaryTablespace) { 
-         this.setDbProperty(this.getResourceBroker() + ".temporaryTablespace", dbTemporaryTablespace);
-     }
-        
-     /** Get the temporary tablespace when creating a new oracle user */
-     public String getDbTemporaryTablespace() {
-         return this.getDbProperty(this.getResourceBroker() + ".temporaryTablespace");
-     }
-          
+    public void setDbDefaultTablespace(String dbDefaultTablespace) {
+        this.setDbProperty(this.getResourceBroker() + ".defaultTablespace", dbDefaultTablespace);
+    }
+
+    /** Get the default tablespace when creating a new oracle user */
+    public String getDbDefaultTablespace() {
+        return this.getDbProperty(this.getResourceBroker() + ".defaultTablespace");
+    }
+
+    /** Set the temporary tablespace when creating a new oracle user */
+    public void setDbTemporaryTablespace(String dbTemporaryTablespace) {
+        this.setDbProperty(this.getResourceBroker() + ".temporaryTablespace", dbTemporaryTablespace);
+    }
+
+    /** Get the temporary tablespace when creating a new oracle user */
+    public String getDbTemporaryTablespace() {
+        return this.getDbProperty(this.getResourceBroker() + ".temporaryTablespace");
+    }
+       
 	public boolean getWizardEnabled() {
 		String dummy = this.getExtProperty("wizard.enabled");
 		return "true".equals(dummy);
--- src/com/opencms/boot/CmsSetupDb.java
+++ src/com/opencms/boot/CmsSetupDb.java
@@ -1,103 +1,104 @@
 package com.opencms.boot;
 
-import java.sql.*;
-import java.util.*;
-import java.io.*;
+import java.io.FileReader;
+import java.io.LineNumberReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
 public class CmsSetupDb {
 
-  private Connection m_con;
-  private Vector m_errors;
-  private String m_basePath;
-  private boolean m_errorLogging = true;
-  private final String C_OCSETUP_FOLDER = "WEB-INF/ocsetup/";
+    private Connection m_con;
+    private Vector m_errors;
+    private String m_basePath;
+    private boolean m_errorLogging = true;
+    private final String C_OCSETUP_FOLDER = "WEB-INF/ocsetup/";
 
-  public CmsSetupDb(String basePath) {
-      m_errors = new Vector();
-      m_basePath = basePath;
-  }
+    public CmsSetupDb(String basePath) {
+        m_errors = new Vector();
+        m_basePath = basePath;
+    }
 
-  public void setConnection(String DbDriver, String DbConStr, String DbUser, String DbPwd) {
-    try {
-        Class.forName(DbDriver);
-        m_con = DriverManager.getConnection(DbConStr,DbUser,DbPwd);
+    public void setConnection(String DbDriver, String DbConStr, String DbUser, String DbPwd) {
+        try {
+            Class.forName(DbDriver);
+            m_con = DriverManager.getConnection(DbConStr, DbUser, DbPwd);
+        } catch (SQLException e) {
+            m_errors.addElement("Could no connect to database via: " + DbConStr + "\n" + e.toString() + "\n");
+        } catch (ClassNotFoundException e) {
+            m_errors.addElement("Error while loading driver: " + DbDriver + "\n" + e.toString() + "\n");
+        }
     }
-    catch (SQLException e)  {
-      m_errors.addElement("Could no connect to database via: " + DbConStr + "\n" + e.toString() + "\n");
-    }
-    catch (ClassNotFoundException e)  {
-      m_errors.addElement("Error while loading driver: " + DbDriver + "\n" + e.toString() + "\n");
-    }
-  }
 
-  public void closeConnection() {
-    try {
-        m_con.close();
+    public void closeConnection() {
+        try {
+            m_con.close();
+        } catch (Exception e) {}
     }
-    catch (Exception e)  {}
-  }
 
-  public void dropDatabase(String resourceBroker, Hashtable replacer) {
-    String file = getScript(resourceBroker+".dropdb");
-    if (file != null) {
-        m_errorLogging = true;
-        parseScript(file,replacer);
+    public void dropDatabase(String resourceBroker, Hashtable replacer) {
+        String file = getScript(resourceBroker + ".dropdb");
+        if (file != null) {
+            m_errorLogging = true;
+            parseScript(file, replacer);
+        } else {
+            m_errors.addElement("Could not open database drop script: " + resourceBroker + ".dropdb");
+        }
     }
-    else  {
-        m_errors.addElement("Could not open database drop script: " + resourceBroker + ".dropdb");
-    }
-  }
 
-  public void createDatabase(String resourceBroker, Hashtable replacer) {
-    String file = getScript(resourceBroker+".createdb");
-    if (file != null) {
-       m_errorLogging = true;
-       parseScript(file,replacer);
+    public void createDatabase(String resourceBroker, Hashtable replacer) {
+        String file = getScript(resourceBroker + ".createdb");
+        if (file != null) {
+            m_errorLogging = true;
+            parseScript(file, replacer);
+        } else {
+            m_errors.addElement("No create database script found: " + resourceBroker + ".createdb \n");
+        }
     }
-    else  {
-      m_errors.addElement("No create database script found: " + resourceBroker + ".createdb \n");
+
+    public void createTables(String resourceBroker) {
+        String file = getScript(resourceBroker + ".createtables");
+        if (file != null) {
+            m_errorLogging = true;
+            parseScript(file, null);
+        } else {
+            m_errors.addElement("No create tables script found: " + resourceBroker + ".createtables \n");
+        }
     }
-  }
 
-  public void createTables(String resourceBroker) {
-    String file = getScript(resourceBroker+".createtables");
-    if (file != null) {
-      m_errorLogging = true;
-      parseScript(file,null);
+    public void dropTables(String resourceBroker) {
+        String file = getScript(resourceBroker + ".droptables");
+        if (file != null) {
+            m_errorLogging = true;
+            parseScript(file, null);
+        } else {
+            m_errors.addElement("No drop tables script found: " + resourceBroker + ".droptables \n");
+        }
     }
-    else  {
-        m_errors.addElement("No create tables script found: " + resourceBroker + ".createtables \n");
-    }
-  }
 
-  public void dropTables(String resourceBroker) {
-      String file = getScript(resourceBroker+".droptables");
-      if (file != null) {
-        m_errorLogging = true;
-        parseScript(file,null);
-      }
-      else  {
-          m_errors.addElement("No drop tables script found: " + resourceBroker + ".droptables \n");
-      }        
-  }
-  
-  private void parseScript(String file, Hashtable replacers)   {
+    private void parseScript(String file, Hashtable replacers) {
 
-      /* indicates if the setup script contains included files (oracle) */
-      boolean includedFile = false;
+        /* indicates if the setup script contains included files (oracle) */
+        boolean includedFile = false;
 
-      /* get and parse the setup script */
-      try {
-          LineNumberReader reader = new LineNumberReader(new FileReader(m_basePath + C_OCSETUP_FOLDER + file));
+        /* get and parse the setup script */
+        try {
+            LineNumberReader reader = new LineNumberReader(new FileReader(m_basePath + C_OCSETUP_FOLDER + file));
             String line = null;
             String statement = "";
-            while(true)  {
+            while (true) {
                 line = reader.readLine();
-                if(line==null)break;
+                if (line == null)
+                    break;
 
                 StringTokenizer st = new StringTokenizer(line);
 
-                while (st.hasMoreTokens())  {
+                while (st.hasMoreTokens()) {
                     String currentToken = st.nextToken();
 
                     /* comment! Skip rest of the line */
@@ -123,25 +124,23 @@ public class CmsSetupDb {
                     /* query complete (terminated by ';') */
                     if (currentToken.endsWith(";")) {
                         /* cut of ';' at the end */
-                        statement = statement.substring(0,(statement.length()-1));
+                        statement = statement.substring(0, (statement.length() - 1));
 
                         /* there is an included File. Get it and execute it */
                         if (includedFile) {
                             if (replacers != null) {
-                                ExecuteStatement(replaceValues(getIncludedFile(statement),replacers));
-                            }
-                            else  {
+                                ExecuteStatement(replaceValues(getIncludedFile(statement), replacers));
+                            } else {
                                 ExecuteStatement(getIncludedFile(statement));
                             }
                             //reset
                             includedFile = false;
                         }
                         /* normal statement. Execute it */
-                        else  {
+                        else {
                             if (replacers != null) {
-                                ExecuteStatement(replaceValues(statement,replacers));
-                            }
-                            else  {
+                                ExecuteStatement(replaceValues(statement, replacers));
+                            } else {
                                 ExecuteStatement(statement);
                             }
                         }
@@ -152,125 +151,117 @@ public class CmsSetupDb {
                 statement += " \n";
             }
             reader.close();
-        }
-        catch (Exception e) {
-          if(m_errorLogging)  {
-            m_errors.addElement(e.toString() + "\n");
-          }
-        }
-    }
-
-  private String replaceValues(String source, Hashtable replacers)  {
-    StringTokenizer tokenizer = new StringTokenizer(source);
-    String temp = "";
-
-    while(tokenizer.hasMoreTokens())  {
-      String token = tokenizer.nextToken();
-
-      // replace identifier found
-      if(token.startsWith("$$") && token.endsWith("$$"))  {
-
-        // look in the hashtable
-        Object value = replacers.get(token);
-
-        //found value
-        if(value != null) {
-            token = value.toString();
-        }
-      }
-      temp += token + " ";
-    }
-    return temp;
-  }
-
-  private String getScript(String key) {
-    /* open properties, get the value */
-    try {
-      Properties properties = new Properties();
-      properties.load(getClass().getClassLoader().getResourceAsStream("com/opencms/boot/dbsetup.properties"));
-      String value = properties.getProperty(key);
-      return value;
-    }
-    catch (Exception e) {
-      if(m_errorLogging)  {
-        m_errors.addElement(e.toString() + " \n");
-      }
-      return null;
-    }
-  }
-
-  private String getIncludedFile(String statement)  {
-    String file;
-    statement = statement.trim();
-
-    if(statement.startsWith("./"))  {
-      /* cut off */
-      file = statement.substring(2);
-    }
-    else if(statement.startsWith("."))  {
-      /* cut off */
-      file = statement.substring(1);
-    }
-    else  {
-      file = statement;
-    }
-
-    /* read and return everything */
-    try {
-        LineNumberReader reader = new LineNumberReader(new FileReader(m_basePath + C_OCSETUP_FOLDER + file));
-        String stat = "";
-        String line = null;
-
-        while (true) {
-            line = reader.readLine();
-            if(line==null)break;
-            line = line.trim();
-            if(line.startsWith("--") || line.startsWith("/"))  {
-            }
-            else  {
-              stat += line + " \n";
+        } catch (Exception e) {
+            if (m_errorLogging) {
+                m_errors.addElement(e.toString() + "\n");
             }
         }
-        reader.close();
-        return stat.trim();
     }
-    catch (Exception e) {
-      if(m_errorLogging)  {
-        m_errors.addElement(e.toString() + "\n");
-      }
-      return null;
-    }
-  }
 
-  private void ExecuteStatement(String statement)  {
-    Statement stat;
-    try  {
-        stat = m_con.createStatement();
-        stat.execute(statement);
-        stat.close();
+    private String replaceValues(String source, Hashtable replacers) {
+        StringTokenizer tokenizer = new StringTokenizer(source);
+        String temp = "";
+
+        while (tokenizer.hasMoreTokens()) {
+            String token = tokenizer.nextToken();
+
+            // replace identifier found
+            if (token.startsWith("$$") && token.endsWith("$$")) {
+
+                // look in the hashtable
+                Object value = replacers.get(token);
+
+                //found value
+                if (value != null) {
+                    token = value.toString();
+                }
+            }
+            temp += token + " ";
+        }
+        return temp;
     }
-    catch (Exception e)  {
-        if(m_errorLogging)  {
-          m_errors.addElement(e.toString() + "\n");
+
+    private String getScript(String key) {
+        /* open properties, get the value */
+        try {
+            Properties properties = new Properties();
+            properties.load(getClass().getClassLoader().getResourceAsStream("com/opencms/boot/dbsetup.properties"));
+            String value = properties.getProperty(key);
+            return value;
+        } catch (Exception e) {
+            if (m_errorLogging) {
+                m_errors.addElement(e.toString() + " \n");
+            }
+            return null;
         }
     }
-  }
 
-  public Vector getErrors() {
-      return m_errors;
-  }
-  public void clearErrors() {
-      m_errors.clear();
-  }
+    private String getIncludedFile(String statement) {
+        String file;
+        statement = statement.trim();
 
-  public boolean noErrors() {
-    return m_errors.isEmpty();
-  }
+        if (statement.startsWith("./")) {
+            /* cut off */
+            file = statement.substring(2);
+        } else if (statement.startsWith(".")) {
+            /* cut off */
+            file = statement.substring(1);
+        } else {
+            file = statement;
+        }
 
-  protected void finalize() {
-    try {
-      m_con.close();
+        /* read and return everything */
+        try {
+            LineNumberReader reader = new LineNumberReader(new FileReader(m_basePath + C_OCSETUP_FOLDER + file));
+            String stat = "";
+            String line = null;
+
+            while (true) {
+                line = reader.readLine();
+                if (line == null)
+                    break;
+                line = line.trim();
+                if (line.startsWith("--") || line.startsWith("/")) {} else {
+                    stat += line + " \n";
+                }
+            }
+            reader.close();
+            return stat.trim();
+        } catch (Exception e) {
+            if (m_errorLogging) {
+                m_errors.addElement(e.toString() + "\n");
+            }
+            return null;
+        }
     }
-    catch (SQLException e)  {}
-  }
+
+    private void ExecuteStatement(String statement) {
+        Statement stat;
+        try {
+            stat = m_con.createStatement();
+            stat.execute(statement);
+            stat.close();
+        } catch (Exception e) {
+            if (m_errorLogging) {
+                m_errors.addElement(e.toString() + "\n");
+            }
+        }
+    }
+
+    public Vector getErrors() {
+        return m_errors;
+    }
+    public void clearErrors() {
+        m_errors.clear();
+    }
+
+    public boolean noErrors() {
+        return m_errors.isEmpty();
+    }
+
+    protected void finalize() {
+        try {
+            m_con.close();
+        } catch (SQLException e) {}
+    }
 }
