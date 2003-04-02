fc89a9f5a42a646ff83d2bed1cc9711a5c1fe4e9
aZahner <a.zahner@alkacon.com>
2003-04-02 13:46:13 +0000

    updated documentation: optional PDF User documentation, MySQL 4, ...

--- doc/documentation/make.bat
+++ doc/documentation/make.bat
@@ -1,12 +1,31 @@
 @rem Use MikTeX (in Windows) to generate this documentation
 @rem You need the "smalle" MikTeX package
-@rem In addition to that, install "koma-script" and "fancyhdr"
+@rem In addition to that, install "koma-script", "fancyhdr" and "hyperref".
 
+@echo off
+cls
+if "%1"=="userdoc" goto userdoc
+
+
+:ocmsdoc
 pdflatex OpenCmsDoc
 makeindex OpenCmsDoc
 pdflatex OpenCmsDoc
 
 del *.aux,*.idx,*.log,*.dvi,*.toc,*.ilg,*.ind
 move OpenCmsDoc.pdf ..\..\..\zip
+goto end
 
 
+:userdoc
+pdflatex UserDoc
+makeindex UserDoc
+pdflatex UserDoc
+
+del *.aux,*.idx,*.log,*.dvi,*.toc,*.ilg,*.ind
+move UserDoc.pdf ..\..\..\zip
+goto end
+
+
+:end
+@echo generated OpenCms PDF documentation!
