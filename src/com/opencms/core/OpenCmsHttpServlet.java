833933f7d6a2f7567682eacb4a40af34b975c9cc
aKandzior <a.kandzior@alkacon.com>
2003-04-25 07:40:20 +0000

    Shorter log entry if resource access is denied

--- src/com/opencms/core/OpenCmsHttpServlet.java
+++ src/com/opencms/core/OpenCmsHttpServlet.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/core/Attic/OpenCmsHttpServlet.java,v $
-* Date   : $Date: 2003/03/28 11:15:32 $
-* Version: $Revision: 1.46 $
+* Date   : $Date: 2003/04/25 07:40:20 $
+* Version: $Revision: 1.47 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -76,7 +76,7 @@ import source.org.apache.java.util.ExtendedProperties;
  * @author Michael Emmerich
  * @author Alexander Kandzior (a.kandzior@alkacon.com)
  * 
- * @version $Revision: 1.46 $ $Date: 2003/03/28 11:15:32 $
+ * @version $Revision: 1.47 $ $Date: 2003/04/25 07:40:20 $
  */
 public class OpenCmsHttpServlet extends HttpServlet implements I_CmsConstants,I_CmsLogChannels {
 
@@ -371,7 +371,7 @@ public class OpenCmsHttpServlet extends HttpServlet implements I_CmsConstants,I_
               case CmsException.C_ACCESS_DENIED:
                   if(canWrite) {
                     if(C_LOGGING && A_OpenCms.isLogging(C_OPENCMS_INFO)) {
-                        A_OpenCms.log(C_OPENCMS_INFO, "[OpenCmsServlet] Access denied. "+e.getStackTraceAsString());
+                        A_OpenCms.log(C_OPENCMS_INFO, "[OpenCmsServlet] Access denied. " + e.getMessage());
                     }
                     requestAuthorization(req, res);
                   }
