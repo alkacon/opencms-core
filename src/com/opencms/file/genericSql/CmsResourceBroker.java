6aec251632e7fcf12c0e20a0332f03f4fe59e92f
aKandzior <a.kandzior@alkacon.com>
2003-05-15 13:10:02 +0000

    Allowed unicode &#123; chars in task names

--- src/com/opencms/file/genericSql/CmsResourceBroker.java
+++ src/com/opencms/file/genericSql/CmsResourceBroker.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/file/genericSql/Attic/CmsResourceBroker.java,v $
-* Date   : $Date: 2003/05/15 12:39:35 $
-* Version: $Revision: 1.378 $
+* Date   : $Date: 2003/05/15 13:10:02 $
+* Version: $Revision: 1.379 $
 
 *
 * This library is part of OpenCms -
@@ -69,7 +69,7 @@ import source.org.apache.java.util.Configurations;
  * @author Michaela Schleich
  * @author Michael Emmerich
  * @author Anders Fugmann
- * @version $Revision: 1.378 $ $Date: 2003/05/15 12:39:35 $
+ * @version $Revision: 1.379 $ $Date: 2003/05/15 13:10:02 $
  *
  */
 public class CmsResourceBroker implements I_CmsResourceBroker, I_CmsConstants {
@@ -7343,7 +7343,9 @@ public void renameFile(CmsUser currentUser, CmsProject currentProject, String ol
                 (c != '-') && (c != '.') &&
                 (c != '_') && (c != '~') &&
                 (c != ' ') && (c != 'ß') &&
-                (c != '/') && (c != '(') && (c != ')') && (c != '\'')
+                (c != '/') && (c != '(') && 
+                (c != ')') && (c != '\'') &&
+                (c != '#') && (c != '&') && (c != ';')
                 ) {
                 throw new CmsException("[" + getClass().getName() + "] " + taskname,
                     CmsException.C_BAD_NAME);
