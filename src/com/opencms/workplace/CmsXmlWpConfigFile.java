23c1c21b9fd30efbe05fdb2680f7f89d2576d5bf
aKandzior <a.kandzior@alkacon.com>
2003-06-04 12:26:22 +0000

    Added possibility to change file type to JSP

--- src/com/opencms/workplace/CmsXmlWpConfigFile.java
+++ src/com/opencms/workplace/CmsXmlWpConfigFile.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/workplace/Attic/CmsXmlWpConfigFile.java,v $
-* Date   : $Date: 2003/01/31 10:25:22 $
-* Version: $Revision: 1.49 $
+* Date   : $Date: 2003/06/04 12:26:22 $
+* Version: $Revision: 1.50 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -42,7 +42,7 @@ import java.util.Vector;
  * @author Alexander Lucas
  * @author Michael Emmerich
  * @author Andreas Schouten
- * @version $Revision: 1.49 $ $Date: 2003/01/31 10:25:22 $
+ * @version $Revision: 1.50 $ $Date: 2003/06/04 12:26:22 $
  */
 
 public class CmsXmlWpConfigFile {
@@ -162,10 +162,12 @@ public class CmsXmlWpConfigFile {
             names.add("othertype"); values.add("explorer_files_new_othertype.html");
             names.add("upload");    values.add("explorer_files_new_upload.html");
         } else {
-            names.add("binary");    values.add("binary");
             names.add("plain");     values.add("plain");
+            names.add("jsp");       values.add("jsp"); 
+            names.add("XMLTemplate");   values.add("XMLTemplate");            
+            names.add("binary");    values.add("binary");
             names.add("image");     values.add("image");
-        }
+        }        
     }
 
 }
