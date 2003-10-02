9fdc8be212272a2a9dd99b6b1a57ed7701a53765
mEmmerich <m.emmerich@alkacon.com>
2003-10-02 09:58:05 +0000

    Bugfix in Link-Galleries, new external links can be created again

--- src/com/opencms/workplace/CmsAdminExtLinkGalleries.java
+++ src/com/opencms/workplace/CmsAdminExtLinkGalleries.java
@@ -1,7 +1,7 @@
 /*
 * File   : $Source: /alkacon/cvs/opencms/src/com/opencms/workplace/Attic/CmsAdminExtLinkGalleries.java,v $
-* Date   : $Date: 2003/09/12 17:38:05 $
-* Version: $Revision: 1.21 $
+* Date   : $Date: 2003/10/02 09:58:05 $
+* Version: $Revision: 1.22 $
 *
 * This library is part of OpenCms -
 * the Open Source Content Mananagement System
@@ -36,10 +36,11 @@ import com.opencms.file.CmsFolder;
 import com.opencms.file.CmsObject;
 import com.opencms.file.CmsResource;
 import com.opencms.file.CmsResourceTypeFolder;
-import com.opencms.file.CmsResourceTypePointer;
+import com.opencms.file.CmsResourceTypeLink;
 import com.opencms.util.CmsLinkCheck;
 import com.opencms.util.Utils;
 
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 /**
@@ -47,7 +48,7 @@ import java.util.Hashtable;
  * <p>
  *
  * @author Edna Falkenhan
- * @version $Revision: 1.21 $ $Date: 2003/09/12 17:38:05 $
+ * @version $Revision: 1.22 $ $Date: 2003/10/02 09:58:05 $
  * @see com.opencms.workplace.CmsXmlWpTemplateFile
  */
 
@@ -128,6 +129,7 @@ public class CmsAdminExtLinkGalleries extends CmsAdminGallery  {
         String action = (String)parameters.get("action");                                
         String title = (String)parameters.get("TITLE"); // both for gallery and file
         String step = (String)parameters.get("step");       
+        
         
         if(foldername == null) {
             foldername = "";
@@ -241,7 +243,7 @@ public class CmsAdminExtLinkGalleries extends CmsAdminGallery  {
                                 checkurl = CmsLinkCheck.checkUrl(link);
                             }
                             if(checkurl){
-                                cms.createResource(foldername, filename, CmsResourceTypePointer.C_RESOURCE_TYPE_ID, prop, link.getBytes());
+                                cms.createResource(foldername, filename, CmsResourceTypeLink.C_RESOURCE_TYPE_ID, prop, link.getBytes());
                             }
                         } catch (CmsException e){
                             error = e.getShortException();
