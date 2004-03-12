9fe87bf6cb23cc77a40655cc3a542744f8b22d05
aZahner <a.zahner@alkacon.com>
2004-03-12 17:01:39 +0000

    Fixed issue getting template URI from original file, not temporary file

--- src/org/opencms/workplace/editor/CmsDefaultPageEditor.java
+++ src/org/opencms/workplace/editor/CmsDefaultPageEditor.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/workplace/editor/Attic/CmsDefaultPageEditor.java,v $
- * Date   : $Date: 2004/02/22 13:52:28 $
- * Version: $Revision: 1.44 $
+ * Date   : $Date: 2004/03/12 17:01:39 $
+ * Version: $Revision: 1.45 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -57,7 +57,7 @@ import javax.servlet.jsp.JspException;
  * Extend this class for all editors that work with the CmsDefaultPage.<p>
  *
  * @author  Andreas Zahner (a.zahner@alkacon.com)
- * @version $Revision: 1.44 $
+ * @version $Revision: 1.45 $
  * 
  * @since 5.1.12
  */
@@ -625,7 +625,7 @@ public abstract class CmsDefaultPageEditor extends CmsEditor {
    public String getUriTemplate() {
        String result = "";
        try {
-           result = getCms().readProperty(getParamResource(), I_CmsConstants.C_PROPERTY_TEMPLATE, true, "");
+           result = getCms().readProperty(getParamTempfile(), I_CmsConstants.C_PROPERTY_TEMPLATE, true, "");
        } catch (CmsException e) {
            OpenCms.getLog(this).warn("Template property could not be read", e);
        }
