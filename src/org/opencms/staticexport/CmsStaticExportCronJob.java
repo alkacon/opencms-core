78c3824c4c5abe6b1ab41f37ecdd2c1f179c72d3
tWeckert <t.weckert@alkacon.com>
2004-04-13 15:04:46 +0000

    Initial import.

---
+++ src/org/opencms/staticexport/CmsStaticExportCronJob.java
@@ -0,0 +1,82 @@
+/*
+ * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/staticexport/Attic/CmsStaticExportCronJob.java,v $
+ * Date   : $Date: 2004/04/13 15:04:46 $
+ * Version: $Revision: 1.1 $
+ *
+ * This library is part of OpenCms -
+ * the Open Source Content Mananagement System
+ *
+ * Copyright (C) 2002 - 2004 Alkacon Software (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
+package org.opencms.staticexport;
+
+import org.opencms.cron.I_CmsCronJob;
+import org.opencms.file.CmsObject;
+import org.opencms.main.CmsException;
+import org.opencms.main.OpenCms;
+import org.opencms.report.CmsLogReport;
+import org.opencms.report.I_CmsReport;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+/**
+ * A Cms cron job to write a complete static export (e.g. nightly exports).<p>
+ * 
+ * @author Thomas Weckert (t.weckert@alkacon.com)
+ * @version $Revision: 1.1 $
+ */
+public class CmsStaticExportCronJob extends Object implements I_CmsCronJob {
+
+    /**
+     * @see org.opencms.cron.I_CmsCronJob#launch(org.opencms.file.CmsObject, java.lang.String)
+     */
+    public String launch(CmsObject cms, String parameter) throws Exception {
+
+        I_CmsReport report = null;
+        
+        try {
+            report = (I_CmsReport) new CmsLogReport();
+            OpenCms.getStaticExportManager().exportFullStaticRender(cms, true, report);
+        } catch (CmsException e) {
+            report.println(e);
+        } catch (IOException e) {
+            report.println(e);            
+        } catch (ServletException e) {
+            report.println(e);
+        }  finally {
+            // append runtime statistics to report
+            StringBuffer stats = new StringBuffer();
+            stats.append(report.key("report.publish_stats"));
+            stats.append(report.key("report.publish_stats_duration"));
+            stats.append(report.formatRuntime());
+            report.println(stats.toString());        
+            report.println(report.key("report.staticexport_end"), I_CmsReport.C_FORMAT_HEADLINE);            
+        }
+        
+        return null;
+    }
+
+}
