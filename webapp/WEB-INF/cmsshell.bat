001aa785151b80af547f8f66c99920a5d74a0a4c
aKandzior <a.kandzior@alkacon.com>
2004-11-05 08:09:42 +0000

    Updated paths to new lib versions

--- webapp/WEB-INF/cmsshell.bat
+++ webapp/WEB-INF/cmsshell.bat
@@ -6,11 +6,9 @@
 @rem set jvm options
 
 set OPTIONS=
-set OPTIONS= %OPTIONS% -Dfile.encoding=ISO-8859-1
-set OPTIONS= %OPTIONS% -Dopencms.disableScheduler=true
 
 @rem add this options to debug OpenCms using the shell
-set OPTIONS= %OPTIONS% -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Djava.compiler=NONE
+rem set OPTIONS= %OPTIONS% -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000 -Djava.compiler=NONE
 
 @rem set libs in classpath
 
@@ -21,12 +19,12 @@ set CLASSPATH= %CLASSPATH%;../../../common/lib/jsp-api.jar
 set CLASSPATH= %CLASSPATH%;lib/commons-dbcp-1.2.1.jar
 set CLASSPATH= %CLASSPATH%;lib/commons-pool-1.2.jar
 set CLASSPATH= %CLASSPATH%;lib/commons-collections-3.1.jar
-set CLASSPATH= %CLASSPATH%;lib/commons-digester-1.5.jar
+set CLASSPATH= %CLASSPATH%;lib/commons-digester-1.6.jar
 set CLASSPATH= %CLASSPATH%;lib/commons-beanutils-1.7.0.jar
 set CLASSPATH= %CLASSPATH%;lib/commons-fileupload-1.0.jar
 set CLASSPATH= %CLASSPATH%;lib/commons-codec-1.3.jar
 set CLASSPATH= %CLASSPATH%;lib/commons-email-1.0-mod.jar
-set CLASSPATH= %CLASSPATH%;lib/commons-httpclient-2.0.1.jar
+set CLASSPATH= %CLASSPATH%;lib/commons-httpclient-2.0.2.jar
 set CLASSPATH= %CLASSPATH%;lib/commons-logging-1.0.4.jar
 set CLASSPATH= %CLASSPATH%;lib/ojdbc14.jar
 set CLASSPATH= %CLASSPATH%;lib/log4j-1.2.8.jar
@@ -34,19 +32,17 @@ set CLASSPATH= %CLASSPATH%;lib/opencms.jar;lib/opencms-legacy.jar
 set CLASSPATH= %CLASSPATH%;lib/activation.jar
 set CLASSPATH= %CLASSPATH%;lib/htmlparser-1.41.jar
 set CLASSPATH= %CLASSPATH%;lib/jakarta-oro-2.0.8.jar
-set CLASSPATH= %CLASSPATH%;lib/mysql-connector-java-3.0.14-production-bin.jar
+set CLASSPATH= %CLASSPATH%;lib/mysql-connector-java-3.0.15-ga-bin.jar
 set CLASSPATH= %CLASSPATH%;lib/jaxen-1.1-beta-4.jar
-set CLASSPATH= %CLASSPATH%;lib/jtidy-04aug2000r7-dev.jar
+set CLASSPATH= %CLASSPATH%;lib/jtidy-r8-05102004.jar
 set CLASSPATH= %CLASSPATH%;lib/mail.jar
-set CLASSPATH= %CLASSPATH%;lib/jug-1.1.1.jar
-@rem set CLASSPATH= %CLASSPATH%;lib/xerces-1_4_4.jar
-set CLASSPATH= %CLASSPATH%;lib/dom4j-1.5-rc1.jar
-set CLASSPATH= %CLASSPATH%;lib/opencms-drivers.jar
+set CLASSPATH= %CLASSPATH%;lib/jug-1.1.2.jar
+set CLASSPATH= %CLASSPATH%;lib/dom4j-1.5.jar
 set CLASSPATH= %CLASSPATH%;lib/lucene-1.4.1.jar
 set CLASSPATH= %CLASSPATH%;lib/poi-2.5.1-final-20040804.jar
 set CLASSPATH= %CLASSPATH%;lib/snowball-1.0.jar
 set CLASSPATH= %CLASSPATH%;lib/PDFBox-0.6.6.jar
-set CLASSPATH= %CLASSPATH%;lib/quartz-1.4.1mod.jar
+set CLASSPATH= %CLASSPATH%;lib/quartz-1.4.2.jar
 set CLASSPATH= %CLASSPATH%;lib/xercesImpl-2.6.2.jar;lib/xml-apis-2.6.2.jar
 set CLASSPATH= %CLASSPATH%;classes
 
