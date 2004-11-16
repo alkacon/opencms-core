ee0faa41167db042d6803c21917fa981fd2e26db
cWeinholz <c.weinholz@alkacon.com>
2004-11-16 16:57:23 +0000

    Added CmsXmlDocument to handler XmlContents for indexing

---
+++ src/org/opencms/search/documents/CmsXmlDocument.java
@@ -0,0 +1,134 @@
+/*
+ * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/search/documents/Attic/CmsXmlDocument.java,v $
+ * Date   : $Date: 2004/11/16 16:57:23 $
+ * Version: $Revision: 1.1 $
+ *
+ * This library is part of OpenCms -
+ * the Open Source Content Mananagement System
+ *
+ * Copyright (C) 2002 - 2004 Alkacon Software (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.opencms.search.documents;
+
+import org.opencms.i18n.CmsLocaleManager;
+import org.opencms.main.CmsException;
+import org.opencms.main.OpenCms;
+import org.opencms.search.CmsIndexException;
+import org.opencms.search.A_CmsIndexResource;
+import org.opencms.xml.A_CmsXmlDocument;
+import org.opencms.xml.content.CmsXmlContentFactory;
+import org.opencms.xml.page.CmsXmlPage;
+import org.opencms.xml.page.CmsXmlPageFactory;
+import org.opencms.xml.types.I_CmsXmlContentValue;
+
+import org.opencms.file.CmsFile;
+import org.opencms.file.CmsObject;
+import org.opencms.file.CmsResource;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+
+/**
+ * Lucene document factory class to extract index data from a cms resource 
+ * of type <code>CmsResourceTypeXmlContent</code>.<p>
+ * 
+ * @version $Revision: 1.1 $ $Date: 2004/11/16 16:57:23 $
+ * @author Carsten Weinholz (c.weinholz@alkacon.com)
+ */
+public class CmsXmlDocument extends CmsVfsDocument {
+
+    /**
+     * Creates a new instance of this lucene document factory.<p>
+     * 
+     * @param cms the cms object
+     * @param name name of the documenttype
+     */
+    public CmsXmlDocument (CmsObject cms, String name) {
+        super(cms, name);
+    }
+    
+    /**
+     * Returns the raw text content of a given vfs resource of type <code>CmsResourceTypeXmlContent</code>.<p>
+     * 
+     * @see org.opencms.search.documents.CmsVfsDocument#getRawContent(org.opencms.search.A_CmsIndexResource, java.lang.String)
+     */
+    public String getRawContent(A_CmsIndexResource indexResource, String language) throws CmsException {
+
+        CmsResource resource = (CmsResource)indexResource.getData();
+        String rawContent = null;
+        
+        try {
+            CmsFile file = CmsFile.upgrade(resource, m_cms);
+            String absolutePath = m_cms.getSitePath(file);
+            A_CmsXmlDocument xmlContent = CmsXmlContentFactory.unmarshal(m_cms, file);
+            CmsXmlPage page = CmsXmlPageFactory.unmarshal(m_cms, file);
+            
+            List locales = xmlContent.getLocales();
+            if (locales.size() == 0) {
+                locales = OpenCms.getLocaleManager().getDefaultLocales(m_cms, absolutePath);
+            }
+            Locale locale = OpenCms.getLocaleManager().getBestMatchingLocale(
+                    CmsLocaleManager.getLocale(language), 
+                    OpenCms.getLocaleManager().getDefaultLocales(m_cms, absolutePath), 
+                    locales);
+            
+            List elements = xmlContent.getNames(locale);
+            StringBuffer content = new StringBuffer();
+            for (Iterator i = elements.iterator(); i.hasNext();) {
+                List values = xmlContent.getValues((String)i.next(), locale);
+                for (Iterator j = values.iterator(); j.hasNext();) {
+                    I_CmsXmlContentValue value = (I_CmsXmlContentValue)j.next();
+                    String plainText = value.getPlainText(m_cms, xmlContent);
+                    if (plainText != null) {
+                        content.append(value);
+                    }
+                }                
+            }
+            
+            CmsHtmlExtractor extractor = new CmsHtmlExtractor();
+            rawContent = extractor.extractText(content.toString(), page.getEncoding());
+            
+        } catch (Exception exc) {
+            throw new CmsIndexException("Reading resource " + resource.getRootPath() + " failed", exc);
+        }
+        
+        return rawContent;
+    }
+    
+    /**
+     * Generates a new lucene document instance from contents of the given resource.<p>
+     * 
+     * @see org.opencms.search.documents.I_CmsDocumentFactory#newInstance(org.opencms.search.A_CmsIndexResource, java.lang.String)
+     */
+    public Document newInstance (A_CmsIndexResource resource, String language) throws CmsException {
+                   
+        Document document = super.newInstance(resource, language);
+        document.add(Field.Text(I_CmsDocumentFactory.DOC_CONTENT, getRawContent(resource, language)));
+        
+        return document;
+    }
+}
