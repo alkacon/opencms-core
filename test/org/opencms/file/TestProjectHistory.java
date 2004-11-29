91a002924fddcb0b61615971a189b3e9de8583d0
tWeckert <t.weckert@alkacon.com>
2004-11-29 15:48:19 +0000

    Changed the exception handling in the test case.

--- test/org/opencms/file/TestProjectHistory.java
+++ test/org/opencms/file/TestProjectHistory.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/test/org/opencms/file/TestProjectHistory.java,v $
- * Date   : $Date: 2004/11/29 13:58:23 $
- * Version: $Revision: 1.1 $
+ * Date   : $Date: 2004/11/29 15:48:19 $
+ * Version: $Revision: 1.2 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -28,10 +28,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
- 
+
 package org.opencms.file;
 
-import org.opencms.main.CmsException;
 import org.opencms.test.OpenCmsTestCase;
 import org.opencms.test.OpenCmsTestProperties;
 
@@ -45,83 +44,82 @@ import junit.framework.TestSuite;
  * Unit test for the project history function of the CmsObject.<p>
  * 
  * @author Thomas Weckert (t.weckert@alkacon.com)
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  * @since 6.0 alpha 2
  */
 public class TestProjectHistory extends OpenCmsTestCase {
-    
+
     /**
      * Default JUnit constructor.<p>
      * 
      * @param arg0 JUnit parameters
-     */    
+     */
     public TestProjectHistory(String arg0) {
-        
+
         super(arg0);
     }
-    
+
     /**
      * Test suite for this test class.<p>
      * 
      * @return the test suite
      */
     public static Test suite() {
+
         OpenCmsTestProperties.initialize(org.opencms.test.AllTests.TEST_PROPERTIES_PATH);
-        
+
         TestSuite suite = new TestSuite();
         suite.setName(TestProjectHistory.class.getName());
-                
+
         suite.addTest(new TestProjectHistory("testProjectHistory"));
-               
+
         TestSetup wrapper = new TestSetup(suite) {
-            
+
             protected void setUp() {
+
                 setupOpenCms("simpletest", "/sites/default/");
             }
-            
+
             protected void tearDown() {
+
                 removeOpenCms();
             }
         };
-        
+
         return wrapper;
     }
-    
+
     /**
      * Tests the project history function of the CmsObject.<p>
      * 
      * @throws Throwable if something goes wrong
      */
     public void testProjectHistory() throws Throwable {
-        
-        CmsObject cms = getCmsObject();  
-        
+
+        CmsObject cms = getCmsObject();
+
         echo("Testing the project history function");
-        projectHistory(cms);   
-    }  
-    
+        projectHistory(cms);
+    }
+
     /**
      * Tests the project history function of the CmsObject.<p>
      * 
      * @param cms the CmsObject
+     * @throws Throwable if something goes wrong
      */
-    public static void projectHistory(CmsObject cms) {
-        
+    public static void projectHistory(CmsObject cms) throws Throwable {
+
         List projectHistory = null;
         CmsBackupProject backupProject = null;
-        
-        try {
-            projectHistory = cms.getAllBackupProjects();
-            
-            // the project history should contain just the setup project here
-            assertEquals(projectHistory.size(), 1);           
-            backupProject = (CmsBackupProject)projectHistory.get(0);
-            assertEquals(backupProject.getId(), 2);
-            assertEquals(backupProject.getName(), "_setupProject");
-        } catch (CmsException e) {
-            fail("Error getting project history: " + e.toString());
-        }
-        
+
+        projectHistory = cms.getAllBackupProjects();
+
+        // the project history should contain just the setup project here
+        assertEquals(projectHistory.size(), 1);
+        backupProject = (CmsBackupProject)projectHistory.get(0);
+        assertEquals(backupProject.getId(), 2);
+        assertEquals(backupProject.getName(), "_setupProject");
     }
 
 }
