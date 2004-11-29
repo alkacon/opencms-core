1627bce1afaec65911eba03c6de2bb51e9cec8aa
tWeckert <t.weckert@alkacon.com>
2004-11-29 14:03:03 +0000

    Fixed column names in readBackupProjects().

--- src/org/opencms/db/oracle/CmsBackupDriver.java
+++ src/org/opencms/db/oracle/CmsBackupDriver.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/db/oracle/CmsBackupDriver.java,v $
- * Date   : $Date: 2004/11/22 18:03:06 $
- * Version: $Revision: 1.41 $
+ * Date   : $Date: 2004/11/29 14:03:03 $
+ * Version: $Revision: 1.42 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -59,7 +59,7 @@ import org.apache.commons.dbcp.DelegatingResultSet;
  * @author Thomas Weckert (t.weckert@alkacon.com)
  * @author Michael Emmerich (m.emmerich@alkacon.com) 
  * @author Carsten Weinholz (c.weinholz@alkacon.com)
- * @version $Revision: 1.41 $ $Date: 2004/11/22 18:03:06 $
+ * @version $Revision: 1.42 $ $Date: 2004/11/29 14:03:03 $
  * @since 5.1
  */
 public class CmsBackupDriver extends org.opencms.db.generic.CmsBackupDriver {
@@ -281,7 +281,7 @@ public class CmsBackupDriver extends org.opencms.db.generic.CmsBackupDriver {
             stmt.setInt(1, 300);
             res = stmt.executeQuery();
             while (res.next()) {
-                Vector resources = m_driverManager.getBackupDriver().readBackupProjectResources(dbc, res.getInt("TAG_ID"));
+                Vector resources = m_driverManager.getBackupDriver().readBackupProjectResources(dbc, res.getInt("PUBLISH_TAG"));
                 projects.addElement(
                     new CmsBackupProject(
                         res.getInt("PUBLISH_TAG"),
