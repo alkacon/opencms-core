26881a958ffa5c851b24f291b2c774d0dea37749
aZahner <a.zahner@alkacon.com>
2005-03-31 10:41:46 +0000

    fixed log output

--- src-modules/org/opencms/frontend/templateone/CmsTemplateStyleSheet.java
+++ src-modules/org/opencms/frontend/templateone/CmsTemplateStyleSheet.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/frontend/templateone/CmsTemplateStyleSheet.java,v $
- * Date   : $Date: 2005/03/31 10:40:18 $
- * Version: $Revision: 1.7 $
+ * Date   : $Date: 2005/03/31 10:41:46 $
+ * Version: $Revision: 1.8 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -49,7 +49,7 @@ import javax.servlet.jsp.PageContext;
  * Provides methods to build the dynamic CSS style sheet of template one.<p>
  * 
  * @author Andreas Zahner (a.zahner@alkacon.com)
- * @version $Revision: 1.7 $
+ * @version $Revision: 1.8 $
  */
 public class CmsTemplateStyleSheet extends CmsJspActionElement {
     
@@ -265,7 +265,9 @@ public class CmsTemplateStyleSheet extends CmsJspActionElement {
         // collect the configuration information 
         try {
             String configUri = req.getParameter(C_PARAM_CONFIGFILE);
-            m_configuration = CmsTemplateBean.getConfigurationFile(configUri, getCmsObject());
+            if (CmsStringUtil.isNotEmpty(configUri)) {
+                m_configuration = CmsTemplateBean.getConfigurationFile(configUri, getCmsObject());
+            }
         } catch (Exception e) {
             // problem getting properties, log error
             if (OpenCms.getLog(this).isDebugEnabled()) {
