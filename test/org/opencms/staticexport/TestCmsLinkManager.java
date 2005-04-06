5e924d1f6165742314282c44e3c7313023653d21
tWeckert <t.weckert@alkacon.com>
2005-04-06 08:28:44 +0000

    Added a new test.

--- test/org/opencms/staticexport/TestCmsLinkManager.java
+++ test/org/opencms/staticexport/TestCmsLinkManager.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/test/org/opencms/staticexport/TestCmsLinkManager.java,v $
- * Date   : $Date: 2005/02/17 12:46:01 $
- * Version: $Revision: 1.2 $
+ * Date   : $Date: 2005/04/06 08:28:44 $
+ * Version: $Revision: 1.3 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -35,7 +35,7 @@ import junit.framework.TestCase;
 
 /** 
  * @author Alexander Kandzior (a.kandzior@alkacon.com)
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  * 
  * @since 5.1
  */
@@ -59,6 +59,10 @@ public class TestCmsLinkManager extends TestCase {
         test = CmsLinkManager.getRelativeUri("/dir1/dir2/index.html", "/dir1/dirB/index.html");
         System.out.println(test);
         assertEquals(test, "../dirB/index.html");
+        
+        test = CmsLinkManager.getRelativeUri("/exp/en/test/index.html", "/exp/de/test/index.html");
+        System.out.println(test);
+        assertEquals(test, "../../de/test/index.html");
 
         test = CmsLinkManager.getAbsoluteUri("../../index.html", "/dir1/dir2/dir3/");        
         System.out.println(test);
