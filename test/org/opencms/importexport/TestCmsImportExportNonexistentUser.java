0b090e0e048aa285727ad413a63801c21b31aa32
tWeckert <t.weckert@alkacon.com>
2005-04-27 14:07:02 +0000

    Initial import.

---
+++ test/org/opencms/importexport/TestCmsImportExportNonexistentUser.java
@@ -0,0 +1,145 @@
+/*
+ * File   : $Source: /alkacon/cvs/opencms/test/org/opencms/importexport/TestCmsImportExportNonexistentUser.java,v $
+ * Date   : $Date: 2005/04/27 14:07:02 $
+ * Version: $Revision: 1.1 $
+ *
+ * This library is part of OpenCms -
+ * the Open Source Content Mananagement System
+ *
+ * Copyright (C) 2002 - 2004 Alkacon Software (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
+package org.opencms.importexport;
+
+import org.opencms.file.CmsObject;
+import org.opencms.file.CmsProject;
+import org.opencms.file.types.CmsResourceTypePlain;
+import org.opencms.main.OpenCms;
+import org.opencms.report.CmsShellReport;
+import org.opencms.test.OpenCmsTestCase;
+import org.opencms.test.OpenCmsTestProperties;
+
+import junit.extensions.TestSetup;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests exporting/import VFS data with nonexistent users.<p>
+ * 
+ * @author Thomas Weckert (t.weckert@alkacon.com)
+ * @version $Revision: 1.1 $
+ */
+public class TestCmsImportExportNonexistentUser extends OpenCmsTestCase {
+
+    /**
+     * Default JUnit constructor.<p>
+     * 
+     * @param arg0 JUnit parameters
+     */
+    public TestCmsImportExportNonexistentUser(String arg0) {
+        super(arg0);
+    }
+    
+    /**
+     * Test suite for this test class.<p>
+     * 
+     * @return the test suite
+     */
+    public static Test suite() {
+        OpenCmsTestProperties.initialize(org.opencms.test.AllTests.TEST_PROPERTIES_PATH);
+        
+        TestSuite suite = new TestSuite();
+        suite.setName(TestCmsImportExport.class.getName());
+                
+        suite.addTest(new TestCmsImportExportNonexistentUser("testImportExportNonexistentUser"));
+        
+        TestSetup wrapper = new TestSetup(suite) {
+            
+            protected void setUp() {
+                setupOpenCms("simpletest", "/sites/default/");
+            }
+            
+            protected void tearDown() {
+                removeOpenCms();
+            }
+        };
+        
+        return wrapper;
+    }
+    
+    /**
+     * Tests exporting and import of VFS data with a nonexistent/deleted user.<p>
+     * 
+     * The username of the deleted user should in the export manifest be replaced 
+     * by the name of the Admin user.<p>
+     * 
+     * @throws Exception if something goes wrong
+     */
+    public void testImportExportNonexistentUser() throws Exception {
+        
+        CmsObject cms = getCmsObject();
+        
+        try {
+            String username = "tempuser";
+            String password = "password";
+            String filename = "/dummy1.txt";
+            String contentStr = "This is a comment. I love comments.";
+            byte[] content = contentStr.getBytes();
+            CmsProject offlineProject = cms.getRequestContext().currentProject();
+            
+            // create a temporary user for this test case
+            cms.createUser(username, password, "Temporary user for import/export test case", null);
+            // add this user to the project managers user group
+            cms.addUserToGroup(username, OpenCms.getDefaultUsers().getGroupProjectmanagers());
+            
+            // switch to the temporary user, offline project and default site
+            cms.loginUser(username, password);
+            cms.getRequestContext().setSiteRoot("/sites/default/");
+            cms.getRequestContext().setCurrentProject(offlineProject);
+            
+            // create a dummy plain text file by the temporary user
+            cms.createResource(filename, CmsResourceTypePlain.getStaticTypeId(), content, null);
+            // publish the dummy plain text file
+            cms.unlockResource(filename);
+            cms.publishResource(filename);
+            
+            // switch back to the Admin user
+            cms.loginUser("Admin", "admin");            
+            // delete the temporary user
+            cms.deleteUser(username);
+            
+            // export the dummy plain text file
+            CmsVfsImportExportHandler vfsExportHandler = new CmsVfsImportExportHandler();
+            vfsExportHandler.setFileName("testImportExportNonexistentUser.zip");
+            vfsExportHandler.setExportPaths(new String[] {filename});
+            vfsExportHandler.setExcludeSystem(true);
+            vfsExportHandler.setExcludeUnchanged(false);
+            vfsExportHandler.setExportUserdata(false);
+            OpenCms.getImportExportManager().exportData(cms, vfsExportHandler, new CmsShellReport());
+        } catch (Exception e) {
+            
+            fail(e.toString());
+        }
+    }
+    
+}
