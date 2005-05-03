f3ebbd38d7a223ed1d8a9a3be601501f28f2d664
aWestermann <a.westermann@alkacon.com>
2005-05-03 13:03:17 +0000

    Illegal whitespace in externalization constant.

--- src/org/opencms/jsp/Messages.java
+++ src/org/opencms/jsp/Messages.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/jsp/Messages.java,v $
- * Date   : $Date: 2005/05/03 12:49:06 $
- * Version: $Revision: 1.5 $
+ * Date   : $Date: 2005/05/03 13:03:17 $
+ * Version: $Revision: 1.6 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -43,6 +43,9 @@ import org.opencms.i18n.I_CmsMessageBundle;
 public final class Messages extends A_CmsMessageBundle {
 
     /** Message constant for key in the resource bundle. */
+    public static final String ERR_COLLECTOR_NOT_FOUND_1 = "ERR_COLLECTOR_NOT_FOUND_1";
+
+    /** Message constant for key in the resource bundle. */
     public static final String ERR_MISSING_CMS_CONTROLLER_1 = "ERR_MISSING_CMS_CONTROLLER_1";
 
     /** Message constant for key in the resource bundle. */
@@ -68,6 +71,9 @@ public final class Messages extends A_CmsMessageBundle {
 
     /** Message constant for key in the resource bundle. */
     public static final String ERR_TAG_CONTENTSHOW_WRONG_PARENT_0 = "ERR_TAG_CONTENTSHOW_WRONG_PARENT_0";
+
+    /** Message constant for key in the resource bundle. */
+    public static final String ERR_TAG_INFO_0 = "ERR_TAG_INFO_0";
 
     /** Message constant for key in the resource bundle. */
     public static final String ERR_TAG_LABEL_0 = "ERR_TAG_LABEL_0";
@@ -126,12 +132,6 @@ public final class Messages extends A_CmsMessageBundle {
     /** Message constant for key in the resource bundle. */
     public static final String LOG_ERR_JSP_BEAN_0 = "LOG_ERR_JSP_BEAN_0";
 
-    /** Message constant for key in the resource bundle. */
-    public static final String ERR_TAG_INFO_0  = "ERR_TAG_INFO_0 ";
-    
-    /** Message constant for key in the resource bundle. */
-    public static final String  ERR_COLLECTOR_NOT_FOUND_1 = "ERR_COLLECTOR_NOT_FOUND_1";
-    
     /** Message constant for key in the resource bundle. */
     public static final String LOG_ERR_JSP_WRITE_0 = "LOG_ERR_JSP_WRITE_0";
 
