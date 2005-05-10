210a939d64ad0b5215f27f4741fcf57bf478b93a
aWestermann <a.westermann@alkacon.com>
2005-05-10 14:32:50 +0000

    Execution of tests for message bundle org.opencms.file.messages added.

--- src/org/opencms/i18n/A_CmsMessageBundle.java
+++ src/org/opencms/i18n/A_CmsMessageBundle.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/i18n/A_CmsMessageBundle.java,v $
- * Date   : $Date: 2005/05/10 07:50:57 $
- * Version: $Revision: 1.21 $
+ * Date   : $Date: 2005/05/10 14:32:50 $
+ * Version: $Revision: 1.22 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -52,6 +52,7 @@ public abstract class A_CmsMessageBundle implements I_CmsMessageBundle {
             org.opencms.cache.Messages.get(),
             org.opencms.configuration.Messages.get(),
             org.opencms.db.Messages.get(),
+            org.opencms.file.Messages.get(),
             org.opencms.file.collectors.Messages.get(),
             org.opencms.flex.Messages.get(),
             org.opencms.importexport.Messages.get(),
