06e4855fd3c5e61dfc8c215808b154ae51b7a120
tWeckert <t.weckert@alkacon.com>
2005-05-12 13:15:00 +0000

    Added the bundle of the generic package.

--- src/org/opencms/i18n/A_CmsMessageBundle.java
+++ src/org/opencms/i18n/A_CmsMessageBundle.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/i18n/A_CmsMessageBundle.java,v $
- * Date   : $Date: 2005/05/12 09:21:15 $
- * Version: $Revision: 1.26 $
+ * Date   : $Date: 2005/05/12 13:15:00 $
+ * Version: $Revision: 1.27 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -52,6 +52,7 @@ public abstract class A_CmsMessageBundle implements I_CmsMessageBundle {
             org.opencms.cache.Messages.get(),
             org.opencms.configuration.Messages.get(),
             org.opencms.db.Messages.get(),
+            org.opencms.db.generic.Messages.get(),
             org.opencms.file.Messages.get(),
             org.opencms.file.collectors.Messages.get(),
             org.opencms.flex.Messages.get(),
