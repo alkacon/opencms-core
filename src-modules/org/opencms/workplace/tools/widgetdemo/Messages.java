fc818f3e0ab9766a5ba124221f48031223e73b38
aWestermann <a.westermann@alkacon.com>
2005-06-01 15:16:15 +0000

    Defined constants for messages

--- src-modules/org/opencms/workplace/tools/widgetdemo/Messages.java
+++ src-modules/org/opencms/workplace/tools/widgetdemo/Messages.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/workplace/tools/widgetdemo/Attic/Messages.java,v $
- * Date   : $Date: 2005/05/31 16:29:31 $
- * Version: $Revision: 1.4 $
+ * Date   : $Date: 2005/06/01 15:16:15 $
+ * Version: $Revision: 1.5 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -41,6 +41,75 @@ import org.opencms.i18n.I_CmsMessageBundle;
  * @since 5.7.3
  */
 public final class Messages extends A_CmsMessageBundle {
+
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_TOOL_NAME_0 = "GUI_WIDGETDEMO_TOOL_NAME_0";
+
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_TOOL_HELP_0 = "GUI_WIDGETDEMO_TOOL_HELP_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_TOOL_GROUP_0 = "GUI_WIDGETDEMO_TOOL_GROUP_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_BASIC_GROUP_0 = "GUI_WIDGETDEMO_BASIC_GROUP_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_ADVANCED_GROUP_0 = "GUI_WIDGETDEMO_ADVANCED_GROUP_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_1_NAME_0 = "GUI_WIDGETDEMO_1_NAME_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_1_HELP_0 = "GUI_WIDGETDEMO_1_HELP_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_2_NAME_0 = "GUI_WIDGETDEMO_2_NAME_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_2_HELP_0 = "GUI_WIDGETDEMO_2_HELP_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_3_NAME_0 = "GUI_WIDGETDEMO_3_NAME_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_3_HELP_0 = "GUI_WIDGETDEMO_3_HELP_0";
+
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_4_NAME_0 = "GUI_WIDGETDEMO_4_NAME_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_4_HELP_0 = "GUI_WIDGETDEMO_4_HELP_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_5_NAME_0 = "GUI_WIDGETDEMO_5_NAME_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_5_HELP_0 = "GUI_WIDGETDEMO_5_HELP_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_6_NAME_0 = "GUI_WIDGETDEMO_6_NAME_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_6_HELP_0 = "GUI_WIDGETDEMO_6_HELP_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_7_NAME_0 = "GUI_WIDGETDEMO_7_NAME_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_7_HELP_0 = "GUI_WIDGETDEMO_7_HELP_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_8_NAME_0 = "GUI_WIDGETDEMO_8_NAME_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_8_HELP_0 = "GUI_WIDGETDEMO_8_HELP_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_9_NAME_0 = "GUI_WIDGETDEMO_9_NAME_0";
+    
+    /** Message contant for key in the resource bundle. */
+    public static final String GUI_WIDGETDEMO_9_HELP_0 = "GUI_WIDGETDEMO_9_HELP_0";
 
     /** Name of the used resource bundle. */
     private static final String BUNDLE_NAME = "org.opencms.workplace.tools.widgetdemo.messages";
