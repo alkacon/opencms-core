9aad16dee2c6528a788c2f1cbc307dde529d8c03
jBaudisch <j.baudisch@alkacon.com>
2005-06-01 07:17:18 +0000

    Fixed wrong usage of CmsMessageContainers

--- src/org/opencms/db/generic/CmsProjectDriver.java
+++ src/org/opencms/db/generic/CmsProjectDriver.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/db/generic/CmsProjectDriver.java,v $
- * Date   : $Date: 2005/05/31 15:10:40 $
- * Version: $Revision: 1.224 $
+ * Date   : $Date: 2005/06/01 07:17:18 $
+ * Version: $Revision: 1.225 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -85,7 +85,7 @@ import org.apache.commons.logging.Log;
  * @author Thomas Weckert (t.weckert@alkacon.com)
  * @author Carsten Weinholz (c.weinholz@alkacon.com)
  * 
- * @version $Revision: 1.224 $
+ * @version $Revision: 1.225 $
  * @since 5.1
  */
 public class CmsProjectDriver implements I_CmsDriver, I_CmsProjectDriver {
@@ -1533,8 +1533,7 @@ public class CmsProjectDriver implements I_CmsDriver, I_CmsProjectDriver {
             }
 
             if (n > 0) {
-                report.println(Messages.get().container(Messages.RPT_PUBLISH_FOLDERS_END_0).key(
-                    report.getLocale()), I_CmsReport.C_FORMAT_HEADLINE);
+                report.println(Messages.get().container(Messages.RPT_PUBLISH_FOLDERS_END_0), I_CmsReport.C_FORMAT_HEADLINE);
             }
 
             ///////////////////////////////////////////////////////////////////////////////////////
