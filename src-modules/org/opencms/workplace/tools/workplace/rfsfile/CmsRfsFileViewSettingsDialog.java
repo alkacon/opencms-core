bd1809acfe9c3e880ee32acaf0f118434256298f
aWestermann <a.westermann@alkacon.com>
2005-06-17 09:28:33 +0000

    - Views for file encoding and enabling preview. - Improved error handling. - Xml-persistance upon commit.

--- src-modules/org/opencms/workplace/tools/workplace/rfsfile/CmsRfsFileViewSettingsDialog.java
+++ src-modules/org/opencms/workplace/tools/workplace/rfsfile/CmsRfsFileViewSettingsDialog.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/workplace/tools/workplace/rfsfile/CmsRfsFileViewSettingsDialog.java,v $
- * Date   : $Date: 2005/06/15 12:51:24 $
- * Version: $Revision: 1.1 $
+ * Date   : $Date: 2005/06/17 09:28:33 $
+ * Version: $Revision: 1.2 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -31,6 +31,7 @@
 
 package org.opencms.workplace.tools.workplace.rfsfile;
 
+import org.opencms.configuration.CmsWorkplaceConfiguration;
 import org.opencms.jsp.CmsJspActionElement;
 import org.opencms.main.CmsSystemInfo;
 import org.opencms.main.OpenCms;
@@ -39,9 +40,15 @@ import org.opencms.widgets.CmsComboWidget;
 import org.opencms.widgets.CmsSelectWidgetOption;
 import org.opencms.workplace.CmsWidgetDialogParameter;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.SortedMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -53,7 +60,7 @@ import javax.servlet.jsp.PageContext;
  * <code>{@link org.opencms.util.CmsRfsFileViewer}</code> bean.<p>
  * 
  * @author  Achim Westermann (a.westermann@alkacon.com)
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  * 
  * @since 6.0
  *  
@@ -81,6 +88,30 @@ public class CmsRfsFileViewSettingsDialog extends A_CmsRfsFileWidgetDialog {
     }
 
     /**
+     * Commits the <code>{@link org.opencms.util.CmsRfsFileViewer}</code> to the 
+     * <code>{@link org.opencms.workplace.CmsWorkplaceManager}</code>. <p>
+     * 
+     * @see org.opencms.workplace.CmsWidgetDialog#actionCommit()
+     */
+    public void actionCommit() {
+
+        List errors = new ArrayList();
+
+        try {
+            // set the edited settings
+            OpenCms.getWorkplaceManager().setFileViewSettings(getCms(), m_logView);
+            // write the configuration
+            OpenCms.writeConfiguration(CmsWorkplaceConfiguration.class);
+            setDialogObject(null);
+        } catch (Throwable t) {
+            errors.add(t);
+        }
+
+        // set the list of errors to display when saving failed
+        setCommitErrors(errors);
+    }
+
+    /**
      * Creates the dialog HTML for all defined widgets of the named dialog (page).<p>
      * 
      * This overwrites the method from the super class to create a layout variation for the widgets.<p>
@@ -101,7 +132,7 @@ public class CmsRfsFileViewSettingsDialog extends A_CmsRfsFileWidgetDialog {
         // create the widgets for the settings page
         result.append(dialogBlockStart(key(Messages.GUI_WORKPLACE_LOGVIEW_SETTINGS_NAME_0)));
         result.append(createWidgetTableStart());
-        result.append(createDialogRowsHtml(0, 2));
+        result.append(createDialogRowsHtml(0, 4));
         result.append(createWidgetTableEnd());
         result.append(dialogBlockEnd());
 
@@ -119,7 +150,7 @@ public class CmsRfsFileViewSettingsDialog extends A_CmsRfsFileWidgetDialog {
         super.defineWidgets();
         addWidget(new CmsWidgetDialogParameter(m_logView, "isLogfile", "page1", new CmsCheckboxWidget("true")));
         addWidget(new CmsWidgetDialogParameter(m_logView, "filePath", "page1", new CmsComboWidget(
-            createComboConfiguration())));
+            createComboConfigurationFileChoice())));
 
         // options for windowsize combowidget:
         List comboOptions = new LinkedList();
@@ -130,14 +161,63 @@ public class CmsRfsFileViewSettingsDialog extends A_CmsRfsFileWidgetDialog {
         comboOptions.add(new CmsSelectWidgetOption("800"));
         addWidget(new CmsWidgetDialogParameter(m_logView, "windowSize", "page1", new CmsComboWidget(comboOptions)));
 
+        // file encoding combowidget;
+        addWidget(new CmsWidgetDialogParameter(m_logView, "fileEncoding", "page1", new CmsComboWidget(
+            createComboConfigurationEncodingChoice())));
+
+        addWidget(new CmsWidgetDialogParameter(m_logView, "enabled", "page1", new CmsCheckboxWidget()));
+
     }
 
     /**
+     * Returns a list of <code>{@link org.opencms.widgets.CmsSelectWidgetOption}</code> instances for the  
+     * <code>{@link CmsComboWidget}</code> with the supported encodings of the 
+     * current system and the default encoding set as default combo option.<p> 
      * 
-     * @return a list of {@link org.opencms.widgets.CmsSelectWidgetOption} instances for the {@link CmsComboWidget} with default file locations of OpenCms. 
+     * @return a list of <code>{@link org.opencms.widgets.CmsSelectWidgetOption}</code> instances for the  
+     *         <code>{@link CmsComboWidget}</code> with the supported encodings of the 
+     *         current system and the default encoding set as default combo option.<p>  
+     */
+    private List createComboConfigurationEncodingChoice() {
+
+        List result = new LinkedList();
+        SortedMap csMap = Charset.availableCharsets();
+        // default charset: see http://java.sun.com/j2se/corejava/intl/reference/faqs/index.html#default-encoding
+        // before java 1.5 there is no other way (System property "file.encoding" is implementation detail not in vmspec.
+        Charset defaultCs = Charset.forName(new OutputStreamWriter(new ByteArrayOutputStream()).getEncoding());
+        Charset cs;
+        Iterator it = csMap.values().iterator();
+        while (it.hasNext()) {
+            cs = (Charset)it.next();
+            // default? no equals required: safety by design!
+            if (cs == defaultCs) {
+                result.add(new CmsSelectWidgetOption(cs.name(), true, null, Messages.get().key(
+                    getLocale(),
+                    Messages.GUI_WORKPLACE_LOGVIEW_FILE_CHARSET_DEF_HELP_0,
+                    null)));
+            } else {
+                if (!cs.name().startsWith("x")) {
+                    result.add(new CmsSelectWidgetOption(cs.name(), false, null, Messages.get().key(
+                        getLocale(),
+                        Messages.GUI_WORKPLACE_LOGVIEW_FILE_CHARSET_HELP_0,
+                        null)));
+                }
+            }
+        }
+
+        return result;
+
+    }
+
+    /**
+     * Returns a list of <code>{@link org.opencms.widgets.CmsSelectWidgetOption}</code> instances for the 
+     *         <code>{@link CmsComboWidget}</code> with default file locations of OpenCms.<p>
+     * 
+     * @return a list of <code>{@link org.opencms.widgets.CmsSelectWidgetOption}</code> instances for the 
+     *         <code>{@link CmsComboWidget}</code> with default file locations of OpenCms
      * 
      */
-    private List createComboConfiguration() {
+    private List createComboConfigurationFileChoice() {
 
         List result = new LinkedList();
         CmsSystemInfo sysInfo = OpenCms.getSystemInfo();
