dd6dab20cef10014b6f85e393616c61dc2994ea9
mMoossen <m.moossen@alkacon.com>
2005-06-26 13:30:31 +0000

    fixed blank page in contenttools as normal user

--- src/org/opencms/workplace/tools/CmsDefaultToolHandler.java
+++ src/org/opencms/workplace/tools/CmsDefaultToolHandler.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/workplace/tools/CmsDefaultToolHandler.java,v $
- * Date   : $Date: 2005/06/26 10:56:53 $
- * Version: $Revision: 1.13 $
+ * Date   : $Date: 2005/06/26 13:30:31 $
+ * Version: $Revision: 1.14 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -44,7 +44,7 @@ import java.util.List;
  * 
  * @author Michael Moossen  
  * 
- * @version $Revision: 1.13 $ 
+ * @version $Revision: 1.14 $ 
  * 
  * @since 6.0.0 
  */
@@ -85,6 +85,6 @@ public class CmsDefaultToolHandler extends A_CmsToolHandler {
                 return true;
             }
         }
-        return !getLink().equals(CmsToolManager.C_VIEW_JSPPAGE_LOCATION);
+        return false;
     }
 }
