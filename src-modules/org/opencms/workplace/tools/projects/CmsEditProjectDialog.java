d561f394d3a4555c7caea909e396635e3722c93d
aWestermann <a.westermann@alkacon.com>
2005-07-13 12:47:55 +0000

    Mappings for german online help.

--- src-modules/org/opencms/workplace/tools/projects/CmsEditProjectDialog.java
+++ src-modules/org/opencms/workplace/tools/projects/CmsEditProjectDialog.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/workplace/tools/projects/CmsEditProjectDialog.java,v $
- * Date   : $Date: 2005/06/27 23:22:16 $
- * Version: $Revision: 1.12 $
+ * Date   : $Date: 2005/07/13 12:47:55 $
+ * Version: $Revision: 1.13 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -60,7 +60,7 @@ import javax.servlet.jsp.PageContext;
  * 
  * @author Michael Moossen 
  * 
- * @version $Revision: 1.12 $ 
+ * @version $Revision: 1.13 $ 
  * 
  * @since 6.0.0 
  */
@@ -104,6 +104,17 @@ public class CmsEditProjectDialog extends A_CmsProjectDialog {
     public CmsEditProjectDialog(PageContext context, HttpServletRequest req, HttpServletResponse res) {
 
         this(new CmsJspActionElement(context, req, res));
+    }
+
+    /**
+     * Overridden to set a custom online help mapping.<p>
+     * 
+     * @see org.opencms.workplace.CmsWorkplace#initWorkplaceMembers(org.opencms.jsp.CmsJspActionElement)
+     */
+    protected void initWorkplaceMembers(CmsJspActionElement jsp) {
+
+        super.initWorkplaceMembers(jsp);
+        setOnlineHelpUriCustom("/projects/project_edit.html");
     }
 
     /**
