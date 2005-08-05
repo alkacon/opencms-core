bba0c821d98caec3a814887e99ec8354e346c6dc
cWeinholz <c.weinholz@alkacon.com>
2005-08-05 08:50:30 +0000

    Changed error handling to avoid broken users when add. info is invalid

--- src/org/opencms/db/generic/CmsUserDriver.java
+++ src/org/opencms/db/generic/CmsUserDriver.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/db/generic/CmsUserDriver.java,v $
- * Date   : $Date: 2005/07/28 10:53:54 $
- * Version: $Revision: 1.107 $
+ * Date   : $Date: 2005/08/05 08:50:30 $
+ * Version: $Revision: 1.108 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -84,7 +84,7 @@ import org.apache.commons.logging.Log;
  * @author Carsten Weinholz 
  * @author Michael Emmerich 
  * 
- * @version $Revision: 1.107 $
+ * @version $Revision: 1.108 $
  * 
  * @since 6.0.0 
  */
@@ -1524,18 +1524,18 @@ public class CmsUserDriver implements I_CmsDriver, I_CmsUserDriver {
         ObjectInputStream oin = new ObjectInputStream(bin);
 
         Map info;
-        if (m_adminUserName.equals(userName)) {
-            // ensure the admin user can be read even if it's additional infos are defect
-            try {
-                info = (Map)oin.readObject();
-            } catch (IOException e) {
-                info = new HashMap();
-            }
-        } else {
-            // no exception handling for normal users
+        // ensure the user is read even if it's additional infos are defect
+        try {
             info = (Map)oin.readObject();
-        }
-
+        } catch (IOException e) {
+            CmsMessageContainer message = Messages.get().container(
+                Messages.ERR_READING_ADDITIONAL_INFO_1,
+                userName);
+            LOG.error(message.key());
+            
+            info = new HashMap();
+        }        
+        
         return new CmsUser(
             new CmsUUID(res.getString(m_sqlManager.readQuery("C_USERS_USER_ID"))),
             userName,
--- src/org/opencms/db/generic/Messages.java
+++ src/org/opencms/db/generic/Messages.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/db/generic/Messages.java,v $
- * Date   : $Date: 2005/07/28 10:53:54 $
- * Version: $Revision: 1.23 $
+ * Date   : $Date: 2005/08/05 08:50:30 $
+ * Version: $Revision: 1.24 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -40,7 +40,7 @@ import org.opencms.i18n.I_CmsMessageBundle;
  * @author Thomas Weckert 
  * @author Jan Baudisch 
  * 
- * @version $Revision: 1.23 $
+ * @version $Revision: 1.24 $
  * 
  * @since 6.0.0 
  */
@@ -205,6 +205,9 @@ public final class Messages extends A_CmsMessageBundle {
     /** Message constant for key in the resource bundle. */
     public static final String ERR_WRITING_TO_OUTPUT_STREAM_1 = "ERR_WRITING_TO_OUTPUT_STREAM_1";
 
+    /** Message constant for key in the resource bundle. */
+    public static final String ERR_READING_ADDITIONAL_INFO_1 = "ERR_READING_ADDITIONAL_INFO_1";
+    
     /** Message constant for key in the resource bundle. */
     public static final String INIT_ASSIGNED_POOL_1 = "INIT_ASSIGNED_POOL_1";
 
