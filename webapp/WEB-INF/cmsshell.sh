bfcfa15ea08913664781c4ee8f36a21a0cfbf58b
cWeinholz <c.weinholz@alkacon.com>
2005-10-04 08:53:05 +0000

    Corrected error in cmsshell.sh

--- webapp/WEB-INF/cmsshell.sh
+++ webapp/WEB-INF/cmsshell.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 # Start script for the OpenCms Shell
-# Please make sure that "servlet.jar" is found, the path used below was tested with Tomcat 4.1
 
 OCMSLIBPATH=""
 for i in lib/*.jar; do
-   OCMSLIBPATH="$OCMSLIBPATH":"$i"
+	OCMSLIBPATH="$OCMSLIBPATH":"$i"
 done
 
 TOMCATLIB="../../../common/lib/servlet.jar:../../../common/lib/servlet-api.jar:../../../common/lib/jsp-api.jar"
 
-java -classpath .:$TOMCATLIB$OCMSLIBPATH:classes org.opencms.main.CmsShell "$@"
+java -classpath .:$TOMCATLIB:$OCMSLIBPATH:classes org.opencms.main.CmsShell "$@"
