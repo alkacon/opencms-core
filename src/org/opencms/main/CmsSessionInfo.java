d433ab582c230d7be464bf4d753315117f6c2020
aKandzior <a.kandzior@alkacon.com>
2005-10-11 17:47:12 +0000

    Protected #update() method

--- src/org/opencms/main/CmsSessionInfo.java
+++ src/org/opencms/main/CmsSessionInfo.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/main/CmsSessionInfo.java,v $
- * Date   : $Date: 2005/06/27 23:22:20 $
- * Version: $Revision: 1.14 $
+ * Date   : $Date: 2006/03/27 14:52:27 $
+ * Version: $Revision: 1.16 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -52,7 +52,7 @@ import org.apache.commons.collections.buffer.BoundedFifoBuffer;
  * @author Alexander Kandzior 
  * @author Andreas Zahner 
  * 
- * @version $Revision: 1.14 $ 
+ * @version $Revision: 1.16 $ 
  * 
  * @since 6.0.0 
  */
@@ -244,7 +244,7 @@ public class CmsSessionInfo implements Comparable {
      * 
      * @param context the requrest context to update the session with
      */
-    public void update(CmsRequestContext context) {
+    protected void update(CmsRequestContext context) {
 
         m_timeUpdated = System.currentTimeMillis();
         m_siteRoot = context.getSiteRoot();
