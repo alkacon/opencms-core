806553073b377f8be64a6258f736ab851899d779
mMoossen <m.moossen@alkacon.com>
2005-10-19 10:04:46 +0000

    forgotten project id for getting the connection

--- src/org/opencms/db/generic/CmsVfsDriver.java
+++ src/org/opencms/db/generic/CmsVfsDriver.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/db/generic/CmsVfsDriver.java,v $
- * Date   : $Date: 2005/10/19 09:37:40 $
- * Version: $Revision: 1.255.2.3 $
+ * Date   : $Date: 2005/10/19 10:04:46 $
+ * Version: $Revision: 1.255.2.4 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -76,7 +76,7 @@ import org.apache.commons.logging.Log;
  * @author Thomas Weckert 
  * @author Michael Emmerich 
  * 
- * @version $Revision: 1.255.2.3 $
+ * @version $Revision: 1.255.2.4 $
  * 
  * @since 6.0.0 
  */
@@ -1686,7 +1686,7 @@ public class CmsVfsDriver implements I_CmsDriver, I_CmsVfsDriver {
         Connection conn = null;
 
         try {
-            conn = m_sqlManager.getConnection(dbc);
+            conn = m_sqlManager.getConnection(dbc, projectId);
             StringBuffer queryBuf = new StringBuffer(256);
             queryBuf.append(m_sqlManager.readQuery(projectId, "C_RESOURCES_READ_TREE"));
             queryBuf.append(conditions);
