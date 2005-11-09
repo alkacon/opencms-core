3eb430f0d9aa327fccbb7463e5f2d85b49648156
aWestermann <a.westermann@alkacon.com>
2005-11-09 16:14:51 +0000

    - Typo in comment, ... thx for enabling nested contentloads!

--- src/org/opencms/jsp/CmsJspTagContentLoad.java
+++ src/org/opencms/jsp/CmsJspTagContentLoad.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/jsp/CmsJspTagContentLoad.java,v $
- * Date   : $Date: 2005/11/09 14:41:19 $
- * Version: $Revision: 1.27.2.5 $
+ * Date   : $Date: 2005/11/09 16:14:51 $
+ * Version: $Revision: 1.27.2.6 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -61,7 +61,7 @@ import javax.servlet.jsp.tagext.Tag;
  * 
  * @author  Alexander Kandzior 
  * 
- * @version $Revision: 1.27.2.5 $ 
+ * @version $Revision: 1.27.2.6 $ 
  * 
  * @since 6.0.0 
  */
@@ -257,7 +257,7 @@ public class CmsJspTagContentLoad extends BodyTagSupport implements I_CmsJspTagC
     }
 
     /**
-     * Load the next file name fomr the initialized list of file names.<p>
+     * Load the next file name from the initialized list of file names.<p>
      * 
      * @throws CmsException if something goes wrong
      */
