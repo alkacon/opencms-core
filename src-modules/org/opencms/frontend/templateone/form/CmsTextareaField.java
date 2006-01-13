cc9c7de29c061c2d4210bc9773fa96b0c11fef42
jBaudisch <j.baudisch@alkacon.com>
2006-01-13 11:49:39 +0000

    HTML-escape field value to prevent cross site scripting

--- src-modules/org/opencms/frontend/templateone/form/CmsTextareaField.java
+++ src-modules/org/opencms/frontend/templateone/form/CmsTextareaField.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/frontend/templateone/form/CmsTextareaField.java,v $
- * Date   : $Date: 2005/09/09 10:31:59 $
- * Version: $Revision: 1.3 $
+ * Date   : $Date: 2006/01/13 11:49:39 $
+ * Version: $Revision: 1.3.2.1 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -38,7 +38,7 @@ import org.opencms.util.CmsStringUtil;
  * Represents a text area.<p>
  * 
  * @author Thomas Weckert (t.weckert@alkacon.com)
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.3.2.1 $
  */
 public class CmsTextareaField extends A_CmsField {
 
@@ -104,7 +104,7 @@ public class CmsTextareaField extends A_CmsField {
         buf.append(messages.key("form.html.field.start"))
             .append("<textarea name=\"").append(getName()).append("\"")
             .append(formHandler.getFormConfiguration().getFormFieldAttributes())
-            .append(">").append(getValue()).append("</textarea>")
+            .append(">").append(CmsStringUtil.escapeHtml(getValue())).append("</textarea>")
             .append(errorMessage)
             .append(messages.key("form.html.field.end")).append("\n");
         
