bca63d32f4da2a74f72844eb394e7293ae6f5ed4
cWeinholz <c.weinholz@alkacon.com>
2006-01-25 16:57:54 +0000

    Corrected error in handling of quote-enclosed items

--- src/org/opencms/workplace/explorer/CmsNewCsvFile.java
+++ src/org/opencms/workplace/explorer/CmsNewCsvFile.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/workplace/explorer/CmsNewCsvFile.java,v $
- * Date   : $Date: 2005/12/09 11:37:13 $
- * Version: $Revision: 1.27 $
+ * Date   : $Date: 2006/01/25 16:57:54 $
+ * Version: $Revision: 1.28 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -82,7 +82,7 @@ import org.apache.commons.logging.Log;
  * 
  * @author Jan Baudisch 
  * 
- * @version $Revision: 1.27 $ 
+ * @version $Revision: 1.28 $ 
  * 
  * @since 6.0.0 
  */
@@ -643,11 +643,14 @@ public class CmsNewCsvFile extends CmsNewResourceUpload {
                 }
                 if (!item.equals(delimiter)) {
                     
+                    // remove enclosing delimiters
+                    item = removeStringDelimiters(item);
+                    
                     // in order to allow links, lines starting and ending with tag delimiters (< ...>) remains unescaped
                     if (item.startsWith(TAG_START_DELIMITER) && item.endsWith(TAG_END_DELIMITER)) {
-                        xml.append(removeStringDelimiters(item));
+                        xml.append(item);
                     } else {
-                        xml.append(CmsStringUtil.escapeHtml(removeStringDelimiters(item)));
+                        xml.append(CmsStringUtil.escapeHtml(item));
                     }
                 } else {
                     xml.append("</td>\n");
