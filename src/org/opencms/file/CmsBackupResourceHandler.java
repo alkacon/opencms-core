308600d88b8df0a558470346c92c13fb9adc9bec
mEmmerich <m.emmerich@alkacon.com>
2006-03-21 08:35:38 +0000

    Added check if request is available

--- src/org/opencms/file/CmsBackupResourceHandler.java
+++ src/org/opencms/file/CmsBackupResourceHandler.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/file/CmsBackupResourceHandler.java,v $
- * Date   : $Date: 2005/11/17 13:55:35 $
- * Version: $Revision: 1.2.2.1 $
+ * Date   : $Date: 2006/03/21 08:35:38 $
+ * Version: $Revision: 1.2.2.2 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -45,7 +45,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Michael Emmerich 
  * 
- * @version $Revision: 1.2.2.1 $
+ * @version $Revision: 1.2.2.2 $
  * 
  * @since 6.0.1 
  */
@@ -79,42 +79,47 @@ public class CmsBackupResourceHandler implements I_CmsResourceInit {
     public CmsResource initResource(CmsResource resource, CmsObject cms, HttpServletRequest req, HttpServletResponse res)
     throws CmsResourceInitException {
 
-        String versionId = req.getParameter(PARAM_VERSIONID);
+        // we only have to check for backup resources if the handler was called
+        // during a real request and NOT during a dummy-request while doing
+        // a static export
+        if (req != null) {
+            String versionId = req.getParameter(PARAM_VERSIONID);
 
-        // only do something if the resource was not found and there was a "versionid" parameter included
-        if (resource == null && versionId != null) {
+            // only do something if the resource was not found and there was a "versionid" parameter included
+            if (resource == null && versionId != null) {
 
-            String uri = cms.getRequestContext().getUri();
-            // check if the resource starts with the BACKUP_HANDLER
-            if (uri.startsWith(BACKUP_HANDLER)) {
-                // test if the current user is allowed to read backup versions of resources
-                // this can be done by trying to read the backup handler resource
-                if (cms.existsResource(BACKUP_HANDLER)) {
-                    try {
-                        // extract the "real" resourcename
-                        uri = uri.substring(BACKUP_HANDLER.length(), uri.length());
-                        int id = new Integer(versionId).intValue();
-                        // we now must switch to the root site to read the backup resource
-                        cms.getRequestContext().saveSiteRoot();
-                        cms.getRequestContext().setSiteRoot("/");
-                        resource = cms.readBackupFile(uri, id);
-                        // store a request attribute to indicate that this is in fact a backup version
-                        req.setAttribute(ATTRIBUTE_NAME, Boolean.TRUE);
-                    } catch (CmsException e) {
-                        if (OpenCms.getLog(this).isErrorEnabled()) {
-                            OpenCms.getLog(this).error(
-                                Messages.get().container(Messages.ERR_BACKUPRESOURCE_2, uri, versionId));
-                        }
-                        throw new CmsResourceInitException(Messages.get().container(
-                            Messages.ERR_SHOWVERSION_2,
-                            uri,
-                            versionId), e);
-                    } finally {
-                        // restore the siteroot and modify the uri to the one of the correct resource
-                        cms.getRequestContext().restoreSiteRoot();
-                        if (resource != null) {
-                            // resource may be null in case of a
-                            cms.getRequestContext().setUri(cms.getSitePath(resource));
+                String uri = cms.getRequestContext().getUri();
+                // check if the resource starts with the BACKUP_HANDLER
+                if (uri.startsWith(BACKUP_HANDLER)) {
+                    // test if the current user is allowed to read backup versions of resources
+                    // this can be done by trying to read the backup handler resource
+                    if (cms.existsResource(BACKUP_HANDLER)) {
+                        try {
+                            // extract the "real" resourcename
+                            uri = uri.substring(BACKUP_HANDLER.length(), uri.length());
+                            int id = new Integer(versionId).intValue();
+                            // we now must switch to the root site to read the backup resource
+                            cms.getRequestContext().saveSiteRoot();
+                            cms.getRequestContext().setSiteRoot("/");
+                            resource = cms.readBackupFile(uri, id);
+                            // store a request attribute to indicate that this is in fact a backup version
+                            req.setAttribute(ATTRIBUTE_NAME, Boolean.TRUE);
+                        } catch (CmsException e) {
+                            if (OpenCms.getLog(this).isErrorEnabled()) {
+                                OpenCms.getLog(this).error(
+                                    Messages.get().container(Messages.ERR_BACKUPRESOURCE_2, uri, versionId));
+                            }
+                            throw new CmsResourceInitException(Messages.get().container(
+                                Messages.ERR_SHOWVERSION_2,
+                                uri,
+                                versionId), e);
+                        } finally {
+                            // restore the siteroot and modify the uri to the one of the correct resource
+                            cms.getRequestContext().restoreSiteRoot();
+                            if (resource != null) {
+                                // resource may be null in case of a
+                                cms.getRequestContext().setUri(cms.getSitePath(resource));
+                            }
                         }
                     }
                 }
