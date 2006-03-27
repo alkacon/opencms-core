1f642867d70255197a1ed7b4663a60310e4f38f4
aKandzior <a.kandzior@alkacon.com>
2006-03-27 13:26:16 +0000

    Added better comment

--- src/org/opencms/i18n/I_CmsMessageBundle.java
+++ src/org/opencms/i18n/I_CmsMessageBundle.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/i18n/I_CmsMessageBundle.java,v $
- * Date   : $Date: 2006/03/25 22:42:36 $
- * Version: $Revision: 1.5.2.2 $
+ * Date   : $Date: 2006/03/27 13:26:16 $
+ * Version: $Revision: 1.5.2.3 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -39,13 +39,38 @@ import java.util.Locale;
  * Every OpenCms core packages should provide one class implementing this interface.
  * This class should be called <code>Messages</code> and be a subclass of 
  * <code>{@link org.opencms.i18n.A_CmsMessageBundle}</code>. Moreover, the implementation 
- * should contain a <code>public static I_CmsMessageBundle get()</code> method, that returns 
+ * should contain a <code>public static CmsMessageBundle#get()</code> method, that returns 
  * an instance of the class. Also the implementation should contain public static members
  * for all available localized keys.<p>
  * 
+ * How to use localization in the OpenCms core:<p>
+ * 
+ * There are 3 main ulitiliy classes that deal with localization. These are<ul>
+ * 
+ * <li>{@link I_CmsMessageBundle} (all classes that implement this interface):
+ * These classes describe a pointer to a resource bundle, but without providing a {@link java.util.Locale}.
+ * So this is like a list of valid keys that can be localized, without the actual localization.</li>
+ * 
+ * <li>{@link org.opencms.i18n.CmsMessages}: This is a fully localized resource bundle, 
+ * with a list of keys and a {@link java.util.Locale} to localize these keys. If you have a {@link I_CmsMessageBundle},
+ * you can use {@link I_CmsMessageBundle#getBundleName()} to initialize a {@link org.opencms.i18n.CmsMessages} instance
+ * using {@link org.opencms.i18n.CmsMessages#CmsMessages(String, Locale)}.</li>
+ * 
+ * <li>{@link org.opencms.i18n.CmsMessageContainer}: In some circumstances, you do not have a {@link java.util.Locale},
+ * but you want to generate a message that contains certain parameters anyway. For example, in the deeper layers of the
+ * OpenCms core an issue may be generated, but you do not have a full user context available. The {@link org.opencms.i18n.CmsMessageContainer}
+ * contains a reference to a {@link I_CmsMessageBundle}, plus the name of the key to use in the bundle, plus a list of 
+ * (optional) parameters. This container is then passed "upwards" in the core until the GUI layer (usually the Workplace)
+ * is reached.</li>
+ * 
+ * <li>{@link org.opencms.i18n.CmsMultiMessages}: This is a convenience class that contains a set 
+ * of {@link org.opencms.i18n.CmsMessages}. A key lookup is automatically done in all the {@link org.opencms.i18n.CmsMessages} 
+ * instances contained in the multi message instance, and the first match is returned.</li>
+ * </ul>
+ * 
  * @author Alexander Kandzior 
  * 
- * @version $Revision: 1.5.2.2 $ 
+ * @version $Revision: 1.5.2.3 $ 
  * 
  * @since 6.0.0 
  */
@@ -110,6 +135,9 @@ public interface I_CmsMessageBundle {
     /** 
      * Returns the localized message bundle wrapped in this instance initialized with the OpenCms default locale.<p> 
      * 
+     * This should be used only for logging and system output, not for generating GUI messages that the
+     * user can see. In this case, use {@link #getBundle(Locale)}.<p> 
+     * 
      * @return the localized message bundle wrapped in this instance initialized with the OpenCms default locale
      */
     CmsMessages getBundle();
@@ -118,6 +146,7 @@ public interface I_CmsMessageBundle {
      * Returns the localized message bundle wrapped in this instance initialized with the provided locale.<p> 
      * 
      * @param locale the locale to use
+     * 
      * @return the localized message bundle wrapped in this instance initialized with the provided locale
      */
     CmsMessages getBundle(Locale locale);
