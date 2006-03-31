9619e7caaba7495f42c464868dd7bd5108c07ece
cWeinholz <c.weinholz@alkacon.com>
2006-03-31 15:25:51 +0000

    Changed csv table upload in order to use utility class for transformation

--- src/org/opencms/workplace/explorer/Messages.java
+++ src/org/opencms/workplace/explorer/Messages.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/workplace/explorer/Messages.java,v $
- * Date   : $Date: 2006/03/28 18:44:59 $
- * Version: $Revision: 1.13 $
+ * Date   : $Date: 2006/03/31 15:25:51 $
+ * Version: $Revision: 1.14 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -39,7 +39,7 @@ import org.opencms.i18n.I_CmsMessageBundle;
  * 
  * @author Jan Baudisch 
  * 
- * @version $Revision: 1.13 $ 
+ * @version $Revision: 1.14 $ 
  * 
  * @since 6.0.0 
  */
@@ -50,9 +50,6 @@ public final class Messages extends A_CmsMessageBundle {
 
     /** Message constant for key in the resource bundle. */
     public static final String ERR_CREATE_LINK_0 = "ERR_CREATE_LINK_0";
-
-    /** Message constant for key in the resource bundle. */
-    public static final String ERR_CSV_XML_TRANSFORMATION_FAILED_0 = "ERR_CSV_XML_TRANSFORMATION_FAILED_0";
 
     /** Message constant for key in the resource bundle. */
     public static final String ERR_NEW_RES_CONSTRUCTOR_NOT_FOUND_1 = "ERR_NEW_RES_CONSTRUCTOR_NOT_FOUND_1";
