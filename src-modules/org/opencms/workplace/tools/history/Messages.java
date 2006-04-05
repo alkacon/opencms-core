e130176e4b826119c1185f698120634e46c88c1b
pRyvkin <p.ryvkin@alkacon.com>
2006-04-05 11:48:22 +0000

    key  ERR_MESSAGE_INVALIDCLEARHISTORYDATA_0 added

--- src-modules/org/opencms/workplace/tools/history/Messages.java
+++ src-modules/org/opencms/workplace/tools/history/Messages.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/workplace/tools/history/Messages.java,v $
- * Date   : $Date: 2006/03/27 14:52:54 $
- * Version: $Revision: 1.8 $
+ * Date   : $Date: 2006/04/05 11:48:22 $
+ * Version: $Revision: 1.9 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -39,7 +39,7 @@ import org.opencms.i18n.I_CmsMessageBundle;
  * 
  * @author Jan Baudisch 
  * 
- * @version $Revision: 1.8 $ 
+ * @version $Revision: 1.9 $ 
  * 
  * @since 6.0.0 
  */
@@ -47,6 +47,9 @@ public final class Messages extends A_CmsMessageBundle {
 
     /** Message constant for key in the resource bundle. */
     public static final String ERR_INVALID_DATE_1 = "ERR_INVALID_DATE_1";
+
+    /** Message constant for key in the resource bundle. */
+    public static final String ERR_MESSAGE_INVALIDCLEARHISTORYDATA_0="ERR_MESSAGE_INVALIDCLEARHISTORYDATA_0";
 
     /** Message constant for key in the resource bundle. */
     public static final String ERR_NO_INT_ENTERED_0 = "ERR_NO_INT_ENTERED_0";
@@ -98,7 +101,7 @@ public final class Messages extends A_CmsMessageBundle {
 
     /** Message constant for key in the resource bundle. */
     public static final String GUI_INPUT_HISTNUMBER_0 = "GUI_INPUT_HISTNUMBER_0";
-
+    
     /** Message constant for key in the resource bundle. */
     public static final String GUI_INPUT_HISTORY_CLEAR_DATE_0 = "GUI_INPUT_HISTORY_CLEAR_DATE_0";
     
@@ -110,13 +113,13 @@ public final class Messages extends A_CmsMessageBundle {
     
     /** Message constant for key in the resource bundle. */
     public static final String GUI_INPUT_HISTORY_CLEAR_SELECT_0 = "GUI_INPUT_HISTORY_CLEAR_SELECT_0";
-    
+
     /** Message constant for key in the resource bundle. */
     public static final String GUI_INPUT_HISTORY_CLEAR_SELECTINFO_0 = "GUI_INPUT_HISTORY_CLEAR_SELECTINFO_0";
 
     /** Message constant for key in the resource bundle. */
     public static final String GUI_INPUT_HISTORY_CLEAR_VERSIONINFO_1 = "GUI_INPUT_HISTORY_CLEAR_VERSIONINFO_1";
-
+    
     /** Message constant for key in the resource bundle. */
     public static final String GUI_LABEL_ADMIN_HISTORY_CLEAR_0 = "GUI_LABEL_ADMIN_HISTORY_CLEAR_0";
 
