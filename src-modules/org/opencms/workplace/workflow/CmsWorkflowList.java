85807a4e2b0c925272dabd915c485e5a78285c98
aKandzior <a.kandzior@alkacon.com>
2006-08-25 08:13:10 +0000

    link management improvements - internal relations validation job - delete dialog extensions

--- src-modules/org/opencms/workplace/workflow/CmsWorkflowList.java
+++ src-modules/org/opencms/workplace/workflow/CmsWorkflowList.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/workplace/workflow/Attic/CmsWorkflowList.java,v $
- * Date   : $Date: 2006/08/22 08:18:54 $
- * Version: $Revision: 1.1.2.2 $
+ * Date   : $Date: 2006/08/25 08:13:10 $
+ * Version: $Revision: 1.1.2.3 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -32,10 +32,12 @@
 package org.opencms.workplace.workflow;
 
 import org.opencms.file.CmsProject;
+import org.opencms.file.CmsUser;
 import org.opencms.jsp.CmsJspActionElement;
 import org.opencms.main.CmsException;
 import org.opencms.main.CmsRuntimeException;
 import org.opencms.main.OpenCms;
+import org.opencms.security.I_CmsPrincipal;
 import org.opencms.workplace.list.A_CmsListDialog;
 import org.opencms.workplace.list.CmsListColumnAlignEnum;
 import org.opencms.workplace.list.CmsListColumnDefinition;
@@ -45,6 +47,7 @@ import org.opencms.workplace.list.CmsListItem;
 import org.opencms.workplace.list.CmsListItemDetails;
 import org.opencms.workplace.list.CmsListItemDetailsFormatter;
 import org.opencms.workplace.list.CmsListMetadata;
+import org.opencms.workplace.list.CmsListOrderEnum;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -60,7 +63,7 @@ import javax.servlet.jsp.PageContext;
  * 
  * @author Alexander Kandzior
  * 
- * @version $Revision: 1.1.2.2 $ 
+ * @version $Revision: 1.1.2.3 $ 
  * 
  * @since 6.5.0 
  */
@@ -88,6 +91,9 @@ public class CmsWorkflowList extends A_CmsListDialog {
     public static final String LIST_COLUMN_TYPE = "ct";
 
     /** list column id constant. */
+    public static final String LIST_COLUMN_HIDDEN = "ch";
+
+    /** list column id constant. */
     public static final String LIST_COLUMN_USER_CREATED = "cd";
 
     /** list item detail id constant. */
@@ -103,7 +109,13 @@ public class CmsWorkflowList extends A_CmsListDialog {
      */
     public CmsWorkflowList(CmsJspActionElement jsp) {
 
-        super(jsp, LIST_ID, Messages.get().container(Messages.GUI_WORKFLOW_LIST_NAME_0), null, null, null);
+        super(
+            jsp,
+            LIST_ID,
+            Messages.get().container(Messages.GUI_WORKFLOW_LIST_NAME_0),
+            LIST_COLUMN_OWNER,
+            CmsListOrderEnum.ORDER_ASCENDING,
+            LIST_COLUMN_NAME);
     }
 
     /**
@@ -188,11 +200,27 @@ public class CmsWorkflowList extends A_CmsListDialog {
             CmsProject workflow = (CmsProject)itWorkflows.next();
             CmsListItem item = getList().newItem("" + workflow.getId());
             item.set(LIST_COLUMN_NAME, OpenCms.getWorkflowManager().getTaskDescription(workflow));
+            item.set(LIST_COLUMN_TYPE, OpenCms.getWorkflowManager().getTaskType(workflow, getLocale()));
+            item.set(LIST_COLUMN_STATE, OpenCms.getWorkflowManager().getTaskState(workflow, getLocale()));
             item.set(LIST_COLUMN_DATE, new Date(OpenCms.getWorkflowManager().getTaskStartTime(workflow)));
             item.set(LIST_COLUMN_USER_CREATED, OpenCms.getWorkflowManager().getTaskOwner(workflow).getName());
-            item.set(LIST_COLUMN_OWNER, OpenCms.getWorkflowManager().getTaskAgent(workflow).getName());
-            item.set(LIST_COLUMN_STATE, OpenCms.getWorkflowManager().getTaskState(workflow, getLocale()));
-            item.set(LIST_COLUMN_TYPE, OpenCms.getWorkflowManager().getTaskType(workflow, getLocale()));
+            I_CmsPrincipal agent = OpenCms.getWorkflowManager().getTaskAgent(workflow);
+            item.set(LIST_COLUMN_OWNER, agent.getName());
+            CmsUser curUser = getCms().getRequestContext().currentUser();
+            boolean myWf = false;
+            if (agent.equals(curUser)) {
+                // if the current user is the agent itself
+                myWf = true;
+            } else {
+                try {
+                    List groups = getCms().getGroupsOfUser(curUser.getName());
+                    // if the current user is member of the agent group
+                    myWf = groups.contains(agent);
+                } catch (Exception e) {
+                    // ignore any exception
+                }
+            }
+            item.set(LIST_COLUMN_HIDDEN, Boolean.valueOf(myWf));
             ret.add(item);
         }
         return ret;
@@ -211,10 +239,22 @@ public class CmsWorkflowList extends A_CmsListDialog {
         metadata.addColumn(iconCol);
 
         // icon action
-        CmsListDirectAction iconAction = new CmsListDirectAction(LIST_ACTION_ICON);
+        CmsListDirectAction iconAction = new CmsListDirectAction(LIST_ACTION_ICON) {
+
+            /**
+             * @see org.opencms.workplace.tools.A_CmsHtmlIconButton#getIconPath()
+             */
+            public String getIconPath() {
+
+                if (((Boolean)getItem().get(LIST_COLUMN_HIDDEN)).booleanValue()) {
+                    return "tools/workflow/buttons/my_workflow.png";
+                } else {
+                    return "tools/workflow/buttons/other_workflow.png";
+                }
+            }
+        };
         iconAction.setName(Messages.get().container(Messages.GUI_WORKFLOW_LIST_ACTION_ICON_NAME_0));
         iconAction.setHelpText(Messages.get().container(Messages.GUI_WORKFLOW_LIST_ACTION_ICON_HELP_0));
-        iconAction.setIconPath("tools/workflow/buttons/workflow.png");
         iconAction.setEnabled(false);
         iconCol.addDirectAction(iconAction);
 
@@ -224,6 +264,20 @@ public class CmsWorkflowList extends A_CmsListDialog {
         nameCol.setWidth("25%");
         nameCol.setSorteable(true);
         metadata.addColumn(nameCol);
+
+        // add column for type
+        CmsListColumnDefinition typeCol = new CmsListColumnDefinition(LIST_COLUMN_TYPE);
+        typeCol.setName(Messages.get().container(Messages.GUI_WORKFLOW_LIST_COLS_TYPE_0));
+        typeCol.setWidth("15%");
+        typeCol.setSorteable(true);
+        metadata.addColumn(typeCol);
+
+        // add column for state
+        CmsListColumnDefinition stateCol = new CmsListColumnDefinition(LIST_COLUMN_STATE);
+        stateCol.setName(Messages.get().container(Messages.GUI_WORKFLOW_LIST_COLS_STATE_0));
+        stateCol.setWidth("15%");
+        stateCol.setSorteable(true);
+        metadata.addColumn(stateCol);
 
         // add column for date created
         CmsListColumnDefinition dateCol = new CmsListColumnDefinition(LIST_COLUMN_DATE);
@@ -247,19 +301,10 @@ public class CmsWorkflowList extends A_CmsListDialog {
         ownerCol.setSorteable(true);
         metadata.addColumn(ownerCol);
 
-        // add column for state
-        CmsListColumnDefinition stateCol = new CmsListColumnDefinition(LIST_COLUMN_STATE);
-        stateCol.setName(Messages.get().container(Messages.GUI_WORKFLOW_LIST_COLS_STATE_0));
-        stateCol.setWidth("15%");
-        stateCol.setSorteable(true);
-        metadata.addColumn(stateCol);
-
-        // add column for type
-        CmsListColumnDefinition typeCol = new CmsListColumnDefinition(LIST_COLUMN_TYPE);
-        typeCol.setName(Messages.get().container(Messages.GUI_WORKFLOW_LIST_COLS_TYPE_0));
-        typeCol.setWidth("15%");
-        typeCol.setSorteable(true);
-        metadata.addColumn(typeCol);
+        // add column for hidden info
+        CmsListColumnDefinition hiddenCol = new CmsListColumnDefinition(LIST_COLUMN_HIDDEN);
+        hiddenCol.setVisible(false);        
+        metadata.addColumn(hiddenCol);
     }
 
     /**
