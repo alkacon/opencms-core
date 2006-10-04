796f830238e156fd5205e5088639778e977d2ca0
mMoossen <m.moossen@alkacon.com>
2006-10-04 15:53:07 +0000

    corrected typo

--- src/org/opencms/workplace/tools/A_CmsHtmlIconButton.java
+++ src/org/opencms/workplace/tools/A_CmsHtmlIconButton.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/workplace/tools/A_CmsHtmlIconButton.java,v $
- * Date   : $Date: 2006/09/26 08:38:57 $
- * Version: $Revision: 1.21.4.2 $
+ * Date   : $Date: 2006/10/04 15:53:07 $
+ * Version: $Revision: 1.21.4.3 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -44,7 +44,7 @@ import java.io.File;
  * 
  * @author Michael Moossen  
  * 
- * @version $Revision: 1.21.4.2 $ 
+ * @version $Revision: 1.21.4.3 $ 
  * 
  * @since 6.0.0 
  */
@@ -233,8 +233,8 @@ public abstract class A_CmsHtmlIconButton implements I_CmsHtmlIconButton {
                 icon.append(iconPath.substring(0, iconPath.lastIndexOf('.')));
                 icon.append("_disabled");
                 icon.append(iconPath.substring(iconPath.lastIndexOf('.')));
-                String resorcesRoot = OpenCms.getSystemInfo().getWebApplicationRfsPath() + "resources";
-                File test = new File(resorcesRoot + "/" + icon.toString());
+                String resourcesRoot = OpenCms.getSystemInfo().getWebApplicationRfsPath() + "resources/";
+                File test = new File(resourcesRoot + icon.toString());
                 if (test.exists()) {
                     html.append(icon);
                 } else {
