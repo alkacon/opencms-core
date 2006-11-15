24c44c1ecb017d5aee45de6fb0dc01a6710940f7
aZahner <a.zahner@alkacon.com>
2006-11-15 14:01:54 +0000

    added XML messages object to override localized keys of a resource bundle

--- src/org/opencms/xml/Messages.java
+++ src/org/opencms/xml/Messages.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/xml/Messages.java,v $
- * Date   : $Date: 2006/08/25 13:16:57 $
- * Version: $Revision: 1.12.4.2 $
+ * Date   : $Date: 2006/11/15 14:01:54 $
+ * Version: $Revision: 1.12.4.3 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -40,7 +40,7 @@ import org.opencms.i18n.I_CmsMessageBundle;
  * @author Alexander Kandzior 
  * @author Jan Baudisch 
  * 
- * @version $Revision: 1.12.4.2 $ 
+ * @version $Revision: 1.12.4.3 $ 
  * 
  * @since 6.0.0 
  */
@@ -98,6 +98,9 @@ public final class Messages extends A_CmsMessageBundle {
     public static final String ERR_INVALID_CONTENT_HANDLER_1 = "ERR_INVALID_CONTENT_HANDLER_1";
 
     /** Message constant for key in the resource bundle. */
+    public static final String ERR_INVALID_INIT_USER_1 = "ERR_INVALID_INIT_USER_1";
+
+    /** Message constant for key in the resource bundle. */
     public static final String ERR_INVALID_XCC_TYPE_REGISTERED_0 = "ERR_INVALID_XCC_TYPE_REGISTERED_0";
 
     /** Message constant for key in the resource bundle. */
@@ -111,6 +114,9 @@ public final class Messages extends A_CmsMessageBundle {
 
     /** Message constant for key in the resource bundle. */
     public static final String ERR_MARSHALLING_XML_DOC_TO_STRING_0 = "ERR_MARSHALLING_XML_DOC_TO_STRING_0";
+
+    /** Message constant for key in the resource bundle. */
+    public static final String ERR_NULL_CMSOBJECT_0 = "ERR_NULL_CMSOBJECT_0";
 
     /** Message constant for key in the resource bundle. */
     public static final String ERR_NULL_LOCALE_0 = "ERR_NULL_LOCALE_0";
