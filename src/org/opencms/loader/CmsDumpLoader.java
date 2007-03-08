9ace61e093c4e1c5b80c96d9d20a12eed99d9ac0
aKandzior <a.kandzior@alkacon.com>
2007-03-08 08:55:20 +0000

    Added fix for issue #1323.

--- src/org/opencms/loader/CmsDumpLoader.java
+++ src/org/opencms/loader/CmsDumpLoader.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/loader/CmsDumpLoader.java,v $
- * Date   : $Date: 2006/11/08 09:28:51 $
- * Version: $Revision: 1.65.4.1 $
+ * Date   : $Date: 2007/03/08 08:55:20 $
+ * Version: $Revision: 1.65.4.2 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -64,7 +64,7 @@ import org.apache.commons.collections.ExtendedProperties;
  *
  * @author  Alexander Kandzior 
  * 
- * @version $Revision: 1.65.4.1 $ 
+ * @version $Revision: 1.65.4.2 $ 
  * 
  * @since 6.0.0 
  */
@@ -305,8 +305,9 @@ public class CmsDumpLoader implements I_CmsResourceLoader {
             // check if the request contains a last modified header
             long lastModifiedHeader = req.getDateHeader(CmsRequestUtil.HEADER_IF_MODIFIED_SINCE);
             if (lastModifiedHeader > -1) {
-                // last modified header is set, compare it to the requested resource 
-                if (resource.getState().isUnchanged() && (resource.getDateLastModified() == lastModifiedHeader)) {
+                // last modified header is set, compare it to the requested resource (remove Msec from resource)
+                if (resource.getState().isUnchanged()
+                    && ((resource.getDateLastModified() / 1000L) == lastModifiedHeader)) {
                     long now = System.currentTimeMillis();
                     if ((resource.getDateReleased() < now) && (resource.getDateExpired() > now)) {
                         CmsFlexController.setDateExpiresHeader(res, resource.getDateExpired(), m_clientCacheMaxAge);
