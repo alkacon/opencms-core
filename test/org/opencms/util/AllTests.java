1d38d1423351a667e921684024c0ba502926e8fe
aKandzior <a.kandzior@alkacon.com>
2007-04-17 14:25:44 +0000

    Added missing test from util package

--- test/org/opencms/util/AllTests.java
+++ test/org/opencms/util/AllTests.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/test/org/opencms/util/AllTests.java,v $
- * Date   : $Date: 2006/07/19 14:58:44 $
- * Version: $Revision: 1.19.4.1 $
+ * Date   : $Date: 2007/04/17 14:25:44 $
+ * Version: $Revision: 1.19.4.2 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -40,7 +40,7 @@ import junit.framework.TestSuite;
  * Main test suite for the package <code>{@link org.opencms.util}</code>.<p>
  * 
  * @author Alexander Kandzior 
- * @version $Revision: 1.19.4.1 $
+ * @version $Revision: 1.19.4.2 $
  * 
  * @since 6.0
  */
@@ -70,6 +70,7 @@ public final class AllTests {
         suite.addTest(new TestSuite(TestCmsFileUtil.class));
         suite.addTest(new TestSuite(TestCmsHtml2TextConverter.class));
         suite.addTest(new TestSuite(TestCmsHtmlConverter.class));
+        suite.addTest(new TestSuite(TestCmsHtmlExtractor.class));
         suite.addTest(new TestSuite(TestCmsHtmlParser.class));
         suite.addTest(new TestSuite(TestCmsMacroResolver.class));
         suite.addTest(new TestSuite(TestCmsResourceTranslator.class));
