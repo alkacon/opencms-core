451d1844fa17a5048f6f6e9d1c20dd831c16c666
pBonrad <p.bonrad@alkacon.com>
2007-06-11 12:52:39 +0000

    Fixed bug not able to turn off clearing history of deleted resources

--- src-modules/org/opencms/workplace/tools/history/CmsHistoryClearThread.java
+++ src-modules/org/opencms/workplace/tools/history/CmsHistoryClearThread.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/workplace/tools/history/CmsHistoryClearThread.java,v $
- * Date   : $Date: 2007/06/11 10:06:32 $
- * Version: $Revision: 1.1.2.1 $
+ * Date   : $Date: 2007/06/11 12:52:39 $
+ * Version: $Revision: 1.1.2.2 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -41,7 +41,7 @@ import org.opencms.report.I_CmsReport;
  * 
  * @author  Andreas Zahner 
  * 
- * @version $Revision: 1.1.2.1 $ 
+ * @version $Revision: 1.1.2.2 $ 
  * 
  * @since 6.0.0 
  */
@@ -87,6 +87,10 @@ public class CmsHistoryClearThread extends A_CmsReportThread {
 
         if (timeDeleted == 0) {
             timeDeleted = -1;
+        }
+        
+        if (!m_historyClear.isClearDeleted()) {
+            versionsDeleted = -1;
         }
         
         // delete the historical files
