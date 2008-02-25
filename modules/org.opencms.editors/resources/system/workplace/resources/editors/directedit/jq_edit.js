59b080dd5d671b2dec65946af8303106d6519ea2
aRoettgers <a.roettgers@alkacon.com>
2008-02-25 11:18:54 +0000

    Added a new direct edit provider uses the JQuery Library

---
+++ modules/org.opencms.editors/resources/system/workplace/resources/editors/directedit/jq_edit.js
@@ -0,0 +1,218 @@
+/** 
+ * Creates the direct edit buttons dynamically and places them at the correct position.
+ * Additionally it activates the hover effects and actions.<p> 
+ *
+ * Version: 1.0
+ *
+ * Requires: 	jQuery 1.2.+
+ * 				jQuery Dimensions 1.2
+ *				jQuery FlyDOM 3.0.5
+ */
+
+
+/** Flag if the buttons should be shown or not. **/
+var ocms_de_visible = true;
+
+/** Contains all data for each direct edit button. **/  
+var ocms_de_data = new Array();
+
+/** For the hover effect. **/
+var ocms_de_mbo=3;
+
+/** Template to create the div and the form. **/   
+var t_ocms_de_template = function() {
+    return [
+    	'div', { className: 'ocms_de_bt', id: 'buttons_' + this.id }, [
+    		'form', {name: 'form_' + this.id, id: 'form_' + this.id, method: 'post', className: 'ocms_nomargin', target: '_top', action: this.editLink},[
+    			'input', { type: 'hidden', name: 'resource', NAME: 'resource', value: this.resource }, [],
+    			'input', { type: 'hidden', name: 'directedit', NAME: 'directedit', value: 'true' }, [],
+    			'input', { type: 'hidden', name: 'elementlanguage', NAME: 'elementlanguage', value: this.language }, [],
+    			'input', { type: 'hidden', name: 'elementname', NAME: 'elementname', value: this.element }, [],
+    			'input', { type: 'hidden', name: 'backlink', NAME: 'backlink', value: this.backlink }, [],
+    			'input', { type: 'hidden', name: 'newlink', NAME: 'newlink', value: this.newlink }, [],
+    			'input', { type: 'hidden', name: 'closelink', NAME: 'closelink', value: this.closelink }, [],
+    			'input', { type: 'hidden', name: 'deletelink', NAME: 'deletelink', value: this.deletelink }, [],
+    			'input', { type: 'hidden', name: 'redirect', NAME: 'redirect', value: 'true' }, [],
+    			'input', { type: 'hidden', name: 'editortitle', NAME: 'editortitle', value: this.editortitle }
+    		]
+	    ]
+    ];
+};
+
+/** Template to create each direct edit button. **/
+var t_ocms_de_buttonEnabled = function() {
+    return [
+    	'a', { href: '#'+ this.id, className:  this.typeClass,  title: this.title, rel: this.typeClass}
+    ];
+};
+/** Template to create a disabled direct edit button. **/
+var t_ocms_de_buttonDisabled = function() {
+    return [
+    	'span', { className:  this.typeClass +' '+ this.typeClass+'_disabled',  title: this.title}
+    ];
+};
+
+/** Template to create the hover effect for the direct edit buttons. **/
+var t_ocms_de_hoverEffect = function() {
+    return [
+    	'div', { className:  'directedit-highlight directedit-highlight-top',  		style: 'width: '+	this.width+'px; left: '	+(this.offset.left-ocms_de_mbo)+'px; top: '+			(this.offset.top-ocms_de_mbo)+'px;'}, [],
+    	'div', { className:  'directedit-highlight directedit-highlight-right',  	style: 'height: '+	this.height+'px; left: '+(this.offset.left+this.width-ocms_de_mbo)+'px; top: '+	this.offset.top+'px;'}, [],
+    	'div', { className:  'directedit-highlight directedit-highlight-bottom',  	style: 'width: '+	this.width+'px; left: '	+(this.offset.left-ocms_de_mbo)+'px; top: '+			(this.offset.top+this.height)+'px;'}, [],
+    	'div', { className:  'directedit-highlight directedit-highlight-left',  	style: 'height: '+	this.height+'px; left: '+(this.offset.left-ocms_de_mbo)+'px; top: '+			this.offset.top+'px;'}, [],
+    	'div', { className:  'directedit-highlight directedit-highlight-all',  		style: 'height: '+	this.height+'px; left: '+(this.offset.left-ocms_de_mbo)+'px; top: '+			this.offset.top+'px; width: '+	this.width+'px;'}
+    ];
+};
+
+$(document).ready(function(){
+	
+	/** Creates the structure with the form and divs. **/
+	$("div[id^='ocms_']").each(function() {
+		$(this).parent().tplAppend(ocms_de_data[this.id], t_ocms_de_template);
+		var btWidth=0;
+		var t_buttons=t_ocms_de_buttonDisabled;
+		if(!ocms_de_data[this.id].disabled){
+			t_buttons=t_ocms_de_buttonEnabled;
+		}
+		// Creates the buttons
+		if(ocms_de_data[this.id].hasEdit){
+			btWidth= btWidth + 20;
+			$('#buttons_'+ this.id).tplAppend({typeClass: 'ocms_de_edit', title: ocms_de_data[this.id].button_edit, id: this.id},t_buttons);
+			$('#buttons_'+ this.id).width(btWidth);
+		}
+		if(ocms_de_data[this.id].hasDelete){
+			btWidth= btWidth+ 20;
+			$('#buttons_'+ this.id).tplAppend({typeClass: 'ocms_de_delete', title: ocms_de_data[this.id].button_delete, id: this.id},t_buttons);
+			$('#buttons_'+ this.id).width(btWidth);
+		}
+		if(ocms_de_data[this.id].hasNew){
+			$('#buttons_'+ this.id).tplAppend({typeClass: 'ocms_de_new', title: ocms_de_data[this.id].button_new, id: this.id},t_buttons);
+			btWidth= btWidth+ 20;
+			$('#buttons_'+ this.id).width(btWidth);
+		}
+		
+	});
+	
+	/** For the first load set the buttons to the correct position. **/
+	$("div[id^='ocms_']").each(func_ocms_de_reposition);
+    
+    /** By the resize of the window set the buttons to the correct position. **/
+    $(window).resize(function(){
+        $("div[id^='ocms_']").each(func_ocms_de_reposition);
+    });
+    
+    /** If "[STRG] or [SHIFT] + [SPACE]" is pressed, then show or not the direct edit buttons. **/
+    $(document).keyup(function(event){
+        if ((event.keyCode == 32) && (event.ctrlKey || event.shiftKey)) {
+            if(ocms_de_visible) {
+               ocms_de_visible=false;
+               $("div[id^='buttons_ocms_']").css('visibility', "hidden");
+            } else {
+                ocms_de_visible=true;
+                $("div[id^='buttons_ocms_']").css('visibility', "visible");
+            }
+        }
+    });
+    
+    /** This sets the effects to the direct edit buttons. **/
+     $(".ocms_de_bt").hover(func_ocms_de_activate,func_ocms_de_deactivate);
+     
+     /** After click of the direct edit button the form is submitted. **/
+     $('a.ocms_de_edit, a.ocms_de_delete, a.ocms_de_new').click(func_ocms_de_submit).mouseover(function(){
+     		var atr=$(this).attr('rel');
+     		if(!$(this).hasClass(atr+'_hover')) {
+       			$(this).addClass(atr+'_hover');
+       		}
+     	}).mouseout(function(){
+     		var atr=$(this).attr('rel');
+     		if($(this).hasClass(atr+'_hover')) {
+       			$(this).removeClass(atr+'_hover');
+       		}
+     	});
+});
+
+
+/** Function to activate the hover effects. **/
+var func_ocms_de_activate = function() {
+	var id = this.id;
+    var type = id.substr(id.indexOf('ocms_'));
+	if(!$('#'+ type).hasClass('ocms_de_selected')) {
+		$('#'+ type).addClass('ocms_de_selected');
+        var pointTo={ offset: $('#'+ type).calcOcmsDeOffset(), width:  ($('#'+ type).width()+2*ocms_de_mbo), height: ($('#'+ type).height()+2*ocms_de_mbo)};
+       	$('#'+ type).parent().tplAppend(pointTo,t_ocms_de_hoverEffect);
+		$(".directedit-highlight-all").opacity(.1);
+	}
+};
+
+/** Function to deactivate the hover effects. **/
+var func_ocms_de_deactivate = function() {
+	var id = this.id;
+	var type = id.substr(id.indexOf('ocms_'));
+	if($('#'+ type).hasClass('ocms_de_selected')) {
+		$('#'+ type).removeClass('ocms_de_selected');
+		$('div.directedit-highlight').remove();
+		$('.directedit-highlight-all').remove();
+	}
+};
+
+/** Function to set the position of the buttons to the correct position. **/
+var func_ocms_de_reposition = function() {
+	var offset = $(this).calcOcmsDeOffset();
+    offset.left = offset.left + $(this).width() - $('#buttons_'+this.id).width();
+    $('#buttons_'+this.id).css(offset);
+    if(ocms_de_visible) {
+       $('#buttons_'+this.id).css('visibility', "visible");
+     } else {
+       $('#buttons_'+this.id).css('visibility', "hidden");
+     }
+};
+
+/** Function submit the form after clicking the direct edit button. **/
+var func_ocms_de_submit = function() {
+	var href = this.href;
+	var id = href.substr(href.indexOf('#')+1);
+
+	if($(this).hasClass('ocms_de_edit')){
+		$("#form_"+id +"> input[name='editortitle']").val('');
+		$("#form_"+id +"> input[name='newlink']").val('');
+		$('#form_'+id).submit();
+		return;
+	} else if($(this).hasClass('ocms_de_delete')){
+		$('#form_'+id).attr('action', $("#form_"+id +"> input[name='deletelink']").val());
+		$('#form_'+id).submit();
+		return;
+	} else if($(this).hasClass('ocms_de_new')){
+		$('#form_'+id).submit();
+		return;
+	}
+	alert("Unknown form action [" + id + "/ ]");
+};
+
+/** Function to calculate the offset or position of this element. **/
+$.fn.calcOcmsDeOffset = function() {		
+	if($.browser.msie && !jQuery.boxModel){
+		return $(this).offset();
+	}
+	return $(this).position();
+}
+
+/**
+ * Opacity function for jQuery
+ *
+ * @name   .opacity
+ * @cat    Plugins/Effects
+ * @author Woody Gilk/woody.gilk@gmail.com
+ *
+ * @example $(this).opacity(.2);
+ */
+$.fn.opacity = function(amount) {
+        if (amount > 1) amount = 1;
+        if (amount < 0) amount = 0;
+        if ($.browser.msie) {
+                amount = (parseFloat(amount) * 100);
+                this.css('filter', 'alpha(opacity='+amount+')');
+        } else {
+                this.css('opacity', amount);
+                this.css('-moz-opacity', amount);
+        }
+        return this;
+}
---
+++ src/org/opencms/workplace/editors/directedit/CmsDirectEditJQueryProvider.java
@@ -0,0 +1,174 @@
+/*
+ * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/workplace/editors/directedit/CmsDirectEditJQueryProvider.java,v $
+ * Date   : $Date: 2008/02/25 11:17:28 $
+ * Version: $Revision: 1.1 $
+ *
+ * This library is part of OpenCms -
+ * the Open Source Content Mananagement System
+ *
+ * Copyright (C) 2005 Alkacon Software GmbH (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software GmbH, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.opencms.workplace.editors.directedit;
+
+import org.opencms.file.CmsObject;
+import org.opencms.i18n.CmsEncoder;
+import org.opencms.util.CmsStringUtil;
+import org.opencms.workplace.editors.Messages;
+
+/**
+ * Provider for the OpenCms graphical "direct edit" buttons. <p>
+ * 
+ * Uses the JQuery library to create the buttons and place it to the correct position.<p>
+ * 
+ * This provider DOES NOT support {@link CmsDirectEditMode#MANUAL} mode.<p>
+ * 
+ * @author Anja Röttgers
+ * 
+ * @version $Revision: 1.1 $
+ * 
+ * @since 7.0.3
+ */
+public class CmsDirectEditJQueryProvider extends CmsDirectEditDefaultProvider {
+
+    /** Default direct edit include file URI for the jQuery direct edit provider. */
+    protected static final String INCLUDE_FILE_JQUERY = "/system/workplace/editors/jquery_direct_edit_include.txt";
+
+    /** Contains the close link.**/
+    private String m_closeLink;
+
+    /**
+     * 
+     * @see org.opencms.workplace.editors.directedit.CmsDirectEditDefaultProvider#getDirectEditIncludes(org.opencms.workplace.editors.directedit.CmsDirectEditParams)
+     */
+    public String getDirectEditIncludes(CmsDirectEditParams params) {
+
+        m_closeLink = getLink(params.getLinkForClose());
+        return super.getDirectEditIncludes(params);
+    }
+
+    /**
+     * @see org.opencms.workplace.editors.directedit.I_CmsDirectEditProvider#init(org.opencms.file.CmsObject, org.opencms.workplace.editors.directedit.CmsDirectEditMode, java.lang.String)
+     */
+    public void init(CmsObject cms, CmsDirectEditMode mode, String fileName) {
+
+        if (CmsStringUtil.isEmpty(fileName)) {
+            fileName = INCLUDE_FILE_JQUERY;
+        }
+        super.init(cms, mode, fileName);
+    }
+
+    /**
+     * @see org.opencms.workplace.editors.directedit.I_CmsDirectEditProvider#newInstance()
+     */
+    public I_CmsDirectEditProvider newInstance() {
+
+        CmsDirectEditJQueryProvider result = new CmsDirectEditJQueryProvider();
+        result.m_configurationParameters = m_configurationParameters;
+        return result;
+    }
+
+    /**
+     * 
+     * @see org.opencms.workplace.editors.directedit.CmsDirectEditDefaultProvider#startDirectEditDisabled(org.opencms.workplace.editors.directedit.CmsDirectEditParams, org.opencms.workplace.editors.directedit.CmsDirectEditResourceInfo)
+     */
+    public String startDirectEditDisabled(CmsDirectEditParams params, CmsDirectEditResourceInfo resourceInfo) {
+
+        return appendDirectEditData(params, false);
+    }
+
+    /**
+     * 
+     * @see org.opencms.workplace.editors.directedit.CmsDirectEditDefaultProvider#startDirectEditEnabled(org.opencms.workplace.editors.directedit.CmsDirectEditParams, org.opencms.workplace.editors.directedit.CmsDirectEditResourceInfo)
+     */
+    public String startDirectEditEnabled(CmsDirectEditParams params, CmsDirectEditResourceInfo resourceInfo) {
+
+        return appendDirectEditData(params, false);
+    }
+
+    /**
+     * Appends the data for the direct edit buttons, which are dynamically created with jQuery.<p>
+     * 
+     * Generates the following code:<p>
+     * <pre>
+     *  &#60;script type="text/javascript" &#62;
+     *      ocms_de_data['key']= {
+     *          id: key,
+     *          resource: res,
+     *          ...
+     *      };
+     *  &#60;/script &#62;
+     *  </pre>
+     * 
+     * @param params the direct edit parameters
+     * @param disabled if the buttons are disabled or not
+     * 
+     * @return the data needed for the direct edit buttons
+     */
+    private String appendDirectEditData(CmsDirectEditParams params, boolean disabled) {
+
+        StringBuffer result = new StringBuffer(512);
+        String editId = getNextDirectEditId();
+
+        result.append("\n<script type=\"text/javascript\">\n");
+
+        result.append("ocms_de_data['").append(editId).append("']= {\n");
+        result.append("\t").append("id: '").append(editId).append("',\n");
+        result.append("\t").append("deDisabled: ").append(disabled).append(",\n");
+        result.append("\t").append("hasEdit: ").append(params.getButtonSelection().isShowEdit()).append(",\n");
+        result.append("\t").append("hasDelete: ").append(params.getButtonSelection().isShowDelete()).append(",\n");
+        result.append("\t").append("hasNew: ").append(params.getButtonSelection().isShowNew()).append(",\n");
+        result.append("\t").append("resource: '").append(params.getResourceName()).append("',\n");
+        result.append("\t").append("editLink: '").append(getLink(params.getLinkForEdit())).append("',\n");
+        result.append("\t").append("language: '").append(m_cms.getRequestContext().getLocale().toString());
+        result.append("',\n");
+        result.append("\t").append("element: '").append(params.getElement()).append("',\n");
+        result.append("\t").append("backlink: '").append(m_cms.getRequestContext().getUri()).append("',\n");
+        result.append("\t").append("newlink: '").append(CmsEncoder.encode(params.getLinkForNew())).append("',\n");
+        result.append("\t").append("closelink: '").append(m_closeLink).append("',\n");
+        result.append("\t").append("deletelink: '").append(getLink(params.getLinkForDelete())).append("',\n");
+        if (!disabled) {
+            result.append("\t").append("button_edit: '");
+            result.append(m_messages.key(Messages.GUI_EDITOR_FRONTEND_BUTTON_EDIT_0)).append("',\n");
+            result.append("\t").append("button_delete: '");
+            result.append(m_messages.key(Messages.GUI_BUTTON_DELETE_0)).append("',\n");
+            result.append("\t").append("button_new: '");
+            result.append(m_messages.key(Messages.GUI_BUTTON_NEW_0)).append("',\n");
+        } else {
+            result.append("\t").append("button_edit: '");
+            result.append(m_messages.key(Messages.GUI_EDITOR_FRONTEND_BUTTON_LOCKED_0)).append("',\n");
+            result.append("\t").append("button_delete: '");
+            result.append(m_messages.key(Messages.GUI_EDITOR_FRONTEND_BUTTON_LOCKED_0)).append("',\n");
+            result.append("\t").append("button_new: '");
+            result.append(m_messages.key(Messages.GUI_EDITOR_FRONTEND_BUTTON_LOCKED_0)).append("',\n");
+        }
+        result.append("\t").append("editortitle: '").append(m_messages.key(Messages.GUI_EDITOR_TITLE_NEW_0));
+
+        result.append("'\n");
+        result.append("};\n");
+        result.append("</script>\n");
+
+        result.append("<div id=\"").append(editId).append("\" class=\"ocms_de_norm\">");
+        return result.toString();
+    }
+}
