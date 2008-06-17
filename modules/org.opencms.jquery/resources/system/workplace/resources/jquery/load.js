bb208f79f559ffc6980a05d908fe7cdb8fc22542
pBonrad <p.bonrad@alkacon.com>
2008-06-17 09:19:42 +0000

    Fixed issue with loading css stylesheets

--- modules/org.opencms.jquery/resources/system/workplace/resources/jquery/load.js
+++ modules/org.opencms.jquery/resources/system/workplace/resources/jquery/load.js
@@ -1,11 +1,18 @@
 function load_script(src, type) {
 	var document_scripts = document.getElementsByTagName(type == 'js' ? "script" : "link");
 	for (document_scripts_index = 0; document_scripts_index < document_scripts.length; ++document_scripts_index) {
-	  var document_script = document_scripts[document_scripts_index];
-	  if (document_script.src) {
-	  	var test = document_script.src;
-	  	if ((test.match(src+"$")==src) || (src.match(test.src+"$")==test)) return false;
-	  }
+		var document_script = document_scripts[document_scripts_index];
+		if (type=='js') {
+			if (document_script.src) {
+				var test = document_script.src;
+				if ((test.match(src+"$")==src) || (src.match(test+"$")==test)) return false;
+			}
+		} else {
+			if (document_script.href) {
+				var test = document_script.href;
+				if ((test.match(src+"$")==src) || (src.match(test+"$")==test)) return false;
+			}
+		}		
 	}
 	var script;
 	if (type=='js') {
