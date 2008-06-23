5d7196daed2373a40691a0a93bab93c4562708fd
mEmmerich <m.emmerich@alkacon.com>
2008-06-23 10:27:24 +0000

    Trailing "/" is handled correctly when reading resources in the VFSDriver.

--- src/org/opencms/db/generic/CmsVfsDriver.java
+++ src/org/opencms/db/generic/CmsVfsDriver.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/db/generic/CmsVfsDriver.java,v $
- * Date   : $Date: 2006/04/28 15:20:57 $
- * Version: $Revision: 1.261 $
+ * Date   : $Date: 2008/06/23 10:27:24 $
+ * Version: $Revision: 1.261.2.1 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Mananagement System
@@ -76,7 +76,7 @@ import org.apache.commons.logging.Log;
  * @author Thomas Weckert 
  * @author Michael Emmerich 
  * 
- * @version $Revision: 1.261 $
+ * @version $Revision: 1.261.2.1 $
  * 
  * @since 6.0.0 
  */
@@ -1417,6 +1417,8 @@ public class CmsVfsDriver implements I_CmsDriver, I_CmsVfsDriver {
 
         // must remove trailing slash
         path = removeTrailingSeparator(path);
+        
+        boolean endsWithSlash = path.endsWith("/");
 
         try {
             conn = m_sqlManager.getConnection(dbc, projectId);
@@ -1424,9 +1426,17 @@ public class CmsVfsDriver implements I_CmsDriver, I_CmsVfsDriver {
 
             stmt.setString(1, path);
             res = stmt.executeQuery();
-
+           
             if (res.next()) {
                 resource = createResource(res, projectId);
+                
+                // check if the resource is a file, it is not allowed to end with a "/" then
+                if (endsWithSlash && resource.isFile()) {
+                    throw new CmsVfsResourceNotFoundException(Messages.get().container(
+                        Messages.ERR_READ_RESOURCE_1,
+                        dbc.removeSiteRoot(path+"/")));
+                }
+                
                 while (res.next()) {
                     // do nothing only move through all rows because of mssql odbc driver
                 }
