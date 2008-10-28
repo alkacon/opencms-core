87cb5abde2d2381c565c0deeb7245fc575050685
cWeinholz <c.weinholz@alkacon.com>
2008-10-28 10:33:58 +0000

    Added interface for protected principal login method

---
+++ src/org/opencms/security/I_CmsLoginAction.java
@@ -0,0 +1,48 @@
+/*
+ * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/security/I_CmsLoginAction.java,v $
+ * Date   : $Date: 2008/10/28 10:33:58 $
+ * Version: $Revision: 1.1 $
+ *
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
+ *
+ * Copyright (C) 2002 - 2008 Alkacon Software (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
+package org.opencms.security;
+
+import org.opencms.file.CmsObject;
+
+/**
+ * Interface used to provide a protected login method.<p> 
+ */
+public interface I_CmsLoginAction {
+    
+    /**
+     * Returns a cms object initialized for the passed principal.<p>
+     * 
+     * @param principal the principal (i.e. name of a user)
+     * @return a cms object or <code>null</code> if the cms object could not be initialized.
+     */
+    CmsObject doLogin(String principal);
+}
