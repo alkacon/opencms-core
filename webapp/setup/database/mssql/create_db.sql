388f514f009f68774bd503085b7f540d8e6df8bd
aWestermann <a.westermann@alkacon.com>
2009-06-08 12:00:57 +0000

    - Enabled mssql to use committed read with  snapshots instead of committed read by locking.

--- webapp/setup/database/mssql/create_db.sql
+++ webapp/setup/database/mssql/create_db.sql
@@ -3,3 +3,7 @@
 ############################
 
 CREATE DATABASE ${database} COLLATE SQL_Latin1_General_CP1_CS_AS;
+
+# Prevents deadlocks (as default transaction management is based on locking Version 2005)
+ALTER DATABASE ${database} SET ALLOW_SNAPSHOT_ISOLATION ON
+ALTER DATABASE ${database} SET READ_COMMITTED_SNAPSHOT ON
