3ea25b2e917d51592dcf25c56bb857ab2a713e7a
mMoossen <m.moossen@alkacon.com>
2009-11-12 14:06:40 +0000

    fixed exception

--- src/org/opencms/db/generic/CmsVfsDriver.java
+++ src/org/opencms/db/generic/CmsVfsDriver.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src/org/opencms/db/generic/CmsVfsDriver.java,v $
- * Date   : $Date: 2009/10/12 08:11:59 $
- * Version: $Revision: 1.285.2.1 $
+ * Date   : $Date: 2009/11/12 14:06:40 $
+ * Version: $Revision: 1.3 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Management System
@@ -89,7 +89,7 @@ import org.apache.commons.logging.Log;
  * @author Thomas Weckert 
  * @author Michael Emmerich 
  * 
- * @version $Revision: 1.285.2.1 $
+ * @version $Revision: 1.3 $
  * 
  * @since 6.0.0 
  */
@@ -1903,7 +1903,7 @@ public class CmsVfsDriver implements I_CmsDriver, I_CmsVfsDriver {
 
         // check if this resource is marked as deleted and if we are allowed to return a deleted resource
         if ((resource != null) && resource.getState().isDeleted() && !includeDeleted) {
-            throw new CmsVfsException(Messages.get().container(
+            throw new CmsVfsResourceNotFoundException(Messages.get().container(
                 Messages.ERR_READ_DELETED_RESOURCE_1,
                 dbc.removeSiteRoot(resource.getRootPath())));
         }
