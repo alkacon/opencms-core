d0032b565f3ecc8f55f8dc5986a1e97f3f8dbd5a
gWestenberger <g.westenberger@alkacon.com>
2010-03-09 07:39:05 +0000

    - input widget demo classes

---
+++ src-modules/org/opencms/gwt/client/ui/input/demo/client/CmsInputDemo.java
@@ -0,0 +1,149 @@
+/*
+ * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/gwt/client/ui/input/demo/client/Attic/CmsInputDemo.java,v $
+ * Date   : $Date: 2010/03/09 07:39:05 $
+ * Version: $Revision: 1.1 $
+ *
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
+ *
+ * Copyright (C) 2002 - 2009 Alkacon Software (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.opencms.gwt.client.ui.input.demo.client;
+
+import org.opencms.gwt.client.ui.input.CmsMultiCheckBox;
+import org.opencms.gwt.client.ui.input.CmsRadioButtonGroup;
+import org.opencms.gwt.client.ui.input.CmsSelectBox;
+import org.opencms.gwt.client.ui.input.CmsTextArea;
+import org.opencms.gwt.client.ui.input.CmsTextBox;
+import org.opencms.gwt.client.ui.input.I_CmsLayoutBundle;
+import org.opencms.gwt.client.util.CmsPair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Position;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+
+/**
+ * Demonstration class for the input package.<p> 
+ */
+public class CmsInputDemo extends Composite { 
+
+    /**
+     * Constructs a new instance.<p>
+     */
+    public CmsInputDemo() {
+
+        super();
+
+        FlowPanel panel = new FlowPanel();
+        initWidget(panel);
+
+        I_CmsLayoutBundle.INSTANCE.inputCss().ensureInjected();
+        Style style = panel.getElement().getStyle();
+        style.setLeft(140, Unit.PX);
+        style.setTop(140, Unit.PX);
+        style.setPosition(Position.ABSOLUTE);
+
+        List<CmsPair<String, String>> items = new ArrayList<CmsPair<String, String>>();
+        items.add(new CmsPair<String, String>("foo", "First item - foo"));
+        items.add(new CmsPair<String, String>("bar", "Second item - bar"));
+        items.add(new CmsPair<String, String>("baz", "Third item - baz"));
+        final CmsSelectBox selectbox = new CmsSelectBox(CmsSelectBox.Mode.TEXT, items);
+        panel.add(selectbox);
+        Button button = new Button("show value");
+        panel.add(button);
+        button.addClickHandler(new ClickHandler() {
+
+            public void onClick(ClickEvent e) {
+
+                Window.alert("value = " + selectbox.getFormValue());
+
+            }
+        });
+
+        final CmsMultiCheckBox multiBox = new CmsMultiCheckBox(items);
+        panel.add(multiBox);
+        Button button2 = new Button("show value");
+        panel.add(button2);
+        button2.addClickHandler(new ClickHandler() {
+
+            public void onClick(ClickEvent e) {
+
+                String selected = "";
+                for (String item : multiBox.getSelected()) {
+                    selected += "|" + item;
+
+                }
+                Window.alert("value = " + selected);
+            }
+        });
+
+        final CmsRadioButtonGroup radioGroup = new CmsRadioButtonGroup(items);
+        Button button3 = new Button("show value");
+        panel.add(button3);
+        button3.addClickHandler(new ClickHandler() {
+
+            public void onClick(ClickEvent e) {
+
+                Window.alert("value = " + radioGroup.getFormValue());
+
+            }
+        });
+
+        panel.add(radioGroup);
+        panel.add(button3);
+        final CmsTextBox text = new CmsTextBox();
+        panel.add(text);
+        Button button4 = new Button("show value");
+        panel.add(button4);
+        button4.addClickHandler(new ClickHandler() {
+
+            public void onClick(ClickEvent e) {
+
+                Window.alert("value = " + text.getFormValue());
+            }
+        });
+
+        final CmsTextArea text2 = new CmsTextArea();
+        panel.add(text2);
+        Button button5 = new Button("show value");
+        panel.add(button5);
+        button5.addClickHandler(new ClickHandler() {
+
+            public void onClick(ClickEvent e) {
+
+                Window.alert("value = " + text2.getFormValue());
+            }
+        });
+
+    }
+}
