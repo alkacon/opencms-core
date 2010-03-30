b4c7143e097992e51c3a2ee2fed71ca98bc89e36
tHerrmann <t.herrmann@alkacon.com>
2010-03-30 07:49:06 +0000

    Improved style injection.

--- src-modules/org/opencms/gwt/client/ui/CmsImageButton.java
+++ src-modules/org/opencms/gwt/client/ui/CmsImageButton.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/gwt/client/ui/Attic/CmsImageButton.java,v $
- * Date   : $Date: 2010/03/04 15:17:19 $
- * Version: $Revision: 1.2 $
+ * Date   : $Date: 2010/03/30 07:49:06 $
+ * Version: $Revision: 1.3 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Management System
@@ -38,7 +38,7 @@ import org.opencms.gwt.client.ui.css.I_CmsLayoutBundle;
  * 
  * @author Tobias Herrmann
  * 
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  * 
  * @since 8.0.0
  */
@@ -49,291 +49,99 @@ public class CmsImageButton extends CmsButton {
      */
     public static enum ICON {
         /**  Icon name. */
-        carat_1_n,
+        alert,
         /**  Icon name. */
-        carat_1_ne,
-        /**  Icon name. */
-        carat_1_e,
-        /**  Icon name. */
-        carat_1_se,
-        /**  Icon name. */
-        carat_1_s,
-        /**  Icon name. */
-        carat_1_sw,
-        /**  Icon name. */
-        carat_1_w,
-        /**  Icon name. */
-        carat_1_nw,
-        /**  Icon name. */
-        carat_2_n_s,
-        /**  Icon name. */
-        carat_2_e_w,
-        /**  Icon name. */
-        triangle_1_n,
-        /**  Icon name. */
-        triangle_1_ne,
-        /**  Icon name. */
-        triangle_1_e,
-        /**  Icon name. */
-        triangle_1_se,
-        /**  Icon name. */
-        triangle_1_s,
-        /**  Icon name. */
-        triangle_1_sw,
-        /**  Icon name. */
-        triangle_1_w,
-        /**  Icon name. */
-        triangle_1_nw,
-        /**  Icon name. */
-        triangle_2_n_s,
-        /**  Icon name. */
-        triangle_2_e_w,
+        arrow_1_e,
         /**  Icon name. */
         arrow_1_n,
         /**  Icon name. */
         arrow_1_ne,
         /**  Icon name. */
-        arrow_1_e,
-        /**  Icon name. */
-        arrow_1_se,
+        arrow_1_nw,
         /**  Icon name. */
         arrow_1_s,
+        /**  Icon name. */
+        arrow_1_se,
         /**  Icon name. */
         arrow_1_sw,
         /**  Icon name. */
         arrow_1_w,
         /**  Icon name. */
-        arrow_1_nw,
+        arrow_2_e_w,
         /**  Icon name. */
         arrow_2_n_s,
         /**  Icon name. */
         arrow_2_ne_sw,
         /**  Icon name. */
-        arrow_2_e_w,
-        /**  Icon name. */
         arrow_2_se_nw,
-        /**  Icon name. */
-        arrowstop_1_n,
-        /**  Icon name. */
-        arrowstop_1_e,
-        /**  Icon name. */
-        arrowstop_1_s,
-        /**  Icon name. */
-        arrowstop_1_w,
-        /**  Icon name. */
-        arrowthick_1_n,
-        /**  Icon name. */
-        arrowthick_1_ne,
-        /**  Icon name. */
-        arrowthick_1_e,
-        /**  Icon name. */
-        arrowthick_1_se,
-        /**  Icon name. */
-        arrowthick_1_s,
-        /**  Icon name. */
-        arrowthick_1_sw,
-        /**  Icon name. */
-        arrowthick_1_w,
-        /**  Icon name. */
-        arrowthick_1_nw,
-        /**  Icon name. */
-        arrowthick_2_n_s,
-        /**  Icon name. */
-        arrowthick_2_ne_sw,
-        /**  Icon name. */
-        arrowthick_2_e_w,
-        /**  Icon name. */
-        arrowthick_2_se_nw,
-        /**  Icon name. */
-        arrowthickstop_1_n,
-        /**  Icon name. */
-        arrowthickstop_1_e,
-        /**  Icon name. */
-        arrowthickstop_1_s,
-        /**  Icon name. */
-        arrowthickstop_1_w,
-        /**  Icon name. */
-        arrowreturnthick_1_w,
-        /**  Icon name. */
-        arrowreturnthick_1_n,
-        /**  Icon name. */
-        arrowreturnthick_1_e,
-        /**  Icon name. */
-        arrowreturnthick_1_s,
-        /**  Icon name. */
-        arrowreturn_1_w,
-        /**  Icon name. */
-        arrowreturn_1_n,
-        /**  Icon name. */
-        arrowreturn_1_e,
-        /**  Icon name. */
-        arrowreturn_1_s,
-        /**  Icon name. */
-        arrowrefresh_1_w,
-        /**  Icon name. */
-        arrowrefresh_1_n,
-        /**  Icon name. */
-        arrowrefresh_1_e,
-        /**  Icon name. */
-        arrowrefresh_1_s,
         /**  Icon name. */
         arrow_4,
         /**  Icon name. */
         arrow_4_diag,
         /**  Icon name. */
-        extlink,
+        arrowrefresh_1_e,
         /**  Icon name. */
-        newwin,
+        arrowrefresh_1_n,
         /**  Icon name. */
-        refresh,
+        arrowrefresh_1_s,
         /**  Icon name. */
-        shuffle,
+        arrowrefresh_1_w,
         /**  Icon name. */
-        transfer_e_w,
+        arrowreturn_1_e,
         /**  Icon name. */
-        transferthick_e_w,
+        arrowreturn_1_n,
         /**  Icon name. */
-        folder_collapsed,
+        arrowreturn_1_s,
         /**  Icon name. */
-        folder_open,
+        arrowreturn_1_w,
         /**  Icon name. */
-        document,
+        arrowreturnthick_1_e,
         /**  Icon name. */
-        document_b,
+        arrowreturnthick_1_n,
         /**  Icon name. */
-        note,
+        arrowreturnthick_1_s,
         /**  Icon name. */
-        mail_closed,
+        arrowreturnthick_1_w,
         /**  Icon name. */
-        mail_open,
+        arrowstop_1_e,
         /**  Icon name. */
-        suitcase,
+        arrowstop_1_n,
         /**  Icon name. */
-        comment,
+        arrowstop_1_s,
         /**  Icon name. */
-        person,
+        arrowstop_1_w,
         /**  Icon name. */
-        print,
+        arrowthick_1_e,
         /**  Icon name. */
-        trash,
+        arrowthick_1_n,
         /**  Icon name. */
-        locked,
+        arrowthick_1_ne,
         /**  Icon name. */
-        unlocked,
+        arrowthick_1_nw,
         /**  Icon name. */
-        bookmark,
+        arrowthick_1_s,
         /**  Icon name. */
-        tag,
+        arrowthick_1_se,
         /**  Icon name. */
-        home,
+        arrowthick_1_sw,
         /**  Icon name. */
-        flag,
+        arrowthick_1_w,
         /**  Icon name. */
-        calendar,
+        arrowthick_2_e_w,
         /**  Icon name. */
-        cart,
+        arrowthick_2_n_s,
         /**  Icon name. */
-        pencil,
+        arrowthick_2_ne_sw,
         /**  Icon name. */
-        clock,
+        arrowthick_2_se_nw,
         /**  Icon name. */
-        disk,
+        arrowthickstop_1_e,
         /**  Icon name. */
-        calculator,
+        arrowthickstop_1_n,
         /**  Icon name. */
-        zoomin,
+        arrowthickstop_1_s,
         /**  Icon name. */
-        zoomout,
-        /**  Icon name. */
-        search,
-        /**  Icon name. */
-        wrench,
-        /**  Icon name. */
-        gear,
-        /**  Icon name. */
-        heart,
-        /**  Icon name. */
-        star,
-        /**  Icon name. */
-        link,
-        /**  Icon name. */
-        cancel,
-        /**  Icon name. */
-        plus,
-        /**  Icon name. */
-        plusthick,
-        /**  Icon name. */
-        minus,
-        /**  Icon name. */
-        minusthick,
-        /**  Icon name. */
-        close,
-        /**  Icon name. */
-        closethick,
-        /**  Icon name. */
-        key,
-        /**  Icon name. */
-        lightbulb,
-        /**  Icon name. */
-        scissors,
-        /**  Icon name. */
-        clipboard,
-        /**  Icon name. */
-        copy,
-        /**  Icon name. */
-        contact,
-        /**  Icon name. */
-        image,
-        /**  Icon name. */
-        video,
-        /**  Icon name. */
-        script,
-        /**  Icon name. */
-        alert,
-        /**  Icon name. */
-        info,
-        /**  Icon name. */
-        notice,
-        /**  Icon name. */
-        help,
-        /**  Icon name. */
-        check,
-        /**  Icon name. */
-        bullet,
-        /**  Icon name. */
-        radio_off,
-        /**  Icon name. */
-        radio_on,
-        /**  Icon name. */
-        pin_w,
-        /**  Icon name. */
-        pin_s,
-        /**  Icon name. */
-        play,
-        /**  Icon name. */
-        pause,
-        /**  Icon name. */
-        seek_next,
-        /**  Icon name. */
-        seek_prev,
-        /**  Icon name. */
-        seek_end,
-        /**  Icon name. */
-        seek_first,
-        /**  Icon name. */
-        stop,
-        /**  Icon name. */
-        eject,
-        /**  Icon name. */
-        volume_off,
-        /**  Icon name. */
-        volume_on,
-        /**  Icon name. */
-        power,
-        /**  Icon name. */
-        signal_diag,
-        /**  Icon name. */
-        signal,
+        arrowthickstop_1_w,
         /**  Icon name. */
         battery_0,
         /**  Icon name. */
@@ -343,57 +151,275 @@ public class CmsImageButton extends CmsButton {
         /**  Icon name. */
         battery_3,
         /**  Icon name. */
-        circle_plus,
+        bookmark,
         /**  Icon name. */
-        circle_minus,
+        bullet,
         /**  Icon name. */
-        circle_close,
+        calculator,
         /**  Icon name. */
-        circle_triangle_e,
+        calendar,
         /**  Icon name. */
-        circle_triangle_s,
+        cancel,
         /**  Icon name. */
-        circle_triangle_w,
+        carat_1_e,
         /**  Icon name. */
-        circle_triangle_n,
+        carat_1_n,
+        /**  Icon name. */
+        carat_1_ne,
+        /**  Icon name. */
+        carat_1_nw,
+        /**  Icon name. */
+        carat_1_s,
+        /**  Icon name. */
+        carat_1_se,
+        /**  Icon name. */
+        carat_1_sw,
+        /**  Icon name. */
+        carat_1_w,
+        /**  Icon name. */
+        carat_2_e_w,
+        /**  Icon name. */
+        carat_2_n_s,
+        /**  Icon name. */
+        cart,
+        /**  Icon name. */
+        check,
         /**  Icon name. */
         circle_arrow_e,
+        /**  Icon name. */
+        circle_arrow_n,
         /**  Icon name. */
         circle_arrow_s,
         /**  Icon name. */
         circle_arrow_w,
         /**  Icon name. */
-        circle_arrow_n,
+        circle_check,
+        /**  Icon name. */
+        circle_close,
+        /**  Icon name. */
+        circle_minus,
+        /**  Icon name. */
+        circle_plus,
+        /**  Icon name. */
+        circle_triangle_e,
+        /**  Icon name. */
+        circle_triangle_n,
+        /**  Icon name. */
+        circle_triangle_s,
+        /**  Icon name. */
+        circle_triangle_w,
         /**  Icon name. */
         circle_zoomin,
         /**  Icon name. */
         circle_zoomout,
         /**  Icon name. */
-        circle_check,
-        /**  Icon name. */
-        circlesmall_plus,
+        circlesmall_close,
         /**  Icon name. */
         circlesmall_minus,
         /**  Icon name. */
-        circlesmall_close,
+        circlesmall_plus,
         /**  Icon name. */
-        squaresmall_plus,
+        clipboard,
         /**  Icon name. */
-        squaresmall_minus,
+        clock,
         /**  Icon name. */
-        squaresmall_close,
+        close,
         /**  Icon name. */
-        grip_dotted_vertical,
+        closethick,
+        /**  Icon name. */
+        comment,
+        /**  Icon name. */
+        contact,
+        /**  Icon name. */
+        copy,
+        /**  Icon name. */
+        disk,
+        /**  Icon name. */
+        document,
+        /**  Icon name. */
+        document_b,
+        /**  Icon name. */
+        eject,
+        /**  Icon name. */
+        extlink,
+        /**  Icon name. */
+        flag,
+        /**  Icon name. */
+        folder_collapsed,
+        /**  Icon name. */
+        folder_open,
+        /**  Icon name. */
+        gear,
+        /**  Icon name. */
+        grip_diagonal_se,
         /**  Icon name. */
         grip_dotted_horizontal,
         /**  Icon name. */
-        grip_solid_vertical,
+        grip_dotted_vertical,
         /**  Icon name. */
         grip_solid_horizontal,
         /**  Icon name. */
+        grip_solid_vertical,
+        /**  Icon name. */
         gripsmall_diagonal_se,
         /**  Icon name. */
-        grip_diagonal_se;
+        heart,
+        /**  Icon name. */
+        help,
+        /**  Icon name. */
+        home,
+        /**  Icon name. */
+        image,
+        /**  Icon name. */
+        info,
+        /**  Icon name. */
+        key,
+        /**  Icon name. */
+        lightbulb,
+        /**  Icon name. */
+        link,
+        /**  Icon name. */
+        locked,
+        /**  Icon name. */
+        mail_closed,
+        /**  Icon name. */
+        mail_open,
+        /**  Icon name. */
+        minus,
+        /**  Icon name. */
+        minusthick,
+        /**  Icon name. */
+        newwin,
+        /**  Icon name. */
+        note,
+        /**  Icon name. */
+        notice,
+        /**  Icon name. */
+        pause,
+        /**  Icon name. */
+        pencil,
+        /**  Icon name. */
+        person,
+        /**  Icon name. */
+        pin_s,
+        /**  Icon name. */
+        pin_w,
+        /**  Icon name. */
+        play,
+        /**  Icon name. */
+        plus,
+        /**  Icon name. */
+        plusthick,
+        /**  Icon name. */
+        power,
+        /**  Icon name. */
+        print,
+        /**  Icon name. */
+        radio_off,
+        /**  Icon name. */
+        radio_on,
+        /**  Icon name. */
+        refresh,
+        /**  Icon name. */
+        scissors,
+        /**  Icon name. */
+        script,
+        /**  Icon name. */
+        search,
+        /**  Icon name. */
+        seek_end,
+        /**  Icon name. */
+        seek_first,
+        /**  Icon name. */
+        seek_next,
+        /**  Icon name. */
+        seek_prev,
+        /**  Icon name. */
+        shuffle,
+        /**  Icon name. */
+        signal,
+        /**  Icon name. */
+        signal_diag,
+        /**  Icon name. */
+        squaresmall_close,
+        /**  Icon name. */
+        squaresmall_minus,
+        /**  Icon name. */
+        squaresmall_plus,
+        /**  Icon name. */
+        star,
+        /**  Icon name. */
+        stop,
+        /**  Icon name. */
+        suitcase,
+        /**  Icon name. */
+        tag,
+        /**  Icon name. */
+        transfer_e_w,
+        /**  Icon name. */
+        transferthick_e_w,
+        /**  Icon name. */
+        trash,
+        /**  Icon name. */
+        triangle_1_e,
+        /**  Icon name. */
+        triangle_1_n,
+        /**  Icon name. */
+        triangle_1_ne,
+        /**  Icon name. */
+        triangle_1_nw,
+        /**  Icon name. */
+        triangle_1_s,
+        /**  Icon name. */
+        triangle_1_se,
+        /**  Icon name. */
+        triangle_1_sw,
+        /**  Icon name. */
+        triangle_1_w,
+        /**  Icon name. */
+        triangle_2_e_w,
+        /**  Icon name. */
+        triangle_2_n_s,
+        /**  Icon name. */
+        unlocked,
+        /**  Icon name. */
+        video,
+        /**  Icon name. */
+        volume_off,
+        /**  Icon name. */
+        volume_on,
+        /**  Icon name. */
+        wrench,
+        /**  Icon name. */
+        zoomin,
+        /**  Icon name. */
+        zoomout;
+    }
+
+    /**
+     * Constructor takes the icon and if to show border and background.<p> 
+     * 
+     * @param upIcon up-face icon
+     * @param hasBorder set <code>true</code> to show border and background
+     */
+    public CmsImageButton(ICON upIcon, boolean hasBorder) {
+
+        this(I_CmsLayoutBundle.INSTANCE.iconsCss().uiIcon() + " " + upIcon.name(), hasBorder);
+    }
+
+    /**
+     * Constructor takes the icons and if to show border and background.<p> 
+     * 
+     * @param upIcon up-face icon
+     * @param downIcon down-face icon
+     * @param hasBorder set <code>true</code> to show border and background
+     */
+    public CmsImageButton(ICON upIcon, ICON downIcon, boolean hasBorder) {
+
+        this(
+            I_CmsLayoutBundle.INSTANCE.iconsCss().uiIcon() + " " + upIcon.name(),
+            I_CmsLayoutBundle.INSTANCE.iconsCss().uiIcon() + " " + downIcon.name(),
+            hasBorder);
     }
 
     /**
@@ -412,7 +438,6 @@ public class CmsImageButton extends CmsButton {
         }
         String upFaceHtml = "<div class='" + upImageClass + "'></div>";
         this.getUpFace().setHTML(upFaceHtml);
-        I_CmsLayoutBundle.INSTANCE.buttonCss().ensureInjected();
     }
 
     /**
@@ -429,31 +454,7 @@ public class CmsImageButton extends CmsButton {
         getDownFace().setHTML(downFaceHtml);
     }
 
-    /**
-     * Constructor takes the icon and if to show border and background.<p> 
-     * 
-     * @param upIcon up-face icon
-     * @param hasBorder set <code>true</code> to show border and background
-     */
-    public CmsImageButton(ICON upIcon, boolean hasBorder) {
-
-        this(I_CmsLayoutBundle.INSTANCE.iconsCss().uiIcon() + " " + upIcon.name(), hasBorder);
-        I_CmsLayoutBundle.INSTANCE.iconsCss().ensureInjected();
-    }
-
-    /**
-     * Constructor takes the icons and if to show border and background.<p> 
-     * 
-     * @param upIcon up-face icon
-     * @param downIcon down-face icon
-     * @param hasBorder set <code>true</code> to show border and background
-     */
-    public CmsImageButton(ICON upIcon, ICON downIcon, boolean hasBorder) {
-
-        this(
-            I_CmsLayoutBundle.INSTANCE.iconsCss().uiIcon() + " " + upIcon.name(),
-            I_CmsLayoutBundle.INSTANCE.iconsCss().uiIcon() + " " + downIcon.name(),
-            hasBorder);
-        I_CmsLayoutBundle.INSTANCE.iconsCss().ensureInjected();
+    static {
+        I_CmsLayoutBundle.INSTANCE.buttonCss().ensureInjected();
     }
 }
