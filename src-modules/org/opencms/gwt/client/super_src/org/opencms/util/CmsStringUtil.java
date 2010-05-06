b6d21c4e8fde8168c6f05931305d037390b77628
gWestenberger <g.westenberger@alkacon.com>
2010-05-06 07:22:11 +0000

    - added more methods to CmsStringUtil

--- src-modules/org/opencms/gwt/client/super_src/org/opencms/util/CmsStringUtil.java
+++ src-modules/org/opencms/gwt/client/super_src/org/opencms/util/CmsStringUtil.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/gwt/client/super_src/org/opencms/util/Attic/CmsStringUtil.java,v $
- * Date   : $Date: 2010/04/19 11:47:45 $
- * Version: $Revision: 1.1 $
+ * Date   : $Date: 2010/05/06 07:22:11 $
+ * Version: $Revision: 1.2 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Management System
@@ -31,12 +31,18 @@
 
 package org.opencms.util;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Client side implementation for {@link org.opencms.util.CmsStringUtil}.<p>
  * 
  * @author Michael Moossen 
  * 
- * @version $Revision: 1.1 $ 
+ * @version $Revision: 1.2 $ 
  * 
  * @since 8.0.0
  * 
@@ -161,6 +167,28 @@ public final class CmsStringUtil {
     }
 
     /**
+     * Returns a string representation for the given list using the given separator.<p>
+     * 
+     * @param <E> type of list entries
+     * @param list the list to write
+     * @param separator the item separator string
+     * 
+     * @return the string representation for the given map
+     */
+    public static <E> String listAsString(List<E> list, String separator) {
+
+        StringBuffer string = new StringBuffer(128);
+        Iterator<E> it = list.iterator();
+        while (it.hasNext()) {
+            string.append(it.next());
+            if (it.hasNext()) {
+                string.append(separator);
+            }
+        }
+        return string.toString();
+    }
+
+    /**
      * Same as {@link org.opencms.util.CmsStringUtil#splitAsArray(String, String)}.<p>
      * 
      * @param str the string to split
@@ -171,5 +199,125 @@ public final class CmsStringUtil {
     public static String[] splitAsArray(String str, String splitter) {
 
         return str.split(splitter);
+    }
+
+    /**
+     * Splits a String into substrings along the provided char delimiter and returns
+     * the result as a List of Substrings.<p>
+     *
+     * @param source the String to split
+     * @param delimiter the delimiter to split at
+     * @param trim flag to indicate if leading and trailing white spaces should be omitted
+     *
+     * @return the List of splitted Substrings
+     */
+    public static List<String> splitAsList(String source, char delimiter, boolean trim) {
+
+        List<String> result = new ArrayList<String>();
+        int i = 0;
+        int l = source.length();
+        int n = source.indexOf(delimiter);
+        while (n != -1) {
+            // zero - length items are not seen as tokens at start or end
+            if ((i < n) || ((i > 0) && (i < l))) {
+                result.add(trim ? source.substring(i, n).trim() : source.substring(i, n));
+            }
+            i = n + 1;
+            n = source.indexOf(delimiter, i);
+        }
+        // is there a non - empty String to cut from the tail? 
+        if (n < 0) {
+            n = source.length();
+        }
+        if (i < n) {
+            result.add(trim ? source.substring(i).trim() : source.substring(i));
+        }
+        return result;
+    }
+
+    /**
+     * Splits a String into substrings along the provided String delimiter and returns
+     * the result as List of Substrings.<p>
+     *
+     * @param source the String to split
+     * @param delimiter the delimiter to split at
+     *
+     * @return the Array of splitted Substrings
+     */
+    public static List<String> splitAsList(String source, String delimiter) {
+
+        return splitAsList(source, delimiter, false);
+    }
+
+    /**
+     * Splits a String into substrings along the provided String delimiter and returns
+     * the result as List of Substrings.<p>
+     * 
+     * @param source the String to split
+     * @param delimiter the delimiter to split at
+     * @param trim flag to indicate if leading and trailing white spaces should be omitted
+     * 
+     * @return the Array of splitted Substrings
+     */
+    public static List<String> splitAsList(String source, String delimiter, boolean trim) {
+
+        int dl = delimiter.length();
+        if (dl == 1) {
+            // optimize for short strings
+            return splitAsList(source, delimiter.charAt(0), trim);
+        }
+
+        List<String> result = new ArrayList<String>();
+        int i = 0;
+        int l = source.length();
+        int n = source.indexOf(delimiter);
+        while (n != -1) {
+            // zero - length items are not seen as tokens at start or end:  ",," is one empty token but not three
+            if ((i < n) || ((i > 0) && (i < l))) {
+                result.add(trim ? source.substring(i, n).trim() : source.substring(i, n));
+            }
+            i = n + dl;
+            n = source.indexOf(delimiter, i);
+        }
+        // is there a non - empty String to cut from the tail? 
+        if (n < 0) {
+            n = source.length();
+        }
+        if (i < n) {
+            result.add(trim ? source.substring(i).trim() : source.substring(i));
+        }
+        return result;
+    }
+
+    /**
+     * Splits a String into substrings along the provided <code>paramDelim</code> delimiter,
+     * then each substring is treat as a key-value pair delimited by <code>keyValDelim</code>.<p>
+     * 
+     * @param source the string to split
+     * @param paramDelim the string to delimit each key-value pair
+     * @param keyValDelim the string to delimit key and value
+     * 
+     * @return a map of splitted key-value pairs
+     */
+    public static Map<String, String> splitAsMap(String source, String paramDelim, String keyValDelim) {
+
+        int keyValLen = keyValDelim.length();
+        // use LinkedHashMap to preserve the order of items 
+        Map<String, String> params = new LinkedHashMap<String, String>();
+        Iterator<String> itParams = CmsStringUtil.splitAsList(source, paramDelim, true).iterator();
+        while (itParams.hasNext()) {
+            String param = itParams.next();
+            int pos = param.indexOf(keyValDelim);
+            String key = param;
+            String value = "";
+            if (pos > 0) {
+                key = param.substring(0, pos);
+                if (pos + keyValLen < param.length()) {
+                    value = param.substring(pos + keyValLen);
+                }
+            }
+            params.put(key, value);
+        }
+        return params;
     }
 }
