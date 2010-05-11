0de6c2dbc2b4fa48e45c78fddb6c9cd0f5a3c274
gWestenberger <g.westenberger@alkacon.com>
2010-05-11 14:21:56 +0000

    - changed the multi check box widget to work with new check boxes

--- src-modules/org/opencms/gwt/client/ui/input/CmsMultiCheckBox.java
+++ src-modules/org/opencms/gwt/client/ui/input/CmsMultiCheckBox.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/gwt/client/ui/input/Attic/CmsMultiCheckBox.java,v $
- * Date   : $Date: 2010/05/06 13:56:27 $
- * Version: $Revision: 1.8 $
+ * Date   : $Date: 2010/05/11 14:21:56 $
+ * Version: $Revision: 1.9 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Management System
@@ -41,13 +41,13 @@ import org.opencms.util.CmsStringUtil;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Panel;
 
 /**
@@ -55,7 +55,7 @@ import com.google.gwt.user.client.ui.Panel;
  * 
  * @author Georg Westenberger
  * 
- * @version $Revision: 1.8 $ 
+ * @version $Revision: 1.9 $ 
  * 
  * @since 8.0.0
  *  
@@ -65,17 +65,17 @@ public class CmsMultiCheckBox extends Composite implements I_CmsFormWidget, I_Cm
     /** The type string for this widget. */
     public static final String WIDGET_TYPE = "multiselect";
 
+    /** The list of checkboxes. */
+    private List<CmsCheckBox> m_checkboxes = new ArrayList<CmsCheckBox>();
+
     /** Error display for this widget. */
     private CmsErrorWidget m_error = new CmsErrorWidget();
 
-    /** The list of items as (key, label) pairs. */
-    private List<CmsPair<String, String>> m_items;
+    /** The select options of the multi check box. */
+    private Map<String, String> m_items = new LinkedHashMap<String, String>();
 
     /** Panel which contains all the components of the widget. */
     private Panel m_panel = new FlowPanel();
-
-    /** The table containing the checkboxes and labels. */
-    private Grid m_table;
 
     /**
      * Constructs a new checkbox group from a list of string pairs.<p>
@@ -87,7 +87,7 @@ public class CmsMultiCheckBox extends Composite implements I_CmsFormWidget, I_Cm
     public CmsMultiCheckBox(List<CmsPair<String, String>> items) {
 
         super();
-        init(items);
+        init(pairsToMap(items));
     }
 
     /**
@@ -101,11 +101,7 @@ public class CmsMultiCheckBox extends Composite implements I_CmsFormWidget, I_Cm
     public CmsMultiCheckBox(Map<String, String> items) {
 
         super();
-        List<CmsPair<String, String>> pairs = new ArrayList<CmsPair<String, String>>();
-        for (Map.Entry<String, String> entry : items.entrySet()) {
-            pairs.add(new CmsPair<String, String>(entry.getKey(), entry.getValue()));
-        }
-        init(pairs);
+        init(items);
     }
 
     /**
@@ -124,6 +120,25 @@ public class CmsMultiCheckBox extends Composite implements I_CmsFormWidget, I_Cm
                 return new CmsMultiCheckBox(widgetParams);
             }
         });
+    }
+
+    /**
+     * Internal helper method for converting a list of string pairs to a string map.<p>
+     * 
+     * The first component of each pair is used as a map key, the second component as the 
+     * value for the key.
+     * 
+     * @param pairs the list of pairs 
+     * 
+     * @return a string map 
+     */
+    private static Map<String, String> pairsToMap(List<CmsPair<String, String>> pairs) {
+
+        LinkedHashMap<String, String> result = new LinkedHashMap<String, String>();
+        for (CmsPair<String, String> pair : pairs) {
+            result.put(pair.getFirst(), pair.getSecond());
+        }
+        return result;
     }
 
     /**
@@ -161,9 +176,9 @@ public class CmsMultiCheckBox extends Composite implements I_CmsFormWidget, I_Cm
 
         Set<String> result = new HashSet<String>();
         int i = 0;
-        for (CmsPair<String, String> pair : m_items) {
-            String key = pair.getFirst();
-            CmsCheckBox checkBox = (CmsCheckBox)m_table.getWidget(i, 0);
+        for (Map.Entry<String, String> entry : m_items.entrySet()) {
+            String key = entry.getKey();
+            CmsCheckBox checkBox = m_checkboxes.get(i);
             if (checkBox.isChecked()) {
                 result.add(key);
             }
@@ -177,8 +192,7 @@ public class CmsMultiCheckBox extends Composite implements I_CmsFormWidget, I_Cm
      */
     public void reset() {
 
-        for (int i = 0; i < m_table.getRowCount(); i++) {
-            CmsCheckBox checkbox = getCheckBox(i);
+        for (CmsCheckBox checkbox : m_checkboxes) {
             checkbox.setChecked(false);
         }
     }
@@ -188,8 +202,7 @@ public class CmsMultiCheckBox extends Composite implements I_CmsFormWidget, I_Cm
      */
     public void setEnabled(boolean enabled) {
 
-        for (int i = 0; i < m_table.getRowCount(); i++) {
-            CmsCheckBox checkbox = getCheckBox(i);
+        for (CmsCheckBox checkbox : m_checkboxes) {
             checkbox.setEnabled(enabled);
         }
     }
@@ -212,9 +225,9 @@ public class CmsMultiCheckBox extends Composite implements I_CmsFormWidget, I_Cm
             List<String> keys = (List<String>)value;
             Set<String> keySet = new HashSet<String>(keys);
             int i = 0;
-            for (CmsPair<String, String> pair : m_items) {
-                String key = pair.getFirst();
-                CmsCheckBox checkbox = getCheckBox(i);
+            for (Map.Entry<String, String> entry : m_items.entrySet()) {
+                String key = entry.getKey();
+                CmsCheckBox checkbox = m_checkboxes.get(i);
                 checkbox.setChecked(keySet.contains(key));
                 i += 1;
             }
@@ -232,36 +245,28 @@ public class CmsMultiCheckBox extends Composite implements I_CmsFormWidget, I_Cm
     }
 
     /**
-     * Initializes the widget with a list of select options.<p>
+     * Initializes the widget given a map of select options.<p>
      * 
-     * @param items the select options 
+     * The keys of the map are the values of the select options, while the values of the map 
+     * are the labels which should be used for the checkboxes.
+     * 
+     * @param items the map of select options
      */
-    protected void init(List<CmsPair<String, String>> items) {
+    protected void init(Map<String, String> items) {
 
-        m_items = items;
-        m_table = new Grid(items.size(), 2);
-        m_panel.add(m_table);
-        m_panel.add(m_error);
         initWidget(m_panel);
+        m_items = new LinkedHashMap<String, String>(items);
         m_panel.setStyleName(I_CmsInputLayoutBundle.INSTANCE.inputCss().multiCheckBox());
         m_panel.addStyleName(I_CmsLayoutBundle.INSTANCE.generalCss().textMedium());
-        int i = 0;
-        for (CmsPair<String, String> pair : items) {
-            String value = pair.getSecond();
-            m_table.setWidget(i, 0, new CmsCheckBox());
-            m_table.setText(i, 1, value);
-            i += 1;
+        for (Map.Entry<String, String> entry : items.entrySet()) {
+            String value = entry.getValue();
+            CmsCheckBox checkbox = new CmsCheckBox(value);
+            // wrap the check boxes in FlowPanels to arrange them vertically 
+            FlowPanel checkboxWrapper = new FlowPanel();
+            checkboxWrapper.add(checkbox);
+            m_panel.add(checkboxWrapper);
         }
+        m_panel.add(m_error);
     }
 
-    /**
-     * Internal function for getting a checkbox from the table used by this widget.<p>
-     * 
-     * @param row the row index in the table
-     * @return a checkbox
-     */
-    private CmsCheckBox getCheckBox(int row) {
-
-        return (CmsCheckBox)m_table.getWidget(row, 0);
-    }
 }
