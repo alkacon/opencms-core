7902cd093a31a52770d9655f70c1b6ff3bb20f22
gWestenberger <g.westenberger@alkacon.com>
2011-05-05 07:11:48 +0000

    - fixed problem with duplicate tabs

--- src-modules/org/opencms/ade/sitemap/client/ui/CmsPropertyPanel.java
+++ src-modules/org/opencms/ade/sitemap/client/ui/CmsPropertyPanel.java
@@ -1,7 +1,7 @@
 /*
  * File   : $Source: /alkacon/cvs/opencms/src-modules/org/opencms/ade/sitemap/client/ui/Attic/CmsPropertyPanel.java,v $
- * Date   : $Date: 2011/05/05 07:03:11 $
- * Version: $Revision: 1.5 $
+ * Date   : $Date: 2011/05/05 07:11:48 $
+ * Version: $Revision: 1.6 $
  *
  * This library is part of OpenCms -
  * the Open Source Content Management System
@@ -62,7 +62,7 @@ import com.google.gwt.user.client.ui.Widget;
  * 
  * @author Ruediger Kurz
  * 
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  * 
  * @since 8.0.0
  */
@@ -132,7 +132,6 @@ public class CmsPropertyPanel extends A_CmsFormFieldPanel {
 
         m_tabPanel.add(CmsPopup.wrapWithBorderPadding(m_simpleTab), Messages.get().key(
             Messages.GUI_PROPERTY_TAB_SIMPLE_0));
-        m_tabPanel.add(m_individualTab, Messages.get().key(Messages.GUI_PROPERTY_TAB_STRUCTURE_0));
         m_showShared = showShared;
         if (m_showShared) {
             m_tabPanel.add(m_individualTab, Messages.get().key(Messages.GUI_PROPERTY_TAB_STRUCTURE_0));
