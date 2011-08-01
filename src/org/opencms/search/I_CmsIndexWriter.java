9e424926e7faac6febd6c4b90cd112cbe5b5fff7
aKandzior <a.kandzior@alkacon.com>
2011-08-01 15:20:21 +0200

    Applied standard source formatting to contributed files.

--- src/org/opencms/search/I_CmsIndexWriter.java
+++ src/org/opencms/search/I_CmsIndexWriter.java
@@ -1,45 +1,80 @@
+/*
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
+ *
+ * Copyright (c) Alkacon Software GmbH (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software GmbH, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package org.opencms.search;
 
 import java.io.IOException;
+
 import org.apache.lucene.document.Document;
 
 /**
- * Provides index manipulation operations.
- * @author Florian Hopf, Synyx GmbH & Co. KG, hopf@synyx.de
+ * Abstracts the index writer implementation for the most important index manipulation operations.
+ * 
+ * @since 8.0.2 
  */
 public interface I_CmsIndexWriter {
 
     /**
-     * Optimizes the index.
-     * @throws IOException
-     */
-    void optimize() throws IOException;
-
-    /**
-     * Commit all previous operations.
-     * @throws IOException
-     */
-    void commit() throws IOException;
-
-    /**
-     * Close this IndexWriter.
+     * Close this IndexWriter.<p>
+     * 
      * @throws IOException
      */
     void close() throws IOException;
 
     /**
-     * Update a document in the index.
-     * @param path
-     * @param document
+     * Commit all previous operations.<p>
+     * 
      * @throws IOException
      */
-    void updateDocument(String path, Document document) throws IOException;
+    void commit() throws IOException;
 
     /**
-     * Delete a document from the index.
-     * @param rootPath
-     * @throws IOException
+     * Delete a document from the index.<p>
+     * 
+     * @param rootPath the root path of the document to delete
+     * 
+     * @throws IOException in case something goes wrong
      */
     void deleteDocuments(String rootPath) throws IOException;
 
+    /**
+     * Optimizes the index.<p>
+     * 
+     * @throws IOException
+     */
+    void optimize() throws IOException;
+
+    /**
+     * Update a document in the index.<p>
+     * 
+     * @param rootPath the root path of the document to update
+     * @param document the document to update
+     * 
+     * @throws IOException in case something goes wrong
+     */
+    void updateDocument(String rootPath, Document document) throws IOException;
 }
