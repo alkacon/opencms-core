26721df6cbfd9b7a4b16d2d9001b0d6e8e87b06f
tHerrmann <t.herrmann@alkacon.com>
2012-02-13 12:45:28 +0100

    Added save button.

--- src-gwt/org/opencms/ade/contenteditor/client/ContentEditor.java
+++ src-gwt/org/opencms/ade/contenteditor/client/ContentEditor.java
@@ -40,36 +40,63 @@ import com.alkacon.vie.client.Entity;
 import com.alkacon.vie.client.I_Vie;
 import com.alkacon.vie.client.Vie;
 import com.alkacon.vie.shared.I_Entity;
+import com.alkacon.vie.shared.I_Type;
 
 import org.opencms.ade.contenteditor.shared.rpc.I_CmsContentService;
 import org.opencms.ade.contenteditor.shared.rpc.I_CmsContentServiceAsync;
+import org.opencms.gwt.client.A_CmsEntryPoint;
 import org.opencms.gwt.client.CmsCoreProvider;
+import org.opencms.gwt.client.rpc.CmsRpcAction;
 import org.opencms.gwt.client.rpc.CmsRpcPrefetcher;
+import org.opencms.gwt.client.ui.CmsPushButton;
 
-import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.rpc.SerializationException;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Content editor entry point.<p>
  */
-public class ContentEditor implements EntryPoint {
+public class ContentEditor extends A_CmsEntryPoint {
 
     /** The content service instance. */
     private static I_CmsContentServiceAsync SERVICE;
 
+    /** The entity id. */
+    protected String m_entityId;
+
+    /** The content locale. */
+    protected String m_locale;
+
+    /** The save button. */
+    protected CmsPushButton m_saveButton;
+
     /**
      * @see com.google.gwt.core.client.EntryPoint#onModuleLoad()
      */
+    @Override
     public void onModuleLoad() {
 
+        super.onModuleLoad();
         I_LayoutBundle.INSTANCE.style().ensureInjected();
+        m_saveButton = new CmsPushButton();
+        m_saveButton.setText("Save");
+        m_saveButton.disable("Nothing to save yet");
+        m_saveButton.addClickHandler(new ClickHandler() {
 
+            public void onClick(ClickEvent event) {
+
+                saveEntity();
+            }
+        });
         I_Vie vie = Vie.getInstance();
         ContentDefinition definition = null;
         try {
@@ -80,8 +107,16 @@ public class ContentEditor implements EntryPoint {
             RootPanel.get().add(new Label(e.getMessage()));
             return;
         }
-
-        I_Entity entity = WidgetService.register(vie, definition);
+        m_entityId = definition.getEntity().getId();
+        m_locale = definition.getLocale();
+        FlowPanel panel = new FlowPanel();
+        RootPanel.get().add(panel);
+        panel.add(m_saveButton);
+        HTML html = new HTML();
+        panel.add(html);
+        I_Type baseType = definition.getTypes().get(definition.getEntity().getTypeName());
+        vie.registerTypes(baseType, definition.getTypes());
+        I_Entity entity = vie.registerEntity(definition.getEntity());
 
         WidgetService service = new WidgetService();
         service.init(definition);
@@ -92,7 +127,7 @@ public class ContentEditor implements EntryPoint {
                 return new StringWidget();
             }
         });
-        service.registerWidgetFactory("tinymce", new I_WidgetFactory() {
+        service.registerWidgetFactory("org.opencms.widgets.CmsHtmlWidget", new I_WidgetFactory() {
 
             public I_EditWidget createWidget(String configuration) {
 
@@ -104,14 +139,13 @@ public class ContentEditor implements EntryPoint {
         service.setDefaultComplexRenderer(inlineRenderer);
         service.setDefaultSimpleRenderer(inlineRenderer);
         I_EntityRenderer renderer = service.getRendererForType(vie.getType(entity.getTypeName()));
-        renderer.render(entity, RootPanel.getBodyElement());
+        renderer.render(entity, html.getElement());
 
         ((Entity)entity).addValueChangeHandler(new ValueChangeHandler<I_Entity>() {
 
             public void onValueChange(ValueChangeEvent<I_Entity> event) {
 
-                RootPanel.get().getElement().getStyle().setBackgroundColor("#77f3f3");
-                RootPanel.get().add(new Label(event.getValue().toJSON()));
+                m_saveButton.enable();
             }
         });
 
@@ -130,6 +164,34 @@ public class ContentEditor implements EntryPoint {
             ((ServiceDefTarget)SERVICE).setServiceEntryPoint(serviceUrl);
         }
         return SERVICE;
+    }
+
+    /**
+     * Saves the entity.<p>
+     */
+    protected void saveEntity() {
+
+        I_Vie vie = Vie.getInstance();
+        final I_Entity entity = vie.getEntity(m_entityId);
+        if (entity != null) {
+            CmsRpcAction<Void> action = new CmsRpcAction<Void>() {
+
+                @Override
+                public void execute() {
+
+                    getService().saveEntity(com.alkacon.acacia.shared.Entity.serializeEntity(entity), m_locale, this);
+
+                }
+
+                @Override
+                protected void onResponse(Void result) {
+
+                    m_saveButton.disable("Nothing to save yet.");
+
+                }
+            };
+            action.execute();
+        }
     }
 
 }
