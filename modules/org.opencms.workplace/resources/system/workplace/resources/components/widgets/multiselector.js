d814161d70517ac809669d88aaa37b074dc02b98
tHerrmann <t.herrmann@alkacon.com>
2012-03-08 10:43:53 +0100

    Fixing selection issue in IE.

--- modules/org.opencms.workplace/resources/system/workplace/resources/components/widgets/multiselector.js
+++ modules/org.opencms.workplace/resources/system/workplace/resources/components/widgets/multiselector.js
@@ -9,11 +9,10 @@ function toggleMultiSelectWidget(source) {
 	// check if the checkbox is checked
 	if ($(source).attr("checked")) {
 		// the checkbox is checked, enable select box
-		$(source).siblings("select").attr("disabled", false).change(onChange);
-		
+		$(source).siblings("select").attr("disabled", false).click(onChange);
 	} else {
 		// the checkbox is not checked
 		// disable select box
-		$(source).siblings("select").attr("disabled", true).unbind("change", onChange);
+		$(source).siblings("select").attr("disabled", true).unbind("click", onChange);
 	}
 }
