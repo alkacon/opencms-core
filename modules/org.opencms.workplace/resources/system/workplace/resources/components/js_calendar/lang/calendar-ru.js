d877fa96b1132e98a2cf99b0f517db784d701ecc
tHerrmann <t.herrmann@alkacon.com>
2012-04-04 16:13:36 +0200

    Added first day of week setting.

--- modules/org.opencms.workplace/resources/system/workplace/resources/components/js_calendar/lang/calendar-ru.js
+++ modules/org.opencms.workplace/resources/system/workplace/resources/components/js_calendar/lang/calendar-ru.js
@@ -43,6 +43,10 @@ Calendar._SDN = new Array
  "суб",
  "вск");
 
+//First day of the week. "0" means display Sunday first, "1" means display
+//Monday first, etc.
+Calendar._FD = 1;
+
 // full month names
 Calendar._MN = new Array
 ("январь",
