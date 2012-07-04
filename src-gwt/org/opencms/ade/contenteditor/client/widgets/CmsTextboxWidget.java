06809a2d69e889f2c3ffa622d06e31147966c1ab
pStrudthoff <p.strudthoff@alkacon.com>
2012-07-04 11:52:30 +0200

    New input widget for the contenteditor, with fading on the right side and input handling.

---
+++ src-gwt/org/opencms/ade/contenteditor/client/widgets/CmsTextboxWidget.java
@@ -0,0 +1,206 @@
+/*
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
+ *
+ * Copyright (c) Alkacon Software GmbH (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.opencms.ade.contenteditor.client.widgets;
+
+import com.alkacon.acacia.client.widgets.I_EditWidget;
+
+import org.opencms.ade.contenteditor.client.css.I_CmsLayoutBundle;
+import org.opencms.gwt.client.ui.css.I_CmsInputCss;
+import org.opencms.gwt.client.ui.css.I_CmsInputLayoutBundle;
+
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.FocusHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.TextBox;
+
+/**
+ * Provides a display only widget, for use on a widget dialog.<p>
+ *  
+ * */
+public class CmsTextboxWidget extends Composite implements I_EditWidget {
+
+    /** The layout bundle. */
+    protected static final I_CmsInputCss CSS = I_CmsInputLayoutBundle.INSTANCE.inputCss();
+
+    /** The input test area.*/
+    TextBox m_textbox = new TextBox();
+
+    /** The token to control activation. */
+    private boolean m_active = true;
+
+    /**
+     * Creates a new display widget.<p>
+     * @param config 
+     */
+    public CmsTextboxWidget(String config) {
+
+        // All composites must call initWidget() in their constructors.
+        Panel mainPanel = new FlowPanel();
+        Panel faidPanel = new SimplePanel();
+        faidPanel.addDomHandler(new ClickHandler() {
+
+            public void onClick(ClickEvent event) {
+
+                m_textbox.setFocus(true);
+                m_textbox.setCursorPos(m_textbox.getText().length());
+            }
+        }, ClickEvent.getType());
+        faidPanel.setStyleName(CSS.inputTextBoxFaider());
+        mainPanel.getElement().getStyle().setMarginRight(12, Unit.PX);
+        mainPanel.add(m_textbox);
+        mainPanel.add(faidPanel);
+        initWidget(mainPanel);
+
+        m_textbox.setStyleName(I_CmsLayoutBundle.INSTANCE.widgetCss().textBox());
+        /* m_textarea.getTextAreaContainer().setStyleName(I_CmsLayoutBundle.INSTANCE.widgetCss().textAreaBoxPanel()); */
+        m_textbox.addValueChangeHandler(new ValueChangeHandler<String>() {
+
+            public void onValueChange(ValueChangeEvent<String> event) {
+
+                fireChangeEvent();
+
+            }
+        });
+        m_textbox.addBlurHandler(new BlurHandler() {
+
+            public void onBlur(BlurEvent event) {
+
+                m_textbox.setCursorPos(0);
+
+            }
+        });
+
+    }
+
+    /**
+     * @see com.google.gwt.event.dom.client.HasFocusHandlers#addFocusHandler(com.google.gwt.event.dom.client.FocusHandler)
+     */
+    public HandlerRegistration addFocusHandler(FocusHandler handler) {
+
+        return null;
+    }
+
+    /**
+     * @see com.google.gwt.event.logical.shared.HasValueChangeHandlers#addValueChangeHandler(com.google.gwt.event.logical.shared.ValueChangeHandler)
+     */
+    public HandlerRegistration addValueChangeHandler(ValueChangeHandler<String> handler) {
+
+        return addHandler(handler, ValueChangeEvent.getType());
+    }
+
+    /**
+     * Represents a value change event.<p>
+     * 
+     */
+    public void fireChangeEvent() {
+
+        String result = "";
+        if (m_textbox.getText() != null) {
+            result = m_textbox.getText();
+        }
+
+        ValueChangeEvent.fire(this, result);
+    }
+
+    /**
+     * @see com.google.gwt.user.client.ui.HasValue#getValue()
+     */
+    public String getValue() {
+
+        return m_textbox.getText();
+    }
+
+    /**
+     * @see com.alkacon.acacia.client.widgets.I_EditWidget#isActive()
+     */
+    public boolean isActive() {
+
+        return m_active;
+    }
+
+    /**
+     * @see com.alkacon.acacia.client.widgets.I_EditWidget#onAttachWidget()
+     */
+    public void onAttachWidget() {
+
+        super.onAttach();
+    }
+
+    /**
+     * @see com.alkacon.acacia.client.widgets.I_EditWidget#setActive(boolean)
+     */
+    public void setActive(boolean active) {
+
+        if (m_active == active) {
+            return;
+        }
+
+        m_active = active;
+        m_textbox.setEnabled(active);
+        if (!active) {
+            m_textbox.setText("");
+        }
+        if (active) {
+            fireChangeEvent();
+        }
+
+    }
+
+    /**
+     * @see com.google.gwt.user.client.ui.HasValue#setValue(java.lang.Object)
+     */
+    public void setValue(String value) {
+
+        // set the value and start changeEvent
+        setValue(value, true);
+
+    }
+
+    /**
+     * @see com.google.gwt.user.client.ui.HasValue#setValue(java.lang.Object, boolean)
+     */
+    public void setValue(String value, boolean fireEvents) {
+
+        // set the saved value to the textArea
+        m_textbox.setText(value);
+        if (fireEvents) {
+            fireChangeEvent();
+        }
+    }
+
+}
