d47337a51f37b60c5b7bf4344bd9073a2c3c6de2
Rüdiger Kurz <r.kurz@alkacon.com>
2012-09-04 18:19:55 +0200

    Optimized code of Solr collector.

--- src/org/opencms/file/collectors/CmsSolrCollector.java
+++ src/org/opencms/file/collectors/CmsSolrCollector.java
@@ -31,11 +31,11 @@
 
 package org.opencms.file.collectors;
 
-import org.opencms.file.CmsDataAccessException;
 import org.opencms.file.CmsObject;
 import org.opencms.file.CmsResource;
 import org.opencms.main.CmsException;
 import org.opencms.main.OpenCms;
+import org.opencms.search.solr.CmsSolrIndex;
 import org.opencms.search.solr.CmsSolrQuery;
 import org.opencms.util.CmsRequestUtil;
 
@@ -43,11 +43,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
-import org.apache.solr.common.params.ModifiableSolrParams;
+import java.util.Map;
 
 /**
  * A Solr collector.<p>
+ * 
+ * @since 8.5.0
  */
 public class CmsSolrCollector extends A_CmsResourceCollector {
 
@@ -56,12 +57,6 @@ public class CmsSolrCollector extends A_CmsResourceCollector {
 
     /** Array list for fast collector name lookup. */
     private static final List<String> COLLECTORS_LIST = Collections.unmodifiableList(Arrays.asList(COLLECTORS));
-
-    /** The name of the priority property to read. */
-    public static final String PROPERTY_PRIORITY_FIELD = "collector.priority_s";
-
-    /** The name of the date property to read. */
-    public static final String PROPERTY_DATE_FIELD = "collector.date_s";
 
     /**
      * @see org.opencms.file.collectors.I_CmsResourceCollector#getCollectorNames()
@@ -74,61 +69,29 @@ public class CmsSolrCollector extends A_CmsResourceCollector {
     /**
      * @see org.opencms.file.collectors.I_CmsResourceCollector#getCreateLink(org.opencms.file.CmsObject, java.lang.String, java.lang.String)
      */
-    public String getCreateLink(CmsObject cms, String collectorName, String param)
-    throws CmsException, CmsDataAccessException {
+    public String getCreateLink(CmsObject cms, String collectorName, String param) {
 
-        throw new CmsDataAccessException(Messages.get().container(Messages.ERR_COLLECTOR_NAME_INVALID_1, collectorName));
+        return null;
     }
 
     /**
      * @see org.opencms.file.collectors.I_CmsResourceCollector#getCreateParam(org.opencms.file.CmsObject, java.lang.String, java.lang.String)
      */
-    public String getCreateParam(CmsObject cms, String collectorName, String param) throws CmsDataAccessException {
+    public String getCreateParam(CmsObject cms, String collectorName, String param) {
 
-        // if action is not set, use default action
-        if (collectorName == null) {
-            collectorName = COLLECTORS[1];
-        }
-
-        switch (COLLECTORS_LIST.indexOf(collectorName)) {
-            case 0:
-            case 1:
-                // "allInFolderPriorityDateAsc", "allInFolderPriorityDateDesc" or "allInFolderPriorityTitleDesc"
-                return param;
-            default:
-                throw new CmsDataAccessException(Messages.get().container(
-                    Messages.ERR_COLLECTOR_NAME_INVALID_1,
-                    collectorName));
-        }
+        return null;
     }
 
-    public List<CmsResource> getResults(CmsObject cms, String collectorName, String param)
-    throws CmsDataAccessException, CmsException {
+    /**
+     * @see org.opencms.file.collectors.I_CmsResourceCollector#getResults(org.opencms.file.CmsObject, java.lang.String, java.lang.String)
+     */
+    public List<CmsResource> getResults(CmsObject cms, String collectorName, String param) throws CmsException {
 
         // if action is not set use default
-        if (collectorName == null) {
-            collectorName = COLLECTORS[1];
-        }
-
-        switch (COLLECTORS_LIST.indexOf(collectorName)) {
-
-            case 0:
-                // "byQuery"
-                return new ArrayList<CmsResource>(OpenCms.getSearchManager().getIndexSolr("Solr Offline").search(
-                    cms,
-                    param));
-            case 1:
-                // "byContext"
-                CmsSolrQuery q = new CmsSolrQuery(cms);
-                if (param != null) {
-                    q.add(new ModifiableSolrParams(CmsRequestUtil.createParameterMap(param)));
-                }
-                return new ArrayList<CmsResource>(
-                    OpenCms.getSearchManager().getIndexSolr("Solr Offline").search(cms, q));
-            default:
-                throw new CmsDataAccessException(Messages.get().container(
-                    Messages.ERR_COLLECTOR_NAME_INVALID_1,
-                    collectorName));
-        }
+        collectorName = collectorName == null ? COLLECTORS[1] : collectorName;
+        Map<String, String[]> pm = CmsRequestUtil.createParameterMap(param);
+        CmsSolrIndex index = OpenCms.getSearchManager().getIndexSolr(pm);
+        CmsSolrQuery q = COLLECTORS_LIST.indexOf(collectorName) == 0 ? new CmsSolrQuery(pm) : new CmsSolrQuery(cms, pm);
+        return new ArrayList<CmsResource>(index.search(cms, q));
     }
 }
