dbf7bf37a796ce7b69abc93cfe669672b3a66a33
pStrudthoff <p.strudthoff@alkacon.com>
2012-09-17 12:13:20 +0200

    Correct issue by scaling the image for preview in ImageGalleryField.

--- src-gwt/org/opencms/ade/galleries/client/ui/CmsImageGalleryField.java
+++ src-gwt/org/opencms/ade/galleries/client/ui/CmsImageGalleryField.java
@@ -675,7 +675,7 @@ implements I_CmsFormWidget, I_CmsHasInit, HasValueChangeHandlers<String>, HasRes
         if (!path.isEmpty()) {
             String imageLink = CmsCoreProvider.get().link(path);
             CmsCroppingParamBean restricted;
-            if (m_croppingParam.getFormatName() == null) {
+            if (m_croppingParam.getScaleParam().isEmpty()) {
                 m_image.setUrl(imageLink + "?__scale=w:80,h:110,t:1,c:white,r:2");
             } else {
                 restricted = m_croppingParam.getRestrictedSizeParam(110, 80);
