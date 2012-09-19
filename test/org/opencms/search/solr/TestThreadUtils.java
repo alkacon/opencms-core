8ffae2adb12cfff2b4d4d52b9c6e4fa9a9a394fd
Ruediger Kurz <r.kurz@alkacon.com>
2012-09-19 11:06:05 +0200

    Improved comments.

--- test/org/opencms/search/solr/TestThreadUtils.java
+++ test/org/opencms/search/solr/TestThreadUtils.java
@@ -1,3 +1,4 @@
+
 package org.opencms.search.solr;
 
 import java.lang.management.ManagementFactory;
@@ -5,152 +6,258 @@ import java.lang.management.MonitorInfo;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 
+/**
+ * Utility class for thread analyzing.<p>
+ */
 public class TestThreadUtils {
 
-    public static TestThreadUtils getInstance() {
-    	return new TestThreadUtils();
+    /**
+     * Hide default constructor.<p>
+     */
+    private TestThreadUtils() {
+
+        // noop
     }
-    
-    private ThreadGroup rootThreadGroup = null;
-    
-    public ThreadInfo[] getAllThreadInfos( ) {
-        final ThreadMXBean thbean = ManagementFactory.getThreadMXBean( );
-        final long[] ids = thbean.getAllThreadIds( );
-     
+
+    /**
+     * Returns all thread infos.<p>
+     * 
+     * @return all thread infos
+     */
+    public static ThreadInfo[] getAllThreadInfos() {
+
+        final ThreadMXBean thbean = ManagementFactory.getThreadMXBean();
+        final long[] ids = thbean.getAllThreadIds();
+
         ThreadInfo[] infos;
-        if ( !thbean.isObjectMonitorUsageSupported( ) ||
-            !thbean.isSynchronizerUsageSupported( ) )
-            infos = thbean.getThreadInfo( ids );
-        else
-            infos = thbean.getThreadInfo( ids, true, true );
-     
+        if (!thbean.isObjectMonitorUsageSupported() || !thbean.isSynchronizerUsageSupported()) {
+            infos = thbean.getThreadInfo(ids);
+        } else {
+            infos = thbean.getThreadInfo(ids, true, true);
+        }
+
         final ThreadInfo[] notNulls = new ThreadInfo[infos.length];
         int nNotNulls = 0;
-        for ( ThreadInfo info : infos )
-            if ( info != null )
+        for (ThreadInfo info : infos) {
+            if (info != null) {
                 notNulls[nNotNulls++] = info;
-        if ( nNotNulls == infos.length )
+            }
+        }
+        if (nNotNulls == infos.length) {
             return infos;
-        return java.util.Arrays.copyOf( notNulls, nNotNulls );
+        }
+        return java.util.Arrays.copyOf(notNulls, nNotNulls);
     }
-    
-    
-    public Thread[] getAllThreads( ) {
-        final ThreadGroup root = getRootThreadGroup( );
-        final ThreadMXBean thbean = ManagementFactory.getThreadMXBean( );
-        int nAlloc = thbean.getThreadCount( );
+
+    /**
+     * Returns all threads.<p>
+     * 
+     * @return all threads
+     */
+    public static Thread[] getAllThreads() {
+
+        final ThreadGroup root = getRootThreadGroup();
+        final ThreadMXBean thbean = ManagementFactory.getThreadMXBean();
+        int nAlloc = thbean.getThreadCount();
         int n = 0;
         Thread[] threads;
         do {
             nAlloc *= 2;
-            threads = new Thread[ nAlloc ];
-            n = root.enumerate( threads, true );
-        } while ( n == nAlloc );
-        return java.util.Arrays.copyOf( threads, n );
+            threads = new Thread[nAlloc];
+            n = root.enumerate(threads, true);
+        } while (n == nAlloc);
+        return java.util.Arrays.copyOf(threads, n);
     }
-    
-    public Thread getBlockingThread( final Thread thread ) {
-        final ThreadInfo info = getThreadInfo( thread );
-        if ( info == null )
+
+    /**
+     * Returns the thread that is blocking the given one.<p>
+     * 
+     * @param thread the thread to get the blocking thread for
+     * 
+     * @return the thread that is blocking the given one
+     */
+    public static Thread getBlockingThread(final Thread thread) {
+
+        final ThreadInfo info = getThreadInfo(thread);
+        if (info == null) {
             return null;
-        final long id = info.getLockOwnerId( );
-        if ( id == -1 )
+        }
+        final long id = info.getLockOwnerId();
+        if (id == -1) {
             return null;
-        return getThread( id );
+        }
+        return getThread(id);
     }
-    public Thread getLockingThread( long identity ) {
-     
-        final Thread[] allThreads = getAllThreads( );
+
+    /**
+     * Returns the thread that is locking the thread identified by the given id.<p>
+     * 
+     * @param identity the thread id to get the locking thread for
+     * 
+     * @return the locaking thread or <code>null</code> if the thread for the given id is not locked 
+     */
+    public static Thread getLockingThread(long identity) {
+
+        final Thread[] allThreads = getAllThreads();
         ThreadInfo info = null;
         MonitorInfo[] monitors = null;
-        for ( Thread thread : allThreads ) {
-            info = getThreadInfo( thread.getId( ) );
-            if ( info == null )
+        for (Thread thread : allThreads) {
+            info = getThreadInfo(thread.getId());
+            if (info == null) {
                 continue;
-            monitors = info.getLockedMonitors( );
-            for ( MonitorInfo monitor : monitors )
-                if ( identity == monitor.getIdentityHashCode( ) )
+            }
+            monitors = info.getLockedMonitors();
+            for (MonitorInfo monitor : monitors) {
+                if (identity == monitor.getIdentityHashCode()) {
                     return thread;
+                }
+            }
         }
         return null;
     }
-        
-    public Thread getLockingThread( final Object object ) {
-        if ( object == null )
-            throw new NullPointerException( "Null object" );
-        final long identity = System.identityHashCode( object );
-     
-        final Thread[] allThreads = getAllThreads( );
+
+    /**
+     * Returns the locking thread for the given object.<p>
+     * 
+     * @param object the object that is potentially locked
+     * 
+     * @return the locking thread for the given object or <code>null</code> if not locked
+     */
+    public static Thread getLockingThread(final Object object) {
+
+        if (object == null) {
+            throw new NullPointerException("Null object");
+        }
+        final long identity = System.identityHashCode(object);
+
+        final Thread[] allThreads = getAllThreads();
         ThreadInfo info = null;
         MonitorInfo[] monitors = null;
-        for ( Thread thread : allThreads ) {
-            info = getThreadInfo( thread.getId( ) );
-            if ( info == null )
+        for (Thread thread : allThreads) {
+            info = getThreadInfo(thread.getId());
+            if (info == null) {
                 continue;
-            monitors = info.getLockedMonitors( );
-            for ( MonitorInfo monitor : monitors )
-                if ( identity == monitor.getIdentityHashCode( ) )
+            }
+            monitors = info.getLockedMonitors();
+            for (MonitorInfo monitor : monitors) {
+                if (identity == monitor.getIdentityHashCode()) {
                     return thread;
+                }
+            }
         }
         return null;
     }
-    
-    public ThreadGroup getRootThreadGroup( ) {
-        if ( rootThreadGroup != null )
-            return rootThreadGroup;
-        ThreadGroup tg = Thread.currentThread( ).getThreadGroup( );
+
+    /**
+     * Returns the thread group of the current thread.<p>
+     * 
+     * @return the thread group
+     */
+    public static ThreadGroup getRootThreadGroup() {
+
+        ThreadGroup tg = Thread.currentThread().getThreadGroup();
         ThreadGroup ptg;
-        while ( (ptg = tg.getParent( )) != null )
+        while ((ptg = tg.getParent()) != null) {
             tg = ptg;
+        }
         return tg;
     }
-    
-    public Thread getThread( final long id ) {
-        final Thread[] threads = getAllThreads( );
-        for ( Thread thread : threads )
-            if ( thread.getId( ) == id )
+
+    /**
+     * Returns the thread for the given id.<p>
+     * 
+     * @param id of a thread
+     *  
+     * @return the thread or <code>null</code> if not existent
+     */
+    public static Thread getThread(final long id) {
+
+        final Thread[] threads = getAllThreads();
+        for (Thread thread : threads) {
+            if (thread.getId() == id) {
                 return thread;
+            }
+        }
         return null;
     }
-    
-    public Thread getThread( final String name ) {
-        if ( name == null )
-            throw new NullPointerException( "Null name" );
-        final Thread[] threads = getAllThreads( );
-        for ( Thread thread : threads )
-            if ( thread.getName( ).equals( name ) )
+
+    /**
+     * Returns the thread for the given name.<p>
+     * 
+     * @param name the name of the thread to get
+     * 
+     * @return the thread for the given name or <code>null</code> if not existent
+     */
+    public static Thread getThread(final String name) {
+
+        if (name == null) {
+            throw new NullPointerException("Null name");
+        }
+        final Thread[] threads = getAllThreads();
+        for (Thread thread : threads) {
+            if (thread.getName().equals(name)) {
                 return thread;
+            }
+        }
         return null;
     }
-    
-    public ThreadInfo getThreadInfo( final long id ) {
-        final ThreadMXBean thbean = ManagementFactory.getThreadMXBean( );
-     
-        if ( !thbean.isObjectMonitorUsageSupported( ) ||
-            !thbean.isSynchronizerUsageSupported( ) )
-            return thbean.getThreadInfo( id );
-     
-        final ThreadInfo[] infos = thbean.getThreadInfo(
-            new long[] { id }, true, true );
-        if ( infos.length == 0 )
+
+    /**
+     * Returns the thread info for the thread identified by the given id.<p>
+     * 
+     * @param id the id og the thread to get the info for
+     * 
+     * @return the thread info for the thread identified by the given id or <code>null</code> if not existent
+     */
+    public static ThreadInfo getThreadInfo(final long id) {
+
+        final ThreadMXBean thbean = ManagementFactory.getThreadMXBean();
+
+        if (!thbean.isObjectMonitorUsageSupported() || !thbean.isSynchronizerUsageSupported()) {
+            return thbean.getThreadInfo(id);
+        }
+
+        final ThreadInfo[] infos = thbean.getThreadInfo(new long[] {id}, true, true);
+        if (infos.length == 0) {
             return null;
+        }
         return infos[0];
     }
 
-    public ThreadInfo getThreadInfo( final String name ) {
-        if ( name == null )
-            throw new NullPointerException( "Null name" );
-        final Thread[] threads = getAllThreads( );
-        for ( Thread thread : threads )
-            if ( thread.getName( ).equals( name ) )
-                return getThreadInfo( thread.getId( ) );
+    /**
+     * Returns the thread info for the thread identified by the given name.<p>
+     * 
+     * @param name the name of the thread to get the info for
+     * 
+     * @return the thread info for the thread identified by the given name or <code>null</code> if not existent
+     */
+    public static ThreadInfo getThreadInfo(final String name) {
+
+        if (name == null) {
+            throw new NullPointerException("Null name");
+        }
+        final Thread[] threads = getAllThreads();
+        for (Thread thread : threads) {
+            if (thread.getName().equals(name)) {
+                return getThreadInfo(thread.getId());
+            }
+        }
         return null;
     }
 
-    public ThreadInfo getThreadInfo( final Thread thread ) {
-        if ( thread == null )
-            throw new NullPointerException( "Null thread" );
-        return getThreadInfo( thread.getId( ) );
+    /**
+     * Returns the thread info of the given thread.<p>
+     * 
+     * @param thread the thread to get the info for
+     * 
+     * @return  the thread info of the given thread or <code>null</code> if not existent
+     */
+    public static ThreadInfo getThreadInfo(final Thread thread) {
+
+        if (thread == null) {
+            throw new NullPointerException("Null thread");
+        }
+        return getThreadInfo(thread.getId());
     }
 
 }
