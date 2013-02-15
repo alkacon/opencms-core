0a3775c2c9436831c328185f97a35ab4f2abd8d4
Alberto <rusgall@rus.uni-stuttgart.de>
2013-02-15 10:44:19 +0100

    Create example ant script using selectionprompt

--- src-components/AntTestTask.java
+++
@@ -1,9 +0,0 @@
-public class AntTestTask extends org.apache.tools.ant.Task {
-    private String param1;
-    public void setParam1(String param1) {
-        this.param1 = param1;
-    }
-    public void execute () throws org.apache.tools.ant.BuildException {
-        System.out.println("This is my param1: " + param1);
-    }
-}
