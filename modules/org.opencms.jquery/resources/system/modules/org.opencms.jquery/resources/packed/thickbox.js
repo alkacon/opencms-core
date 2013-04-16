35c850813461c7237a80545166480bb701ab92ec
Andreas Zahner <a.zahner@alkacon.com>
2013-04-16 16:24:15 +0200

    Fixed Thickbox closing issue when used with new jQuery/UI versions.

--- modules/org.opencms.jquery/resources/system/modules/org.opencms.jquery/resources/packed/thickbox.js
+++ modules/org.opencms.jquery/resources/system/modules/org.opencms.jquery/resources/packed/thickbox.js
@@ -287,7 +287,8 @@ function tb_showIframe(){
 function tb_remove() {
  	$("#TB_imageOff").unbind("click");
 	$("#TB_closeWindowButton").unbind("click");
-	$("#TB_window").fadeOut("fast",function(){$('#TB_window,#TB_overlay,#TB_HideSelect').trigger("unload").unbind().remove();});
+	// does not work anymore with UI 1.8.x: $("#TB_window").fadeOut("fast",function(){$('#TB_window,#TB_overlay,#TB_HideSelect').trigger("unload").unbind().remove();});
+	$("#TB_window").fadeOut("fast",function(){$('#TB_window,#TB_overlay,#TB_HideSelect').unload("#TB_ajaxContent").unbind().remove();}); 
 	$("#TB_load").remove();
 	if (typeof document.body.style.maxHeight == "undefined") {//if IE 6
 		$("body","html").css({height: "auto", width: "auto"});
