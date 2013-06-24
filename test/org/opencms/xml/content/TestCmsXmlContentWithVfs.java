74cb1171a096b731d54efaba30742663ae70cd5a
gWestenberger <g.westenberger@alkacon.com>
2013-06-24 12:05:16 +0200

    Fixed bug with removal of mapped properties (github issue #61).

--- test/org/opencms/xml/content/TestCmsXmlContentWithVfs.java
+++ test/org/opencms/xml/content/TestCmsXmlContentWithVfs.java
@@ -105,6 +105,7 @@ public class TestCmsXmlContentWithVfs extends OpenCmsTestCase {
 
     /** Schema id 6. */
     private static final String SCHEMA_SYSTEM_ID_6 = "http://www.opencms.org/test6.xsd";
+
     /** Schema id 7. */
     private static final String SCHEMA_SYSTEM_ID_7 = "http://www.opencms.org/test7.xsd";
 
@@ -1815,7 +1816,7 @@ public class TestCmsXmlContentWithVfs extends OpenCmsTestCase {
         CmsXmlEntityResolver.cacheSystemId(SCHEMA_SYSTEM_ID_1L2, content.getBytes(CmsEncoder.ENCODING_UTF_8));
 
         CmsXmlContent xmlcontentDE = CmsXmlContentFactory.createDocument(cms, Locale.GERMAN, content, definition);
-        // the unicode represents äöüÄÖÜß€
+        // the unicode represents ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ß€
         assertEquals(
             "Dies ist etwas Text EINS \u00E4\u00F6\u00FC\u00C4\u00D6\u00DC\u00DF\u20AC",
             xmlcontentDE.getStringValue(cms, "StringOne", Locale.GERMAN));
