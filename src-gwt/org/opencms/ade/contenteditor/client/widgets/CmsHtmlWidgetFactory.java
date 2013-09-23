1d2f06614ef3be6aec7af23fd7cfb2583ebf39d8
tHerrmann <t.herrmann@alkacon.com>
2013-09-23 11:44:31 +0200

    Added missing source code plugin.

--- src-gwt/org/opencms/ade/contenteditor/client/widgets/CmsHtmlWidgetFactory.java
+++ src-gwt/org/opencms/ade/contenteditor/client/widgets/CmsHtmlWidgetFactory.java
@@ -94,118 +94,118 @@ public class CmsHtmlWidgetFactory implements I_WidgetFactory, I_CmsHasInit {
      */
     private native JavaScriptObject generateOptionsForTiny(String configuration)/*-{
 
-        var options = null;
-        try {
-            var config = @org.opencms.gwt.client.util.CmsDomUtil::parseJSON(Ljava/lang/String;)(configuration);
-            options = {
-                // the browser call back function is defined in /system/workplace/editors/tinymce/opencms_plugin.js
-                file_browser_callback : $wnd.cmsTinyMceFileBrowser
-            };
-            if (config.language) {
-                options.language = config.language;
-            }
-            if (config.content_css) {
-                options.content_css = config.content_css;
-            }
-            if (config.height) {
-                options.editorHeight = config.height;
-            }
-            if (config.block_formats) {
-                options.block_formats = config.block_formats;
-            }
-            if (config.style_formats) {
-                var temp = null;
-                try {
-                    temp = eval('(' + config.style_formats + ')');
-                } catch (error) {
-                    $wnd.alert("Could not parse WYSIWYG editor options: "
-                            + error);
-                }
-                if (typeof temp != 'undefined' && temp != null) {
-                    options.style_formats = temp;
-                }
-            }
-            if (config.cmsGalleryEnhancedOptions) {
-                options.cmsGalleryEnhancedOptions = config.cmsGalleryEnhancedOptions;
-            }
-            if (config.cmsGalleryUseThickbox) {
-                options.cmsGalleryUseThickbox = config.cmsGalleryUseThickbox;
-            }
-            options.plugins = "autolink,lists,pagebreak,layer,table,save,hr,image,link,emoticons,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,template,wordcount,advlist,-opencms";
-            if (config.fullpage) {
-                options.plugins += ",fullpage";
-            }
+                                                                                var options = null;
+                                                                                try {
+                                                                                var config = @org.opencms.gwt.client.util.CmsDomUtil::parseJSON(Ljava/lang/String;)(configuration);
+                                                                                options = {
+                                                                                // the browser call back function is defined in /system/workplace/editors/tinymce/opencms_plugin.js
+                                                                                file_browser_callback : $wnd.cmsTinyMceFileBrowser
+                                                                                };
+                                                                                if (config.language) {
+                                                                                options.language = config.language;
+                                                                                }
+                                                                                if (config.content_css) {
+                                                                                options.content_css = config.content_css;
+                                                                                }
+                                                                                if (config.height) {
+                                                                                options.editorHeight = config.height;
+                                                                                }
+                                                                                if (config.block_formats) {
+                                                                                options.block_formats = config.block_formats;
+                                                                                }
+                                                                                if (config.style_formats) {
+                                                                                var temp = null;
+                                                                                try {
+                                                                                temp = eval('(' + config.style_formats + ')');
+                                                                                } catch (error) {
+                                                                                $wnd.alert("Could not parse WYSIWYG editor options: "
+                                                                                + error);
+                                                                                }
+                                                                                if (typeof temp != 'undefined' && temp != null) {
+                                                                                options.style_formats = temp;
+                                                                                }
+                                                                                }
+                                                                                if (config.cmsGalleryEnhancedOptions) {
+                                                                                options.cmsGalleryEnhancedOptions = config.cmsGalleryEnhancedOptions;
+                                                                                }
+                                                                                if (config.cmsGalleryUseThickbox) {
+                                                                                options.cmsGalleryUseThickbox = config.cmsGalleryUseThickbox;
+                                                                                }
+                                                                                options.plugins = "autolink,lists,pagebreak,layer,table,save,hr,image,link,emoticons,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,template,wordcount,advlist,code,-opencms";
+                                                                                if (config.fullpage) {
+                                                                                options.plugins += ",fullpage";
+                                                                                }
 
-            if (config.toolbar_items) {
-                // assemble the toolbar
+                                                                                if (config.toolbar_items) {
+                                                                                // assemble the toolbar
 
-                // translation map
-                var BUTTON_TRANSLATION = {
-                    alignleft : "justifyleft",
-                    aligncenter : "justifycenter",
-                    alignright : "justifyright",
-                    justify : "justifyfull",
-                    style : "styleselect",
-                    paste : "paste,pastetext,pasteword",
-                    find : "search",
-                    unorderedlist : "bullist",
-                    orderedlist : "numlist",
-                    editorlink : "link",
-                    source : "code",
-                    subscript : "sub",
-                    superscript : "sup",
-                    specialchar : "charmap",
-                    spellcheck : "iespell",
-                    fitwindow : "fullscreen",
-                    imagegallery : "OcmsImageGallery",
-                    downloadgallery : "OcmsDownloadGallery",
-                    linkgallery : "OcmsLinkGallery",
-                    htmlgallery : "OcmsHtmlGallery",
-                    tablegallery : "OcmsTableGallery",
-                    link : "link"
-                };
+                                                                                // translation map
+                                                                                var BUTTON_TRANSLATION = {
+                                                                                alignleft : "justifyleft",
+                                                                                aligncenter : "justifycenter",
+                                                                                alignright : "justifyright",
+                                                                                justify : "justifyfull",
+                                                                                style : "styleselect",
+                                                                                paste : "paste,pastetext,pasteword",
+                                                                                find : "search",
+                                                                                unorderedlist : "bullist",
+                                                                                orderedlist : "numlist",
+                                                                                editorlink : "link",
+                                                                                source : "code",
+                                                                                subscript : "sub",
+                                                                                superscript : "sup",
+                                                                                specialchar : "charmap",
+                                                                                spellcheck : "iespell",
+                                                                                fitwindow : "fullscreen",
+                                                                                imagegallery : "OcmsImageGallery",
+                                                                                downloadgallery : "OcmsDownloadGallery",
+                                                                                linkgallery : "OcmsLinkGallery",
+                                                                                htmlgallery : "OcmsHtmlGallery",
+                                                                                tablegallery : "OcmsTableGallery",
+                                                                                link : "link"
+                                                                                };
 
-                var toolbarGroup = "";
-                // iterate over all toolbar items and generate toobar groups
-                for ( var i = 0; i < config.toolbar_items.length; i++) {
-                    var item = config.toolbar_items[i];
-                    // ignore duplicate items
-                    if (item != config.toolbar_items[i - 1]) {
-                        // check for an item translation
-                        if (BUTTON_TRANSLATION[item]) {
-                            item = BUTTON_TRANSLATION[item];
-                        }
-                        // |,[,],- are group separators indicating to add the group and start a new one
-                        if (item == "|" || item == "[" || item == "]"
-                                || item == "-"
-                                || i == config.toolbar_items.length - 1) {
-                            // don't add empty groups
-                            if (toolbarGroup != "") {
-                                toolbarGroup += " |";
-                            }
-                        } else {
-                            // add item to the group 
-                            if (toolbarGroup != "") {
-                                toolbarGroup += " ";
-                            }
-                            toolbarGroup += item;
-                        }
-                    }
-                }
+                                                                                var toolbarGroup = "";
+                                                                                // iterate over all toolbar items and generate toobar groups
+                                                                                for ( var i = 0; i < config.toolbar_items.length; i++) {
+                                                                                var item = config.toolbar_items[i];
+                                                                                // ignore duplicate items
+                                                                                if (item != config.toolbar_items[i - 1]) {
+                                                                                // check for an item translation
+                                                                                if (BUTTON_TRANSLATION[item]) {
+                                                                                item = BUTTON_TRANSLATION[item];
+                                                                                }
+                                                                                // |,[,],- are group separators indicating to add the group and start a new one
+                                                                                if (item == "|" || item == "[" || item == "]"
+                                                                                || item == "-"
+                                                                                || i == config.toolbar_items.length - 1) {
+                                                                                // don't add empty groups
+                                                                                if (toolbarGroup != "") {
+                                                                                toolbarGroup += " |";
+                                                                                }
+                                                                                } else {
+                                                                                // add item to the group 
+                                                                                if (toolbarGroup != "") {
+                                                                                toolbarGroup += " ";
+                                                                                }
+                                                                                toolbarGroup += item;
+                                                                                }
+                                                                                }
+                                                                                }
 
-                options.toolbar1 = toolbarGroup;
+                                                                                options.toolbar1 = toolbarGroup;
 
-                if (config.tinyMceOptions) {
-                    for ( var tinyMceOptionKey in config.tinyMceOptions) {
-                        options[tinyMceOptionKey] = config.tinyMceOptions[tinyMceOptionKey];
-                    }
-                }
-            }
+                                                                                if (config.tinyMceOptions) {
+                                                                                for ( var tinyMceOptionKey in config.tinyMceOptions) {
+                                                                                options[tinyMceOptionKey] = config.tinyMceOptions[tinyMceOptionKey];
+                                                                                }
+                                                                                }
+                                                                                }
 
-        } catch (e) {
-            // nothing to do
-        }
+                                                                                } catch (e) {
+                                                                                // nothing to do
+                                                                                }
 
-        return options;
-    }-*/;
+                                                                                return options;
+                                                                                }-*/;
 }
