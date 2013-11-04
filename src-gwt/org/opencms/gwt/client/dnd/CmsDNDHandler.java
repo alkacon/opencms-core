028980d404e5b24f03696ec06d1e30bb552f7c93
tHerrmann <t.herrmann@alkacon.com>
2013-11-04 11:38:46 +0100

    Improved animation positioning.

--- src-gwt/org/opencms/gwt/client/dnd/CmsDNDHandler.java
+++ src-gwt/org/opencms/gwt/client/dnd/CmsDNDHandler.java
@@ -31,6 +31,7 @@ import org.opencms.gwt.client.util.CmsDebugLog;
 import org.opencms.gwt.client.util.CmsDomUtil;
 import org.opencms.gwt.client.util.CmsDomUtil.Style;
 import org.opencms.gwt.client.util.CmsMoveAnimation;
+import org.opencms.gwt.client.util.CmsPositionBean;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -690,7 +691,8 @@ public class CmsDNDHandler implements MouseDownHandler {
                 clear();
             }
         };
-        showEndAnimation(callback, m_placeholder.getAbsoluteTop(), m_placeholder.getAbsoluteLeft());
+        CmsPositionBean placeholderPosition = CmsPositionBean.getInnerDimensions(m_placeholder);
+        showEndAnimation(callback, placeholderPosition.getTop(), placeholderPosition.getLeft());
     }
 
     /**
