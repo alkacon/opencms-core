69aa0ede2591ee8cf08977b2988a5f2c0cc5e63c
gWestenberger <g.westenberger@alkacon.com>
2014-05-22 10:21:14 +0200

    Fixed preview in history dialog for previously deleted  resources.

--- src/org/opencms/gwt/CmsVfsService.java
+++ src/org/opencms/gwt/CmsVfsService.java
@@ -232,7 +232,7 @@ public class CmsVfsService extends CmsGwtService implements I_CmsVfsService {
     public static String getNoPreviewReason(CmsObject cms, CmsResource resource) {
 
         String noPreviewReason = null;
-        if (resource.getState().isDeleted()) {
+        if (resource.getState().isDeleted() && !(resource instanceof I_CmsHistoryResource)) {
             noPreviewReason = Messages.get().getBundle().key(Messages.GUI_NO_PREVIEW_DELETED_0);
         } else if (resource.isFolder()) {
             noPreviewReason = Messages.get().getBundle().key(Messages.GUI_NO_PREVIEW_FOLDER_0);
