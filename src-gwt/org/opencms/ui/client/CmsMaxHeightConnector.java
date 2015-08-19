85695671b5b21a36a48abfa2191a0fda583c8e14
tHerrmann <t.herrmann@alkacon.com>
2015-08-19 12:10:27 +0200

    Added max height extension.

---
+++ src-gwt/org/opencms/ui/client/CmsMaxHeightConnector.java
@@ -0,0 +1,132 @@
+/*
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
+ *
+ * Copyright (c) Alkacon Software GmbH (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.opencms.ui.client;
+
+import org.opencms.ui.components.extensions.CmsMaxHeightExtension;
+import org.opencms.ui.shared.components.CmsMaxHeightState;
+import org.opencms.ui.shared.rpc.I_CmsMaxHeightServerRpc;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.ui.Widget;
+import com.vaadin.client.ComponentConnector;
+import com.vaadin.client.ServerConnector;
+import com.vaadin.client.extensions.AbstractExtensionConnector;
+import com.vaadin.shared.ui.Connect;
+
+/**
+ * This connector will manipulate the CSS classes of the extended widget depending on the scroll position.<p>
+ */
+@Connect(CmsMaxHeightExtension.class)
+public class CmsMaxHeightConnector extends AbstractExtensionConnector {
+
+    /** The serial version id. */
+    private static final long serialVersionUID = -3661096843568550285L;
+
+    /** The native mutation observer. */
+    private JavaScriptObject m_mutationObserver;
+
+    /** The RPC proxy. */
+    private I_CmsMaxHeightServerRpc m_rpc;
+
+    /** The widget to enhance. */
+    private Widget m_widget;
+
+    /**
+     * Constructor.<p>
+     */
+    public CmsMaxHeightConnector() {
+        m_rpc = getRpcProxy(I_CmsMaxHeightServerRpc.class);
+    }
+
+    /**
+     * @see com.vaadin.client.ui.AbstractConnector#getState()
+     */
+    @Override
+    public CmsMaxHeightState getState() {
+
+        return (CmsMaxHeightState)super.getState();
+    }
+
+    /**
+     * @see com.vaadin.client.extensions.AbstractExtensionConnector#extend(com.vaadin.client.ServerConnector)
+     */
+    @Override
+    protected void extend(ServerConnector target) {
+
+        // Get the extended widget
+        m_widget = ((ComponentConnector)target).getWidget();
+        addMutationObserver(m_widget.getElement());
+    }
+
+    /**
+     * Handles the widget mutation.<p>
+     */
+    protected void handleMutation() {
+
+        int maxHeight = getState().getMaxHeight();
+        if ((maxHeight > 0) && (m_widget.getOffsetHeight() > maxHeight)) {
+            m_rpc.fixHeight(maxHeight);
+            removeObserver();
+        }
+    }
+
+    /**
+     * Adds a native mutation observer to the widget element.<p>
+     *
+     * @param element the element
+     */
+    private native void addMutationObserver(Element element)/*-{
+        var self = this;
+        var observer = new MutationObserver(function(mutations) {
+            self.@org.opencms.ui.client.CmsMaxHeightConnector::handleMutation()();
+        });
+        this.@org.opencms.ui.client.CmsMaxHeightConnector::m_mutationObserver = observer;
+
+        // configuration of the observer:
+        var config = {
+            attributes : true,
+            childList : true,
+            characterData : true
+        };
+
+        // pass in the target node, as well as the observer options
+        observer.observe(element, config);
+
+    }-*/;
+
+    /**
+     * Removes the mutation observer.<p>
+     */
+    private native void removeObserver()/*-{
+        if (this.@org.opencms.ui.client.CmsMaxHeightConnector::m_mutationObserver != null) {
+            this.@org.opencms.ui.client.CmsMaxHeightConnector::m_mutationObserver.disconnect();
+            this.@org.opencms.ui.client.CmsMaxHeightConnector::m_mutationObserver = null;
+        }
+    }-*/;
+}
---
+++ src/org/opencms/ui/components/extensions/CmsMaxHeightExtension.java
@@ -0,0 +1,78 @@
+/*
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
+ *
+ * Copyright (c) Alkacon Software GmbH (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.opencms.ui.components.extensions;
+
+import org.opencms.ui.shared.components.CmsMaxHeightState;
+import org.opencms.ui.shared.rpc.I_CmsMaxHeightServerRpc;
+
+import com.vaadin.server.AbstractExtension;
+import com.vaadin.server.Sizeable.Unit;
+import com.vaadin.ui.AbstractComponent;
+
+/**
+ * Allows the use of max height in combination with vaadin layout components.<p>
+ */
+public class CmsMaxHeightExtension extends AbstractExtension implements I_CmsMaxHeightServerRpc {
+
+    /** The serial version id. */
+    private static final long serialVersionUID = 3978957151754705873L;
+
+    /** The extended component. */
+    private AbstractComponent m_component;
+
+    /**
+     * Constructor.<p>
+     *
+     * @param component the component to extend
+     * @param maxHeight the max height
+     */
+    public CmsMaxHeightExtension(AbstractComponent component, int maxHeight) {
+        m_component = component;
+        extend(component);
+        registerRpc(this);
+        getState().setMaxHeight(maxHeight);
+    }
+
+    /**
+     * @see org.opencms.ui.shared.rpc.I_CmsMaxHeightServerRpc#fixHeight(int)
+     */
+    public void fixHeight(int height) {
+
+        m_component.setHeight(height, Unit.PIXELS);
+    }
+
+    /**
+     * @see com.vaadin.server.AbstractClientConnector#getState()
+     */
+    @Override
+    protected CmsMaxHeightState getState() {
+
+        return (CmsMaxHeightState)super.getState();
+    }
+
+}
