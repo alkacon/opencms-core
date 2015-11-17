f026104ca45fcee8e7d77d12a15958b1b361ed4d
tHerrmann <t.herrmann@alkacon.com>
2015-11-17 16:52:51 +0100

    Fixing null pointer issue.

--- src/org/opencms/xml/containerpage/CmsFormatterBean.java
+++ src/org/opencms/xml/containerpage/CmsFormatterBean.java
@@ -550,7 +550,7 @@ public class CmsFormatterBean implements I_CmsFormatterBean {
     @Override
     public boolean isMatchAll() {
 
-        return m_matchAll || m_containerTypes.contains(WILDCARD_TYPE);
+        return m_matchAll || ((m_containerTypes != null) && m_containerTypes.contains(WILDCARD_TYPE));
     }
 
     /**
