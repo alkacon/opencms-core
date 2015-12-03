167dc9dc19b59c69d3e0e620939715dad3d8b167
gWestenberger <g.westenberger@alkacon.com>
2015-12-03 11:47:32 +0100

    Fixed broken references to editor button icons which don't exist anymore in the RFS

--- modules/org.opencms.editors.tinymce/resources/system/workplace/editors/tinymce/opencms_plugin.js
+++ modules/org.opencms.editors.tinymce/resources/system/workplace/editors/tinymce/opencms_plugin.js
@@ -390,31 +390,31 @@ tinymce.create('tinymce.opencms', {
       
       ed.addButton('OcmsImageGallery', {
          title: '<fmt:message key="GUI_IMAGE_GALLERY_TITLE_0" />',
-         image: '<%= org.opencms.workplace.CmsWorkplace.getResourceUri("editors/tinymce/toolbar/oc-imagegallery.png") %>',
+         image: '<%= org.opencms.workplace.CmsWorkplace.getStaticResourceUri("editors/tinymce/toolbar/oc-imagegallery.png") %>',
          cmd: "cmsImageGallery"
        });
       
       ed.addButton('OcmsDownloadGallery', {
          title: '<fmt:message key="GUI_DOWNLOAD_GALLERY_TITLE_0" />',
-         image: '<%= org.opencms.workplace.CmsWorkplace.getResourceUri("editors/tinymce/toolbar/oc-downloadgallery.png") %>',
+         image: '<%= org.opencms.workplace.CmsWorkplace.getStaticResourceUri("editors/tinymce/toolbar/oc-downloadgallery.png") %>',
          cmd: "cmsDownloadGallery"
       });
       
       ed.addButton('oc-link', {
          title: '<%= CmsEncoder.encodeJavaEntities(OpenCms.getWorkplaceManager().getMessages(OpenCms.getWorkplaceManager().getWorkplaceLocale(cms.getCmsObject())).key(org.opencms.workplace.editors.Messages.GUI_BUTTON_LINKTO_0), encoding) %>',
-         image: '<%= org.opencms.workplace.CmsWorkplace.getResourceUri("editors/tinymce/toolbar/oc-link.gif") %>',
+         image: '<%= org.opencms.workplace.CmsWorkplace.getStaticResourceUri("editors/tinymce/toolbar/oc-link.gif") %>',
          cmd: "cmsLink"
        });
       
       ed.addButton('OcmsHtmlGallery', {
          title : '<%=org.opencms.workplace.galleries.Messages.get().getBundle(OpenCms.getWorkplaceManager().getWorkplaceLocale(cms.getCmsObject())).key(org.opencms.workplace.galleries.Messages.GUI_HTMLGALLERY_EDITOR_TOOLTIP_0)%>',
-         image : '<%= org.opencms.workplace.CmsWorkplace.getResourceUri("editors/tinymce/toolbar/oc-htmlgallery.gif") %>',
+         image : '<%= org.opencms.workplace.CmsWorkplace.getStaticResourceUri("editors/tinymce/toolbar/oc-htmlgallery.gif") %>',
          cmd: 'cmsHtmlGallery'
       });
       
       ed.addButton('OcmsLinkGallery', { 
          title : '<%=org.opencms.workplace.galleries.Messages.get().getBundle(OpenCms.getWorkplaceManager().getWorkplaceLocale(cms.getCmsObject())).key(org.opencms.workplace.galleries.Messages.GUI_LINKGALLERY_EDITOR_TOOLTIP_0)%>',
-         image: '<%= org.opencms.workplace.CmsWorkplace.getResourceUri("editors/tinymce/toolbar/oc-linkgallery.gif") %>',
+         image: '<%= org.opencms.workplace.CmsWorkplace.getStaticResourceUri("editors/tinymce/toolbar/oc-linkgallery.gif") %>',
          cmd : 'cmsLinkGallery'
       });
       ed.addMenuItem('OcmsImageGallery', {
