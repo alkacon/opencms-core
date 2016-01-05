9b9aa51cb615f7b97b247f9d3dbf48c96d086b9f
Andreas Zahner <a.zahner@alkacon.com>
2016-01-05 12:41:35 +0100

    Added CodeMirror plugin to TinyMCE.

---
+++ modules/org.opencms.editors.tinymce/resources/system/workplace/resources/editors/tinymce/jscripts/tinymce/plugins/codemirror/langs/cs.js
@@ -0,0 +1,8 @@
+tinymce.addI18n('cs',{
+	'HTML source code': 'Zdrojov\u00fd k\u00f3d',
+	'Start search': 'Spustit vyhled\u00E1v\u00E1n\u00ED',
+	'Find next': 'Naj\u00edt dal\u0161\u00ed',
+	'Find previous': 'Naj\u00edt p\u0159edchoz\u00ed',
+	'Replace': 'Nahradit',
+	'Replace all': 'Nahradit v\u0161e'
+});
---
+++ modules/org.opencms.editors.tinymce/resources/system/workplace/resources/editors/tinymce/jscripts/tinymce/plugins/codemirror/langs/es.js
@@ -0,0 +1,8 @@
+tinymce.addI18n('es',{
+	'HTML source code': 'C\u00f3digo fuente',
+	'Start search': 'Iniciar busqueda',
+	'Find next': 'Buscar siguiente',
+	'Find previous': 'Buscar anterior',
+	'Replace': 'Reemplazar',
+	'Replace all': 'Reemplazar todo'
+});
