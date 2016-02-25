d8393384fd67b23d5b566665f330f3f9543cfaa2
gWestenberger <g.westenberger@alkacon.com>
2016-02-25 14:53:20 +0100

    Changed copyright date in login screen.

--- src/org/opencms/ui/login/CmsLoginHelper.java
+++ src/org/opencms/ui/login/CmsLoginHelper.java
@@ -303,7 +303,7 @@ public class CmsLoginHelper extends CmsJspLoginBean {
         html.append(Messages.get().getBundle(locale).key(Messages.GUI_LOGIN_TRADEMARKS_0));
         html.append("</div>\n");
         html.append("<div style=\"text-align: center; font-size: 10px; white-space: nowrap;\">");
-        html.append("&copy; 2002 - 2015 Alkacon Software GmbH. ");
+        html.append("&copy; 2002 - 2016 Alkacon Software GmbH. ");
         html.append(Messages.get().getBundle(locale).key(Messages.GUI_LOGIN_RIGHTS_RESERVED_0));
         html.append("</div>\n");
         return html.toString();
