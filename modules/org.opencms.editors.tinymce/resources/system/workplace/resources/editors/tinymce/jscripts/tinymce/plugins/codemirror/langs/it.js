35960daeb66ef855f7205fa0252a84ecb77d9f59
tHerrmann <t.herrmann@alkacon.com>
2016-03-21 11:08:42 +0100

    Removing no longer needed resources.

--- modules/org.opencms.editors.tinymce/resources/system/workplace/resources/editors/tinymce/jscripts/tinymce/plugins/codemirror/langs/it.js
+++
@@ -1,8 +0,0 @@
-tinymce.addI18n('it',{
-	'HTML source code': 'Codice Sorgente',
-	'Start search': 'Lanciare Ricerca',
-	'Find next': 'Trova Successivo',
-	'Find previous': 'Trova Precedente',
-	'Replace': 'Sostituisci',
-	'Replace all': 'Sostituisci Tutto'
-});
