22bea9a5ace7679d1d75d160809918fe11cfa0f1
dSeidel <d.seidel@alkacon.com>
2016-04-18 13:23:46 +0200

    Localized git tool in EN, fixed bug with missing "Deselect all" button

---
+++ src-modules/org/opencms/workplace/tools/git/Messages.java
@@ -0,0 +1,159 @@
+/*
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
+ *
+ * Copyright (c) Alkacon Software GmbH (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.opencms.workplace.tools.git;
+
+import org.opencms.i18n.A_CmsMessageBundle;
+import org.opencms.i18n.I_CmsMessageBundle;
+
+/**
+ * Message bundle class.<p>
+ */
+public final class Messages extends A_CmsMessageBundle {
+
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_LOG_OUTPUT_0 = "GUI_GIT_LOG_OUTPUT_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_CONFIGURATION_SELECTION_0 = "GUI_GIT_CONFIGURATION_SELECTION_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_MODULE_SELECTION_0 = "GUI_GIT_MODULE_SELECTION_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_MODULES_DESELECT_ALL_0 = "GUI_GIT_MODULES_DESELECT_ALL_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_SETTINGS_0 = "GUI_GIT_SETTINGS_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_CHECKIN_0 = "GUI_GIT_CHECKIN_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_USER_0 = "GUI_GIT_USER_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_NOT_ADJUSTED_0 = "GUI_GIT_NOT_ADJUSTED_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_EMAIL_0 = "GUI_GIT_EMAIL_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_COMMIT_MESSAGE_0 = "GUI_GIT_COMMIT_MESSAGE_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_TOGGLE_OPTIONS_0 = "GUI_GIT_TOGGLE_OPTIONS_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_IGNORE_UNCLEAN_0 = "GUI_GIT_IGNORE_UNCLEAN_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_IGNORE_UNCLEAN_DESC_0 = "GUI_GIT_IGNORE_UNCLEAN_DESC_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_PULL_FIRST_0 = "GUI_GIT_PULL_FIRST_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_PULL_FIRST_DESC_0 = "GUI_GIT_PULL_FIRST_DESC_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_COPY_UNZIP_0 = "GUI_GIT_COPY_UNZIP_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_COPY_UNZIP_DESC_0 = "GUI_GIT_COPY_UNZIP_DESC_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_ADD_COMMIT_0 = "GUI_GIT_ADD_COMMIT_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_PULL_AFTER_0 = "GUI_GIT_PULL_AFTER_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_PUSH_AUTOMATICALLY_0 = "GUI_GIT_PUSH_AUTOMATICALLY_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_EXCLUDE_LIB_FOLDER_0 = "GUI_GIT_EXCLUDE_LIB_FOLDER_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_IMPORT_0 = "GUI_GIT_IMPORT_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_FETCH_REMOTE_0 = "GUI_GIT_FETCH_REMOTE_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_BUTTON_CHECK_IN_0 = "GUI_GIT_BUTTON_CHECK_IN_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_APP_HELP_TEXT_0 = "GUI_GIT_APP_HELP_TEXT_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_APP_NAME_0 = "GUI_GIT_APP_NAME_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_APP_UNCONFIGURED_0 = "GUI_GIT_APP_UNCONFIGURED_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_SCRIPT_RESULTS_0 = "GUI_GIT_SCRIPT_RESULTS_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_BUTTON_RESET_HEAD_0 = "GUI_GIT_BUTTON_RESET_HEAD_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_BUTTON_RESET_HEAD_DESC_0 = "GUI_GIT_BUTTON_RESET_HEAD_DESC_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_BUTTON_RESET_REMOTE_HEAD_0 = "GUI_GIT_BUTTON_RESET_REMOTE_HEAD_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_BUTTON_RESET_REMOTE_HEAD_DESC_0 = "GUI_GIT_BUTTON_RESET_REMOTE_HEAD_DESC_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_CHECKIN_SUCCESSFUL_0 = "GUI_GIT_CHECKIN_SUCCESSFUL_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_CHECKIN_FAILED_0 = "GUI_GIT_CHECKIN_FAILED_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_IMPORT_SUCCESSFUL_0 = "GUI_GIT_IMPORT_SUCCESSFUL_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_IMPORT_FAILED_0 = "GUI_GIT_IMPORT_FAILED_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_RESET_SUCCESSFUL_0 = "GUI_GIT_RESET_SUCCESSFUL_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_RESET_FAILED_0 = "GUI_GIT_RESET_FAILED_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_OPTIONS_HIDE_0 = "GUI_GIT_OPTIONS_HIDE_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_OPTIONS_SHOW_0 = "GUI_GIT_OPTIONS_SHOW_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_BUTTON_IMPORT_0 = "GUI_GIT_BUTTON_IMPORT_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_CONFIGURATION_SWITCH_FAILED_0 = "GUI_GIT_CONFIGURATION_SWITCH_FAILED_0";
+    /** Message constant for key in the resource bundle. */
+    public static final String GUI_GIT_CONFIGURATION_SWITCH_FAILED_DESC_0 = "GUI_GIT_CONFIGURATION_SWITCH_FAILED_DESC_0";
+
+    /** Name of the used resource bundle. */
+    private static final String BUNDLE_NAME = "org.opencms.workplace.tools.git.messages";
+
+    /** Static instance member. */
+    private static final I_CmsMessageBundle INSTANCE = new Messages();
+
+    /**
+     * Hides the public constructor for this utility class.<p>
+     */
+    private Messages() {
+
+        // hide the constructor
+    }
+
+    /**
+     * Returns an instance of this localized message accessor.<p>
+     *
+     * @return an instance of this localized message accessor
+     */
+    public static I_CmsMessageBundle get() {
+
+        return INSTANCE;
+    }
+
+    /**
+     * Returns the bundle name for this OpenCms package.<p>
+     *
+     * @return the bundle name for this OpenCms package
+     */
+    public String getBundleName() {
+
+        return BUNDLE_NAME;
+    }
+
+}
