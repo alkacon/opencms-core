0b96004fce30ce4a6171a569afe3c69bda0513dc
tHerrmann <t.herrmann@alkacon.com>
2016-05-20 08:13:01 +0200

    Fixing null pointer issue.

--- src-gwt/org/opencms/ade/sitemap/client/hoverbar/CmsSetDefaultModelMenuEntry.java
+++ src-gwt/org/opencms/ade/sitemap/client/hoverbar/CmsSetDefaultModelMenuEntry.java
@@ -91,6 +91,7 @@ public class CmsSetDefaultModelMenuEntry extends A_CmsSitemapMenuEntry {
         CmsClientSitemapEntry entry = getHoverbar().getEntry();
         final CmsUUID id = entry.getId();
         boolean show = CmsSitemapView.getInstance().isModelPageMode()
+            && (CmsSitemapView.getInstance().getModelPageEntry(id) != null)
             && !CmsSitemapView.getInstance().getModelPageEntry(id).isDefault();
         setVisible(show);
     }
