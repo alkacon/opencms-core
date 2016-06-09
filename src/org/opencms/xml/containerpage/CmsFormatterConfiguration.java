0a229e06269867c2620aa6ff5132d4456e9c69db
tHerrmann <t.herrmann@alkacon.com>
2016-06-09 08:33:34 +0200

    Fixing issue #400 to avoid possible null pointer issue.

--- src/org/opencms/xml/containerpage/CmsFormatterConfiguration.java
+++ src/org/opencms/xml/containerpage/CmsFormatterConfiguration.java
@@ -357,7 +357,7 @@ public final class CmsFormatterConfiguration {
      */
     public I_CmsFormatterBean getDisplayFormatter() {
 
-        if (!getDetailFormatters().isEmpty()) {
+        if (!getDisplayFormatters().isEmpty()) {
             return getDisplayFormatters().get(0);
         }
         return null;
