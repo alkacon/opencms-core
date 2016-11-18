c80c32ad6d8bedafad464cb43b09252cc3d9195d
Tang LiangZhao <lztang1964@gmail.com>
2016-11-18 11:53:33 +0800

    add tinymce/plugin/codemirror/lang/zh.js and ja.js

---
+++ modules/org.opencms.editors.tinymce/static/editors/tinymce/jscripts/tinymce/plugins/codemirror/langs/ja.js
@@ -0,0 +1,8 @@
+tinymce.addI18n("ja",{
+	"HTML source code": "HTML\u30bd\u30fc\u30b9\u30b3\u30fc\u30c9",
+	"Start search": "\u691c\u7d22\u3092\u958b\u59cb\u3059\u308b",
+	"Find next": "\u6b21\u3092\u898b\u3064\u3051\u308b",
+	"Find previous": "\u524d\u306e\u691c\u7d22",
+	"Replace": "\u7f6e\u304d\u63db\u3048\u308b",
+	"Replace all": "\u3059\u3079\u3066\u3092\u7f6e\u304d\u63db\u3048\u308b"
+});
