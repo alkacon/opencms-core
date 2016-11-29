2d5d2213ed367785af6c78b7eefd426aa32cb6f9
tHerrmann <t.herrmann@alkacon.com>
2016-11-29 17:24:26 +0100

    Fixing issue #455 by adding Portuguese language file.

---
+++ modules/org.opencms.editors.tinymce/static/editors/tinymce/jscripts/tinymce/plugins/codemirror/langs/pt.js
@@ -0,0 +1,8 @@
+tinymce.addI18n('pt',{
+	'HTML source code': 'Código Fonte HTML',
+	'Start search': 'Iniciar pesquisa',
+	'Find next': 'Próximo',
+	'Find previous': 'Anterior',
+	'Replace': 'Substituir',
+	'Replace all': 'Substituir todos'
+});
