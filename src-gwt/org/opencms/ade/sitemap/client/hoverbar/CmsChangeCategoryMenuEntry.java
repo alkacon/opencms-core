9345ebb41254c679b84e24e04438af0b2fd3d4d6
gWestenberger <g.westenberger@alkacon.com>
2017-02-22 09:15:39 +0100

    Replaced 'edit category' dialog in sitemap editor with property dialog.

--- src-gwt/org/opencms/ade/sitemap/client/hoverbar/CmsChangeCategoryMenuEntry.java
+++ src-gwt/org/opencms/ade/sitemap/client/hoverbar/CmsChangeCategoryMenuEntry.java
@@ -28,23 +28,15 @@
 package org.opencms.ade.sitemap.client.hoverbar;
 
 import org.opencms.ade.sitemap.client.Messages;
-import org.opencms.ade.sitemap.client.control.CmsSitemapController;
-import org.opencms.ade.sitemap.client.hoverbar.CmsCreateCategoryMenuEntry.CmsCategoryTitleAndName;
-import org.opencms.file.CmsResource;
-import org.opencms.gwt.client.ui.CmsPopup;
-import org.opencms.gwt.client.ui.input.form.CmsBasicFormField;
-import org.opencms.gwt.client.ui.input.form.CmsDialogFormHandler;
-import org.opencms.gwt.client.ui.input.form.CmsForm;
-import org.opencms.gwt.client.ui.input.form.CmsFormDialog;
-import org.opencms.gwt.client.ui.input.form.I_CmsFormSubmitHandler;
-import org.opencms.gwt.shared.CmsCategoryTreeEntry;
+import org.opencms.gwt.client.ui.contenteditor.I_CmsContentEditorHandler;
+import org.opencms.gwt.client.ui.contextmenu.CmsEditProperties;
+import org.opencms.gwt.client.ui.contextmenu.CmsEditProperties.PropertyEditingContext;
+import org.opencms.gwt.client.ui.contextmenu.I_CmsContextMenuCommand;
+import org.opencms.gwt.client.ui.contextmenu.I_CmsContextMenuHandler;
+import org.opencms.gwt.client.util.I_CmsSimpleCallback;
 import org.opencms.util.CmsUUID;
-import org.opencms.xml.content.CmsXmlContentProperty;
 
 import java.util.Map;
-import java.util.Set;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
  * Menu entry for changing categories.<p>
@@ -68,30 +60,51 @@ public class CmsChangeCategoryMenuEntry extends A_CmsSitemapMenuEntry {
      */
     public void execute() {
 
-        final CmsSitemapController controller = getHoverbar().getController();
+        CmsUUID id = getHoverbar().getId();
+        PropertyEditingContext context = new PropertyEditingContext();
+        I_CmsContextMenuHandler handler = new I_CmsContextMenuHandler() {
 
-        final CmsUUID id = getHoverbar().getId();
-        CmsCategoryTreeEntry entry = controller.getCategoryData().getEntryById(id);
-        if (entry != null) {
-            String title = entry.getTitle();
-            String name = CmsResource.getName(entry.getPath()).replaceAll("/", "");
-            String caption = Messages.get().key(Messages.GUI_EDIT_CATEGORY_CAPTION_1, entry.getTitleOrName());
+            // this context menu handler only implements the refreshResource method
 
-            askForCategoryInfo(caption, title, name, new AsyncCallback<CmsCategoryTitleAndName>() {
+            public void ensureLockOnResource(CmsUUID structureId, I_CmsSimpleCallback<Boolean> callback) {
+                // do nothing
+            }
 
-                public void onFailure(Throwable caught) {
+            public Map<String, I_CmsContextMenuCommand> getContextMenuCommands() {
 
-                    // do nothing
-                }
+                // do nothing
+                return null;
+            }
 
-                public void onSuccess(CmsCategoryTitleAndName result) {
+            public String getContextType() {
 
-                    controller.changeCategory(id, result.getTitle(), result.getName());
-                }
-            });
+                return null;
+            }
 
-        }
+            public I_CmsContentEditorHandler getEditorHandler() {
 
+                // do nothing
+                return null;
+            }
+
+            public void leavePage(String targetUri) {
+                // do nothing
+            }
+
+            public void onSiteOrProjectChange(String sitePath, String serverLink) {
+                // do nothing
+            }
+
+            public void refreshResource(CmsUUID structureId) {
+
+                getHoverbar().getController().loadCategories(true);
+            }
+
+            public void unlockResource(CmsUUID structureId) {
+                // do nothing
+            }
+        };
+        CmsEditProperties.editProperties(id, handler, true, null, false, context);
     }
 
     /**
@@ -106,69 +119,4 @@ public class CmsChangeCategoryMenuEntry extends A_CmsSitemapMenuEntry {
         setVisible(visible);
     }
 
-    /**
-     * Creates a property configuration for a simple named string field.<p>
-     *
-     * @param name the name of the field
-     * @param niceName the display name of the field
-     *
-     * @return the property configuration
-     */
-    CmsXmlContentProperty createBasicStringProperty(String name, String niceName) {
-
-        CmsXmlContentProperty prop = new CmsXmlContentProperty(
-            name, //name
-            "string", // type
-            "string", // widget
-            "", // widgetconfig
-            null, //regex
-            null, //ruletype
-            null, //default
-            niceName, //nicename
-            null, //description
-            null, //error
-            null //preferfolder
-        );
-        return prop;
-    }
-
-    /**
-     * Asks the user for the changed category info.<p>
-     *
-     * @param caption the dialog caption
-     * @param title the old title
-     * @param name the old name
-     * @param callback the callback to call when done
-     *
-     */
-    private void askForCategoryInfo(
-        String caption,
-        String title,
-        String name,
-        final AsyncCallback<CmsCategoryTitleAndName> callback) {
-
-        CmsFormDialog dlg = new CmsFormDialog(caption, new CmsForm(true));
-        dlg.setWidth(CmsPopup.DEFAULT_WIDTH);
-        CmsDialogFormHandler fh = new CmsDialogFormHandler();
-        fh.setSubmitHandler(new I_CmsFormSubmitHandler() {
-
-            public void onSubmitForm(CmsForm form, Map<String, String> fieldValues, Set<String> editedFields) {
-
-                String formtitle = fieldValues.get("title");
-                String formname = fieldValues.get("name");
-                callback.onSuccess(new CmsCategoryTitleAndName(formtitle, formname));
-
-            }
-        });
-
-        dlg.setFormHandler(fh);
-        fh.setDialog(dlg);
-        String titleLabel = Messages.get().key(Messages.GUI_CATEGORY_TITLE_LABEL_0);
-        dlg.getForm().addField(CmsBasicFormField.createField(createBasicStringProperty("title", titleLabel)), title);
-        String nameLabel = Messages.get().key(Messages.GUI_CATEGORY_NAME_LABEL_0);
-        dlg.getForm().addField(CmsBasicFormField.createField(createBasicStringProperty("name", nameLabel)), name);
-        dlg.getForm().render();
-        dlg.center();
-
-    }
 }
