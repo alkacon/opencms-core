f61cc327d28d7cfe47bd790d9c3752daabd5580c
tHerrmann <t.herrmann@alkacon.com>
2017-03-16 15:53:20 +0100

    Allowing multiple workplace servers.

--- src/org/opencms/configuration/CmsSystemConfiguration.java
+++ src/org/opencms/configuration/CmsSystemConfiguration.java
@@ -953,7 +953,7 @@ public class CmsSystemConfiguration extends A_CmsXmlConfiguration {
 
         // add site configuration rule
         digester.addObjectCreate("*/" + N_SYSTEM + "/" + N_SITES, CmsSiteManagerImpl.class);
-        digester.addCallMethod("*/" + N_SYSTEM + "/" + N_SITES + "/" + N_WORKPLACE_SERVER, "setWorkplaceServer", 0);
+        digester.addCallMethod("*/" + N_SYSTEM + "/" + N_SITES + "/" + N_WORKPLACE_SERVER, "addWorkplaceServer", 0);
         digester.addCallMethod("*/" + N_SYSTEM + "/" + N_SITES + "/" + N_DEFAULT_URI, "setDefaultUri", 0);
         digester.addSetNext("*/" + N_SYSTEM + "/" + N_SITES, "setSiteManager");
 
