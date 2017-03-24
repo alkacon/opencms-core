36ce9e59fcc25a63e370d937409fa0d35da18726
tHerrmann <t.herrmann@alkacon.com>
2017-03-24 14:33:31 +0100

    Fixing issue where the configured workplace servers where not persisted.

--- src/org/opencms/configuration/CmsSystemConfiguration.java
+++ src/org/opencms/configuration/CmsSystemConfiguration.java
@@ -1405,7 +1405,9 @@ public class CmsSystemConfiguration extends A_CmsXmlConfiguration {
 
         // create <sites> node
         Element sitesElement = systemElement.addElement(N_SITES);
-        sitesElement.addElement(N_WORKPLACE_SERVER).addText(m_siteManager.getWorkplaceServer());
+        for (String server : m_siteManager.getWorkplaceServers()) {
+            sitesElement.addElement(N_WORKPLACE_SERVER).addText(server);
+        }
         sitesElement.addElement(N_DEFAULT_URI).addText(m_siteManager.getDefaultUri());
         String sharedFolder = m_siteManager.getSharedFolder();
         if (sharedFolder != null) {
