77294bb189af9f70c0a418b4e500e0713de96a11
sHuewe <s.huewe@alkacon.com>
2017-05-30 14:36:42 +0200

    CmsSourceSearchApp: fix issue to read-out state correctly

--- src/org/opencms/ui/apps/search/CmsSourceSearchApp.java
+++ src/org/opencms/ui/apps/search/CmsSourceSearchApp.java
@@ -135,7 +135,7 @@ public class CmsSourceSearchApp extends A_CmsWorkplaceApp implements I_CmsCachab
      *
      * @return the state string
      */
-    static String generateState(CmsSearchReplaceSettings settings) {
+    public static String generateState(CmsSearchReplaceSettings settings) {
 
         String state = "";
         state = A_CmsWorkplaceApp.addParamToState(state, SITE_ROOT, settings.getSiteRoot());
@@ -172,7 +172,7 @@ public class CmsSourceSearchApp extends A_CmsWorkplaceApp implements I_CmsCachab
             settings.setPaths(Collections.singletonList(A_CmsWorkplaceApp.getParamFromState(state, FOLDER)));
             String resType = A_CmsWorkplaceApp.getParamFromState(state, RESOURCE_TYPE);
             if (resType != null) {
-                settings.setTypes(typeString);
+                settings.setTypes(resType);
             }
             String project = A_CmsWorkplaceApp.getParamFromState(state, PROJECT);
             if (project != null) {
