8d08c50c11dae2fff37b00065e26a0afda4ac064
dSeidel <d.seidel@alkacon.com>
2017-07-27 10:52:38 +0200

    Added xml content type OpenCmsSerialDate and configured the serial date widget as default widget for it.
    The type essentially validates the string value specifying the series.
    Otherwise it's just like OpenCmsString.

---
+++ src/org/opencms/xml/types/CmsXmlSerialDateValue.java
@@ -0,0 +1,149 @@
+/*
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
+ *
+ * Copyright (c) Alkacon Software GmbH & Co. KG (http://www.alkacon.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about Alkacon Software GmbH & Co. KG, please see the
+ * company website: http://www.alkacon.com
+ *
+ * For further information about OpenCms, please see the
+ * project website: http://www.opencms.org
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.opencms.xml.types;
+
+import org.opencms.widgets.serialdate.CmsSerialDateValueWrapper;
+import org.opencms.xml.I_CmsXmlDocument;
+
+import java.util.Locale;
+
+import org.dom4j.Element;
+
+/**
+ * Describes the XML content type "OpenCmsSerialDate".<p>
+ *
+ * @since 11.0.0
+ */
+public class CmsXmlSerialDateValue extends A_CmsXmlValueTextBase {
+
+    /** The name of this type as used in the XML schema. */
+    public static final String TYPE_NAME = "OpenCmsSerialDate";
+
+    /**
+     * Creates a new, empty schema type descriptor of type "OpenCmsDateTime".<p>
+     */
+    public CmsXmlSerialDateValue() {
+
+        // empty constructor is required for class registration
+    }
+
+    /**
+     * Creates a new XML content value of type "OpenCmsSerialDate".<p>
+     *
+     * @param document the XML content instance this value belongs to
+     * @param element the XML element that contains this value
+     * @param locale the locale this value is created for
+     * @param type the type instance to create the value for
+     */
+    public CmsXmlSerialDateValue(I_CmsXmlDocument document, Element element, Locale locale, I_CmsXmlSchemaType type) {
+
+        super(document, element, locale, type);
+    }
+
+    /**
+     * Creates a new schema type descriptor for the type "OpenCmsDateTime".<p>
+     *
+     * @param name the name of the XML node containing the value according to the XML schema
+     * @param minOccurs minimum number of occurrences of this type according to the XML schema
+     * @param maxOccurs maximum number of occurrences of this type according to the XML schema
+     */
+    public CmsXmlSerialDateValue(String name, String minOccurs, String maxOccurs) {
+
+        super(name, minOccurs, maxOccurs);
+    }
+
+    /**
+     * @see org.opencms.xml.types.A_CmsXmlContentValue#createValue(I_CmsXmlDocument, org.dom4j.Element, Locale)
+     */
+    public I_CmsXmlContentValue createValue(I_CmsXmlDocument document, Element element, Locale locale) {
+
+        return new CmsXmlSerialDateValue(document, element, locale, this);
+    }
+
+    /**
+     * @see org.opencms.xml.types.A_CmsXmlContentValue#getDefault(Locale)
+     */
+    @Override
+    public String getDefault(Locale locale) {
+
+        if (m_defaultValue != null) {
+            return m_defaultValue;
+        }
+        return "";
+    }
+
+    /**
+     * @see org.opencms.xml.types.I_CmsXmlSchemaType#getSchemaDefinition()
+     */
+    public String getSchemaDefinition() {
+
+        return "<xsd:simpleType name=\"" + TYPE_NAME + "\"><xsd:restriction base=\"xsd:string\" /></xsd:simpleType>";
+    }
+
+    /**
+     * @see org.opencms.xml.types.A_CmsXmlContentValue#getTypeName()
+     */
+    public String getTypeName() {
+
+        return TYPE_NAME;
+    }
+
+    /**
+     * @see org.opencms.xml.types.A_CmsXmlContentValue#isSearchable()
+     */
+    @Override
+    public boolean isSearchable() {
+
+        // there is no point in searching date/time values
+        return false;
+    }
+
+    /**
+     * @see org.opencms.xml.types.A_CmsXmlContentValue#newInstance(java.lang.String, java.lang.String, java.lang.String)
+     */
+    public I_CmsXmlSchemaType newInstance(String name, String minOccurs, String maxOccurs) {
+
+        return new CmsXmlSerialDateValue(name, minOccurs, maxOccurs);
+    }
+
+    /**
+     * @see org.opencms.xml.types.I_CmsXmlSchemaType#validateValue(java.lang.String)
+     */
+    @Override
+    public boolean validateValue(String value) {
+
+        try {
+            // convert to value to see if the string is valid.
+            CmsSerialDateValueWrapper wrapper = new CmsSerialDateValueWrapper(value);
+            return wrapper.isValid();
+        } catch (@SuppressWarnings("unused") Exception e) {
+            // Do nothing, just return false
+        }
+        return false;
+    }
+}
