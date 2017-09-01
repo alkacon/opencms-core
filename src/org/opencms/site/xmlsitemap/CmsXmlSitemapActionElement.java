9ac0ae15212a01587ea58cab16898e0a42f7e60b
gWestenberger <g.westenberger@alkacon.com>
2017-09-01 15:10:58 +0200

    Moved xml sitemap generator instantiation / configuration to static methods.

--- src/org/opencms/site/xmlsitemap/CmsXmlSitemapActionElement.java
+++ src/org/opencms/site/xmlsitemap/CmsXmlSitemapActionElement.java
@@ -85,7 +85,8 @@ public class CmsXmlSitemapActionElement extends CmsJspActionElement {
      *
      * @throws CmsException if something goes wrong
      */
-    public CmsXmlSitemapGenerator createSitemapGenerator(String className, String folderRootPath) throws CmsException {
+    public static CmsXmlSitemapGenerator createSitemapGenerator(String className, String folderRootPath)
+    throws CmsException {
 
         if (CmsStringUtil.isEmptyOrWhitespaceOnly(className)) {
             className = (String)(OpenCms.getRuntimeProperty(PARAM_DEFAULT_SITEMAP_GENERATOR));
@@ -108,29 +109,37 @@ public class CmsXmlSitemapActionElement extends CmsJspActionElement {
     }
 
     /**
-     * Writes the XML sitemap to the response.<p>
+     * Constructs an XML sitemap generator given an XML sitemap configuration file.<p>
      *
-     * @throws Exception if something goes wrong
+     * @param cms the CMS context
+     * @param sitemapXml the sitemap XML file
+     * @return the sitemap generator
+     *
+     * @throws CmsException if something goes wrong
      */
-    public void renderXmlSitemap() throws Exception {
+    public static CmsXmlSitemapGenerator prepareSitemapGenerator(CmsObject cms, CmsResource sitemapXml)
+    throws CmsException {
 
-        CmsObject cms = getCmsObject();
-        String baseFolderRootPath = CmsFileUtil.removeTrailingSeparator(
-            CmsResource.getParentFolder(cms.getRequestContext().addSiteRoot(cms.getRequestContext().getUri())));
-        CmsXmlSitemapGenerator xmlSitemapGenerator = createSitemapGenerator(
-            m_configuration.getSitemapGeneratorClassName(),
-            baseFolderRootPath);
-        xmlSitemapGenerator.setComputeContainerPageDates(m_configuration.shouldComputeContainerPageModificationDates());
-        CmsPathIncludeExcludeSet inexcludeSet = xmlSitemapGenerator.getIncludeExcludeSet();
-        for (String include : m_configuration.getIncludes()) {
-            inexcludeSet.addInclude(include);
+        CmsXmlSeoConfiguration config = new CmsXmlSeoConfiguration();
+        config.load(cms, sitemapXml);
+        if (config.getMode().equals(CmsXmlSeoConfiguration.MODE_XML_SITEMAP)) {
+            String baseFolderRootPath = CmsFileUtil.removeTrailingSeparator(
+                CmsResource.getParentFolder(sitemapXml.getRootPath()));
+            CmsXmlSitemapGenerator xmlSitemapGenerator = createSitemapGenerator(
+                config.getSitemapGeneratorClassName(),
+                baseFolderRootPath);
+            xmlSitemapGenerator.setComputeContainerPageDates(config.shouldComputeContainerPageModificationDates());
+            CmsPathIncludeExcludeSet inexcludeSet = xmlSitemapGenerator.getIncludeExcludeSet();
+            for (String include : config.getIncludes()) {
+                inexcludeSet.addInclude(include);
+            }
+            for (String exclude : config.getExcludes()) {
+                inexcludeSet.addExclude(exclude);
+            }
+            xmlSitemapGenerator.setServerUrl(config.getServerUrl());
+            return xmlSitemapGenerator;
         }
-        for (String exclude : m_configuration.getExcludes()) {
-            inexcludeSet.addExclude(exclude);
-        }
-        xmlSitemapGenerator.setServerUrl(m_configuration.getServerUrl());
-        String xmlSitemap = xmlSitemapGenerator.renderSitemap();
-        getResponse().getWriter().print(xmlSitemap);
+        return null;
     }
 
     /**
@@ -149,7 +158,8 @@ public class CmsXmlSitemapActionElement extends CmsJspActionElement {
         if (mode.equals(CmsXmlSeoConfiguration.MODE_ROBOTS_TXT)) {
             showRobotsTxt();
         } else {
-            renderXmlSitemap();
+            CmsXmlSitemapGenerator generator = prepareSitemapGenerator(cms, seoFile);
+            getResponse().getWriter().print(generator.renderSitemap());
         }
     }
 
