9b048324c53337a04686812c18f43912f24b69ef
dSeidel <d.seidel@alkacon.com>
2018-09-11 14:08:54 +0200

    Gallery search results: display results of type function_config in the workplace locale.

--- src/org/opencms/search/galleries/CmsGallerySearchResult.java
+++ src/org/opencms/search/galleries/CmsGallerySearchResult.java
@@ -31,6 +31,7 @@ import org.opencms.file.CmsObject;
 import org.opencms.file.CmsProperty;
 import org.opencms.file.CmsPropertyDefinition;
 import org.opencms.file.CmsResource;
+import org.opencms.file.types.CmsResourceTypeFunctionConfig;
 import org.opencms.file.types.CmsResourceTypeXmlContainerPage;
 import org.opencms.i18n.CmsLocaleManager;
 import org.opencms.loader.CmsLoaderException;
@@ -179,6 +180,10 @@ public class CmsGallerySearchResult implements Comparable<CmsGallerySearchResult
 
         if (null == doc) {
             throw new IllegalArgumentException();
+        }
+
+        if (doc.getType().equals(CmsResourceTypeFunctionConfig.TYPE_NAME)) {
+            locale = OpenCms.getWorkplaceManager().getWorkplaceLocale(cms);
         }
 
         m_score = score; //(int)doc.getScore();
