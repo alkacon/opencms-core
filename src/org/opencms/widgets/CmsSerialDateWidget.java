604fe5e92197ae17406ebb5b05f033014b104c11
dSeidel <d.seidel@alkacon.com>
2019-03-18 12:57:10 +0100

    Changed comments for CmsSerialDateWidget.

--- src/org/opencms/widgets/CmsSerialDateWidget.java
+++ src/org/opencms/widgets/CmsSerialDateWidget.java
@@ -1,33 +1,28 @@
 /*
- * File   : $Source: /alkacon/cvs/alkacon/com.alkacon.opencms.v8.calendar/src/com/alkacon/opencms/v8/calendar/CmsSerialDateWidget.java,v $
- * Date   : $Date: 2008/04/25 14:50:41 $
- * Version: $Revision: 1.1 $
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
  *
- * This file is part of the Alkacon OpenCms Add-On Module Package
+ * Copyright (c) Alkacon Software GmbH & Co. KG (http://www.alkacon.com)
  *
- * Copyright (c) 2008 Alkacon Software GmbH (http://www.alkacon.com)
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * The Alkacon OpenCms Add-On Module Package is free software:
- * you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The Alkacon OpenCms Add-On Module Package is distributed
- * in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with the Alkacon OpenCms Add-On Module Package.
- * If not, see http://www.gnu.org/licenses/.
- *
- * For further information about Alkacon Software GmbH, please see the
- * company website: http://www.alkacon.com.
+ * For further information about Alkacon Software GmbH & Co. KG, please see the
+ * company website: http://www.alkacon.com
  *
  * For further information about OpenCms, please see the
- * project website: http://www.opencms.org.
+ * project website: http://www.opencms.org
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 package org.opencms.widgets;
@@ -44,11 +39,11 @@ import java.util.Locale;
 /**
  * Provides a serial date widget, for use on a widget dialog.<p>
  *
- * @author Andreas Zahner
+ * @author Daniel Seidel
  *
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.0 $
  *
- * @since 7.0.2
+ * @since 11.0.0
  */
 public class CmsSerialDateWidget extends A_CmsWidget implements I_CmsADEWidget {
 
