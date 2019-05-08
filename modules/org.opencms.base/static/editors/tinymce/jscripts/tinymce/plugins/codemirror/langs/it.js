8e9e33875fa7dc05c786a335ba5e2f5863519ef9
Giorgio Giudetti <g.giudetti@inera.it>
2019-05-08 11:17:02 +0200

    Adds italian locale for the codemirror plugin of tinymce

---
+++ modules/org.opencms.base/static/editors/tinymce/jscripts/tinymce/plugins/codemirror/langs/it.js
@@ -0,0 +1,8 @@
+tinymce.addI18n('it',{
+	'HTML source code': 'Codice sorgente HTML',
+	'Start search': 'Inizia ricerca',
+	'Find next': 'Trova successivo',
+	'Find previous': 'Trova precedente',
+	'Replace': 'Sostituisci',
+	'Replace all': 'Sostituisci tutti'
+});
