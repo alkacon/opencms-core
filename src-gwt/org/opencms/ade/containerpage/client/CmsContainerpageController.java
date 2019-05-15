d778044b60cd8be5105e3431197de5702cd79cff
tHerrmann <t.herrmann@alkacon.com>
2019-05-15 20:50:27 +0200

    Fixing issue where group element vanished after creating a new element inside.

--- src-gwt/org/opencms/ade/containerpage/client/CmsContainerpageController.java
+++ src-gwt/org/opencms/ade/containerpage/client/CmsContainerpageController.java
@@ -3362,8 +3362,9 @@ public final class CmsContainerpageController {
         }
         element.setId(newElementData.getClientId());
         element.setSitePath(newElementData.getSitePath());
-
-        setPageChanged();
+        if (!isGroupcontainerEditing()) {
+            setPageChanged();
+        }
         getHandler().hidePageOverlay();
         getHandler().openEditorForElement(element, inline);
     }
