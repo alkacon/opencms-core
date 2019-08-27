c0ffab6e950ee49a211ab840c8885d61463b3b5d
Gebert, Anne <a.gebert@comundus.com>
2019-08-27 11:34:21 +0200

    fixed bug in flex cache causing concurrent modification exception

--- src/org/opencms/flex/CmsFlexCache.java
+++ src/org/opencms/flex/CmsFlexCache.java
@@ -168,15 +168,16 @@ public class CmsFlexCache extends Object implements I_CmsEventListener {
             if ((m == null) || (m.size() == 0)) {
                 return true;
             }
-            Collection<I_CmsLruCacheObject> entries = m.values();
-            synchronized (m_variationCache) {
-                for (I_CmsLruCacheObject e : entries) {
-                    m_variationCache.remove(e);
-                }
-                v.m_map.clear();
-                v.m_map = null;
-                v.m_key = null;
+            Iterator<I_CmsLruCacheObject> allEntries = v.m_map.values().iterator();
+            while(allEntries.hasNext()) {
+                I_CmsLruCacheObject nextObject = allEntries.next();
+                allEntries.remove();
+                m_variationCache.remove(nextObject);
             }
+            v.m_map.clear();
+            v.m_map = null;
+            v.m_key = null;
+            
             return true;
         }
     }
@@ -780,7 +781,7 @@ public class CmsFlexCache extends Object implements I_CmsEventListener {
         if (o == null) {
             // No variation map for this resource yet, so create one
             CmsFlexCacheVariation variationMap = new CmsFlexCacheVariation(key);
-            m_keyCache.put(key.getResource(), variationMap);
+            synchronizedCopyMap(m_keyCache).put(key.getResource(), variationMap);
             if (LOG.isDebugEnabled()) {
                 LOG.debug(Messages.get().getBundle().key(Messages.LOG_FLEXCACHE_ADD_KEY_1, key.getResource()));
             }
