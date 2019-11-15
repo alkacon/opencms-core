aeb04f538bc39d3a26cb020d8a0376d8bdf40583
dSeidel <d.seidel@alkacon.com>
2019-11-15 13:56:15 +0100

    cms:search - ignore facet filters for empty request parameters.

--- src/org/opencms/jsp/search/controller/CmsSearchControllerFacetQuery.java
+++ src/org/opencms/jsp/search/controller/CmsSearchControllerFacetQuery.java
@@ -123,7 +123,10 @@ public class CmsSearchControllerFacetQuery implements I_CmsSearchControllerFacet
             if (!m_state.getIgnoreChecked() && parameters.containsKey(m_config.getParamKey())) {
                 final String[] checked = parameters.get(m_config.getParamKey());
                 for (int i = 0; i < checked.length; i++) {
-                    m_state.addChecked(checked[i]);
+                    String checkedEntry = checked[i];
+                    if ((null != checkedEntry) && !checkedEntry.isEmpty()) {
+                        m_state.addChecked(checkedEntry);
+                    }
                 }
 
             }
