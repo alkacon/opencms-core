90a06a96e03ca6b3fbf46e8d3e54361c0f0d0108
gWestenberger <g.westenberger@alkacon.com>
2024-03-27 11:08:00 +0100

    Updated copyright year.

--- src/org/opencms/ui/login/CmsLoginHelper.java
+++ src/org/opencms/ui/login/CmsLoginHelper.java
@@ -310,7 +310,7 @@ public class CmsLoginHelper extends CmsJspLoginBean {
         html.append(Messages.get().getBundle(locale).key(Messages.GUI_LOGIN_TRADEMARKS_0));
         html.append("</div>\n");
         html.append("<div style=\"text-align: center; font-size: 10px; white-space: nowrap;\">");
-        html.append("&copy; 2002 - 2023 Alkacon Software GmbH &amp; Co. KG. ");
+        html.append("&copy; 2002 - 2024 Alkacon Software GmbH &amp; Co. KG. ");
         html.append(Messages.get().getBundle(locale).key(Messages.GUI_LOGIN_RIGHTS_RESERVED_0));
         html.append("</div>\n");
         return html.toString();
