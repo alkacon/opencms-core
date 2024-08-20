a4e5d194d42a99dcedc6574472321530b388563a
dSeidel <d.seidel@alkacon.com>
2024-08-20 13:08:12 +0200

    Fixed typo in method name: CmsJspResourceWrapper#getPropertyKeywords.

--- src/org/opencms/jsp/CmsJspResourceWrapper.java
+++ src/org/opencms/jsp/CmsJspResourceWrapper.java
@@ -599,21 +599,6 @@ public class CmsJspResourceWrapper extends CmsResource {
     }
 
     /**
-     * Returns the locale specific evaluated "Keywords" property for the resource.
-     * In case the resource is a default file, also the parent folders "Keywords" property is read as fallback.
-     *
-     * @return the locale specific description property for the resource.
-     */
-    public String getPropertKeywords() {
-
-        return CmsVfsUtil.readPropertyValueWithFolderFallbackForDefaultFiles(
-            m_cms,
-            this,
-            CmsPropertyDefinition.PROPERTY_KEYWORDS,
-            m_cms.getRequestContext().getLocale());
-    }
-
-    /**
      * Returns the direct properties of this resource in a map.<p>
      *
      * This is without "search", so it will not include inherited properties from the parent folders.<p>
@@ -647,6 +632,21 @@ public class CmsJspResourceWrapper extends CmsResource {
             m_cms,
             this,
             CmsPropertyDefinition.PROPERTY_DESCRIPTION,
+            m_cms.getRequestContext().getLocale());
+    }
+
+    /**
+     * Returns the locale specific evaluated "Keywords" property for the resource.
+     * In case the resource is a default file, also the parent folders "Keywords" property is read as fallback.
+     *
+     * @return the locale specific description property for the resource.
+     */
+    public String getPropertyKeywords() {
+
+        return CmsVfsUtil.readPropertyValueWithFolderFallbackForDefaultFiles(
+            m_cms,
+            this,
+            CmsPropertyDefinition.PROPERTY_KEYWORDS,
             m_cms.getRequestContext().getLocale());
     }
 
