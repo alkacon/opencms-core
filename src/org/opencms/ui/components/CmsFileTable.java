34a8510700ee9e79ac14f9b9601aa5ef5ce803b5
gWestenberger <g.westenberger@alkacon.com>
2025-05-26 10:58:33 +0200

    Fixed file table column collapse logic.
    The saved column collapse state in CmsFileExplorerSettings is now only
    applied for the default columns from CmsResoureTableProperties, leaving
    the collapse state of additional columns (defined by subclasses)
    untouched.

--- src/org/opencms/ui/components/CmsFileTable.java
+++ src/org/opencms/ui/components/CmsFileTable.java
@@ -257,35 +257,35 @@ public class CmsFileTable extends CmsResourceTable {
             } else if ((CmsResourceTableProperty.PROPERTY_TYPE_ICON.equals(propertyId)
                 || CmsResourceTableProperty.PROPERTY_NAVIGATION_TEXT.equals(propertyId))
                 && (item1.getItemProperty(CmsResourceTableProperty.PROPERTY_NAVIGATION_POSITION) != null)) {
-                    int result;
-                    Float pos1 = (Float)item1.getItemProperty(
-                        CmsResourceTableProperty.PROPERTY_NAVIGATION_POSITION).getValue();
-                    Float pos2 = (Float)item2.getItemProperty(
-                        CmsResourceTableProperty.PROPERTY_NAVIGATION_POSITION).getValue();
-                    if (pos1 == null) {
-                        result = pos2 == null
-                        ? compareProperty(CmsResourceTableProperty.PROPERTY_RESOURCE_NAME, true, item1, item2)
-                        : 1;
-                    } else {
-                        result = pos2 == null ? -1 : Float.compare(pos1.floatValue(), pos2.floatValue());
-                    }
-                    if (!sortDirection) {
-                        result = result * (-1);
-                    }
-                    return result;
-                } else if (((CmsResourceTableProperty)propertyId).getColumnType().equals(String.class)) {
-                    String value1 = (String)item1.getItemProperty(propertyId).getValue();
-                    String value2 = (String)item2.getItemProperty(propertyId).getValue();
-                    // Java collators obtained by java.text.Collator.getInstance(...) ignore spaces, and we don't want to ignore them, so we use
-                    // ICU collators instead
-                    com.ibm.icu.text.Collator collator = com.ibm.icu.text.Collator.getInstance(
-                        com.ibm.icu.util.ULocale.ROOT);
-                    int result = collator.compare(value1, value2);
-                    if (!sortDirection) {
-                        result = -result;
-                    }
-                    return result;
+                int result;
+                Float pos1 = (Float)item1.getItemProperty(
+                    CmsResourceTableProperty.PROPERTY_NAVIGATION_POSITION).getValue();
+                Float pos2 = (Float)item2.getItemProperty(
+                    CmsResourceTableProperty.PROPERTY_NAVIGATION_POSITION).getValue();
+                if (pos1 == null) {
+                    result = pos2 == null
+                    ? compareProperty(CmsResourceTableProperty.PROPERTY_RESOURCE_NAME, true, item1, item2)
+                    : 1;
+                } else {
+                    result = pos2 == null ? -1 : Float.compare(pos1.floatValue(), pos2.floatValue());
                 }
+                if (!sortDirection) {
+                    result = result * (-1);
+                }
+                return result;
+            } else if (((CmsResourceTableProperty)propertyId).getColumnType().equals(String.class)) {
+                String value1 = (String)item1.getItemProperty(propertyId).getValue();
+                String value2 = (String)item2.getItemProperty(propertyId).getValue();
+                // Java collators obtained by java.text.Collator.getInstance(...) ignore spaces, and we don't want to ignore them, so we use
+                // ICU collators instead
+                com.ibm.icu.text.Collator collator = com.ibm.icu.text.Collator.getInstance(
+                    com.ibm.icu.util.ULocale.ROOT);
+                int result = collator.compare(value1, value2);
+                if (!sortDirection) {
+                    result = -result;
+                }
+                return result;
+            }
             return super.compareProperty(propertyId, sortDirection, item1, item2);
             //@formatter:on
         }
@@ -485,12 +485,12 @@ public class CmsFileTable extends CmsResourceTable {
                     style += " " + OpenCmsTheme.HOVER_COLUMN;
                 } else if ((CmsResourceTableProperty.PROPERTY_NAVIGATION_TEXT == propertyId)
                     || (CmsResourceTableProperty.PROPERTY_TITLE == propertyId)) {
-                        if ((item.getItemProperty(CmsResourceTableProperty.PROPERTY_IN_NAVIGATION) != null)
-                            && ((Boolean)item.getItemProperty(
-                                CmsResourceTableProperty.PROPERTY_IN_NAVIGATION).getValue()).booleanValue()) {
-                            style += " " + OpenCmsTheme.IN_NAVIGATION;
-                        }
+                    if ((item.getItemProperty(CmsResourceTableProperty.PROPERTY_IN_NAVIGATION) != null)
+                        && ((Boolean)item.getItemProperty(
+                            CmsResourceTableProperty.PROPERTY_IN_NAVIGATION).getValue()).booleanValue()) {
+                        style += " " + OpenCmsTheme.IN_NAVIGATION;
                     }
+                }
                 for (Table.CellStyleGenerator generator : m_additionalStyleGenerators) {
                     String additional = generator.getStyle(source, itemId, propertyId);
                     if (CmsStringUtil.isNotEmptyOrWhitespaceOnly(additional)) {
@@ -916,6 +916,10 @@ public class CmsFileTable extends CmsResourceTable {
             m_fileTable.setSortAscending(state.isSortAscending());
             Object[] visibleCols = m_fileTable.getVisibleColumns();
             for (int i = 0; i < visibleCols.length; i++) {
+                if (!CmsFileTable.DEFAULT_TABLE_PROPERTIES.containsKey(visibleCols[i])) {
+                    // custom column defined in a subclass - ignore it, leaving the collapse state as it currently is
+                    continue;
+                }
                 boolean isCollapsed;
                 // Originally, just the collapsed columns would be stored in the user settings.
                 // The problem with this is that if new columns were added in a new version of OpenCms,
