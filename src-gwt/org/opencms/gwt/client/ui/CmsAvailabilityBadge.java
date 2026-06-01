05cf8aa8c552e3a602aa1b2d658c6cca244eeaa2
gWestenberger <g.westenberger@alkacon.com>
2026-06-01 17:28:01 +0200

    Changed styling for availability badges.

--- src-gwt/org/opencms/gwt/client/ui/CmsAvailabilityBadge.java
+++ src-gwt/org/opencms/gwt/client/ui/CmsAvailabilityBadge.java
@@ -33,8 +33,11 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
+
+import elemental2.dom.Element;
+import jsinterop.base.Js;
 
 /**
  * Widget to show availability information for container elements or list elements.
@@ -48,27 +51,33 @@ public class CmsAvailabilityBadge extends Composite {
 
     public static final String CLASS_HIDE_AVAILABILITY_BADGE = "oc-hide-availability-badge";
 
+    /** Initial HTML for the availability lines. */
+    public static final String INITIAL_LINE_HTML = "<span class=\"oc-availability-icon\"></span> - ";
+
     /** The ui binder instance. */
     private static I_UiBinder m_uiBinder = GWT.create(I_UiBinder.class);
 
-    /** Label to show release information. */
-    @UiField
-    protected Label m_releaseLabel;
-
     /** Label to show expiration information. */
     @UiField
-    protected Label m_expireLabel;
+    protected HTML m_expireLabel;
+
+    /** Label to show release information. */
+    @UiField
+    protected HTML m_releaseLabel;
 
     public CmsAvailabilityBadge(CmsAvailabilityInfo info) {
 
         initWidget(m_uiBinder.createAndBindUi(this));
+        m_releaseLabel.setHTML(INITIAL_LINE_HTML);
+        m_expireLabel.setHTML(INITIAL_LINE_HTML);
+
         if (info.getReleasedText() != null) {
-            m_releaseLabel.setText(info.getReleasedText());
+            setAvailabilityText(m_releaseLabel, info.getReleasedText());
         } else {
             m_releaseLabel.removeFromParent();
         }
         if (info.getExpiredText() != null) {
-            m_expireLabel.setText(info.getExpiredText());
+            setAvailabilityText(m_expireLabel, info.getExpiredText());
         } else {
             m_expireLabel.removeFromParent();
         }
@@ -89,6 +98,24 @@ public class CmsAvailabilityBadge extends Composite {
             return null;
         }
         return new CmsAvailabilityBadge(availabilityInfo);
+    }
+
+    /**
+     * Sets the availability text in the given HTML widget.
+     *
+     * @param html the widget
+     * @param text the text to set
+     */
+    private void setAvailabilityText(HTML html, String text) {
+
+        Element elem = Js.cast(html.getElement());
+        // this assumes there's exactly one text node child!
+        elem.childNodes.forEach((currentValue, currentIndex, listObj) -> {
+            if (currentValue.nodeType == elemental2.dom.Node.TEXT_NODE) {
+                currentValue.textContent = text;
+            }
+            return null;
+        });
     }
 
 }
